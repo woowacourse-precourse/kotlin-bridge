b27a29fb017c3bd5908e110749b04409ea1e8865
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-16 22:39:19 +0900

    feat[bridge]: 입력된 다리길이가 숫자가 아닐 경우 예외 처리하는 기능 구현

--- src/main/kotlin/bridge/Validator.kt
+++ src/main/kotlin/bridge/Validator.kt
@@ -10,9 +10,18 @@ class Validator {
         require(input != null) { INPUT_NULL_ERROR_MSG }
     }
 
+    fun checkLengthIsNum(input: String) {
+        try {
+            input.toInt()
+        } catch (e : NumberFormatException) {
+            throw IllegalArgumentException(BRIDGE_LENGTH_NOT_NUM_ERROR_MSG)
+        }
+    }
+
     companion object {
         const val ERROR_MSG = "[ERROR]"
         const val EMPTY_INPUT_ERROR_MSG = ERROR_MSG + " 값이 입력되지 않았습니다."
         const val INPUT_NULL_ERROR_MSG = ERROR_MSG +  " 값이 null입니다."
+        const val BRIDGE_LENGTH_NOT_NUM_ERROR_MSG = ERROR_MSG + " 다리길이는 숫자여야 합니다."
     }
 }
