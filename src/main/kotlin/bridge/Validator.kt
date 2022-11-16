8439ed5cf7f2b212f5c4fb1ef44b9703421c6591
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-16 22:04:06 +0900

    feat[bridge]: 입력값이 null일 경우 예외 사항 처리하는 기능 추가

--- src/main/kotlin/bridge/Validator.kt
+++ src/main/kotlin/bridge/Validator.kt
@@ -6,8 +6,13 @@ class Validator {
         require(input != "") { EMPTY_INPUT_ERROR_MSG }
     }
 
+    fun checkInputNull(input: String?) {
+        require(input != null) { INPUT_NULL_ERROR_MSG }
+    }
+
     companion object {
         const val ERROR_MSG = "[ERROR]"
         const val EMPTY_INPUT_ERROR_MSG = ERROR_MSG + " 값이 입력되지 않았습니다."
+        const val INPUT_NULL_ERROR_MSG = ERROR_MSG +  " 값이 null입니다."
     }
 }
