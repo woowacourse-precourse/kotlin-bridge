6e404a1459f64217e05581c1657fb1907766d72c
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-16 22:53:26 +0900

    feat[bridge]: 다리길이가 3이상 20이하가 아닌 경우 예외 처리하는 기능 추가

--- src/main/kotlin/bridge/Validator.kt
+++ src/main/kotlin/bridge/Validator.kt
@@ -18,10 +18,21 @@ class Validator {
         }
     }
 
+    fun checkLengthRange(input: String) {
+        val length = input.toInt()
+        require(length >= BRIDGE_LENGTH_MIN && length <= BRIDGE_LENGTH_MAX) {
+            BRIDGE_LENGTH_RANGE_ERROR_MSG
+        }
+    }
+
     companion object {
+        const val BRIDGE_LENGTH_MIN = 3
+        const val BRIDGE_LENGTH_MAX = 20
+
         const val ERROR_MSG = "[ERROR]"
         const val EMPTY_INPUT_ERROR_MSG = ERROR_MSG + " 값이 입력되지 않았습니다."
         const val INPUT_NULL_ERROR_MSG = ERROR_MSG +  " 값이 null입니다."
-        const val BRIDGE_LENGTH_NOT_NUM_ERROR_MSG = ERROR_MSG + " 다리길이는 숫자여야 합니다."
+        const val BRIDGE_LENGTH_NOT_NUM_ERROR_MSG = ERROR_MSG + " 다리 길이는 숫자여야 합니다."
+        const val BRIDGE_LENGTH_RANGE_ERROR_MSG = ERROR_MSG + " 다리 길이는 3부터 20 사이의 숫자여야 합니다."
     }
 }
