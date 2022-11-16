96b9b1a5cd8b24cf43c704fef6c8949242017fde
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-16 22:38:10 +0900

    test[bridge]: 입력된 다리길이가 숫자가 아닌 경우 예외 테스트 코드 추가

--- src/test/kotlin/bridge/ValidatorTest.kt
+++ src/test/kotlin/bridge/ValidatorTest.kt
@@ -2,6 +2,8 @@ package bridge
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class ValidatorTest {
 
@@ -18,6 +20,14 @@ class ValidatorTest {
     fun `입력값이 null일 경우 예외 테스트`() {
         assertThrows<IllegalArgumentException> {
             validator.checkInputNull(null)
+        }
+    }
+
+    @ValueSource(strings = ["1?", "?", "ㅁ", "a"])
+    @ParameterizedTest
+    fun `입력된 다리길이가 숫자가 아닌 경우`(input : String) {
+        assertThrows<IllegalArgumentException> {
+            validator.checkLengthIsNum(input)
         }
     }
 }
