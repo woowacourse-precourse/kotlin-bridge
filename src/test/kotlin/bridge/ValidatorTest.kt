62a675148dc8c1821f439104fa89534317cf9e38
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-16 21:41:41 +0900

    test[bridge]: 입력값이 빈 문자열일 경우 예외 테스트 코드 추가

---
+++ src/test/kotlin/bridge/ValidatorTest.kt
@@ -0,0 +1,16 @@
+package bridge
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class ValidatorTest {
+
+    val validator = Validator()
+
+    @Test
+    fun `입력값이 빈 문자열일 경우 예외 테스트`() {
+        assertThrows<IllegalArgumentException> {
+            validator.checkEmptyInput("")
+        }
+    }
+}
