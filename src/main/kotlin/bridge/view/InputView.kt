e4f335510618e13d62da8e3f29ab7548cbaa5a86
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-17 20:20:57 +0900

    feat(bridge): 게임 재시작/종료 여부를 입력 받는 기능 구현

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -34,8 +34,12 @@ class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    fun readGameCommand(): String {
-        return ""
+    fun readGameCommand(inputValidation: InputValidation): String {
+        println(INPUT_GAME_COMMAND_MESSAGE)
+        val gameCommand = Console.readLine()
+        inputValidation.checkInputLength(gameCommand)
+        inputValidation.checkUserRestartValidation(gameCommand)
+        return gameCommand
     }
 
     companion object {
