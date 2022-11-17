efe55bf750243967008ad93aa12232bf7a4ba1e2
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-18 03:26:05 +0900

    test(input): 다리 길이 입력 기능 테스트 추가

--- src/test/kotlin/bridge/view/InputViewTest.kt
+++ src/test/kotlin/bridge/view/InputViewTest.kt
@@ -1,8 +1,58 @@
 package bridge.view
 
+import org.junit.jupiter.api.BeforeEach
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class InputViewTest : NsTest() {
 
+    private lateinit var inputView: InputView
+
+    @BeforeEach
+    fun beforeEach() {
+        inputView = InputView()
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["3", "10", "20"])
+    fun `다리길이입력_3to20숫자_정상`(input: String) {
+        assertSimpleTest {
+            run(input)
+            inputView.readBridgeSize()
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["3a", ".5", "1,3", "1.2"])
+    fun `다리길이입력_문자포함_에러`(input: String) {
+        assertSimpleTest {
+            val thrown = assertThrows<IllegalArgumentException> {
+                runException(input)
+                inputView.readBridgeSize()
+            }
+            assertThat(thrown.message).contains(ERROR_MESSAGE)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["-1", "0", "2", "21", "100"])
+    fun `다리길이입력_벗어난범위_에러`(input: String) {
+        assertSimpleTest {
+            val thrown = assertThrows<IllegalArgumentException> {
+                runException(input)
+                inputView.readBridgeSize()
+            }
+            assertThat(thrown.message).contains(ERROR_MESSAGE)
+        }
+    }
+
     override fun runMain() {}
+
+    companion object {
+        private const val ERROR_MESSAGE = "[ERROR]"
+    }
 }
