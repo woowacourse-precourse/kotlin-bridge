c6b22066b19b6ec80c39492281766d53dd431693
ponopono0322 <ponopono0322@gmail.com>
2022-11-17 22:51:07 +0900

    feat: InputView 클래스 함수 구현
    - readBridgeSize: 다리 길이 입력
    - readMoving: 이동할 칸 입력
    - readGameCommand: 게임을 다시 시도할지 종료할지 여부를 입력

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,4 +1,6 @@
 package bridge
+import camp.nextstep.edu.missionutils.Console
+import java.lang.NumberFormatException
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -8,20 +10,38 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        val input: Int
+        try {
+            input = Console.readLine().toInt()
+            if (input !in 3..20) {
+                throw IllegalArgumentException()
+            }
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException()
+        }
+
+       return input
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        val input = Console.readLine()
+        if (input.length != 1 || input !in listOf("U", "D", "u", "d")) {
+            throw IllegalArgumentException()
+        }
+        return input.uppercase()
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        val input = Console.readLine()
+        if (input.length != 1 || input !in listOf("R", "Q", "r", "q")) {
+            throw IllegalArgumentException()
+        }
+        return input.uppercase()
     }
 }
