b53bf000d0ff331a56a0cd3db7f49a43a02b19cc
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-17 22:29:16 +0900

    test[bridge]: 다리 이동 결과 확인하는 테스트 코드 추가

--- src/test/kotlin/bridge/BridgeTest.kt
+++ src/test/kotlin/bridge/BridgeTest.kt
@@ -10,7 +10,7 @@ class BridgeTest {
     val numberGenerator: BridgeNumberGenerator = ApplicationTest.TestNumberGenerator(listOf(1, 0, 0))
     val bridgeMaker = BridgeMaker(numberGenerator)
     val bridgeMap: List<String> = bridgeMaker.makeBridge(3)
-    val bridge = Bridge(3)
+    var bridge = Bridge(3)
 
     @Test
     fun `이동 가능한 칸인지 확인`() {
@@ -36,6 +36,16 @@ class BridgeTest {
     fun `이동 불가능한 칸이면 'X'인지 확인`() {
         val result = "X"
         assertThat(bridge.movePossibleMark(false)).isEqualTo(result)
+    }
+
+    @Test
+    fun `다리 이동 결과 확인하는 기능`() {
+        bridge.move("U", bridgeMap)
+        bridge.move("D", bridgeMap)
+        val upResult = "[ O |   ]"
+        val downResult = "[   | O ]"
+        assertThat(bridge.upBridgeResult()).isEqualTo(upResult)
+        assertThat(bridge.downBridgeResult()).isEqualTo(downResult)
     }
 
 
