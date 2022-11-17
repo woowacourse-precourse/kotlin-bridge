f72bf9e70894ef95558debf5426e86cadb0b740a
junjange <wnswkd486@naver.com>
2022-11-17 17:18:46 +0900

    feat: 사용자가 다리 길이를 잘못 입력했을 때에 에러 기능 추가

--- src/main/kotlin/bridge/Exception.kt
+++ src/main/kotlin/bridge/Exception.kt
@@ -1,4 +1,13 @@
 package bridge
 
-class Exception {
+import bridge.Constant.INPUT_RANGE_ERROR_MESSAGE
+import bridge.Constant.INPUT_TYPE_ERROR_MESSAGE
+
+fun String.bridgeSizeInputTypeException(): Int {
+    return try {
+        if (this.toInt() < 3 || this.toInt() > 20)  throw IllegalArgumentException(INPUT_RANGE_ERROR_MESSAGE)
+        else this.toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException(INPUT_TYPE_ERROR_MESSAGE)
+    }
 }
