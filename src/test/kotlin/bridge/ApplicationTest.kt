a9967b843e1545d9392f3d9332b585f4ae28d31e
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-17 18:03:52 +0900

    refactor: 패키지 생성 및 이동
    add packages domain, view

--- src/test/kotlin/bridge/ApplicationTest.kt
+++ src/test/kotlin/bridge/ApplicationTest.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import bridge.domain.BridgeMaker
+import bridge.domain.BridgeNumberGenerator
 import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
