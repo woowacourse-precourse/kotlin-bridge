f3ced7886150d5656b815fe9c0e9734fdc4cfd1b
82104 <tir2986@naver.com>
2022-11-17 20:43:35 +0900

    feat: 숫자를 입력받을 때, 숫자가 아닌 것이 입력되었다면 예외를 발생시킨다.

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -1,5 +1,7 @@
 package bridge.view
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -7,21 +9,29 @@ class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    fun readBridgeSize(): Int {
-        return 0
+    fun readBridgeSize(): Int = try {
+        readLineWithTrim().toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException(ERROR_NOT_NUMBER)
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        return readLineWithTrim()
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        return readLineWithTrim()
+    }
+
+    private fun readLineWithTrim() = Console.readLine().trim()
+
+    companion object {
+        private const val ERROR_NOT_NUMBER = "숫자가 아닙니다."
     }
 }
