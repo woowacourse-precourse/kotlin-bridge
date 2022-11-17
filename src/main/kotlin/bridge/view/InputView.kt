cf278f5fd456f395e793ef82617265a9525e2024
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-17 20:11:55 +0900

    refactor(bridge): inputView의 inputvalidation과 converter를 함수 인자로 넘김

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -8,13 +8,10 @@ import camp.nextstep.edu.missionutils.Console
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 class InputView {
-    private val inputValidation = InputValidation()
-    private val converter = Converter()
-
     /**
      * 다리의 길이를 입력받는다.
      */
-    fun readBridgeSize(): Int {
+    fun readBridgeSize(inputValidation: InputValidation,converter: Converter): Int {
         println(INPUT_BRIDGE_SIZE_MESSAGE)
         val input = Console.readLine()
         inputValidation.checkInputInteger(input)
@@ -23,11 +20,10 @@ class InputView {
         return bridgeSize
     }
 
-
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    fun readMoving(): String {
+    fun readMoving(inputValidation: InputValidation): String {
         println(INPUT_MOVING_MESSAGE)
         val moving = Console.readLine()
         inputValidation.checkInputLength(moving)
@@ -45,5 +41,6 @@ class InputView {
     companion object {
         const val INPUT_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요."
         const val INPUT_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
+        const val INPUT_GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
     }
 }
