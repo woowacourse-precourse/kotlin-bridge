a4ea304a0d4fa21cf82cd2d699972ad6bd0931d5
zqwz <zqwz@naver.com>
2022-11-17 12:00:53 +0900

    feat(InputView): 입력을 받는 기능 추가
    다리의 길이 입력
    사용자가 이동할 칸 입력
    게임 재시작 여부 입력

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,20 +10,27 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        val line = Console.readLine()
+        Exception.validateNumber(line)
+        Exception.validateSizeRange(line.toInt())
+        return line.toInt()
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        val line = Console.readLine()
+        Exception.validateDirectionRange(line)
+        return line
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        val line = Console.readLine()
+        Exception.validateRestartRange(line)
+        return line
     }
 }
