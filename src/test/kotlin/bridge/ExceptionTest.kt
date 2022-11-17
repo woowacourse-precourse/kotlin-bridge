49ae40b46acce9247ee14f250ff2296066658019
zqwz <zqwz@naver.com>
2022-11-17 11:34:20 +0900

    test(ExceptionTest): 입력의 예외 사항 테스트 추가
    다리 길이 입력이 숫자로 변경 가능한지 확인
    다리 길이 입력이 3-20 사이의 숫자인지 확인
    이동할 칸 입력이 위/아래인지 확인
    재시작 여부 입력이 재시작/종료인지 확인

---
+++ src/test/kotlin/bridge/ExceptionTest.kt
@@ -0,0 +1,40 @@
+package bridge
+
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class ExceptionTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 21, 22])
+    fun `다리의 길이가 3-20 사이가 아니면 예외가 발생한다`(input: Int) {
+        assertThrows<IllegalArgumentException> {
+            Exception.validateSizeRange(input)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["2", "q", "f"])
+    fun `이동할 칸이 위(U)와 아래(D)가 아니면 예외가 발생한다`(input: String) {
+        assertThrows<IllegalArgumentException> {
+            Exception.validateDirectionRange(input)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "s", " d", "재시작"])
+    fun `재시작(R)과 종료(Q)가 아니면 예외가 발생한다`(input: String) {
+        assertThrows<IllegalArgumentException> {
+            Exception.validateRestartRange(input)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["number", "aaa"])
+    fun `숫자로 변경 가능한 문자가 아니면 예외가 발생한다`(input: String) {
+        assertThrows<IllegalArgumentException> {
+            Exception.validateNumber(input)
+        }
+    }
+}
