3f9779d9015d421f266ee072522ff825093bff28
zqwz <zqwz@naver.com>
2022-11-17 15:52:04 +0900

    refactor(Exception): 에러 출력 문구로 변경

--- src/main/kotlin/bridge/Exception.kt
+++ src/main/kotlin/bridge/Exception.kt
@@ -3,18 +3,18 @@ package bridge
 class Exception {
     companion object {
         fun validateNumber(readLine: String) = require(readLine.matches(Regex("\\d+"))) {
-            throw IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.")
+            println("[ERROR] 숫자만 입력 가능합니다.")
         }
         fun validateSizeRange(readLine: Int) = require(readLine in 3..20) {
-            throw IllegalArgumentException("[ERROR] 3-20 사이의 숫자만 입력 가능합니다.")
+            println("[ERROR] 3-20 사이의 숫자만 입력 가능합니다.")
         }
 
         fun validateDirectionRange(readLine: String) = require(readLine in listOf("U", "D")) {
-            throw IllegalArgumentException("[ERROR] 위(U)나 아래(D)만 입력 가능합니다.")
+            println("[ERROR] 위(U)나 아래(D)만 입력 가능합니다.")
         }
 
         fun validateRestartRange(readLine: String) = require(readLine in listOf("R", "Q")) {
-            throw IllegalArgumentException("[ERROR] 재시작(R)이나 종료(Q)만 입력 가능합니다.")
+            println("[ERROR] 재시작(R)이나 종료(Q)만 입력 가능합니다.")
         }
     }
 }
