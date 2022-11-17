48d49125861f49501c21f8849087ec3a760a56f5
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-17 15:13:30 +0900

    feat(bridge): 다리 길이만큼 다리를 생성해주는 기능 추가

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,19 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        val bridge = mutableListOf<String>()
+        for (number in 1..size) bridge.add(convertBridgeNumberToString(bridgeNumberGenerator.generate()))
+        return bridge
+    }
+
+    private fun convertBridgeNumberToString(bridgeNumber: Int): String {
+        if (bridgeNumber == UP_NUMBER) return UP
+        return DOWN
+    }
+
+    companion object {
+        const val UP = "U"
+        const val DOWN = "D"
+        const val UP_NUMBER = 1
     }
 }
