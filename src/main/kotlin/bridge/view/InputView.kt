eba933d6f4f6f2a20b5cdcf3482defe84089ef0d
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-11-17 18:09:35 +0900

    refactor: view의 패키지 변경
    bridge -> bridge.view 패키지로 변경

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -1,9 +1,20 @@
-package bridge
+package bridge.view
+
+import camp.nextstep.edu.missionutils.Console
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 class InputView {
+
+    /*
+    * 제공된 InputView 클래스를 활용해 구현해야 한다.
+    * InputView의 패키지는 변경할 수 있다.
+    * InputView의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
+    * 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
+    *
+    */
+
     /**
      * 다리의 길이를 입력받는다.
      */
