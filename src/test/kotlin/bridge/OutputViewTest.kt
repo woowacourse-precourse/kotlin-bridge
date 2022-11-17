5f24cb14b74cef32b6c38f329167dd00f77e3a0c
kyounggudev <homin222@naver.com>
2022-11-18 07:40:35 +0900

    test : 현재 다리 출력 테스트 케이스 추가

---
+++ src/test/kotlin/bridge/OutputViewTest.kt
@@ -0,0 +1,30 @@
+package bridge
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class OutputViewTest : NsTest(){
+    @Test
+    fun `현재 다리 출력`(){
+        order=1
+        BridgeGame.upBridge= mutableListOf("O"," ")
+        BridgeGame.downBridge= mutableListOf(" ","O")
+        assertSimpleTest{
+            OutputView().printMap()
+            assertThat(output()).contains(
+                "[ O |   ]",
+                "[   | O ]"
+            )
+        }
+    }
+
+    override fun runMain() {
+        main()
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE = "[ERROR]"
+    }
+}
