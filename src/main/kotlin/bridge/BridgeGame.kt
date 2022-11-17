894514571d9340ad459133a62ee1383d0c2afffd
lgy15 <lgy159@naver.com>
2022-11-17 09:54:35 +0900

    feat: 최종 게임 결과 출력 구현

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -20,18 +20,18 @@ class BridgeGame(val bridge: List<String>, val retry: Int) {
 
         if (bridge[location] == moveLine) answer = true
         OutputView().printMap(bridge, location, answer)
-        if (location < bridge.size && answer) move(location + 1)
+        if (location < bridge.size - 1 && answer) move(location + 1)
+        if (location == bridge.size - 1 && answer) retry(answer)
+        if (!answer) retry(answer)
     }
-    // 사용자가 오답을 입력했을 때
-    fun fail() {}
 
-    // 사용자가 끝까지 다리를 건넜을 때
-    fun success() {}
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      *
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry() {}
+    fun retry(winCheck: Boolean) {
+        val retryCheck = InputView().readGameCommand()
+    }
 }
