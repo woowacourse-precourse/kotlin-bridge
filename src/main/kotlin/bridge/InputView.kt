2b9748b934199ba03dcbe0ae9efbafe371ae7dbb
dltkd1395 <dltkd13956@naver.com>
2022-11-18 04:16:14 +0900

    feat(InputView) : 다리 길이 입력 기능 구현

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -7,9 +9,7 @@ class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    fun readBridgeSize(): Int {
-        return 0
-    }
+    fun readBridgeSize(): String = Console.readLine()
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
