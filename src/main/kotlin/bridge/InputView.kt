b510dec8c50fcbe2e1b53d1787732714f885ccd5
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-17 16:29:59 +0900

    refactor : validator for bridgeSize

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,8 +1,7 @@
 package bridge
 
+import bridge.validator.BridgeSizeValidator
 import camp.nextstep.edu.missionutils.Console
-import java.lang.Exception
-import java.lang.NumberFormatException
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -12,28 +11,14 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return try{
+        return try {
             val input = Console.readLine()
-            checkBridgeSizeException(input)
+            BridgeSizeValidator().checkInput(input)
             input.toInt()
-        } catch (exception : IllegalArgumentException){
+        } catch (exception: IllegalArgumentException) {
             println("[ERROR] 입력값이 올바르지 않습니다.")
             readBridgeSize()
         }
-    }
-
-    private fun checkBridgeSizeException(input : String){
-        if(!checkBridgeSizeScopeException(input) || !checkBridgeSizeIsNumeric(input)){
-            require(false)
-        }
-    }
-
-    private fun checkBridgeSizeScopeException(input : String) : Boolean{
-        return input.toInt() in 1..20
-    }
-
-    private fun checkBridgeSizeIsNumeric(input : String) : Boolean{
-        return input.all { char -> char.isDigit() }
     }
 
     /**
---
+++ src/main/kotlin/bridge/validator/Vaildator.kt
@@ -0,0 +1,5 @@
+package bridge.validator
+
+abstract class Validator {
+    abstract fun checkInput(input: String)
+}
