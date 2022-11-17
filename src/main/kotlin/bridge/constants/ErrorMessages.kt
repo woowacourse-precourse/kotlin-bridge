e89b630b70990b6ed1be969b9c0fe09dfbd93e4c
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-18 01:45:40 +0900

    feat(constants): 에러 메시지 enum class 생성

---
+++ src/main/kotlin/bridge/constants/ErrorMessages.kt
@@ -0,0 +1,24 @@
+package bridge.constants
+
+import bridge.data.Bridge
+
+object ErrorMessages {
+
+    private const val PREFIX = "[ERROR]"
+
+    private fun String.applyPrefix(): String = "$PREFIX $this"
+
+    enum class Common(private val message: String) {
+
+        InvalidNumberFormat("올바른 숫자를 입력해주세요."),;
+
+        override fun toString(): String = message.applyPrefix()
+    }
+
+    enum class InputViewEnum(private val message: String) {
+
+        OutOfBridgeSize("다리 길이는 ${Bridge.SIZE_MIN}부터 ${Bridge.SIZE_MAX} 사이의 숫자여야 합니다."),;
+
+        override fun toString(): String = message.applyPrefix()
+    }
+}
