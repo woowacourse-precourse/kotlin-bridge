2f5c7a2adf10d612bb22a7e7378e70ae1ca304ab
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-17 17:28:16 +0900

    test(bridge): 입력값이 정수가 아니라면 예외 발생 기능 테스트 구현

---
+++ src/test/kotlin/bridge/InputValidationTest.kt
@@ -0,0 +1,26 @@
+package bridge
+
+import bridge.validation.InputValidation
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+
+class InputValidationTest {
+
+    private val inputValidation = InputValidation()
+
+    @Test
+    fun `입력값이 정수가 아니라면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            inputValidation.checkInputInteger("십삼")
+        }
+        assertThrows<IllegalArgumentException> {
+            inputValidation.checkInputInteger("1 ")
+        }
+        assertThrows<IllegalArgumentException> {
+            inputValidation.checkInputInteger("13.3")
+        }
+    }
+
+
+}
