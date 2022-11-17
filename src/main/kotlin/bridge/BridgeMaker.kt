d5ce00f9195545025b34d21de477893bcd251a60
82104 <tir2986@naver.com>
2022-11-17 16:12:23 +0900

    feat: 특정 길이의 다리를 만들 수 있다

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import bridge.model.Direction
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -9,6 +11,13 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        val bridge = mutableListOf<String>()
+        // TODO: repeat with 음수??
+        repeat(size) {
+            val value = bridgeNumberGenerator.generate()
+            val direction = Direction.valueOf(value)
+            bridge.add(direction.toString())
+        }
+        return bridge
     }
 }
--- src/main/kotlin/bridge/model/Bridge.kt
+++ src/main/kotlin/bridge/model/Bridge.kt
@@ -1,4 +1,23 @@
 package bridge.model
 
-class Bridge {
+import bridge.BridgeMaker
+import bridge.BridgeRandomNumberGenerator
+
+class Bridge(private val directionNames: List<String>) {
+
+    init {
+        require(directionNames.size in MIN_LENGTH..MAX_LENGTH)
+        require(directionNames.all { it in Direction.names() })
+    }
+
+    companion object {
+        const val MIN_LENGTH = 3
+        const val MAX_LENGTH = 20
+
+        fun generate(size: Int): Bridge {
+            val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
+            val directionNames = bridgeMaker.makeBridge(size)
+            return Bridge(directionNames)
+        }
+    }
 }
