6f15e5efe79a43efa2d99862f667d8717a53f157
junjange <wnswkd486@naver.com>
2022-11-17 21:32:42 +0900

    test: 다리의 끝까지 이동한 경우 게임이 종료된다

--- src/test/kotlin/bridge/BridgeGameTest.kt
+++ src/test/kotlin/bridge/BridgeGameTest.kt
@@ -4,6 +4,17 @@ import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
 
 class BridgeGameTest {
+    @Test
+    fun `다리의 끝까지 이동한 경우 게임이 종료된다`() {
+        val bridgeMaker = BridgeMaker(TestNumberGenerator(listOf(1, 1, 0))).makeBridge(3)
+        val bridgeGame = BridgeGame()
+
+        println(bridgeMaker)
+        bridgeGame.move(bridgeMaker[0], "U")
+        bridgeGame.move(bridgeMaker[1], "U")
+        bridgeGame.move(bridgeMaker[2], "D")
+        assertThat(bridgeGame.isGameEnd(bridgeMaker)).isEqualTo(true)
+    }
 
     class TestNumberGenerator(numbers: List<Int>) : BridgeNumberGenerator {
         private val numbers: MutableList<Int> = numbers.toMutableList()
