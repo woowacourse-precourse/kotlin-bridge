126feee214c5fcfaff21e9e4345d3229d097d07c
junjange <wnswkd486@naver.com>
2022-11-17 19:57:45 +0900

    feat: printMap 기능 추가

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -13,7 +13,10 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(upBridge: List<String>, downBridge: List<String>) {
+        println(upBridge.toString().replace(",", " |").replace("[", "[ ").replace("]", " ]"))
+        println(downBridge.toString().replace(",", " |").replace("[", "[ ").replace("]", " ]"))
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
