553733f7f227e57a551f3da19de993a797ca9580
junjange <wnswkd486@naver.com>
2022-11-17 21:32:24 +0900

    test: TestNumberGenerator 추가

--- src/test/kotlin/bridge/BridgeGameTest.kt
+++ src/test/kotlin/bridge/BridgeGameTest.kt
@@ -4,5 +4,12 @@ import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
 
 class BridgeGameTest {
- 
+
+    class TestNumberGenerator(numbers: List<Int>) : BridgeNumberGenerator {
+        private val numbers: MutableList<Int> = numbers.toMutableList()
+
+        override fun generate(): Int {
+            return numbers.removeAt(0)
+        }
+    }
 }
