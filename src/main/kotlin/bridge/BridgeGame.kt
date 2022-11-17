7fec49efdd607afdb659cbb740550e6e97652096
kyounggudev <homin222@naver.com>
2022-11-18 07:41:08 +0900

    feat : 현재 다리 출력 기능 추가

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -10,7 +10,39 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun move(bridge : List<String>) {
+        val move = InputView().readMoving()
+        when(move == bridge[order]){
+            true -> moveSuccess(bridge)
+            false -> moveFail(bridge)
+        }
+    }
+
+    fun moveSuccess(bridge: List<String>){
+        if(bridge[order]=="U"){
+            upBridge.add("O")
+            downBridge.add(" ")
+        }
+        if(bridge[order]=="D"){
+            upBridge.add(" ")
+            downBridge.add("O")
+        }
+        OutputView().printMap()
+        order++
+    }
+
+    fun moveFail(bridge: List<String>){
+        if(bridge[order]=="U"){
+            upBridge.add(" ")
+            downBridge.add("X")
+        }
+        if(bridge[order]=="D"){
+            upBridge.add("X")
+            downBridge.add(" ")
+        }
+        OutputView().printMap()
+        InputView().readGameCommand()
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -19,4 +51,9 @@ class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry() {}
+
+    companion object{
+        var upBridge = mutableListOf<String>()
+        var downBridge = mutableListOf<String>()
+    }
 }
