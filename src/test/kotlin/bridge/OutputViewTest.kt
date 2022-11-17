be8a5db19c2107a83478fa1b6125a42591d96fdb
kyounggudev <homin222@naver.com>
2022-11-18 08:25:30 +0900

    test : 최종 결과 출력 테스트 케이스 추가

--- src/test/kotlin/bridge/OutputViewTest.kt
+++ src/test/kotlin/bridge/OutputViewTest.kt
@@ -20,6 +20,40 @@ class OutputViewTest : NsTest() {
         }
     }
 
+    @Test
+    fun `최종 결과 성공 출력`() {
+        order = 2
+        BridgeGame.upBridge = mutableListOf("O", " ", "O")
+        BridgeGame.downBridge = mutableListOf(" ", "O", " ")
+        assertSimpleTest {
+            OutputView().printResult(true)
+            assertThat(output()).contains(
+                "최종 게임 결과",
+                "[ O |   | O ]",
+                "[   | O |   ]",
+                "게임 성공 여부: 성공",
+                "총 시도한 횟수: 1"
+            )
+        }
+    }
+
+    @Test
+    fun `최종 결과 실패 출력`() {
+        order = 1
+        BridgeGame.upBridge = mutableListOf("O", " ")
+        BridgeGame.downBridge = mutableListOf(" ", "X")
+        assertSimpleTest {
+            OutputView().printResult(false)
+            assertThat(output()).contains(
+                "최종 게임 결과",
+                "[ O |   ]",
+                "[   | X ]",
+                "게임 성공 여부: 실패",
+                "총 시도한 횟수: 1"
+            )
+        }
+    }
+
     override fun runMain() {
         main()
     }
