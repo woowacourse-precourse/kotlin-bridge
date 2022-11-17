c04c9f67a547905aa9d3903e1eb2db706f12683d
kyounggudev <homin222@naver.com>
2022-11-18 07:42:17 +0900

    feat : 현재 다리 출력 기능에 맞게 변수 추가

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,14 @@
 package bridge
 
+var order : Int = 0
+var count : Int = 0
+
 fun main() {
-    // TODO: 프로그램 구현
+    val bridge = initBridge()
+    BridgeGame().move(bridge)
+}
+
+fun initBridge(): List<String> {
+    val size = InputView().readBridgeSize()
+    return BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(size)
 }
