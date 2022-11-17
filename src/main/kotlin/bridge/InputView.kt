5e8c333c7b06807987a201f7586e4c389be81024
kyounggudev <homin222@naver.com>
2022-11-18 06:45:28 +0900

    test : 재시작 여부 입력 기능 추가

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -38,7 +38,14 @@ class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+        while(true){
+            try{
+                return commandException(readLine())
+            }catch(e: IllegalArgumentException){
+                continue
+            }
+        }
     }
 
     private fun sizeException(size: String?) : Int{
@@ -55,10 +62,17 @@ class InputView {
         return move
     }
 
+    private fun commandException(command: String?) : String{
+        require(!command.isNullOrEmpty()) { println(ERROR_NULL) }
+        require(command =="R"||command=="Q") { println(ERROR_NOT_R_Q) }
+        return command
+    }
+
     companion object {
         const val ERROR_NULL = "[ERROR] 아무것도 입력하지 않았습니다. 다시 입력해주세요."
         const val ERROR_NOT_NUMBER = "[ERROR] 숫자를 입력해 주세요."
         const val ERROR_NOT_INRANGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."
         const val ERROR_NOT_U_D = "[ERROR] U 또는 D 를 입력해주세요."
+        const val ERROR_NOT_R_Q = "[ERROR] R 또는 Q 를 입력해주세요."
     }
 }
