96b403441fee7574167efb6bbc063b538a326ede
kyounggudev <homin222@naver.com>
2022-11-18 01:44:47 +0900

    style : 코드 포맷팅

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -10,8 +10,8 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      */
     fun makeBridge(size: Int): List<String> {
         val bridge = mutableListOf<String>()
-        for(index in 0 until size){
-            when(bridgeNumberGenerator.generate()){
+        for (index in 0 until size) {
+            when (bridgeNumberGenerator.generate()) {
                 0 -> bridge.add("D")
                 1 -> bridge.add("U")
             }
