20a5fec044115ff2d32e8f27586b2ce3b4505295
dltkd1395 <dltkd13956@naver.com>
2022-11-18 04:24:50 +0900

    test(MovementTest) : 이동 방향이 숫자인 경우 예외 테스트 추가

---
+++ src/test/kotlin/bridge/MovementTest.kt
@@ -0,0 +1,15 @@
+package bridge
+
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class MovementTest {
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "100", "2"])
+    fun `이동 방향이 숫자인 경우 예외 테스트`(direction: String) {
+        assertThrows<IllegalArgumentException> {
+            Movement(direction)
+        }
+    }
+}
