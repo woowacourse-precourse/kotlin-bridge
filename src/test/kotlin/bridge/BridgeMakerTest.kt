c22f8056c3159c6500caff2223d8f9279cd4f422
junjange <wnswkd486@naver.com>
2022-11-17 23:58:13 +0900

    test: 입력받은 다리 길이만큼 다리 길이가 생성되는지 확인하는 테스트

--- src/test/kotlin/bridge/BridgeMakerTest.kt
+++ src/test/kotlin/bridge/BridgeMakerTest.kt
@@ -4,11 +4,18 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
 class BridgeMakerTest {
+
     @Test
     fun `입력받은 다리 길이만큼 다리가 생성되었다`() {
         val bridgeMaker = BridgeMaker(TestNumberGenerator(listOf(1, 1, 0))).makeBridge(3)
 
         Assertions.assertThat(bridgeMaker.toString()).isEqualTo("[U, U, D]")
+    }
+    @Test
+    fun `입력받은 다리 길이만큼 다리 길이가 생성되었다`() {
+        val bridgeMaker = BridgeMaker(TestNumberGenerator(listOf(1, 1, 0))).makeBridge(3)
+
+        Assertions.assertThat(bridgeMaker).hasSize(3)
     }
 
     class TestNumberGenerator(numbers: List<Int>) : BridgeNumberGenerator {
