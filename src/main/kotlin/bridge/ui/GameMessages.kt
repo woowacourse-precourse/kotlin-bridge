ee2115deb5fa2e34dc07c91d0a714428d71741c4
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-17 19:20:24 +0900

    feat(GameMessages): 게임 메시지를 저장하는 enum 클래스 추가

---
+++ src/main/kotlin/bridge/ui/GameMessages.kt
@@ -0,0 +1,13 @@
+package bridge.ui
+
+enum class GameMessages(private val messages: String) {
+    START("다리 건너기 게임을 시작합니다."),
+    INPUT_BRIDGE_SIZE("다리의 길이를 입력해주세요."),
+    INPUT_UP_DOWN("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    INPUT_RESTART_QUIT("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    RESULT("최종 게임 결과"),
+    IS_CLEAR("게임 성공 여부:"),
+    TRY_COUNT("총 시도한 횟수:");
+
+    override fun toString(): String = messages
+}
