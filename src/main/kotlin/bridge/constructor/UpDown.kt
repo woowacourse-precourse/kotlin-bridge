bc6cd42f6baf6901f0b783dab68878c589034a2f
권용민 <briandr@naver.com>
2022-11-17 19:17:34 +0900

    fix(UpDown): 예외 처리 메시지 담기
     - 예외처리 메시지 수정
     - 예외 처리 메시지를 상수로 만들어놓고 사용하지 않아 추가

--- src/main/kotlin/bridge/constructor/UpDown.kt
+++ src/main/kotlin/bridge/constructor/UpDown.kt
@@ -5,12 +5,12 @@ enum class UpDown() {
     U;
 
     companion object {
-        private const val SHOULD_ZERO_OR_ONE = "랜덤 생성 번호는 0 혹은 1이어야합니다."
+        private const val SHOULD_ZERO_OR_ONE = "[ERROR] 랜덤 생성 번호는 0 혹은 1이어야합니다."
         fun judgeUpDown(randomNumber: Int): String {
             values().forEach {
                 if (it.ordinal == randomNumber) return it.name
             }
-            throw IllegalArgumentException()
+            throw IllegalArgumentException(SHOULD_ZERO_OR_ONE)
         }
     }
 }
