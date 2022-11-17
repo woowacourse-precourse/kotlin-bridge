51197a6795c091f5326f7331072ff2702d324243
junjange <wnswkd486@naver.com>
2022-11-17 20:01:56 +0900

    refactor: bridgeSizeInputTypeException 기능 상수 값으로 수정

--- src/main/kotlin/bridge/Exception.kt
+++ src/main/kotlin/bridge/Exception.kt
@@ -1,13 +1,16 @@
 package bridge
 
+import bridge.Constant.DOWN
 import bridge.Constant.INPUT_RANGE_ERROR_MESSAGE
 import bridge.Constant.INPUT_TYPE_ERROR_MESSAGE
+import bridge.Constant.QUIT
+import bridge.Constant.REPLAY
+import bridge.Constant.UP
 
 fun String.bridgeSizeInputTypeException(): Int {
     return try {
-        if (this.toInt() < 3 || this.toInt() > 20)  throw IllegalArgumentException(INPUT_RANGE_ERROR_MESSAGE)
+        if (this.toInt() < 3 || this.toInt() > 20) throw IllegalArgumentException(INPUT_RANGE_ERROR_MESSAGE)
         else this.toInt()
     } catch (e: NumberFormatException) {
         throw IllegalArgumentException(INPUT_TYPE_ERROR_MESSAGE)
     }
-}
