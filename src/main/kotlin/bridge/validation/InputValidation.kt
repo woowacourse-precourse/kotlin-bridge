7d7cdf456c4090b6efa753df3b95759e1e0ee9bf
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-17 19:00:20 +0900

    feat(bridge): 사용자 이동 입력값이 U 또는 D가 아니라면 예외 발생 기능 구현

--- src/main/kotlin/bridge/validation/InputValidation.kt
+++ src/main/kotlin/bridge/validation/InputValidation.kt
@@ -18,10 +18,15 @@ class InputValidation {
         if(input.length!= CORRECT_INPUT_LENGTH) throw IllegalArgumentException(ErrorType.INCORRECT_INPUT_LENGTH_ERROR.message)
     }
 
+    fun checkUserMovingValidation(input: String){
+        if(!(input=="U" || input=="D")) throw IllegalArgumentException(ErrorType.INCORRECT_MOVING_INPUT_ERROR.message)
+    }
+
     enum class ErrorType(val message: String) {
         NOT_INTEGER_ERROR("$INITIAL_ERROR_MESSAGE 정수가 아닌 수가 포함되어 있습니다(공백포함). 정수만 입력해 주세요."),
         BRIDGE_SIZE_RANGE_ERROR("$INITIAL_ERROR_MESSAGE 3이상 20이하의 숫자만 입력해주세요."),
-        INCORRECT_INPUT_LENGTH_ERROR("$INITIAL_ERROR_MESSAGE 입력값의 길이는 1이어야 합니다(공백포함).")
+        INCORRECT_INPUT_LENGTH_ERROR("$INITIAL_ERROR_MESSAGE 입력값의 길이는 1이어야 합니다(공백포함)."),
+        INCORRECT_MOVING_INPUT_ERROR("$INITIAL_ERROR_MESSAGE 이동할 칸의 입력은 \"U\" 또는 \"D\" 만 가능합니다.")
 
     }
 
