3308c8168160243874b1505c97f833eaaefaae7f
junjange <wnswkd486@naver.com>
2022-11-18 00:06:57 +0900

    test: 다리를 끝까지 건너면 게임을 성공하는지 테스트

--- src/test/kotlin/bridge/BridgeGameTest.kt
+++ src/test/kotlin/bridge/BridgeGameTest.kt
@@ -38,6 +38,16 @@ class BridgeGameTest {
         assertThat(bridgeGame.isGameEnd(bridgeMaker)).isEqualTo(true)
     }
 
+    @Test
+    fun `다리를 끝까지 건너면 게임을 성공한다`() {
+        val bridgeMaker = BridgeMaker(TestNumberGenerator(listOf(1, 1, 0))).makeBridge(3)
+        val bridgeGame = BridgeGame()
+        bridgeGame.move(bridgeMaker[0],"U")
+        bridgeGame.move(bridgeMaker[1],"U")
+        bridgeGame.move(bridgeMaker[2],"D")
+        assertThat(bridgeGame.isSuccess).isEqualTo(true)
+    }
+
     class TestNumberGenerator(numbers: List<Int>) : BridgeNumberGenerator {
         private val numbers: MutableList<Int> = numbers.toMutableList()
 
