c87eec00684c182922e3a46985f1935d40d8ac59
권용민 <briandr@naver.com>
2022-11-17 15:31:27 +0900

    feat(UpDown): 위아래 구분을 위한 enum 클래스 생성

---
+++ src/main/kotlin/bridge/UpDown.kt
@@ -0,0 +1,16 @@
+package bridge
+
+enum class UpDown() {
+    D,
+    U;
+
+    companion object {
+        private const val SHOULD_ZERO_OR_ONE = "랜덤 생성 번호는 0 혹은 1이어야합니다."
+        fun judgeUpDown(randomNumber: Int): String {
+            values().forEach {
+                if (it.ordinal == randomNumber) return it.name
+            }
+            throw IllegalArgumentException()
+        }
+    }
+}
