60d635cd916438e6d589a042021665a82edf7740
SeungHyeon Shim <tladhehd@naver.com>
2022-11-17 17:29:23 +0900

    feat: InputView 에러 시 해당 부분부터 재입력 받기 기능 추가

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -2,23 +2,52 @@ package bridge
 
 import camp.nextstep.edu.missionutils.Console.readLine
 
+
 /*** 사용자로부터입력을받는역할을한다.*/
 class InputView {
     /*** 다리의길이를입력받는다.*/
     fun readBridgeSize(): Int {
-        val bridgeSize = readLine()
-        return bridgeSize.toInt()
+        try{
+            val bridgeSize = readLine()
+            if(!isNumeric(bridgeSize)) {
+                throw IllegalArgumentException()
+            }
+                return bridgeSize.toInt()
+        }catch (e: IllegalArgumentException){
+            println(e.message)
+            return readBridgeSize()
+        }
+    }
+    // 숫자인지 물어보는 함수
+    fun isNumeric(s: String): Boolean {
+        return s.chars().allMatch { Character.isDigit(it) }
     }
 
     /*** 사용자가이동할칸을입력받는다.*/
     fun readMoving(): String {
-        val moving = readLine()
-        return moving
+        try {
+            val moving = readLine()
+            if (moving!="U" && moving!="D") {
+                throw IllegalArgumentException()
+            }
+            return moving
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            return readMoving()
+        }
     }
 
     /*** 사용자가게임을다시시도할지종료할지여부를입력받는다.*/
     fun readGameCommand(): String {
-        val gameCommand = readLine()
-        return gameCommand
+        try{
+            val gameCommand = readLine()
+            if (gameCommand!="R" && gameCommand!="Q") {
+                throw IllegalArgumentException()
+            }
+            return gameCommand
+        }catch (e: IllegalArgumentException){
+            println(e.message)
+            return readGameCommand()
+        }
     }
 }
