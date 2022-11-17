7fc9ed0b714494bfe152179f7866514bbcc9558d
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-17 19:44:33 +0900

    feat(bridge): 재시작/종료 입력값이 R 또는 Q가 아니라면 예외 발생 기능 구현

--- src/main/kotlin/bridge/validation/InputValidation.kt
+++ src/main/kotlin/bridge/validation/InputValidation.kt
@@ -19,15 +19,19 @@ class InputValidation {
     }
 
     fun checkUserMovingValidation(input: String){
-        if(!(input=="U" || input=="D")) throw IllegalArgumentException(ErrorType.INCORRECT_MOVING_INPUT_ERROR.message)
+        if(!(input==DOWN|| input==UP)) throw IllegalArgumentException(ErrorType.INCORRECT_MOVING_INPUT_ERROR.message)
+    }
+
+    fun checkUserRestartValidation(input: String){
+        if(!(input== RESTART || input==QUIT)) throw IllegalArgumentException(ErrorType.INCORRECT_RESTART_INPUT_ERROR.message)
     }
 
     enum class ErrorType(val message: String) {
         NOT_INTEGER_ERROR("$INITIAL_ERROR_MESSAGE 정수가 아닌 수가 포함되어 있습니다(공백포함). 정수만 입력해 주세요."),
-        BRIDGE_SIZE_RANGE_ERROR("$INITIAL_ERROR_MESSAGE 3이상 20이하의 숫자만 입력해주세요."),
-        INCORRECT_INPUT_LENGTH_ERROR("$INITIAL_ERROR_MESSAGE 입력값의 길이는 1이어야 합니다(공백포함)."),
-        INCORRECT_MOVING_INPUT_ERROR("$INITIAL_ERROR_MESSAGE 이동할 칸의 입력은 \"U\" 또는 \"D\" 만 가능합니다.")
-
+        BRIDGE_SIZE_RANGE_ERROR("$INITIAL_ERROR_MESSAGE 올바르지 않은 숫자입니다. 3이상 20이하의 숫자만 입력해주세요."),
+        INCORRECT_INPUT_LENGTH_ERROR("$INITIAL_ERROR_MESSAGE 올바르지 않은 입력값 길이입니다. 입력값의 길이는 1이어야 합니다(공백포함)."),
+        INCORRECT_MOVING_INPUT_ERROR("$INITIAL_ERROR_MESSAGE 올바르지 않은 입력값 입니다. 이동할 칸의 입력은 \"$UP\" 또는 \"$DOWN\" 만 가능합니다."),
+        INCORRECT_RESTART_INPUT_ERROR("$INITIAL_ERROR_MESSAGE 올바르지 않은 입력값 입니다. 게임 재시도를 원하시면 $RESTART 게임 종료를 원하시면 ${QUIT}를 입력해 주세요.")
     }
 
     companion object {
@@ -35,5 +39,9 @@ class InputValidation {
         const val MIN_BRIDGE_SIZE = 3
         const val MAX_BRIDGE_SIZE = 20
         const val CORRECT_INPUT_LENGTH =1
+        const val UP ="U"
+        const val DOWN ="D"
+        const val RESTART = "R"
+        const val QUIT ="Q"
     }
 }
