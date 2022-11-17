53c137939d4a4547eda5a3f78ccc6512294afb5a
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-17 14:54:35 +0900

    refactor[bridge]: 다리 입력받는 기능 BridgeGame -> Application로 이동

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -3,3 +3,19 @@ package bridge
 fun main() {
     // TODO: 프로그램 구현
 }
+
+
+fun inputBridgeSize() : Int {
+    OutputView().printInputBridgeSizeMessage()
+    return getBridgeSize(InputView().readBridgeSize())
+}
+
+fun getBridgeSize(input : String?) : Int {
+    try {
+        Validator().checkBridgeLength(input)
+        return input!!.toInt()
+    } catch(e : IllegalArgumentException) {
+        OutputView().printError(e)
+        return inputBridgeSize()
+    }
+}
--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -23,18 +23,4 @@ class BridgeGame {
      */
     fun retry() {}
 
-    fun inputBridgeSize() : Int {
-        OutputView().printInputBridgeSizeMessage()
-        return getBridgeSize(InputView().readBridgeSize())
-    }
-
-    fun getBridgeSize(input : String?) : Int {
-        try {
-            Validator().checkBridgeLength(input)
-            return input!!.toInt()
-        } catch(e : IllegalArgumentException) {
-            OutputView().printError(e)
-            return inputBridgeSize()
-        }
-    }
 }
