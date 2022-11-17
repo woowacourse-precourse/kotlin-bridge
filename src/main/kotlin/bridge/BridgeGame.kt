ef8011312a389945744348d685ad39674e6257e8
junjange <wnswkd486@naver.com>
2022-11-17 19:55:51 +0900

    feat: downMove 기능 추가

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -38,6 +38,18 @@ class BridgeGame {
         }
     }
 
+    fun downMove(isCorrect: Boolean) {
+        isSuccess = if (isCorrect) {
+            upBridge.add(EMPTY)
+            downBridge.add(ANSWER)
+            true
+        } else {
+            upBridge.add(FALL)
+            downBridge.add(EMPTY)
+            false
+        }
+    }
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      *
