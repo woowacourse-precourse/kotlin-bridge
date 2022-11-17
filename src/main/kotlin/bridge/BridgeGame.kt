0f7bb55a4c51ad83e417dac3c5d4110532a93004
junjange <wnswkd486@naver.com>
2022-11-17 19:54:02 +0900

    feat: BridgeGame 변수 추가

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -4,6 +4,12 @@ package bridge
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+    var upBridge: MutableList<String> = mutableListOf<String>()
+    var downBridge: MutableList<String> = mutableListOf<String>()
+    var isSuccess = true
+    var totalCount: Int = 1
+    var count: Int = 0
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
