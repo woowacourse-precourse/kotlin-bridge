7328403f4024c8c7dbf8334f71cf1fd57c190910
junjange <wnswkd486@naver.com>
2022-11-17 22:10:01 +0900

    test: 입력된 다리길이가 null인 경우 예외 테스트

--- src/test/kotlin/bridge/InputViewTest.kt
+++ src/test/kotlin/bridge/InputViewTest.kt
@@ -7,6 +7,14 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class InputViewTest {
 
+    @ValueSource(strings = [""])
+    @ParameterizedTest
+    fun `입력된 다리길이가 null인 경우 예외 테스트`(input: String) {
+        assertThrows<IllegalArgumentException> {
+            input.bridgeSizeInputTypeException()
+        }
+    }
+
     @ValueSource(strings = ["a", "?", "ㅈ"])
     @ParameterizedTest
     fun `입력된 다리길이가 숫자가 아닌 경우 예외 테스트`(input: String) {
