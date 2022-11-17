47d81f90ff8334267e56c503005b9b30df0486e7
권용민 <briandr@naver.com>
2022-11-17 22:57:13 +0900

    feat(Output): 다리 건너기 출력 로직 작성
     - 플레이어의 선택에 따른 결과 다리 건너기를 출력한다.

--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -15,7 +15,21 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(discoveredBridge: DiscoveredBridge) {
+        printBlocks(discoveredBridge.upperBlocks)
+        printBlocks(discoveredBridge.downerBlocks)
+    }
+
+    private fun printBlocks(blocks: List<String>) {
+        print(BRIDGE_START)
+        blocks.forEach {
+            print(it)
+            if (it != blocks.last()) {
+                print(DIVISION_LINE)
+            }
+        }
+        println(BRIDGE_END)
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
