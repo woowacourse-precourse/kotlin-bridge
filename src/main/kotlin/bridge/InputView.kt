0407009f5bdebcd09fbfe5aa0ddad087b1e045e3
junjange <wnswkd486@naver.com>
2022-11-17 17:19:03 +0900

    feat: readBridgeSize 기능 추가

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,7 +10,14 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        println("다리 건너기 게임을 시작합니다.\n")
+        return try {
+            println("다리의 길이를 입력해주세요.")
+            Console.readLine()!!.bridgeSizeInputTypeException()
+        } catch (e: IllegalArgumentException) {
+            println(Constant.ERROR_MESSAGE)
+            readBridgeSize()
+        }
     }
 
     /**
