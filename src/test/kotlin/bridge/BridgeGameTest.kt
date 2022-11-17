ff73d2f2c1281331153a2098d82dcc8e756d80ed
junjange <wnswkd486@naver.com>
2022-11-17 21:33:00 +0900

    test: 마지막 다리에서 실패하더라도 게임이 종료된다

--- src/test/kotlin/bridge/BridgeGameTest.kt
+++ src/test/kotlin/bridge/BridgeGameTest.kt
@@ -16,6 +16,17 @@ class BridgeGameTest {
         assertThat(bridgeGame.isGameEnd(bridgeMaker)).isEqualTo(true)
     }
 
+    @Test
+    fun `마지막 다리에서 실패하더라도 게임이 종료된다`() {
+        val bridgeMaker = BridgeMaker(TestNumberGenerator(listOf(1, 1, 0))).makeBridge(3)
+        val bridgeGame = BridgeGame()
+
+        bridgeGame.move(bridgeMaker[0], "U")
+        bridgeGame.move(bridgeMaker[1], "U")
+        bridgeGame.move(bridgeMaker[2], "U")
+        assertThat(bridgeGame.isGameEnd(bridgeMaker)).isEqualTo(true)
+    }
+
     class TestNumberGenerator(numbers: List<Int>) : BridgeNumberGenerator {
         private val numbers: MutableList<Int> = numbers.toMutableList()
 
