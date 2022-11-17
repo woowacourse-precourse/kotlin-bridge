6f96398ea99b19e8912b3f6fea8119c81554cc2f
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-18 01:46:38 +0900

    feat(data): Bridge 클래스 생성 및 상수 추가

---
+++ src/main/kotlin/bridge/data/Bridge.kt
@@ -0,0 +1,12 @@
+package bridge.data
+
+class Bridge {
+
+
+    companion object {
+        const val SIZE_MIN = 3
+        const val SIZE_MAX = 20
+
+        val SizeRange = SIZE_MIN..SIZE_MAX
+    }
+}
