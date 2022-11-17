8c798756c23dec2ecc26729f1123932b4b75cea9
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-17 18:03:36 +0900

    feat(BridgeMaker) : make correct bridge

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,20 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
+        val bridge = mutableListOf<String>()
+        for(number in 0 until size){
+            bridge.add(convertNumberToMoving())
+        }
         return listOf()
+    }
+
+    private fun convertNumberToMoving() : String{
+        if(bridgeNumberGenerator.generate()==0){
+            return "D"
+        }
+        else if(bridgeNumberGenerator.generate()==1){
+            return "U"
+        }
+        return ""
     }
 }
