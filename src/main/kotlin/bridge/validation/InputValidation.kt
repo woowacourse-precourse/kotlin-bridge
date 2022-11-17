3dcd7bb30da09646bb4d7db0cc8b0f186a6102eb
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-17 17:34:33 +0900

    feat(bridge): 다리길이가 3이상 20이하가 아니라면 예외 발생

--- src/main/kotlin/bridge/validation/InputValidation.kt
+++ src/main/kotlin/bridge/validation/InputValidation.kt
@@ -2,20 +2,27 @@ package bridge.validation
 
 class InputValidation {
 
-    fun checkInputInteger(input:String){
+    fun checkInputInteger(input: String) {
         try {
             input.toInt()
-        }catch (e:IllegalArgumentException){
+        } catch (e: IllegalArgumentException) {
             throw IllegalArgumentException(ErrorType.NOT_INTEGER_ERROR.message)
         }
     }
 
-    enum class ErrorType(val message:String){
+    fun checkBridgeSizeRange(size: Int) {
+        if (size< MIN_BRIDGE_SIZE || size > MAX_BRIDGE_SIZE ) throw IllegalArgumentException(ErrorType.BRIDGE_SIZE_RANGE_ERROR.message)
+    }
+
+    enum class ErrorType(val message: String) {
         NOT_INTEGER_ERROR("$INITIAL_ERROR_MESSAGE 정수가 아닌 수가 포함되어 있습니다(공백포함). 정수만 입력해 주세요."),
+        BRIDGE_SIZE_RANGE_ERROR("$INITIAL_ERROR_MESSAGE 3이상 20이하의 숫자만 입력해주세요."),
 
     }
 
-    companion object{
+    companion object {
         const val INITIAL_ERROR_MESSAGE = "[ERROR]"
+        const val MIN_BRIDGE_SIZE = 3
+        const val MAX_BRIDGE_SIZE = 20
     }
 }
