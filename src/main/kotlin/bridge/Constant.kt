6040255e559c58c91dfed60ad1fa9bb1486db20e
junjange <wnswkd486@naver.com>
2022-11-17 17:18:11 +0900

    feat: 다리 길이 Input 에러 메시지 상수 추가

--- src/main/kotlin/bridge/Constant.kt
+++ src/main/kotlin/bridge/Constant.kt
@@ -1,4 +1,8 @@
 package bridge
 
 object Constant {
+    const val ERROR_MESSAGE = "[ERROR] 잘못입력하셨습니다."
+    const val INPUT_TYPE_ERROR_MESSAGE = "[ERROR] 입력한 값이 숫자가 아닙니다."
+    const val INPUT_RANGE_ERROR_MESSAGE = "[ERROR] 입력한 값이 3이상 20이하에 숫자가 아닙니다."
+
 }
