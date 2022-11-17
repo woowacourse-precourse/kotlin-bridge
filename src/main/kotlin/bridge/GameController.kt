3f9f81a46bc4faa8a1380681bdc9d50be82dedfe
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-17 16:32:30 +0900

    refactor : migrate view to view package

--- src/main/kotlin/bridge/GameController.kt
+++ src/main/kotlin/bridge/GameController.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import bridge.view.InputView
+
 class GameController {
     private val inputView = InputView()
 
--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -1,4 +1,4 @@
-package bridge
+package bridge.view
 
 import bridge.validator.BridgeSizeValidator
 import camp.nextstep.edu.missionutils.Console
