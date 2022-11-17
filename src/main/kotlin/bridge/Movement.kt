2da4ddc2abb9d43a9b6f789cabfb15dea5a39ae1
dltkd1395 <dltkd13956@naver.com>
2022-11-18 04:34:45 +0900

    feat(Movement) : 이동 방향이 1글자가 아닌 경우 알 수 있는 기능 구현

--- src/main/kotlin/bridge/Movement.kt
+++ src/main/kotlin/bridge/Movement.kt
@@ -4,6 +4,7 @@ class Movement(private val direction: String) {
     init {
         require(isLetter()) { ErrorType.DIRECTION_LETTER.errorMessage }
         require(isUpperCaes()) { ErrorType.DIRECTION_UPPER.errorMessage }
+        require(isLength()) { ErrorType.DIRECTION_LENGTH.errorMessage }
     }
 
     private fun isLetter(): Boolean =
@@ -11,4 +12,11 @@ class Movement(private val direction: String) {
 
     private fun isUpperCaes(): Boolean =
         direction.all { char -> char.isUpperCase() }
+
+    private fun isLength(): Boolean =
+        direction.length == DIRECTION_LENGTH
+
+    companion object {
+        const val DIRECTION_LENGTH = 1
+    }
 }
