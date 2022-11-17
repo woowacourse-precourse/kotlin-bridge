b0e954a6d04e47c8cd504cb4f232f0ff312a70f5
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-17 17:57:42 +0900

    feat(bridge): 입력값의 길이가 1이 아니라면 예외 발생 기능 구현

--- src/main/kotlin/bridge/validation/InputValidation.kt
+++ src/main/kotlin/bridge/validation/InputValidation.kt
@@ -14,9 +14,14 @@ class InputValidation {
         if (size< MIN_BRIDGE_SIZE || size > MAX_BRIDGE_SIZE ) throw IllegalArgumentException(ErrorType.BRIDGE_SIZE_RANGE_ERROR.message)
     }
 
+    fun checkInputLength(input: String){
+        if(input.length!= CORRECT_INPUT_LENGTH) throw IllegalArgumentException(ErrorType.INCORRECT_INPUT_LENGTH_ERROR.message)
+    }
+
     enum class ErrorType(val message: String) {
         NOT_INTEGER_ERROR("$INITIAL_ERROR_MESSAGE 정수가 아닌 수가 포함되어 있습니다(공백포함). 정수만 입력해 주세요."),
         BRIDGE_SIZE_RANGE_ERROR("$INITIAL_ERROR_MESSAGE 3이상 20이하의 숫자만 입력해주세요."),
+        INCORRECT_INPUT_LENGTH_ERROR("$INITIAL_ERROR_MESSAGE 입력값의 길이는 1이어야 합니다(공백포함).")
 
     }
 
@@ -24,5 +29,6 @@ class InputValidation {
         const val INITIAL_ERROR_MESSAGE = "[ERROR]"
         const val MIN_BRIDGE_SIZE = 3
         const val MAX_BRIDGE_SIZE = 20
+        const val CORRECT_INPUT_LENGTH =1
     }
 }
