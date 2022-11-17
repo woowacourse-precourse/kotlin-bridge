499db2512ef0e9aab9bed3bcf4ac7b08429fddd7
윤동현 <donghyunyoon81@gmail.com>
2022-11-17 18:54:36 +0900

    feat<BridgeMaker> : create makeOneColumn function
    무엇을 :  다리 한칸을 만드는 기능
    왜 : 위칸 아래칸이 있는 다리에서 건널 수 있는 칸과 없는 칸을 구분하기 위해서
    어떻게 : 0 또는 1이 무작위로 나오는 함수에서 0 또는 1인지를 비교하여 위칸 아래칸을 구분한다
    
    무엇을

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -8,7 +8,18 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    fun makeBridge(size: Int): List<String> {
+    fun makeBridge(size: Int,oneColum:String): List<String> {
+
         return listOf()
+    }
+
+    fun makeOneColumn(): String {
+        val bridgeNumberGenerator = bridgeNumberGenerator.generate()
+        return if (bridgeNumberGenerator == RANDOM_LOWER_INCLUSIVE) "UD" else "DU"
+    }
+
+    companion object {
+        const val RANDOM_LOWER_INCLUSIVE = 0
+        const val RANDOM_UPPER_INCLUSIVE = 1
     }
 }
