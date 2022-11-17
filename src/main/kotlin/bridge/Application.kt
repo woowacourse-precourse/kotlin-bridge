de79709a3f7d436426379c09fb97bd07af38522f
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2022-11-18 02:47:52 +0900

    feat: 게임 시작 문구를 출력하는 기능 구현

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,5 @@
 package bridge
 
 fun main() {
-    // TODO: 프로그램 구현
+    OutputView().printStart()
 }
---
+++ src/main/kotlin/constant/Message.kt
@@ -0,0 +1,5 @@
+package constant
+
+enum class Message(val message: String) {
+    GAME_START("다리 건너기 게임을 시작합니다.")
+}
