e4081c4c9416966be1653494fda4483dbec0ca3f
82104 <tir2986@naver.com>
2022-11-17 18:07:15 +0900

    feat: 재시작 여부를 물어볼 수 있다.

--- src/main/kotlin/bridge/model/BridgeGame.kt
+++ src/main/kotlin/bridge/model/BridgeGame.kt
@@ -3,11 +3,14 @@ package bridge.model
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame(private val bridge: Bridge, private var currentPosition: Int = 0) {
+class BridgeGame(private val bridge: Bridge) {
+
+    private var currentPosition: Int = -1
+
     /**
      * @param moving 이동 방향
      * @return 이동 성공 여부(방향 일치 여부만 확인)
-     * @throws IllegalStateException
+     * @throws IllegalStateException 현재 위치와 다리 길이 매칭 오류
      */
     fun move(moving: String): Boolean {
         return try {
@@ -19,10 +22,18 @@ class BridgeGame(private val bridge: Bridge, private var currentPosition: Int = 
     }
 
     /**
-     * @param
-     * @return
+     * @param command 게임 커맨드 문자
+     * @return 재시도 여부
+     * @throws IllegalArgumentException 없는 커맨드로 호출
      */
-    fun retry() {}
+    fun retry(command: String): Boolean {
+        require(command in setOf(COMMAND_QUIT, COMMAND_RETRY)) { ERROR_COMMAND_MATCH }
+        if (command == COMMAND_RETRY) {
+            currentPosition -= 1
+            return true
+        }
+        return false
+    }
 
     fun isEnd(): Boolean {
         return bridge.isBridgeEnd(currentPosition)
@@ -30,5 +41,8 @@ class BridgeGame(private val bridge: Bridge, private var currentPosition: Int = 
 
     companion object {
         private const val ERROR_POSITION_BOUND = "플레이어 현재 위치가 올바르지 않습니다."
+        private const val ERROR_COMMAND_MATCH = "커맨드가 올바르지 않습니다."
+        private const val COMMAND_RETRY = "R"
+        private const val COMMAND_QUIT = "Q"
     }
 }
