23de4792d646a6cd86b0d19a029de7e05ef86d63
junjange <wnswkd486@naver.com>
2022-11-17 19:55:10 +0900

    feat: move 기능 추가

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -17,6 +17,14 @@ class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun move() {}
+    fun move(moveAnswer: String, moveOrder: String) {
+        when (moveAnswer) {
+            UP -> if (moveOrder == UP) upMove(true) else upMove(false)
+            DOWN -> if (moveOrder == DOWN) downMove(true) else downMove(false)
+            else -> throw IllegalArgumentException(ERROR_MESSAGE)
+        }
+        count++
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
