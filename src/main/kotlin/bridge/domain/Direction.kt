8b2649ebd03168894183d99c0f580df6b6f142e3
phs <gustjd0808@pusan.ac.kr>
2022-11-18 13:33:19 +0900

    feat(direction): add method that get Direction by hot key

--- src/main/kotlin/bridge/domain/Direction.kt
+++ src/main/kotlin/bridge/domain/Direction.kt
@@ -1,9 +1,21 @@
 package bridge.domain
 
+import bridge.sentence.ErrorSentence
+
 enum class Direction(private val hotKey: String, private val number: Int) {
     UP("U", 1),
     DOWN("D", 0);
 
     fun isMatch(hotKey: String) = this.hotKey == hotKey
     fun isMatch(number: Int) = this.number == number
+
+    companion object{
+        fun getDirection(hotKey: String): Direction {
+            return when{
+                UP.isMatch(hotKey) -> UP
+                DOWN.isMatch(hotKey) -> DOWN
+                else -> throw IllegalArgumentException(ErrorSentence.MOVE.sentence())
+            }
+        }
+    }
 }
