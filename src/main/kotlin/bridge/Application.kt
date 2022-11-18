dbbb48d63fce84af27e33c4105b96ed748ce9e17
hyunji1203 <hj27720@gmail.com>
2022-11-19 03:05:33 +0900

    feat: 다리 생성
    1과 0을 랜덤으로 뽑아 각각 U 혹은 D인 다리가 되도록 게임에 사용될 다리를 생성한다.

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,9 +1,11 @@
 package bridge
 
 fun main() {
-    //게임 시작
     println("다리 건너기 게임을 시작합니다.")
 
     var bridgeLength = InputView().readBridgeSize()
+
+    var bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
+    var bridge = bridgeMaker.makeBridge(bridgeLength)
 
 }
