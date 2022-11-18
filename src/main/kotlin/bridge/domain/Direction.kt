54bb1fe3389a59ff31f18536ca59684d6bf10cd5
phs <gustjd0808@pusan.ac.kr>
2022-11-18 20:25:19 +0900

    feat(direction): add method that get Direction by number

--- src/main/kotlin/bridge/domain/Direction.kt
+++ src/main/kotlin/bridge/domain/Direction.kt
@@ -8,6 +8,8 @@ enum class Direction(private val hotKey: String, private val number: Int) {
 
     fun isMatch(hotKey: String) = this.hotKey == hotKey
     fun isMatch(number: Int) = this.number == number
+    fun hotKey() = hotKey
+    fun number() = number
 
     companion object{
         fun getDirection(hotKey: String): Direction {
@@ -15,6 +17,14 @@ enum class Direction(private val hotKey: String, private val number: Int) {
                 UP.isMatch(hotKey) -> UP
                 DOWN.isMatch(hotKey) -> DOWN
                 else -> throw IllegalArgumentException(ErrorSentence.MOVE.sentence())
+            }
+        }
+
+        fun getDirection(number: Int): Direction {
+            return when{
+                UP.isMatch(number) -> UP
+                DOWN.isMatch(number) -> DOWN
+                else -> throw IllegalArgumentException(ErrorSentence.MOVE_NUMBER.sentence())
             }
         }
     }
