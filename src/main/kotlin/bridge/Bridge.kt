b433b2b3dd53c3cc1060992633dc94f4251dcd42
minju kim <62362753+alswn2348@users.noreply.github.com>
2022-11-18 23:39:44 +0900

    refactor(Bridge) : 브릿지 클래스 리팩토링
    필드 변경 hitBridge(MutableList) -> hit(String)
    매서드 이름 변경
    hit 변수 getter setter 추가

--- src/main/kotlin/bridge/Bridge.kt
+++ src/main/kotlin/bridge/Bridge.kt
@@ -4,35 +4,35 @@ class Bridge (private val bridge:List<String>){
     var upBridge = ""
     var downBridge =""
     var currentLocation = 0
-    private val hitBridge= mutableListOf<String>("")
-    fun checkCurrentLocation(upDown : String): Boolean {
+    private var hit = ""
+    fun matches(upDown : String): Boolean {
         if(bridge[currentLocation] == upDown){
             return true
         }
         return false
     }
 
-    fun move(hit: String) {
-        currentLocation++
-        hitBridge.add(hit)
-
+    fun setHit(hit: String) {
+        this.hit = hit
     }
-    fun getCurrentLocationHit() = hitBridge[currentLocation]
-    fun retry() {
+    fun getHit() = hit
+    fun clear() {
         currentLocation = 0
         upBridge = ""
         downBridge = ""
-        hitBridge.clear()
-        hitBridge.add("")
-
+        hit =""
     }
 
     fun finish(): Boolean {
         if(currentLocation == bridge.size){
-            if(getCurrentLocationHit() == "O")
+            if(getHit() == "O")
             return true
         }
         return false
+    }
+
+    fun moveCurrentLocation() {
+        currentLocation++
     }
 
 }
