389c709dabc1b908dee0d36fe9771572640f7276
meltnia <themtale@gmail.com>
2022-11-19 00:26:08 +0900

    <Feat> 유저에게 움직임을 받는 것 예외처리 이전에 구현

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -21,7 +21,10 @@ class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        val userinput = Console.readLine()
+
+
+        return userinput
     }
 
     /**
