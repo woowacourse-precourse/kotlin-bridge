efd9bfaacf11cc0cecd4d0b2f7d236a533686149
82104 <tir2986@naver.com>
2022-11-18 23:11:41 +0900

    test: BridgeGame 클래스 테스트 추가

--- src/test/kotlin/bridge/model/BridgeGameTest.kt
+++ src/test/kotlin/bridge/model/BridgeGameTest.kt
@@ -10,33 +10,33 @@ class BridgeGameTest {
     private val directions = listOf("U", "D", "D")
 
     @BeforeEach
-    private fun makeGame() {
+    private fun initializeBridgeGame() {
         _game = BridgeGame(Bridge(directions))
     }
 
     @Test
     fun `플레이어가 건널 수 있는 방향으로 이동하면 게임 상태는 ONGOING 이다`() {
-        val moving = "U"
+        val moving = directions[0]
         game.move(moving)
         assert(game.currentState == BridgeGameState.ONGOING)
     }
 
     @Test
     fun `플레이어가 건널 수 없는 방향으로 이동하면 게임 상태는 FAIL 이다`() {
-        val moving = "D"
+        val moving = getIncorrectMoving(0)
         game.move(moving)
         assert(game.currentState == BridgeGameState.FAIL)
     }
 
     @Test
     fun `플레이어가 다리를 모두 건넜다면 상태는 PASS 이다`() {
-        setGameEnd()
+        setGamePassed()
         assert(game.currentState == BridgeGameState.PASS)
     }
 
     @Test
     fun `플레이어가 다리가 끝났는데도 이동하려하면 예외를 발생시킨다`() {
-        setGameEnd()
+        setGamePassed()
         assertThrows<IllegalStateException> {
             game.move("U")
         }
@@ -45,7 +45,7 @@ class BridgeGameTest {
     @Test
     fun `재시도 시 총 시도 횟수가 1 증가한다`() {
         val beforeTryCount = game.tryCount
-        setGameEnd()
+        setGameFailed()
         game.retry("R")
         val afterTryCount = game.tryCount
         assert(
@@ -54,15 +54,39 @@ class BridgeGameTest {
     }
 
     @Test
-    fun `이동 과정과 성공 여부를 기록해야 한다`() {
-        setGameEnd()
+    fun `실패 종료 시 총 시도 횟수는 변하지 않는다`() {
+        val beforeTryCount = game.tryCount
+        setGameFailed()
+        game.retry("Q")
+        val afterTryCount = game.tryCount
+        assert(
+            afterTryCount == beforeTryCount
+        )
+    }
+
+    @Test
+    fun `이동 결과들을 기록해야 한다`() {
+        setGamePassed()
         val expects = directions.map {
             BridgeGame.MovingResult(Direction.getByDisplayName(it), true)
         }
         assert(game.movingResults == expects)
     }
 
-    private fun setGameEnd() {
+    private fun setGamePassed() {
         directions.forEach { game.move(it) }
+    }
+
+    private fun setGameFailed() {
+        val incorrectMoving = getIncorrectMoving(0)
+        game.move(incorrectMoving)
+    }
+
+    private fun getIncorrectMoving(position: Int): String {
+        return if (directions[position] == "U") {
+            "D"
+        } else {
+            "U"
+        }
     }
 }
