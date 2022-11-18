445f84dbc414686b1726aab02b7dd70de4aec69e
idra79haza <idra79haza@github.com>
2022-11-18 20:38:58 +0900

    feat(Application): initial application
     initial application

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -3,7 +3,6 @@ package bridge
 import model.BridgeGame
 import java.util.StringJoiner
 import javax.print.DocFlavor.STRING
-
 fun main() {
     // 다리 건너기 게임을 시작합니다.
     view.OutputView().printGameStart()
@@ -20,6 +19,8 @@ fun main() {
     var trialCount = 1
     val moveHashmapList = MutableList(bridgeSize) { it -> HashMap<String, String>() }
     var totalMovePath = mutableListOf<String>("", "")
+    val tempList = MutableList(bridgeSize) { it -> ""}
+    println("tempList.size: ${tempList.size}")
 
     // 여기부터가 새롭게 반복되는 구간
     while (true) {
@@ -29,10 +30,21 @@ fun main() {
         val moveInput = view.InputView().readMoving()
         var flag = true
         // 인풋을 받아서 결과를 도출하는 함수
-        val resultMoveHashmap = BridgeGame().move(moveInput, answerMove)
-        answerMove = answerMove.slice(1 until answerMove.size)
+        var i = 0
+
+        println("answerMove[i]: ${answerMove[i]}")
+        println("i: $i")
+        val resultMoveHashmap = BridgeGame().move(moveInput, answerMove[i])
+        tempList[trialCount-1] = answerMove[0]
+        println("tempList: $tempList")
+//        answerMove = answerMove.slice(1 until answerMove.size)
+//        answerMove.toMutableList().
         println(answerMove)
         println("resultMoveHashmap: $resultMoveHashmap")
+
+        // 횟수 늘리기
+        trialCount += 1
+        i += 1
 
         moveHashmapList[trialCount - 1] = resultMoveHashmap
         println("moveHashmapList: ${moveHashmapList}")
@@ -45,14 +57,24 @@ fun main() {
             println(totalMovePath[0])
             println(totalMovePath[1])
             flag = BridgeGame().retry()
+
+//            if (flag) {
+//                for(i in 0 until tempList.size){
+//                    println("tempList[i]: ${tempList[i]}")
+//                    answerMove.toMutableList().add(i, tempList[i])
+//                    println("answerMove: $answerMove")
+//                    println("answerMove[i]: ${answerMove[i]}")
+//                    answerMove
+//                }
+//            }
+
+            // 그다음에 종료 체크
+            if (!flag) {
+                break
+            }
             continue
         }
 
-        // 그다음에 종료 체크
-        if (!flag) {
-            answerMove = originAnswerMove
-            break
-        }
 
         // 그 결과를 프린트 하는 함수
 //        if (trialCount == 1) {
@@ -67,11 +89,11 @@ fun main() {
         println(totalMovePath[1])
 
         if (trialCount == bridgeSize) {
-            answerMove = originAnswerMove
+            for(i in 0 until tempList.size){
+                answerMove.toMutableList()[i] = tempList[i]
+            }
             break
         }
-        // 횟수 늘리기
-        trialCount += 1
     }
     // 최종 게임 결과
     view.OutputView().printResultFinalGame()
