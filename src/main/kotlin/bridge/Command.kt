d77d3863dfe1fd277fc091c8a5bed428e4c6e683
dltkd1395 <dltkd13956@naver.com>
2022-11-18 17:36:40 +0900

    feat(Command) : 게임 명령어가 소문자인 경우 알 수 있는 기능 구현

--- src/main/kotlin/bridge/Command.kt
+++ src/main/kotlin/bridge/Command.kt
@@ -3,8 +3,12 @@ package bridge
 class Command(private val execution: String) {
     init {
         require(isLetter()) { ErrorType.COMMAND_LETTER.errorMessage }
+        require(isUpperCaese()) { ErrorType.COMMAND_UPPER.errorMessage }
     }
 
     private fun isLetter(): Boolean =
         execution.all { char -> char.isLetter() }
+
+    private fun isUpperCaese(): Boolean =
+        execution.all { char -> char.isUpperCase() }
 }
