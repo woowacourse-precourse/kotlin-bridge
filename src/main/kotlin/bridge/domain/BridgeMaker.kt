ced6b7bf51ee5958e22b77d0c6e88f735d1f4f7b
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-18 14:30:14 +0900

    feat(BridgeMaker): 다리 생성 함수 구현

--- src/main/kotlin/bridge/domain/BridgeMaker.kt
+++ src/main/kotlin/bridge/domain/BridgeMaker.kt
@@ -11,6 +11,13 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        val bridge: MutableList<String> = mutableListOf()
+        for (i in 0 until size) {
+            when(bridgeNumberGenerator.generate()) {
+                1 -> bridge.add("U")
+                0 -> bridge.add("D")
+            }
+        }
+        return bridge
     }
 }
