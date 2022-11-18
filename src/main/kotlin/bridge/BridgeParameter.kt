23278161fedceaf140c0fe5530795394c1f81c26
meltnia <themtale@gmail.com>
2022-11-18 22:42:24 +0900

    <Feat> BridgeMessage 클래스 작성, 게임 예시의 문장들 기입. 파일명 변경은 추후에?

--- src/main/kotlin/bridge/BridgeParameter.kt
+++ src/main/kotlin/bridge/BridgeParameter.kt
@@ -4,3 +4,12 @@ enum class BridgeParameter(value: Int) {
     BridgeMinLength(3),
     BridgeMaxLength(20)
 }
+
+enum class BridgeMessage(word : String){
+    Start("다리 건너기 게임을 시작합니다."),
+    End("최종 게임 결과"),
+    WinorLose("게임 성공 여부: "),
+    Win("승리"),
+    Lose("실패"),
+    AreYouWantRetry("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+}
