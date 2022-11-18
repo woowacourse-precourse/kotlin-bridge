ed4bde25f086fbf8bb634821ad5df5bb3e34039a
lgy15 <lgy159@naver.com>
2022-11-18 11:20:17 +0900

    refactor: constant 리팩토링

--- src/main/kotlin/bridge/resources/Constatns.kt
+++ src/main/kotlin/bridge/resources/Constatns.kt
@@ -2,16 +2,18 @@ package bridge.resources
 
 
 // FOR INPUT VIEW
-const val INPUT_BRIDGE_GAME_START = "다리 건너기 게임을 시작합니다."
 const val INPUT_BRIDGE_LENGTH = "\n다리의 길이를 입력해주세요."
 const val INPUT_CHOICE_ROOM = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)"
 const val INPUT_CHOICE_RESTART = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
+
+// FOR OUTPUT VIEW
+const val INPUT_BRIDGE_GAME_START = "다리 건너기 게임을 시작합니다."
 const val END_GAME = "\n최종 게임 결과"
 const val GAME_RESULT_WIN = "\n게임 성공 여부: 성공"
 const val GAME_RESULT_LOSE = "\n게임 성공 여부: 실패"
 const val GAME_TRY_COUNT = "총 시도한 횟수: %d"
 
-// FOR OUTPUT VIEW
+// FOR SIGN VIEW
 const val START_BRACKET = "[ "
 const val END_BRACKET = " ]"
 const val SEPARATOR = " | "
