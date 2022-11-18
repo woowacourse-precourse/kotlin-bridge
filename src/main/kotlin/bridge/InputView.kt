3fcef0b8951d8ad29d9556e9f249f976ad88c9f0
hyunji1203 <hj27720@gmail.com>
2022-11-19 05:19:56 +0900

    feat: 게임 실패 시 게임 재시작 or 종료 입력 받기
    게임에 실패했을 때 사용자에게 게임 재시작 혹은 종료를 선택하기 위해 'R' or 'Q'를 입력받는다.

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -27,5 +27,9 @@ class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    fun readGameCommand(): String {}
+    fun readGameCommand(): String {
+        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+        var gameControl = Console.readLine()
+        return gameControl
+    }
 }
