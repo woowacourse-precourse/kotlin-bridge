969f86585b1ef3d30d52eb49838299a78a71f568
ChunBaee <mljung10@naver.com>
2022-11-18 19:20:46 +0900

    test: 유저가 입력하는 다리 길이 입력값과 관련된 테스트 케이스 작성

---
+++ src/test/kotlin/bridge/BridgeTest.kt
@@ -0,0 +1,51 @@
+package bridge
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class BridgeTest : NsTest() {
+
+    /** 다리 길이 입력 관련 테스트 **/
+
+    @Test
+    fun `다리 길이 입력 값에 문자열이 포함될 경우 예외가 발생한다`() {
+        assertSimpleTest {
+            runException("a")
+            Assertions.assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    @Test
+    fun `다리 길이 입력 값이 공백일 경우 예외가 발생한다`() {
+        assertSimpleTest {
+            runException(" ")
+            Assertions.assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    @Test
+    fun `다리 길이 입력 값이 범위를 벗어날 경우 예외가 발생한다 - 넘을 경우`() {
+        assertSimpleTest {
+            runException("36")
+            Assertions.assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    @Test
+    fun `다리 길이 입력 값이 범위를 벗어날 경우 예외가 발생한다 - 모자랄 경우`() {
+        assertSimpleTest {
+            runException("2")
+            Assertions.assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    override fun runMain() {
+        main()
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE = "[ERROR]"
+    }
+}
