0ab4aa4d3f1beadf5ae1bc60bd9fd7d2ab27d4f1
phs <gustjd0808@pusan.ac.kr>
2022-11-18 13:42:31 +0900

    feat: add enum class for game command

---
+++ src/main/kotlin/bridge/GameCommand.kt
@@ -0,0 +1,20 @@
+package bridge
+
+import bridge.sentence.ErrorSentence
+
+enum class GameCommand(private val hotKey: String) {
+    RESTART("R"),
+    QUIT("Q");
+
+    fun isMatch(hotKey: String) = this.hotKey == hotKey
+
+    companion object{
+        fun getGameCommand(hotKey: String): GameCommand {
+            return when{
+                RESTART.isMatch(hotKey) -> RESTART
+                QUIT.isMatch(hotKey) -> QUIT
+                else -> throw IllegalArgumentException(ErrorSentence.RESTART.sentence())
+            }
+        }
+    }
+}
