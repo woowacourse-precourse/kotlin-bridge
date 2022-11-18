695d345fb57198d8a0dec5b7f00a549d26bec50d
phs <gustjd0808@pusan.ac.kr>
2022-11-18 12:34:10 +0900

    chore: remove method that validate direction input

--- src/main/kotlin/bridge/InputValidator.kt
+++ src/main/kotlin/bridge/InputValidator.kt
@@ -4,14 +4,8 @@ import bridge.enum.Direction
 import bridge.enum.ErrorSentence
 
 object InputValidator {
-
     fun validateDigitNumber(input: String) {
         if(input.any { !it.isDigit() })
             throw IllegalArgumentException(ErrorSentence.BRIDGE_LENGTH.sentence())
-    }
-
-    fun validateDirection(input: String) {
-        if(!Direction.UP.isMatch(input) && !Direction.DOWN.isMatch(input))
-            throw IllegalArgumentException(ErrorSentence.MOVE.sentence())
     }
 }
