a9a73f14fdcc2a41a20e35cd75c07f2e7f0515e9
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-18 22:51:02 +0900

    feat[bridge]: 게임을 총 시도한 횟수를 구하는 기능 구현

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -6,14 +6,29 @@ import camp.nextstep.edu.missionutils.Console
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
+class BridgeGame(private val size : Int) {
+
+    private var gameCount : Int = 1
+    private var idx : Int = 0
+    private var bridgeMaker : BridgeMaker
+    private var bridgeMap: List<String>
+    private var bridge : Bridge
+
+    init {
+        bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
+        bridgeMap = bridgeMaker.makeBridge(size)
+        bridge = Bridge(size)
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun move(input : String) {
+        bridge.move(BridgeDirection.valueOf(input), bridgeMap, idx)
+        idx++
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -21,6 +36,18 @@ class BridgeGame {
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry() {}
+    fun retry(){
+        gameCount++
+        idx = 0
+        bridge = Bridge(size)
+    }
 
+    fun getInputMovePossible(input: String) : Boolean {
+        if(bridge.movePossible(BridgeDirection.valueOf(input), bridgeMap, idx)) return true
+        return false
+    }
+
+    fun getBridge() : Bridge = bridge
+
+    fun getGameCount() : Int = gameCount
 }
