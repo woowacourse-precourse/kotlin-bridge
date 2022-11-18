b9aba7df446bfcab3256d8cd83db61cc3a858e63
zqwz <zqwz@naver.com>
2022-11-18 16:59:23 +0900

    feat(InputView): 예외 사항 발생시 재입력 기능 추가

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -10,27 +10,39 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        val line = Console.readLine()
-        Exception.validateNumber(line)
-        Exception.validateSizeRange(line.toInt())
-        return line.toInt()
+        return try {
+            val line = Console.readLine()
+            Exception.validateNumber(line)
+            Exception.validateSizeRange(line.toInt())
+            line.toInt()
+        } catch (e: IllegalArgumentException) {
+            readBridgeSize()
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        val line = Console.readLine()
-        Exception.validateDirectionRange(line)
-        return line
+        return try {
+            val line = Console.readLine()
+            Exception.validateDirectionRange(line)
+            line
+        } catch (e: IllegalArgumentException) {
+            readMoving()
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        val line = Console.readLine()
-        Exception.validateRestartRange(line)
-        return line
+        return try {
+            val line = Console.readLine()
+            Exception.validateRestartRange(line)
+            line
+        } catch (e: IllegalArgumentException) {
+            readGameCommand()
+        }
     }
 }
