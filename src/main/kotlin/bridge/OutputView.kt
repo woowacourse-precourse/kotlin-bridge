fcf5bba6748dd09f8b18b31512d16bb02c3651c1
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-18 13:17:49 +0900

    feat[bridge]: 최종 결과를 출력하는 기능 구현

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -19,7 +19,10 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+    fun printResult(bridge: Bridge) {
+        println(FINAL_RESULT_MESSAGE)
+        printMap(bridge)
+    }
 
     fun printError(error : IllegalArgumentException) {
         println(error.message)
@@ -41,5 +44,6 @@ class OutputView {
         const val BRIDGE_SIZE_INPUT_MESSAGE = "다리의 길이를 입력해주세요."
         const val MOVE_DIRECTION_INPUT_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
         const val RESTART_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
+        const val FINAL_RESULT_MESSAGE = "최종 게임 결과"
     }
 }
