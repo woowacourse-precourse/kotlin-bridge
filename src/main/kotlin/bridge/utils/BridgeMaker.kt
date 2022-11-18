bce83ea374f391d97cc4bae38cae3b2fbd13434d
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-18 17:00:08 +0900

    feat: add makeBridge function Add function that generate bridge

--- src/main/kotlin/bridge/utils/BridgeMaker.kt
+++ src/main/kotlin/bridge/utils/BridgeMaker.kt
@@ -11,7 +11,14 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
+        val bridge = mutableListOf<String>()
+        for(i in 0 until size) {
+            if(bridgeNumberGenerator.generate() == 0)
+                bridge.add("D")
+            else
+                bridge.add("U")
+        }
 
-        return listOf()
+        return bridge.toList()
     }
 }
