1b992640e894b587fbe26ac1460ed78c0f4488b1
Lim-Joosung <kinglim1224@naver.com>
2022-11-18 19:11:38 +0900

    feat: InputView.kt readBridgeSize() 구현

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,5 @@
 package bridge
-
+import camp.nextstep.edu.missionutils.Console
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,7 +8,8 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        val readBridge = Console.readLine()
+        return readBridge.toInt()
     }
 
     /**
