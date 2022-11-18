155deb422e3aaf84e1786942345d0144c6d31809
idra79haza <idra79haza@github.com>
2022-11-18 16:10:04 +0900

    feat(makeBridge): makeBridge to make bridge
    initial commit of makeBridge with else if use

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,16 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+
+        val bridge = mutableListOf<String>()
+        for(i in 0 until size) {
+            val randomNum = bridgeNumberGenerator.generate()
+            println(randomNum)
+            if (randomNum == 0) {
+                bridge.add(i, "D")
+            } else if(randomNum == 1)
+            bridge.add(i, "U")
+        }
+        return bridge
     }
 }
