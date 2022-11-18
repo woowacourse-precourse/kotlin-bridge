f5477b035be2bb5b7eb91a53223e6b23d956b91b
ChunBaee <mljung10@naver.com>
2022-11-18 16:35:26 +0900

    feat: 게임 시작 초석작업

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -12,8 +12,13 @@ private fun onProgramStart() {
 private fun onStartGame() {
     val userInputBridgeLength = getUserInputBridgeLength()
     val bridge = makeBridge(userInputBridgeLength)
+    playBridgeGame(bridge)
 }
 
 private fun getUserInputBridgeLength() = InputView().readBridgeSize()
 
 private fun makeBridge(userInputNumber : Int) : List<String> = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(userInputNumber)
+
+private fun playBridgeGame(bridge : List<String>) {
+    BridgeGame(bridge).move()
+}
