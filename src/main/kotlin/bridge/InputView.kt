14dddbb52cadbfe6ae6162b8a9ce563a7521dbf4
ponopono0322 <ponopono0322@gmail.com>
2022-11-19 00:13:21 +0900

    refactor: InputView 클래스 내 메소드 추가 및 수정
    - readBridgeSize: 예외 기능 분할
    - readMoving: 예외 기능 분할
    - readGameCommand: 예외 기능 분할
    - lengthChecker: 허용 입력 길이 초과 검증
    - numberChecker: 숫자 입력 검증
    - stringChecker: 문자 입력 검증
    - numberRangeChecker: 허용 숫자 범위 검증
    - stringRangeChecker: 허용 문자 입력 범위 검증
    - companion object: 하드 텍스트 제거

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,6 +1,5 @@
 package bridge
 import camp.nextstep.edu.missionutils.Console
-import java.lang.NumberFormatException
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -10,27 +9,22 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        val input: Int
-        try {
-            input = Console.readLine().toInt()
-            if (input !in 3..20) {
-                throw IllegalArgumentException()
-            }
-        } catch (e: NumberFormatException) {
-            throw IllegalArgumentException()
-        }
+        val input = Console.readLine().trim()
+        this.numberChecker(input)
+        this.numberRangeChecker(input)
 
-       return input
+        return input.toInt()
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        val input = Console.readLine()
-        if (input.length != 1 || input !in listOf("U", "D", "u", "d")) {
-            throw IllegalArgumentException()
-        }
+        val input = Console.readLine().trim()
+        this.lengthChecker(input)
+        this.stringChecker(input)
+        this.stringRangeChecker(input, MOVING_RANGE)
+
         return input.uppercase()
     }
 
@@ -38,10 +32,57 @@ class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        val input = Console.readLine()
-        if (input.length != 1 || input !in listOf("R", "Q", "r", "q")) {
-            throw IllegalArgumentException()
-        }
+        val input = Console.readLine().trim()
+        this.lengthChecker(input)
+        this.stringChecker(input)
+        this.stringRangeChecker(input, COMMAND_RANGE)
+
         return input.uppercase()
     }
+
+    private fun lengthChecker(input: String) {
+        if (input.length != 1) {
+            throw IllegalArgumentException(NOT_ALLOWED_STRING_RANGE_INPUT)
+        }
+    }
+
+    private fun numberChecker(input: String) {
+        if (input.toIntOrNull() == null) {
+            throw IllegalArgumentException(NOT_ALLOWED_NUMBER_INPUT)
+        }
+    }
+
+    private fun stringChecker(input: String) {
+        if (input.toIntOrNull() != null) {
+            throw IllegalArgumentException(NOT_ALLOWED_STRING_INPUT)
+        }
+    }
+
+    private fun numberRangeChecker(input: String) {
+        if (input.toInt() !in MIN_BRIDGE_SIZE..MAX_BRIDGE_SIZE) {
+            throw IllegalArgumentException(NOT_ALLOWED_NUMBER_RANGE_INPUT)
+        }
+    }
+
+    private fun stringRangeChecker(input: String, range: List<String>) {
+        if (input !in range) {
+            when(range) {
+                COMMAND_RANGE -> throw IllegalArgumentException(NOT_ALLOWED_COMMAND_RANGE)
+                MOVING_RANGE -> throw IllegalArgumentException(NOT_ALLOWED_MOVING_RANGE)
+            }
+        }
+    }
+    companion object {
+        const val MIN_BRIDGE_SIZE = 3
+        const val MAX_BRIDGE_SIZE = 20
+        const val NOT_ALLOWED_NUMBER_INPUT = "[ERROR] 입력이 올바르지 않습니다(HINT: 숫자 입력)."
+        const val NOT_ALLOWED_STRING_INPUT = "[ERROR] 입력이 올바르지 않습니다(HINT: 문자 입력)."
+        const val NOT_ALLOWED_NUMBER_RANGE_INPUT = "[ERROR] 허용된 입력 범위가 아닙니다(HINT: $MIN_BRIDGE_SIZE ~ $MAX_BRIDGE_SIZE 사이 숫자입력)."
+        const val NOT_ALLOWED_STRING_RANGE_INPUT = "[ERROR] 허용된 입력 범위가 아닙니다(HINT: 하나의 문자만 입력)"
+        val COMMAND_RANGE = listOf("R", "Q", "r", "q")
+        val MOVING_RANGE = listOf("U", "D", "u", "d")
+        const val NOT_ALLOWED_COMMAND_RANGE = "[ERROR] 허용된 입력 범위가 아닙니다(HINT: R, Q 중 하나 입력)"
+        const val NOT_ALLOWED_MOVING_RANGE = "[ERROR] 허용된 입력 범위가 아닙니다(HINT: U, D 중 하나 입력)"
+    }
+
 }
