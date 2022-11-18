f50b418514577c5b477a82697c7ddb6a7f805a1e
Yerin <myheaven_b@naver.com>
2022-11-18 16:42:58 +0900

    feat: add function move in BridgeGame

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -19,4 +19,38 @@ class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry() {}
+class BridgeGame(private val bridge: List<String>) {
+    var playerBridge = mutableListOf<String>()
+    var resultUpBridge = mutableListOf<String>()
+    var resultDownBridge = mutableListOf<String>()
+    var playCount = 0
+
+    fun move(moving: String) {
+        playerBridge.add(moving)
+        if (moving == "U") {
+            updateUpBridge(moving)
+        }
+        if (moving == "D") {
+            updateDownBridge(moving)
+        }
+    }
+
+    private fun updateUpBridge(moving: String) {
+        if (bridge[playerBridge.size -1] == moving) {
+            resultUpBridge.add("O")
+        }
+        else {
+            resultUpBridge.add("X")
+        }
+    }
+
+    private fun updateDownBridge(moving: String) {
+        if (bridge[playerBridge.size -1] == moving) {
+            resultDownBridge.add("O")
+        }
+        else {
+            resultDownBridge.add("X")
+        }
+    }
+
 }
