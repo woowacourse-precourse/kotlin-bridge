4c3bdcbea26e54cb4fa982455be31b15cc328183
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-18 17:13:57 +0900

    test(InputViewTest): 이동 입력 테스트 추가

--- src/test/kotlin/bridge/domain/InputViewTest.kt
+++ src/test/kotlin/bridge/domain/InputViewTest.kt
@@ -11,7 +11,8 @@ import org.junit.jupiter.params.provider.CsvSource
 class InputViewTest : NsTest() {
     private val inputView = InputView()
 
-    @ParameterizedTest
+
+    @ParameterizedTest(name = "다리 길이 입력 테스트: {0}")
     @CsvSource(DIGIT_EXCEPTION, RANGE_EXCEPTION)
     fun `다리 길이 입력 테스트`(bridgeSize: String) {
         assertSimpleTest {
@@ -20,8 +21,19 @@ class InputViewTest : NsTest() {
         }
     }
 
+    @ParameterizedTest(name = "이동 입력 테스트: {0}")
+    @CsvSource(MOVE_DIGIT_EXCEPTION, MOVE_LETTER_EXCEPTION)
+    fun `이동 입력 테스트`(moving: String) {
+        assertSimpleTest {
+            runException(moving)
+            Assertions.assertThat(output()).contains(PREFIX)
+        }
+    }
+
     override fun runMain() {
-        inputView.retryReadBridgeSize()
+        // 다리 길이 입력 테스트: inputView.retryReadBridgeSize()
+        // 이동 입력 테스트:
+        inputView.retryReadMoving()
     }
 
     companion object {
@@ -29,5 +41,8 @@ class InputViewTest : NsTest() {
 
         private const val DIGIT_EXCEPTION = "a"
         private const val RANGE_EXCEPTION = "21"
+
+        private const val MOVE_DIGIT_EXCEPTION = "3"
+        private const val MOVE_LETTER_EXCEPTION = "F"
     }
 }
