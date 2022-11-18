0c91e5eb46c1497b55c021e5ba4e800e5fadfeea
tmdgh1592 <tmdgh1592@naver.com>
2022-11-18 10:00:27 +0900

    feat: add feature to create bridges

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -1,6 +1,9 @@
 package bridge
 
-import bridge.BridgeNumberGenerator
+import bridge.BridgeRandomNumberGenerator.Companion.RANDOM_LOWER_INCLUSIVE
+import bridge.BridgeRandomNumberGenerator.Companion.RANDOM_UPPER_INCLUSIVE
+import bridge.util.DOWN
+import bridge.util.UP
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -10,7 +13,14 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    fun makeBridge(size: Int): List<String> {
-        return listOf()
+    fun makeBridge(size: Int): List<String> = arrayListOf<String>().apply {
+        while (this.size < size) addRandomDirection()
+    }
+
+    private fun ArrayList<String>.addRandomDirection() {
+        when (bridgeNumberGenerator.generate()) {
+            RANDOM_UPPER_INCLUSIVE -> add(UP)
+            RANDOM_LOWER_INCLUSIVE -> add(DOWN)
+        }
     }
 }
