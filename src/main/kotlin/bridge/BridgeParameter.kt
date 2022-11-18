248a97a473a0914e0177f4e980f419ff27319dca
meltnia <themtale@gmail.com>
2022-11-18 22:54:56 +0900

    <Feat> BridgeMessage 클래스 추가 작성(시도 횟수, 길이 입력받기, 이동할 칸)

--- src/main/kotlin/bridge/BridgeParameter.kt
+++ src/main/kotlin/bridge/BridgeParameter.kt
@@ -7,9 +7,12 @@ enum class BridgeParameter(value: Int) {
 
 enum class BridgeMessage(word : String){
     Start("다리 건너기 게임을 시작합니다."),
+    GiveMeBridgeSize("다리의 길이를 입력해주세요."),
+    WhereAreYouGo("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     End("최종 게임 결과"),
     WinorLose("게임 성공 여부: "),
     Win("승리"),
     Lose("실패"),
+    HowManyTimeDidYouTry("총 시도한 횟수: "),
     AreYouWantRetry("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
 }
