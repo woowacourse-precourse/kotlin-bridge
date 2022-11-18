fb9fdafe9a7be28409f59c7f912f540828ca06e1
tmdgh1592 <tmdgh1592@naver.com>
2022-11-19 01:46:49 +0900

    feat: add player movement function

--- src/main/kotlin/bridge/controller/BridgeGame.kt
+++ src/main/kotlin/bridge/controller/BridgeGame.kt
@@ -1,22 +1,24 @@
 package bridge.controller
 
+import bridge.domain.Path
+import bridge.presentation.InputView
+import bridge.presentation.OutputView
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     *
-     *
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    fun move() {}
+    private val inputView: InputView by lazy { InputView() }
+    private val outputView: OutputView by lazy { OutputView() }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     *
-     *
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    fun retry() {}
+    private var path = Path()
+
+    fun retry() {
+
+    }
+
+    private fun move() {
+        outputView.printDirectionQuestion()
+        path = path.createNewPath(inputView.readDirection())
+    }
 }
