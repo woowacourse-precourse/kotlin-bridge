dccb1af24002baeca9d10f4afe20256a728c939e
cyjadela <adelacho@naver.com>
2022-11-18 15:05:40 +0900

    feat: random으로 다리 만들기

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -2,7 +2,10 @@ package bridge
 
 fun main() {
     println("다리 건너기 게임을 시작합니다.\n")
-    
+
     val inputView = InputView()
     var bridgeLength = inputView.readBridgeSize()
+
+    val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
+    val answerBridge = bridgeMaker.makeBridge(bridgeLength)
 }
--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,18 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        //number가 0 , 1 일 때 U,D로 리스트 생성하기
+        var answerBridge = listOf<String>()
+        for (i in 1..size) {
+            when (bridgeNumberGenerator.generate()) {
+                0 -> {
+                    answerBridge = answerBridge.plus("D")
+                }
+                1 -> {
+                    answerBridge = answerBridge.plus("U")
+                }
+            }
+        }
+        return answerBridge
     }
 }
