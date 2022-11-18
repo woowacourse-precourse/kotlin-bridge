12848cb4c398b778b5c31bc42ab63ebb9de182d7
no1msh <no1msh1217@gmail.com>
2022-11-18 15:42:48 +0900

    chore: 클래스별 요구사항 주석으로 명시

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -2,6 +2,10 @@ package bridge
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
+ * 요구사항 ⬇
+ * 제공된 BridgeMaker 클래스를 활용해 구현해야 한다.
+ * BridgeMaker의 필드를 변경할 수 없다.
+ * BridgeMaker의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
  */
 class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
     /**
--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -2,6 +2,12 @@ package bridge
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
+ * 요구사항 ⬇
+ * 제공된 InputView 클래스를 활용해 구현해야 한다.
+ * InputView의 패키지는 변경할 수 있다.
+ * InputView의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
+ * 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
+ * InputView 클래스에서만 camp.nextstep.edu.missionutils.Console 의 readLine() 메서드를 이용해 사용자의 입력을 받을 수 있다.
  */
 class InputView {
     /**
