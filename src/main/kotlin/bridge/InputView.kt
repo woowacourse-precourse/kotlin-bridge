657e7d885304a631fd9831060e4c611807ee94fe
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-19 00:53:01 +0900

    [Feat] (InputView.kt) 사용자가 이동할 칸 입력 받는 기능 완성
    readMoving: 사용자로 부터 U, D를 입력 받음

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -19,7 +19,8 @@ class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
+        return InputValidator.validateMoveInput()
     }
 
     /**
