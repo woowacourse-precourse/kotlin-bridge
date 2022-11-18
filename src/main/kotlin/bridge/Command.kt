5da01a8fff906bbef623878a418acc9b785be1f0
dltkd1395 <dltkd13956@naver.com>
2022-11-18 22:13:47 +0900

    refactor(Command, Movement) : 중복되는 예외 처리 수정

--- src/main/kotlin/bridge/Command.kt
+++ src/main/kotlin/bridge/Command.kt
@@ -2,26 +2,14 @@ package bridge
 
 class Command(private val execution: String) {
     init {
-        require(isLetter()) { ErrorType.COMMAND_LETTER.errorMessage }
-        require(isUpperCaese()) { ErrorType.COMMAND_UPPER.errorMessage }
-        require(isLength()) { ErrorType.COMMAND_LENGTH.errorMessage }
-        require(isRightCommand()) { ErrorType.COMMAND.errorMessage }
+        CommandException(COMMAND, execution)
+        require(isRightCommand()) { ERROR.format(COMMAND + ErrorType.COMMAND.errorMessage) }
     }
 
-    private fun isLetter(): Boolean =
-        execution.all { char -> char.isLetter() }
-
-    private fun isUpperCaese(): Boolean =
-        execution.all { char -> char.isUpperCase() }
-
-    private fun isLength(): Boolean =
-        execution.length == COMMAND_LENGTH
-
     private fun isRightCommand(): Boolean =
-        execution == RESTART || execution ==QUIT
+        execution == RESTART || execution == QUIT
 
     companion object {
-        const val COMMAND_LENGTH = 1
         const val RESTART = "R"
         const val QUIT = "Q"
     }
--- src/main/kotlin/bridge/Movement.kt
+++ src/main/kotlin/bridge/Movement.kt
@@ -2,26 +2,14 @@ package bridge
 
 class Movement(private val direction: String) {
     init {
-        require(isLetter()) { ErrorType.DIRECTION_LETTER.errorMessage }
-        require(isUpperCaese()) { ErrorType.DIRECTION_UPPER.errorMessage }
-        require(isLength()) { ErrorType.DIRECTION_LENGTH.errorMessage }
-        require(isRightDirection()) { ErrorType.DIRECTION.errorMessage }
+        CommandException(MOVEMENT, direction)
+        require(isRightDirection()) { ERROR.format(MOVEMENT + ErrorType.DRIECTION.errorMessage) }
     }
-
-    private fun isLetter(): Boolean =
-        direction.all { char -> char.isLetter() }
-
-    private fun isUpperCaese(): Boolean =
-        direction.all { char -> char.isUpperCase() }
-
-    private fun isLength(): Boolean =
-        direction.length == DIRECTION_LENGTH
 
     private fun isRightDirection(): Boolean =
         direction == UP || direction == DOWN
 
     companion object {
-        const val DIRECTION_LENGTH = 1
         const val UP = "U"
         const val DOWN = "D"
     }
