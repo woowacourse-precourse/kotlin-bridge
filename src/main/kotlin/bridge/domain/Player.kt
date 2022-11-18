aa7affb36a9946599b5b081780eec5093d08d4aa
phs <gustjd0808@pusan.ac.kr>
2022-11-18 22:10:46 +0900

    feat(domain): add method that reset blcoks and increment tries

--- src/main/kotlin/bridge/domain/Player.kt
+++ src/main/kotlin/bridge/domain/Player.kt
@@ -5,4 +5,9 @@ class Player(
     private var tries: Int = 1
 ) {
     fun addPlayerBlock(playerBlock: PlayerBlock) = blocks.add(playerBlock)
+
+    fun resetBlocks() {
+        blocks.clear()
+        tries++
+    }
 }
