8574bc99e5201708f6979b1da0a343b05bc78f95
dltkd1395 <dltkd13956@naver.com>
2022-11-18 22:14:08 +0900

    fix(BridgeNumber) : 예외 처리 오류 수정

--- src/main/kotlin/bridge/BridgeNumber.kt
+++ src/main/kotlin/bridge/BridgeNumber.kt
@@ -2,19 +2,19 @@ package bridge
 
 class BridgeNumber(private val size: String) {
     init {
-        require(isNumberRange()) { ErrorType.LENGTH.errorMessage }
-        require(isNumber()) { ErrorType.NUMBER.errorMessage }
-        require(isEmtpy()) { ErrorType.EMPTY.errorMessage }
+        require(isEmtpy()) { ERROR.format(ErrorType.EMPTY.errorMessage) }
+        require(isNumber()) { ERROR.format(BRIDGE + ErrorType.NUMBER.errorMessage) }
+        require(isNumberRange()) { ERROR.format(BRIDGE + ErrorType.RANGE.errorMessage) }
     }
 
     private fun isNumberRange(): Boolean =
-        size.toInt() !in MIN_LENGTH..MAX_LENGTH
+        size.toInt() in MIN_LENGTH..MAX_LENGTH
 
     private fun isNumber(): Boolean =
-        size.any { char -> char.isLetter() }
+        size.all { char -> char.isDigit() }
 
     private fun isEmtpy(): Boolean =
-        size.isEmpty() || size.equals("")
+        size.isNotEmpty() || size.length > 0
 
     companion object {
         const val MIN_LENGTH = 3
