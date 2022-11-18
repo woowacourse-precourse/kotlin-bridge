df83ea18893a8c1f15fee2d72a55a251b240dece
zqwz <zqwz@naver.com>
2022-11-19 04:14:26 +0900

    feat(BridgeController): 게임 재시작과 종료를 처리하는 기능 추가

--- src/main/kotlin/bridge/BridgeController.kt
+++ src/main/kotlin/bridge/BridgeController.kt
@@ -23,6 +23,8 @@ class BridgeController {
     private fun gameProgress() {
         while (true) {
             move()
+            if (checkEnd() || checkFail())
+                break
         }
     }
 
@@ -31,5 +33,29 @@ class BridgeController {
         val direction = input.readMoving()
         bridgeGame.move(direction)
         output.printMap(bridgeGame.getUpSide(), bridgeGame.getDownSide())
+    }
+
+    private fun checkEnd(): Boolean {
+        if (bridgeGame.checkEnd()) {
+            output.printEndGame()
+            output.printMap(bridgeGame.getUpSide(), bridgeGame.getDownSide())
+            output.printResult(bridgeGame.getTryCount(), "성공")
+            return true
+        }
+        return false
+    }
+
+    private fun checkFail(): Boolean {
+        if (bridgeGame.checkFail()) {
+            output.printRestart()
+            when (input.readGameCommand()) {
+                "R" -> bridgeGame.retry()
+                "Q" -> {
+                    output.printResult(bridgeGame.getTryCount(), "실패")
+                    return true
+                }
+            }
+        }
+        return false
     }
 }
