325dc96557aeb5f68145ff3c959f171bb181b968
dltkd1395 <dltkd13956@naver.com>
2022-11-18 16:48:42 +0900

    feat(InputView) : 이동할 칸을 입력하는 기능 구현

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -14,9 +14,7 @@ class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    fun readMoving(): String {
-        return ""
-    }
+    fun readMoving(): String = Console.readLine()
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
