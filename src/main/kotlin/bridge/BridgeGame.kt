8766986e6c941cee421b660c665aecbadfd35f82
Yerin <myheaven_b@naver.com>
2022-11-18 16:43:25 +0900

    feat: add functions in BridgeGame

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -1,24 +1,5 @@
 package bridge
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
-class BridgeGame {
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     *
-     *
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    fun move() {}
-
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     *
-     *
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    fun retry() {}
 class BridgeGame(private val bridge: List<String>) {
     var playerBridge = mutableListOf<String>()
     var resultUpBridge = mutableListOf<String>()
@@ -52,5 +33,18 @@ class BridgeGame(private val bridge: List<String>) {
             resultDownBridge.add("X")
         }
     }
+
+    fun retry() {
+        resultUpBridge = mutableListOf()
+        resultDownBridge = mutableListOf()
+        playCount++
+
+    }
+    fun check(): Boolean {
+        val index = playerBridge.size
+        return bridge[index] == playerBridge[index]
+    }
+
+    fun over(): Boolean = bridge.size == playerBridge.size
 
 }
