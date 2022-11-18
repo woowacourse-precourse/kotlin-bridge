86dd20f1b4563e989b916c2ed68a0495c0d46acb
lgy15 <lgy159@naver.com>
2022-11-18 10:49:37 +0900

    refactor: moveInput 함수 별도 생성

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -15,8 +15,7 @@ class BridgeGame(val bridge: List<String>, private val retry: Int) {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun move(location: Int) {
-        val moveLine = InputView().readMoving()
-        val answer = bridge[location] == moveLine
+        val answer = bridge[location] == inputMove()
         OutputView().printMap(bridge, location, answer)
         if (location < bridge.size - 1 && answer) move(location + NEXT_MOVE) // 범위 안에서 재귀
         if (location == bridge.size - 1 && answer) { // 끝까지 건넜을 때
@@ -26,13 +25,15 @@ class BridgeGame(val bridge: List<String>, private val retry: Int) {
         if (!answer) retry(location)
     }
 
+    private fun inputMove() = InputView().readMoving()
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      *
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    private fun retry(location: Int) {
+    fun retry(location: Int) {
         val retryCheck = InputView().readGameCommand()
         if (retryCheck == RETRY) BridgeGame(this.bridge, this.retry + NEXT_TRY).move(START_LOCATION)
         if (retryCheck == QUIT) {
