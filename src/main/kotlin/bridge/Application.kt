e4578e0fb890755c84800e2fb827b1c6640d34e9
junsu1023 <12jangdokg55@gmail.com>
2022-11-19 03:32:51 +0900

    feat: 최종 기능 추가

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,46 @@
 package bridge
 
 fun main() {
-    // TODO: 프로그램 구현
+    playGame()
+}
+
+fun playGame() {
+    val input = InputView()
+    val output = OutputView()
+
+    output.printGameStart()
+    val bridgeGame = BridgeGame(BridgeMaker(BridgeRandomNumberGenerator()), input.readBridgeSize())
+
+    moveBlock(bridgeGame, input, output)
+}
+
+fun moveBlock(bridgeGame: BridgeGame, input: InputView, output: OutputView) {
+    println()
+    while(!bridgeGame.isEnd()) {
+        bridgeGame.move(input.readMoving())
+        output.printMap(bridgeGame)
+    }
+
+    isSuccessGame(bridgeGame, input, output)
+}
+
+fun isSuccessGame(bridgeGame: BridgeGame, input: InputView, output: OutputView) {
+    if(bridgeGame.isSuccess()) {
+        output.printResult(bridgeGame)
+        return
+    }
+
+    isRetry(bridgeGame, input, output)
+}
+
+fun isRetry(bridgeGame: BridgeGame, input: InputView, output: OutputView) {
+    when(input.readGameCommand()) {
+        "R" -> {
+            bridgeGame.retry()
+            moveBlock(bridgeGame, input, output)
+        }
+        "Q" -> {
+            output.printResult(bridgeGame)
+        }
+    }
 }
