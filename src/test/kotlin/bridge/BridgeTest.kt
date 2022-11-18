bdcdcc3b5e18efafc114849e69cce6af512ee387
ChunBaee <mljung10@naver.com>
2022-11-18 19:34:53 +0900

    test: 유저가 입력하는 다리 이동 문자열 관련된 테스트 케이스 추가

--- src/test/kotlin/bridge/BridgeTest.kt
+++ src/test/kotlin/bridge/BridgeTest.kt
@@ -41,6 +41,46 @@ class BridgeTest : NsTest() {
         }
     }
 
+    @Test
+    fun `다리 길이 입력 값 성공`() {
+        assertSimpleTest {
+            Regex("3").checkBridgeLengthRegexes()
+            Assertions.assertThat(output())
+        }
+    }
+
+    /** 사용자가 입력하는 다리 이동 문자열 관련 테스트 **/
+    @Test
+    fun `유저가 이동할 칸을 소문자로 입력했을 경우 예외가 발생한다`() {
+        assertSimpleTest {
+            Regex("u").checkUserMovementRegexes()
+            Assertions.assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    @Test
+    fun `유저가 이동할 칸을 다른 문자로 입력했을 경우 예외가 발생한다`() {
+        assertSimpleTest {
+            Regex("R").checkUserMovementRegexes()
+            Assertions.assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    @Test
+    fun `유저가 이동할 칸을 공백으로 입력했을 경우 예외가 발생한다`() {
+        assertSimpleTest {
+            Regex("").checkUserMovementRegexes()
+            Assertions.assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    @Test
+    fun `유저가 이동할 칸을 빈칸으로 입력했을 경우 예외가 발생한다`() {
+        assertSimpleTest {
+            Regex(" ").checkUserMovementRegexes()
+            Assertions.assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
 
     override fun runMain() {
         main()
