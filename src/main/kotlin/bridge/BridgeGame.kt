5d74ae667a2dd1af10af6424e3479e25a24bd1a7
SeungHyeon Shim <tladhehd@naver.com>
2022-11-18 20:38:09 +0900

    feat: mapBuild 메서드를 통해 게임 결과 저장

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -29,6 +29,47 @@ class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry() {
+        // map을 초기화해준다.
 
     }
+
+    fun askRetry() {
+        // map의 첫번째, 두번째 리스트 마지막 요소가 X인지 확인하고
+
+    }
+
+    val answer = arrayListOf<String>("", "")
+    fun buildMap(choose: String, oOrX: String) : List<String> {
+        if (choose=="U") {
+            answer[0]+=oOrX
+            answer[1]+=" "
+        }else if (choose=="D"){
+            answer[0]+=" "
+            answer[1]+=oOrX
+        }
+        return answer
+    }
+    var mapBuild1 = ""
+    fun mapBuilder1(temp: String, cnt:Int) : String {
+        for (i in 0 until 4){
+            if (i==2) mapBuild1+=temp[cnt]
+            else if (i%2==1) mapBuild1+=" "
+            else if (i==0 && cnt!=0) mapBuild1+="|"
+
+        }
+        println(mapBuild1)
+        return mapBuild1
+    }
+
+    var mapBuild2 = ""
+    fun mapBuilder2(temp: String, cnt:Int) : String {
+        for (i in 0 until 4){
+            if (i%2==1) mapBuild2+=" "
+            else if (i==0 && cnt !=0) mapBuild2+="|"
+            else if (i==2) mapBuild2+=temp[cnt]
+        }
+        println(mapBuild2)
+        return mapBuild2
+    }
+
 }
--- src/main/kotlin/bridge/playBridgeGameTest.kt
+++ src/main/kotlin/bridge/playBridgeGameTest.kt
@@ -4,19 +4,36 @@ class playBridgeGameTest {
     val OV = OutputView()
     val IV = InputView()
     val BG = BridgeGame()
+    var temp = listOf<String>()
+    var cnt = 0
+    val win = "성공"
+    val lose = "실패"
 
-    fun bridgeGameStart() : List<String> {
+    constructor(){
         OV.printStartGame()
         OV.inputBridgeLength()
+    }
+
+    fun bridgeGameStart() : List<String> {
         val size = IV.readBridgeSize()
         val bridge = BridgeMaker(bridgeNumberGenerator = BridgeRandomNumberGenerator()).makeBridge(size)
         println(bridge) // 나중에 지우면 됨
         return bridge
     }
 
-    fun duringGame(bridge: List<String>) {
+    fun duringGame(bridge: List<String>, index: Int) {
         OV.chooseMove()
         var choose = IV.readMoving()
-        println(BG.move(choose, bridge, index=0))
+        temp = BG.buildMap(choose ,BG.move(choose, bridge, index))
+//        println(temp)
+//        println(temp[0])
+//        println(temp[1])
+        BG.mapBuilder1(temp[0], cnt)
+        BG.mapBuilder2(temp[1], cnt)
+        cnt+=1
     }
+
+
+
+
 }
