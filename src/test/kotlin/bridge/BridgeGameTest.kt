8b864e36fda787d1c9162f6a272217b4ccd81891
kyounggudev <homin222@naver.com>
2022-11-19 02:41:42 +0900

    test : 테스트케이스 리팩토링

--- src/test/kotlin/bridge/BridgeGameTest.kt
+++ src/test/kotlin/bridge/BridgeGameTest.kt
@@ -4,43 +4,35 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.io.ByteArrayInputStream
-import java.lang.String
 
 class BridgeGameTest : NsTest() {
 
     @Test
     fun `다리 건너기 성공 테스트`() {
         initGame()
-        val bridge = listOf("U", "U", "D")
+        BridgeMaker.bridge.add("U")
+        BridgeMaker.bridge.add("U")
+        BridgeMaker.bridge.add("D")
         tryCount = 0
-        System.setIn("U".byteInputStream())
         assertSimpleTest {
-            BridgeGame().move(bridge)
-            assertThat(output()).contains(
-                "[ O ]",
-                "[   ]"
-            )
+            assertThat(BridgeGame().move("U"))
         }
     }
 
     @Test
     fun `다리 건너기 실패 테스트`() {
         initGame()
-        val bridge = listOf("U", "U", "D")
+        BridgeMaker.bridge.add("U")
+        BridgeMaker.bridge.add("U")
+        BridgeMaker.bridge.add("D")
         tryCount = 0
-        val buf = String.join("\n", "D\nR").toByteArray()
-        System.setIn(ByteArrayInputStream(buf))
         assertSimpleTest {
-            BridgeGame().move(bridge)
-            assertThat(output()).contains(
-                "[   ]",
-                "[ X ]"
-            )
+            assertThat(!BridgeGame().move("D"))
         }
     }
 
     fun initGame(){
+        BridgeMaker.bridge.clear()
         BridgeGame.upBridge.clear()
         BridgeGame.downBridge.clear()
         order=0
