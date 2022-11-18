dd16c58c2b9cf98b6da64010db0447223f7d228e
Yerin <myheaven_b@naver.com>
2022-11-18 16:40:39 +0900

    style: delete remarks

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,13 +1,6 @@
 package bridge
 
 import camp.nextstep.edu.missionutils.Console.readLine
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- *
- * InputView의 패키지는 변경할 수 있다.
- * InputView의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
- * 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
- */
 
 class InputView {
     private val exceptions = Exceptions()
@@ -23,9 +16,6 @@ class InputView {
         return moving
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     fun readGameCommand(): String {
         val command = readLine()
         exceptions.validateCommand(command)
