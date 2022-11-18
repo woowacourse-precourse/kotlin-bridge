c47adc0b6a552578a2d321f5975dc73a629a01a9
junsu1023 <12jangdokg55@gmail.com>
2022-11-19 03:32:21 +0900

    feat: 현재 다리 상태 출력 함수, 최종 결과 출력 함수 추가

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -1,6 +1,6 @@
 package bridge
 
-class OutputView {
+class OutputView() {
     fun printGameStart() {
         println("다리 건너기 게임을 시작합니다.\n")
     }
@@ -15,7 +15,34 @@ class OutputView {
 
     fun printRestart() = println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
 
-    fun printMap() {}
+    fun printMap(bridgeGame: BridgeGame) {
+        val bridgeState = bridgeGame.makeBridge
+        val userState = bridgeGame.moveBlock
 
-    fun printResult() {}
+        printBridgeState(bridgeState, userState, "U")
+        printBridgeState(bridgeState, userState, "D")
+        println()
+    }
+
+    private fun printBridgeState(bridgeState: List<String>, userState: List<String>, flag: String) {
+        print("[ ")
+        userState.forEachIndexed { index, state ->
+            if(index != 0) print(" | ")
+            if(state != flag) print(" ")
+            if(state == flag && state == bridgeState[index]) print("O")
+            if(state == flag && state != bridgeState[index]) print("X")
+        }
+        println(" ]")
+    }
+
+    fun printResult(bridgeGame: BridgeGame) {
+        println("최종 게임 결과")
+        printMap(bridgeGame)
+        print("게임 성공 여부: ")
+        when(bridgeGame.isSuccess()) {
+            true -> println("성공")
+            false -> println("실패")
+        }
+        println("총 시도한 횟수: ${bridgeGame.tryCnt}")
+    }
 }
