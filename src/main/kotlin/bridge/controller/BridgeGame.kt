93b188425b06b154c640fbb89e48a6a3518b72bd
tmdgh1592 <tmdgh1592@naver.com>
2022-11-19 01:51:40 +0900

    feat: add game start function

--- src/main/kotlin/bridge/controller/BridgeGame.kt
+++ src/main/kotlin/bridge/controller/BridgeGame.kt
@@ -1,20 +1,73 @@
 package bridge.controller
 
-import bridge.domain.Path
+import bridge.BridgeMaker
+import bridge.BridgeRandomNumberGenerator
+import bridge.domain.*
 import bridge.presentation.InputView
 import bridge.presentation.OutputView
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
+class BridgeGame : Game {
     private val inputView: InputView by lazy { InputView() }
     private val outputView: OutputView by lazy { OutputView() }
 
+    private val bridgeMaker: BridgeMaker by lazy { BridgeMaker(BridgeRandomNumberGenerator()) }
+    private lateinit var bridge: Bridge
+    private val counter: Counter by lazy { Counter() }
     private var path = Path()
 
-    fun retry() {
+    override fun start() {
+        outputView.printGameStart()
+        val bridgeSize = askBridgeSize()
+        bridge = Bridge(bridgeMaker.makeBridge(bridgeSize))
 
+        do {
+            val (gameResult, finalBridgeRoute) = startGame()
+            if (gameResult == Result.SUCCESS) { // 성공시 결과 출력
+                val finalRoute = finalBridgeRoute.route()
+                result(gameResult, finalRoute)
+                break
+            } else { // 실패시
+
+            }
+        } while (true)
+    }
+
+    private fun startGame(): Pair<Result, BridgeRoute> {
+        val bridgeSize = bridge.size()
+        val bridgeRoute = BridgeRoute()
+
+        counter.plus()
+        for (round in 0 until bridgeSize) {
+            move()
+
+            val route = bridgeRoute.makeRoute(path, bridge)
+            drawRoute(route)
+
+            val isFail = path.checkDirection(bridge.getDirection(round), round).not()
+            if (isFail) {
+                return Pair(Result.FAIL, bridgeRoute)
+            }
+        }
+        return Pair(Result.SUCCESS, bridgeRoute)
+    }
+
+    private fun askBridgeSize(): Int {
+        outputView.printBridgeLengthQuestion()
+        val bridgeSize = inputView.readBridgeSize()
+        outputView.printNewLine()
+
+        return bridgeSize
+    }
+
+    private fun drawRoute(route: List<List<Mark>>) {
+        outputView.printMap(route)
+    }
+
+    private fun result(gameResult: Result, map: List<List<Mark>>) {
+        outputView.printResult(gameResult, counter.count(), map)
     }
 
     private fun move() {
---
+++ src/main/kotlin/bridge/controller/Game.kt
@@ -0,0 +1,5 @@
+package bridge.controller
+
+interface Game {
+    fun start()
+}
