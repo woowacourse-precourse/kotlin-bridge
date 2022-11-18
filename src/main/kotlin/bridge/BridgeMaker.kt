61809ec6b5cf42db63fe3a7ef3daa383163e20b2
ponopono0322 <ponopono0322@gmail.com>
2022-11-18 15:24:46 +0900

    feat: BridgeMaker 클래스 함수 구현
    - makeBridge: BridgeRandomNumberGenerator 클래스 메소드를 이용해 위 칸이면 "U", 아래 칸이면 "D"인 리스트 반환

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,13 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        val bridge = mutableListOf<String>()
+        for (i in 0 until  size) {
+            when(bridgeNumberGenerator.generate()) {
+                BridgeRandomNumberGenerator.RANDOM_UPPER_INCLUSIVE -> bridge.add("U")
+                BridgeRandomNumberGenerator.RANDOM_LOWER_INCLUSIVE -> bridge.add("D")
+            }
+        }
+        return bridge
     }
 }
