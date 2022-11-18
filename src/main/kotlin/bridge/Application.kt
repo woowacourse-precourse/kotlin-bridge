2cef4c662ffad51021721cd1ce766e731adefeab
cyjadela <adelacho@naver.com>
2022-11-18 16:24:46 +0900

    style: 불필요한 공백 제거

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -3,13 +3,10 @@ package bridge
 fun main() {
     println("다리 건너기 게임을 시작합니다.\n")
 
-    val inputView = InputView()
-    var bridgeLength = inputView.readBridgeSize()
-
+    val bridgeLength = InputView().readBridgeSize()
     val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
     val answerBridge = bridgeMaker.makeBridge(bridgeLength)
 
     var trial = 1
     BridgeGame().move(answerBridge, trial)
-
 }
