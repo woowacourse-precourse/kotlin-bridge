e6f16f76f7bf43a90e41b0cc9a202f798dc558ee
junsu1023 <12jangdokg55@gmail.com>
2022-11-19 02:21:55 +0900

    feat: 각 입력 함수 및 예외처리 추가

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,27 +1,71 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 class InputView {
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+    private val output = OutputView()
+
     fun readBridgeSize(): Int {
-        return 0
-    }
+        output.printBridgeLength()
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
+        return try {
+            getBridgeSize()
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            readBridgeSize()
+        }
+    }
+    
     fun readMoving(): String {
-        return ""
+        output.printSelectBlock()
+
+        return try {
+            getMoving()
+        }
+        catch (e: IllegalArgumentException) {
+            println(e.message)
+            readMoving()
+        }
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     fun readGameCommand(): String {
-        return ""
+        output.printRestart()
+
+        return try {
+            getGameCommand()
+        }
+        catch (e: IllegalArgumentException) {
+            println(e.message)
+            readGameCommand()
+        }
+    }
+
+    private fun getBridgeSize(): Int {
+        val bridgeSize: Int
+        try {
+            bridgeSize = Console.readLine().toInt()
+            if (bridgeSize !in 3..20) throw IllegalArgumentException("[ERROR] 다리 길이는 3과 20 사이여야 합니다.")
+        }
+        catch (e: NumberFormatException) {
+            throw IllegalArgumentException("[ERROR] 입력은 숫자로만 이루어져야 합니다.")
+        }
+
+        return bridgeSize
+    }
+
+    private fun getMoving(): String {
+        val selectMove = Console.readLine()
+        if(selectMove != "U" && selectMove != "D") throw IllegalArgumentException("이동할 칸은 U 또는 D를 입력 가능합니다..")
+
+        return selectMove
+    }
+
+    private fun getGameCommand(): String {
+        val isRestart = Console.readLine()
+        if(isRestart == "R" || isRestart == "Q") return isRestart
+        throw IllegalArgumentException("재시작 여부는 R 또는 Q만 입력 가능합니다.")
     }
 }
