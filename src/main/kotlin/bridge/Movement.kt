18fea099b53be18ddd517af2d180a069c46cf25f
dltkd1395 <dltkd13956@naver.com>
2022-11-18 16:41:05 +0900

    feat(Movement) : 이동 방향이 U, D가 아닌 경우 알 수 있는 기능 구현

--- src/main/kotlin/bridge/Movement.kt
+++ src/main/kotlin/bridge/Movement.kt
@@ -5,6 +5,7 @@ class Movement(private val direction: String) {
         require(isLetter()) { ErrorType.DIRECTION_LETTER.errorMessage }
         require(isUpperCaes()) { ErrorType.DIRECTION_UPPER.errorMessage }
         require(isLength()) { ErrorType.DIRECTION_LENGTH.errorMessage }
+        require(isRightDirection()) { ErrorType.DIRECTION.errorMessage }
     }
 
     private fun isLetter(): Boolean =
@@ -16,7 +17,12 @@ class Movement(private val direction: String) {
     private fun isLength(): Boolean =
         direction.length == DIRECTION_LENGTH
 
+    private fun isRightDirection(): Boolean =
+        direction == UP || direction == DOWN
+
     companion object {
         const val DIRECTION_LENGTH = 1
+        const val UP = "U"
+        const val DOWN = "D"
     }
 }
