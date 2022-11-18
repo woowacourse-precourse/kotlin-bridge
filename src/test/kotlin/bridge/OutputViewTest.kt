16d778335a097b4f3d2157f429015c7e30ba0b59
agfalcon <agfalcon12@gmail.com>
2022-11-18 18:48:26 +0900

    startGameTest 구현 완료

--- src/test/kotlin/bridge/OutputViewTest.kt
+++ src/test/kotlin/bridge/OutputViewTest.kt
@@ -1,25 +1,23 @@
 package bridge
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
-class OutputView {
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.provider.ValueSource
 
-    fun printData(data: String){
+class OutputViewTest {
 
+    private val ov = OutputView()
+    @Test
+    fun startGameTest(){
+        assertThat(ov.startGame()).isEqualTo("다리 건너기 게임을 시작합니다.")
     }
-
-    fun startGame(): String{
-        return("다리 건너기 게임을 시작합니다.")
-    }
-
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap(uList:List<Int>, dList:List<Int>):String{
-        return ""
+    fun printMapTest(){
+       
     }
 
     /**
@@ -27,7 +25,5 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() :String{
-        return ""
-    }
+    fun printResult() :String{}
 }
