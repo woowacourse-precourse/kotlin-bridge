d1780ba7d40dd45b8430f45141d40a2ac6cef5a3
phs <gustjd0808@pusan.ac.kr>
2022-11-18 22:07:55 +0900

    feat(domain): add class for player and method to add player block

---
+++ src/main/kotlin/bridge/domain/Player.kt
@@ -0,0 +1,8 @@
+package bridge.domain
+
+class Player(
+    private val blocks: MutableList<PlayerBlock> = mutableListOf(),
+    private var tries: Int = 1
+) {
+    fun addPlayerBlock(playerBlock: PlayerBlock) = blocks.add(playerBlock)
+}
