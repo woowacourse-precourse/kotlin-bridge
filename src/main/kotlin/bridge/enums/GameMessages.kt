277e56de51c99b0461a285bc72f730ba899dad8f
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-19 21:27:14 +0900

    feat(GameMessages): 게임 메시지 종류 중 성공 실패 추가

--- src/main/kotlin/bridge/enums/GameMessages.kt
+++ src/main/kotlin/bridge/enums/GameMessages.kt
@@ -6,7 +6,8 @@ enum class GameMessages(private val messages: String) {
     INPUT_UP_DOWN("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     INPUT_RESTART_QUIT("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     RESULT("최종 게임 결과"),
-    IS_CLEAR("게임 성공 여부:"),
+    CLEAR("게임 성공 여부: 성공"),
+    FAIL("게임 성공 여부: 실패"),
     TRY_COUNT("총 시도한 횟수:");
 
     override fun toString(): String = messages
