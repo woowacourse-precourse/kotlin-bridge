8dbaf9afbb75c9d999c07befeddf5081e3b33285
YuGyeong98 <kuk3097@naver.com>
2022-11-20 02:29:50 +0900

    feat: 칸을 이동할 때마다 게임 결과 저장

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -10,7 +10,21 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    private var index = 0
+    private val upList = mutableListOf<String>()
+    private val downList = mutableListOf<String>()
+    private val up = mutableMapOf<String, List<String>>()
+    private val down = mutableMapOf<String, List<String>>()
+
+    fun move(space: String, bridge: List<String>): Map<String, List<String>> {
+        if (space == bridge[index]) {
+            addToList(space, "O")
+            index += 1
+        } else {
+            addToList(space, "X")
+        }
+        return up + down
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -19,4 +33,16 @@ class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry() {}
+
+    private fun addToList(space: String, ox: String) {
+        if (space == "U") {
+            upList.add(ox)
+            downList.add(" ")
+            up[space] = upList
+        } else if (space == "D") {
+            upList.add(" ")
+            downList.add(ox)
+            down[space] = downList
+        }
+    }
 }
