12573acc1a3d365e63e08aefb57d2fecf2f098eb
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-19 17:29:28 +0900

    feat(InputView) : 문구 출력 및 입력을 받는 기능 구현

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,20 +10,29 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        println("다리의 길이를 입력해주세요.\n")
+        val bridgeSize = Console.readLine().toInt()
+
+        return bridgeSize
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        println("이동할 칸을 선택해주세요. (위: U, 아래: D)\n")
+        val direction = Console.readLine()
+
+        return direction
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n")
+        val retryFlag = Console.readLine()
+
+        return retryFlag
     }
 }
