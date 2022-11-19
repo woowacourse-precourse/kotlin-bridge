c08eaf7861d7ef76e206c294be525a26c644a61e
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-19 13:37:18 +0900

    [Test] (OutViewTest.kt) 첫 라운드 U로 건널 수 있을 때 올바른 출력 테스트 추가
    OutpuValidator로 테스트 코드 분리
    다리, 라운드, 이동 방향 매개변수 필요

---
+++ src/test/kotlin/bridge/OutViewTest.kt
@@ -0,0 +1,12 @@
+package bridge
+
+import bridge.validator.OutputValidator
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class OutViewTest {
+    @Test
+    fun `첫 라운드 U를 입력하여 건널 수 있을 때 올바른 출력 테스트`() {
+        assertThat("[ O ]\n[   ]").isEqualTo(OutputValidator.validatePrintMap(listOf("U","D","D"), 0, "U"))
+    }
+}
