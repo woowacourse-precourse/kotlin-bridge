ab98b66a57297fda35f244c4ebe40b86b195dd49
SeungHyeon Shim <tladhehd@naver.com>
2022-11-19 19:17:06 +0900

    refactor: 메인문 10줄 이내로 줄여서 실행

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -2,7 +2,11 @@ package bridge
 
 fun main() {
     val user = playBridgeGameTest()
-    val bridge = user.bridgeGameStart()
-    user.duringGame(bridge)
-
+    val bridge = user.bridgeGameStart() // 다리는 최초 한번 생성
+    var index=0
+    while (true){
+        if (bridge.size==index) break
+        index=user.duringGame(bridge, index)
+        user.duringGame2(index)
+    }
 }
