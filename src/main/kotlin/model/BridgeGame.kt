30d8447c64aafa4b7bcdb4a06434ba30f7ae0f7c
idra79haza <idra79haza@github.com>
2022-11-19 22:18:04 +0900

    refactor(move & answerMove): refactor move & add answerMove
    refactor move & add answerMove

--- src/main/kotlin/model/BridgeGame.kt
+++ src/main/kotlin/model/BridgeGame.kt
@@ -1,5 +1,8 @@
 package model
 
+import bridge.BridgeMaker
+import bridge.BridgeRandomNumberGenerator
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -10,24 +13,15 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move(inputMove : String, answerMoveSingle: String) : HashMap<String, String> {
-        val hashMap = HashMap<String, String>()
-        if (inputMove == "U") {
-            if (inputMove == answerMoveSingle) {
-                hashMap["U"] = "O"
-                return hashMap
-            }
-            hashMap["U"] = "X"
-            return hashMap
-        } else if (inputMove == "D") {
-            if (inputMove == answerMoveSingle) {
-                hashMap["D"] = "O"
-                return hashMap
-            }
-            hashMap["D"] = "X"
-            return hashMap
+    fun move(moveInput: String, answerMoveSingle: String): HashMap<String, String> {
+        val resultHashMap = HashMap<String, String>()
+        when {
+            (moveInput == answerMoveSingle) && (moveInput == "U") -> resultHashMap["U"] = "O"
+            (moveInput == answerMoveSingle) && (moveInput == "D") -> resultHashMap["D"] = "O"
+            (moveInput != answerMoveSingle) && (moveInput == "U") -> resultHashMap["U"] = "X"
+            (moveInput != answerMoveSingle) && (moveInput == "D") -> resultHashMap["D"] = "X"
         }
-        return hashMap
+        return resultHashMap
     }
 
     /**
@@ -36,14 +30,19 @@ class BridgeGame {
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry() : Boolean {
-        // 게임을 다시 시도할지 여부를 입력해주세요.
+    fun retry(): Boolean {
         view.OutputView().printAskGameResult()
         val inputGameRestartCommand = view.InputView().readGameCommand()
         if (inputGameRestartCommand == "R") {
             return true
         }
-        // 예외 처리 해야함.
         return false
+    }
+
+    fun getAnswerMove(bridgeSize: Int): List<String> {
+        val bridge = BridgeMaker(BridgeRandomNumberGenerator()) // 상속과 반환!? : 뒤에?!
+        val answerMove = bridge.makeBridge(bridgeSize) // [U, D, U] 이런 식으로 현재 리스트가 만들어짐
+        println(answerMove)
+        return answerMove
     }
 }
