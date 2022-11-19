ff4b7bc7bebd222b7054e25e0e13ed2b1dc21d67
tmdgh1592 <tmdgh1592@naver.com>
2022-11-19 15:51:20 +0900

    refactor: Simplify counting function and eliminate unnecessary function

--- src/main/kotlin/bridge/domain/counter/TryCounter.kt
+++ src/main/kotlin/bridge/domain/counter/TryCounter.kt
@@ -4,17 +4,9 @@ package bridge.domain.counter
  * @param count 다리 건너기 시도 횟수
  * */
 class TryCounter(private var count: Int = 0) {
-    fun plus(): Int {
-        return ++count
-    }
+    fun plus(): Int = ++count
 
-    fun minus(): Int {
-        return --count
-    }
+    fun minus(): Int = --count
 
     fun count(): Int = count
-
-    fun init() {
-        count = 0
-    }
 }
