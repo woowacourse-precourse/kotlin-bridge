c4b65008fbf75e0cd2852b29e455709bd3f07319
EmilyCh0 <seoyeon3089@naver.com>
2022-11-19 20:08:58 +0900

    fix(Output): 다리 상태 출력 joinToString을 사용하도록 수정

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -14,31 +14,9 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap(playerResult: ArrayList<Pair<String, Boolean>>) {
-        val firstRow = StringBuilder()
-        val secondRow = StringBuilder()
-        firstRow.append("[")
-        secondRow.append("[")
-        for (stage in 0 until playerResult.size - 1) {
-            if (playerResult[stage].first == "U") {
-                firstRow.append(" O |")
-                secondRow.append("   |")
-                continue
-            }
-            firstRow.append("   |")
-            secondRow.append(" O |")
-        }
-        if (playerResult[playerResult.size - 1].first == "U") {
-            if (playerResult[playerResult.size - 1].second) firstRow.append(" O ]")
-            else firstRow.append(" X ]")
-            secondRow.append("   ]")
-        } else {
-            if (playerResult[playerResult.size - 1].second) secondRow.append(" O ]")
-            else secondRow.append(" X ]")
-            firstRow.append("   ]")
-        }
-        firstRow.append('\n').append(secondRow)
-        println(firstRow.toString())
+    fun printMap(firstRowResult: ArrayList<String>, secondRowResult: ArrayList<String>) {
+        println(firstRowResult.joinToString(prefix = "[ ", separator = " | ", postfix = " ]"))
+        println(secondRowResult.joinToString(prefix = "[ ", separator = " | ", postfix = " ]"))
     }
 
     /**
