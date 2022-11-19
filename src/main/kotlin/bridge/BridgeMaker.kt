452d287760868a15f0ab5feb1a2c0c3307b16e8c
vrexpert <vrexpert2302@gmail.com>
2022-11-19 23:47:29 +0900

    feat(BridgeMaker): 다리의 길이를 입력 받아서 다리를 생성
    BridgeMaker.makeBridge() : 다리의 길이(size)를 입력 받아 U or D 의 값을 랜덤하게 가지는 다리 리스트 생성

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,14 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        var randomBridge = mutableListOf<String>()
+        var upOrDown = ""
+        for(index in 0 until size){
+            val number = bridgeNumberGenerator.generate()
+            if(number == 0) upOrDown = "D"
+            else if (number == 1) upOrDown = "U"
+            randomBridge.add(upOrDown)
+        }
+        return randomBridge
     }
 }
