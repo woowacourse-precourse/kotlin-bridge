1537752272d312e3bf19dd03105ab3021b9f5e79
sujin-kk <sjoo0002@gmail.com>
2022-11-20 01:56:32 +0900

    test(ui/validator): 다리 크기 입력에 대한 예외 테스트한다.

--- src/test/kotlin/bridge/ui/validator/ValidatorTest.kt
+++ src/test/kotlin/bridge/ui/validator/ValidatorTest.kt
@@ -1,7 +1,15 @@
 package bridge.ui.validator
 
+import camp.nextstep.edu.missionutils.test.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
 
 class ValidatorTest {
-
+    @Test
+    fun `다리 크기가 3 이상 20 이하의 숫자가 아니면 예외 발생`() {
+        Assertions.assertSimpleTest {
+            assertThrows<IllegalArgumentException> { InputValidator.validateBridgeSize(bridgeSize = "21") }
+        }
+    }
 }
