772a9f19122ab9299a82bc27226f58a937c19826
dltkd1395 <dltkd13956@naver.com>
2022-11-20 03:52:16 +0900

    feat(Player) : 게임 플레이 관련 데이터 설정 기능 구현

---
+++ src/main/kotlin/bridge/Player.kt
@@ -0,0 +1,23 @@
+package bridge
+
+data class Player(
+    var isGameOver: Boolean = true,
+    var isGameSuccess: Boolean = true,
+    var retryCount: Int = 1
+) {
+    fun setGameFailure() {
+        isGameSuccess = false
+    }
+
+    fun setGameRetry() {
+        isGameOver = true
+    }
+
+    fun setRetryCount() {
+        retryCount += 1
+    }
+
+    fun setGameQuit() {
+        isGameOver = false
+    }
+}
