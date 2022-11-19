49360e42ed91f179076cbdc4bec45e69cd8ffe6d
kjyook <106497893+kjyook@users.noreply.github.com>
2022-11-19 16:59:03 +0900

    사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는 함수

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -41,6 +41,11 @@ class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        val temp = Console.readLine() ?: "A"
+
+        if(temp != "R"&&temp !="Q")
+            throw  IllegalArgumentException("[ERROR] 올바른 입력을 하세요")
+
+        return temp
     }
 }
