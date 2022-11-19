dbfc70baaa15cd6b4543876382124e74e938cd23
YuGyeong98 <kuk3097@naver.com>
2022-11-20 02:13:23 +0900

    feat: 다리 길이에 따라 다리 모양 생성

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -2,12 +2,18 @@ package bridge
 
 fun main() {
     val inputView = InputView()
+    val numberGenerator = BridgeRandomNumberGenerator()
+    val bridgeMaker = BridgeMaker(numberGenerator)
 
     println("다리 건너기 게임을 시작합니다.\n")
+
     println("다리의 길이를 입력해주세요.")
-    inputView.readBridgeSize()
+    val size = inputView.readBridgeSize()
+    val bridge = bridgeMaker.makeBridge(size)
+
     println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
     inputView.readMoving()
+
     println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
     inputView.readGameCommand()
 }
--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,14 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        val shapeOfBridge = mutableListOf<String>()
+        for (i in 0 until size) {
+            if (bridgeNumberGenerator.generate() == 1) {
+                shapeOfBridge.add("U")
+            } else {
+                shapeOfBridge.add("D")
+            }
+        }
+        return shapeOfBridge
     }
 }
