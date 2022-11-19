ca0a7ca7e0a333b948a285f92ade04287f5ed11d
otter <a33397397@gmail.com>
2022-11-20 01:13:51 +0900

    docs: 출력 기능 목록 추가

--- src/main/kotlin/ui/OutputView.kt
+++ src/main/kotlin/ui/OutputView.kt
@@ -19,7 +19,8 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printResult(bridgeGame: BridgeGame) {
-        // todo 최종 게임 결과 [...] [...]
+        // todo print 최종 게임 결과  print [...] [...]
+
         if(bridgeGame.isSuccess()) {
             println("게임 성공 여부: 성공")
         } else println("게임 성공 여부: 실패")
