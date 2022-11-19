fdb9172efbe2e455ec2763ff3b201e5b365a8ce1
zqwz <zqwz@naver.com>
2022-11-19 18:28:06 +0900

    refactor(Exception): 함수명 변경

--- src/main/kotlin/util/Exception.kt
+++ src/main/kotlin/util/Exception.kt
@@ -1,19 +1,19 @@
-package bridge
+package util
 
 class Exception {
     companion object {
         fun validateNumber(readLine: String) = require(readLine.matches(Regex("\\d+"))) {
             println("[ERROR] 숫자만 입력 가능합니다.")
         }
-        fun validateSizeRange(readLine: Int) = require(readLine in 3..20) {
+        fun validateBridgeSizeRange(readLine: Int) = require(readLine in 3..20) {
             println("[ERROR] 3-20 사이의 숫자만 입력 가능합니다.")
         }
 
-        fun validateDirectionRange(readLine: String) = require(readLine in listOf("U", "D")) {
+        fun validateMovingCommand(readLine: String) = require(readLine in listOf("U", "D")) {
             println("[ERROR] 위(U)나 아래(D)만 입력 가능합니다.")
         }
 
-        fun validateRestartRange(readLine: String) = require(readLine in listOf("R", "Q")) {
+        fun validateGameCommand(readLine: String) = require(readLine in listOf("R", "Q")) {
             println("[ERROR] 재시작(R)이나 종료(Q)만 입력 가능합니다.")
         }
     }
