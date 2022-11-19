12850a19c26299317654fed3e85d87ac73b15c63
YuGyeong98 <kuk3097@naver.com>
2022-11-20 02:05:01 +0900

    feat: 사용자 입력 받기

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,13 @@
 package bridge
 
 fun main() {
-    // TODO: 프로그램 구현
+    val inputView = InputView()
+
+    println("다리 건너기 게임을 시작합니다.\n")
+    println("다리의 길이를 입력해주세요.")
+    inputView.readBridgeSize()
+    println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
+    inputView.readMoving()
+    println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+    inputView.readGameCommand()
 }
--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,27 +1,23 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
-class InputView {
+class InputView { // TODO 예외 처리
     /**
      * 다리의 길이를 입력받는다.
      */
-    fun readBridgeSize(): Int {
-        return 0
-    }
+    fun readBridgeSize(): Int = Console.readLine().toInt()
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    fun readMoving(): String {
-        return ""
-    }
+    fun readMoving(): String = Console.readLine()
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    fun readGameCommand(): String {
-        return ""
-    }
+    fun readGameCommand(): String = Console.readLine()
 }
