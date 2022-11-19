09f9bf964cf780efa3ea8e4200cb2fc52addc553
Gloria <ippnsj@gmail.com>
2022-11-19 21:37:53 +0900

    feat: 다리 길이가 3 부터 20 사이의 숫자인지 확인하는 기능 구현

--- src/main/kotlin/bridge/utils/BridgeValidator.kt
+++ src/main/kotlin/bridge/utils/BridgeValidator.kt
@@ -1,5 +1,13 @@
 package bridge.utils
 
 class BridgeValidator : Validator {
-    override fun validate(input: String) {}
+    override fun validate(input: String) {
+        require(input.toIntOrNull() in MINIMUM_VALUE..MAXIMUM_VALUE) { ERROR_MESSAGE }
+    }
+
+    companion object {
+        const val MINIMUM_VALUE = 3
+        const val MAXIMUM_VALUE = 20
+        const val ERROR_MESSAGE = "다리 길이는 3 부터 20 사이의 숫자여야 합니다."
+    }
 }
