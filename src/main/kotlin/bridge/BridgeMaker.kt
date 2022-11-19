1b25236d5869215250b98f245a5bb542a1791602
DormunDoorlock <skrudgh2001@naver.com>
2022-11-19 22:34:06 +0900

    feat: 4번 다리 길이에 맞는 Bridge 생성 기능 구현

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -1,5 +1,6 @@
 package bridge
 
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -9,6 +10,21 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        val bridge = mutableListOf<String>()
+        for (i in 1..size){
+            val num = bridgeNumberGenerator.generate()
+            if (num==Location.Down.locationInt)
+                bridge.add(Location.Down.locationString)
+            if (num==Location.Up.locationInt)
+                bridge.add(Location.Up.locationString)
+        }
+        return bridge
+    }
+    enum class Location(
+            val locationInt: Int,
+            val locationString: String
+    ) {
+        Up(1,"U"),
+        Down(0,"D")
     }
 }
