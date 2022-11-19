334be3775a36746615d7156401a34f07c4056add
kjyook <106497893+kjyook@users.noreply.github.com>
2022-11-19 17:15:30 +0900

    사용자가 칸을 이동하면 이동한 후의 List<String>을 반환해주는 move함수

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,4 +1,5 @@
 package bridge
 
 fun main() {
+
 }
--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -10,7 +10,17 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun move(mybridge: List<String>,updown : String): List<String> {
+        val temp = mutableListOf<String>()
+
+        for(i in mybridge){
+            temp.add(i)
+        }
+
+        temp.add(updown)
+
+        return temp
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
