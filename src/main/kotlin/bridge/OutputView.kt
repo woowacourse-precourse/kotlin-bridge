cd92b0e055c7a10e1b27c067d54395444e784b8f
dpwns1234 <dpwns1234@naver.com>
2022-11-19 11:55:51 +0900

    feat: Output 클래스 기능 추가

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -9,12 +9,36 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(bridgeMap: List<String>) {}
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+
+    // 인자 줄여보기
+    fun printResult(bridgeMap: List<String>, successOrFail: String, tryingCount: Int) {
+        println(GAME_OVER_RESULT)
+        printMap(bridgeMap)
+        println(GAME_SUCCESS_OR_FAIL.format(successOrFail))
+        println(TOTAL_TRYING_COUNT.format(tryingCount))
+    }
+
+    fun printGameStart() {
+        println(GAME_START)
+    }
+
+    fun printInputBridgeLength() {
+        println(INPUT_BRIDGE_LENGTH)
+    }
+
+    companion object {
+        // output에서만 사용할거니까 message는 없애자
+        const val GAME_START = "다리 건너기 게임을 시작합니다."
+        const val INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요."
+        const val GAME_OVER_RESULT = "최종 게임 결과"
+        const val GAME_SUCCESS_OR_FAIL = "게임 성공 여부: %s"
+        const val TOTAL_TRYING_COUNT = "총 시도한 횟수: %d"
+    }
 }
