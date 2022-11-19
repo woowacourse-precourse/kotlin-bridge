bc868c76b2dead13db488984d6eec0c053919ab7
dltkd1395 <dltkd13956@naver.com>
2022-11-20 07:26:24 +0900

    feat(BridgeGame) : 다리 건너는 기능 구현

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -10,7 +10,12 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun move(bridge: Bridge, direction: String, bridgeMaker: List<String>): String {
+        val result = Judgement().compareDirection(direction, bridgeMaker[bridge.size])
+        moveBridge(bridge, direction, result)
+        bridge.setBridgeSize()
+        return result
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -19,4 +24,28 @@ class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry() {}
+
+    private fun moveBridge(bridge: Bridge, direction: String, result: String) {
+        when (direction) {
+            "U" -> moveUpBridge(bridge, result)
+            "D" -> moveDownBridge(bridge, result)
+        }
+    }
+
+    private fun moveUpBridge(bridge: Bridge, result: String) {
+        bridge.setUpBridge(getMovingResult(bridge.size, result))
+        bridge.setDownBridge(getMovingResult(bridge.size, " "))
+    }
+
+    private fun moveDownBridge(bridge: Bridge, result: String) {
+        bridge.setUpBridge(getMovingResult(bridge.size, " "))
+        bridge.setDownBridge(getMovingResult(bridge.size, result))
+    }
+
+    private fun getMovingResult(size: Int, result: String): String {
+        if (size == 0) {
+            return " $result "
+        }
+        return "| $result "
+    }
 }
