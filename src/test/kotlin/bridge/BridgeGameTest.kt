b546adab2bc298bd32f6a00395aeb315fdfb1714
dltkd1395 <dltkd13956@naver.com>
2022-11-20 08:07:08 +0900

    test(BridgeGameTest) : 게임 성공 여부 기능 테스트 추가

--- src/test/kotlin/bridge/BridgeGameTest.kt
+++ src/test/kotlin/bridge/BridgeGameTest.kt
@@ -3,6 +3,7 @@ package bridge
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class BridgeGameTest {
 
@@ -15,7 +16,16 @@ class BridgeGameTest {
         assertThat("[${bridge.down}]").isEqualTo(down)
     }
 
+    @ParameterizedTest
+    @CsvSource("true,성공", "false,실패")
+    fun `게임 성공 여부 기능 테스트`(isSuccess: Boolean, expected: String) {
+        player.isGameSuccess = isSuccess
+        val actual = BridgeGame().isSuccess(player)
+        assertThat(actual).isEqualTo(expected)
+    }
+
     companion object {
         private val bridge = Bridge()
+        private val player = Player()
     }
 }
