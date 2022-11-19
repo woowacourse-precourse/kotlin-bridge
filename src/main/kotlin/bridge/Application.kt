9007886fe33ddf4f0674e8cfa25489cbbc694c4e
kjyook <106497893+kjyook@users.noreply.github.com>
2022-11-19 17:31:03 +0900

    게임을 시작하면 다리를 만드는 함수 구현

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,17 @@
 package bridge
 
+fun playgame(){
+    val bridge = buildBridge()
+
+
+}
+fun buildBridge(): List<String>{
+    val length = InputView().readBridgeSize()
+    val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
+    val bridge: List<String> = bridgeMaker.makeBridge(length)
+
+    return bridge
+}
 fun main() {
 
 }
