c964223b15d398a8ecf1d4400b6df0ace3e6215a
ponopono0322 <ponopono0322@gmail.com>
2022-11-19 21:46:48 +0900

    refactor: 재시도 입력 함수 적용 코드 변경
    bridgeManager 적용

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -5,28 +5,14 @@ fun gameManager() {
     val outputView = OutputView()
     val bridgeManager = BridgeManager(inputView, outputView)
 
-    var position = 0
-    while (bridgeManager.getBridgeGame().isContinue && position < bridgeManager.getBridgeSize()) {
-        bridgeManager.moveBridge(position)
-        position++
-
-        // 중간에 실패한 경우
-        if (!bridgeManager.getBridgeGame().isContinue && position < bridgeManager.getBridgeSize()) {
-            outputView.printRetry()
-            val retryCommand = inputView.readGameCommand()
-            if (retryCommand == "R") {
-                bridgeManager.getBridgeGame().retry()
-                position = 0
-            } else {
-                val triple = Triple(position, bridgeManager.getBridgeGame().getUpBridge(), bridgeManager.getBridgeGame().getDownBridge())
-                outputView.printResult(triple, false)
-                outputView.printTryNumber(bridgeManager.getBridgeGame().getRetryCounter())
-                return
-            }
+    while (bridgeManager.getBridgeGame().isContinue && bridgeManager.getPosition() < bridgeManager.getBridgeSize()) {
+        bridgeManager.moveBridge()
+        if (bridgeManager.failChecker()) {
+            return
         }
     }
 
-    val triple = Triple(position-1, bridgeManager.getBridgeGame().getUpBridge(), bridgeManager.getBridgeGame().getDownBridge())
+    val triple = Triple(bridgeManager.getPosition()-1, bridgeManager.getBridgeGame().getUpBridge(), bridgeManager.getBridgeGame().getDownBridge())
     outputView.printResult(triple, true)
     outputView.printTryNumber(bridgeManager.getBridgeGame().getRetryCounter())
 }
