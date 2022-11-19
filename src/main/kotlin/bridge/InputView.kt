93950d72a9c1e93f0244b4a17992db28e6995822
jklom0326 <jklom0326@gmail.com>
2022-11-20 03:08:07 +0900

    feat(InputView.kt): 함수 기능 구현
    - readBridge 함수 구현
    - readMoving 함수 구현
    - readGameCommand 함수 구현

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,20 +10,28 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        val bridgeSize = Console.readLine()
+            .toInt()
+        // 예외 처리
+        return bridgeSize
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        val moveSize = Console.readLine()
+        // 예외 처리
+
+        return moveSize
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        val gameCommand = Console.readLine()
+        // 예외 처리
+        return gameCommand
     }
 }
