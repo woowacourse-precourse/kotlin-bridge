912629a87379e86489107f3f1c1cbe72ca19a89b
EmilyCh0 <seoyeon3089@naver.com>
2022-11-19 18:30:00 +0900

    feat(Result): 플레이어의 선택과 결과 Result에 추가

--- src/main/kotlin/bridge/Result.kt
+++ src/main/kotlin/bridge/Result.kt
@@ -3,15 +3,15 @@ package bridge
 class Result {
 
     var isSuccess: Boolean = false
-    var playerResult: List<Pair<String, Boolean>> = arrayListOf()
+    var playerResult: ArrayList<Pair<String, Boolean>> = arrayListOf()
     var tryCount: Int = 0
 
     fun setSuccess() {
 
     }
 
-    fun addPlayerResult() {
-
+    fun addPlayerResult(playerMove: String, isCrossable: Boolean) {
+        playerResult.add(playerMove to isCrossable)
     }
 
     fun increaseTryCount() {
