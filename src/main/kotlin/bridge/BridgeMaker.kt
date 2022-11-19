d4c92855be6b1c7cebfe660c60df68014081d158
dpwns1234 <dpwns1234@naver.com>
2022-11-19 16:25:00 +0900

    feat(BridgeMaker.kt): 다리 맵 만드는 기능 추가 및 다리 생성 테스트 통과

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,20 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        val bridgeMap = mutableListOf<String>()
+        // todo as 안써도 되면 안쓰기 근데 아마 써야하지 않을까?
+        // bridgeNumberGenerator as BridgeRandomNumberGenerator
+
+        // todo size만큼 반복하는 건 좀 더 간단한 반복문이 있지 않을까?
+        for(i in 0 until size) {
+            val direction = bridgeNumberGenerator.generate()
+            // todo 이거 리팩터 가능할 듯?
+            if(direction == 0)
+                bridgeMap.add("D")
+            else
+                bridgeMap.add("U")
+        }
+
+        return bridgeMap
     }
 }
