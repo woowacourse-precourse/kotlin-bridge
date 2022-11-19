ea2dd79f5a9adfb3d90dce57b6ef69ecec7d554e
Joonhoo <mnc5@naver.com>
2022-11-19 21:47:42 +0900

    feat: 다리의 길이 만큼의 리스트 생성 기능 추가

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -1,5 +1,9 @@
 package bridge
 
+import bridge.util.NUMBER_ONE
+import bridge.util.UP
+import bridge.util.DOWN
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -9,6 +13,14 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        return List(size) {
+            val number = bridgeNumberGenerator.generate()
+            convertNumberToString(number)
+        }
+    }
+
+    // 숫자에 해당하는 문자로 반환
+    private fun convertNumberToString(number: Int): String {
+        return if (number == NUMBER_ONE) UP else DOWN
     }
 }
---
+++ src/main/kotlin/bridge/util/Constants.kt
@@ -0,0 +1,6 @@
+package bridge.util
+
+
+const val NUMBER_ONE = 1
+const val UP = "U"
+const val DOWN = "D"
