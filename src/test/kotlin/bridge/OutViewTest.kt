73d4c570d945911283dcc9c9c40f0b2643e43dc0
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-19 14:59:42 +0900

    [Test] (OutViewTest.kt) 첫 라운드 U 입력시 건널 수 없을 때 올바른 출력 테스트 추가

--- src/test/kotlin/bridge/OutViewTest.kt
+++ src/test/kotlin/bridge/OutViewTest.kt
@@ -9,4 +9,9 @@ class OutViewTest {
     fun `첫 라운드 U를 입력하여 건널 수 있을 때 올바른 출력 테스트`() {
         assertThat("[ O ]\n[   ]").isEqualTo(OutputValidator.validatePrintMap(listOf("U","D","D"), 0, "U"))
     }
+
+    @Test
+    fun `첫 라운드 U를 입력하여 건널 수 없을 때 올바른 출력 테스트`() {
+        assertThat("[   ]\n[ X ]").isEqualTo(OutputValidator.validatePrintMap(listOf("U","D","D"), 0, "D"))
+    }
 }
