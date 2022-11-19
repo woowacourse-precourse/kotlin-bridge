4a5586c31f171d6c36111104177688c410967e2f
윤동현 <donghyunyoon81@gmail.com>
2022-11-19 21:20:35 +0900

    feat<BridgeGame> : 다리를 건널때 결과값을 받는 isPassable 기능 구현
    무엇을 : 다리를 건널때 결과 값을 받는 기능
    어떻게 : 다리의 칸에 받은 0 또는 1을 인수로 받아 if를 사용하여 1일 경우에 O 그외에는 X 값을 리턴 하도록
    왜 : 다리를 지날때 성공 여부를 확인하기 위해서

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: 프로그램 구현
 }
--- src/main/kotlin/bridge/domain/usecase/BridgeGame.kt
+++ src/main/kotlin/bridge/domain/usecase/BridgeGame.kt
@@ -1,5 +1,7 @@
 package bridge.domain.usecase
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -10,7 +12,16 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    // val p = listOf<String>("U1,D0", "U0,D1", "U0,D1", "U0,D1")
+    fun move() {
+    }
+
+    private fun isPassable(passableNumber: Int): String {
+        return if (passableNumber == RANDOM_UPPER_INCLUSIVE) "O" else "X"
+    }
+
+
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -18,5 +29,12 @@ class BridgeGame {
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
+
+    companion object {
+        const val RANDOM_UPPER_INCLUSIVE = 1
+        const val ONE_COLUMN_UP = 'U'
+        const val ONE_COLUMN_DOWN = 'D'
+    }
+
     fun retry() {}
 }
