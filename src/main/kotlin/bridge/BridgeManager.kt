5398ffb5f824714d2718a6e84cc02ba3c4d1b83e
ponopono0322 <ponopono0322@gmail.com>
2022-11-19 21:46:09 +0900

    refactor: main 함수 재시도 입력 함수화(gameManager)
    main 함수 내 게임 실패에 따른 재시도 선택 여부, 해당 결과에 따른 다음 실행 부분 함수화
    
    - failChecker(): 입력과 다리 배열의 일치 확인
    - restartGame(): 게임 재시도 선택으로 초기화 진행
    - quitGame(): 게임 종료 선택으로 종료 결과 출력
    - getRetryCommand(): 재시도 입력 함수, 오입력시 반복 적용

--- src/main/kotlin/bridge/BridgeManager.kt
+++ src/main/kotlin/bridge/BridgeManager.kt
@@ -31,16 +31,54 @@ class BridgeManager(private val inputView: InputView, private val outputView: Ou
                 this.outputView.printMove()
                 return inputView.readMoving()
             } catch (e: IllegalArgumentException) {
+                println(e.message)
                 continue
             }
         }
     }
 
-    fun moveBridge(position: Int) {
+    fun moveBridge() {
         val move = getMoveCommand()
-        this.bridgeGame.move(this.bridge, move, position)
-        val triple = Triple(position, this.bridgeGame.getUpBridge(), this.bridgeGame.getDownBridge())
+        this.bridgeGame.move(this.bridge, move, this.position)
+        val triple = Triple(this.position, this.bridgeGame.getUpBridge(), this.bridgeGame.getDownBridge())
         this.outputView.printMap(triple)
+        this.position++
+    }
+
+    fun failChecker(): Boolean {
+        if (!this.bridgeGame.isContinue && this.position <  this.bridgeSize) {
+            val retryCommand = getRetryCommand()
+            if (retryCommand == "R") {
+                this.restartGame()
+            } else {
+                this.quitGame()
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun restartGame() {
+        this.bridgeGame.retry()
+        this.position = 0
+    }
+
+    private fun quitGame() {
+        val triple = Triple(this.position, this.bridgeGame.getUpBridge(), this.bridgeGame.getDownBridge())
+        this.outputView.printResult(triple, false)
+        this.outputView.printTryNumber(this.bridgeGame.getRetryCounter())
+    }
+
+    private fun getRetryCommand(): String {
+        while (true) {
+            try {
+                this.outputView.printRetry()
+                return this.inputView.readGameCommand()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+                continue
+            }
+        }
     }
 
     fun getBridgeSize() = this.bridgeSize
