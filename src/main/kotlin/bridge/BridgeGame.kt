1ff560f3a4f5dbf13ad8befe38c8914c49054b65
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-20 03:41:06 +0900

    refactor(BridgeGame) : 출력 구문 제외 및 다리를 이동하는 기능과 재시작하는 기능 수정

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -3,18 +3,17 @@ package bridge
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
+class BridgeGame(
+    private val bridge: List<String>,
+) {
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move(direction: String): Int {
-        println("이동할 칸을 선택해주세요. (위: U, 아래: D)\n")
+    fun move(direction: String, turn: Int): Boolean = direction == bridge[turn]
 
-        return direction.decideNextPosition()
-    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -22,17 +21,8 @@ class BridgeGame {
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry(retryFlag: String): Boolean {
-        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n")
+    fun retry(retryFlag: String): Boolean = retryFlag.decideRetryOrQuit()
 
-        return retryFlag.decideRetryOrQuit()
-    }
 
-    fun String.decideNextPosition() = if (this == "U") {
-        1
-    } else {
-        0
-    }
-
-    fun String.decideRetryOrQuit() = this == "R"
+    private fun String.decideRetryOrQuit() = this == "R"
 }
