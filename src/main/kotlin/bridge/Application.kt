5a1406cccd54d4480c40d99d65207f709431e27b
ponopono0322 <ponopono0322@gmail.com>
2022-11-19 21:08:29 +0900

    refactor: BridgeManager 클래스 적용
    클래스 필드와 메소드로 적용

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -3,44 +3,39 @@ package bridge
 fun gameManager() {
     val inputView = InputView()
     val outputView = OutputView()
-
-    outputView.printBridge()
-    val bridgeSize = inputView.readBridgeSize()
-    val bridgeGame = BridgeGame(bridgeSize)
-    val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)
+    val bridgeManager = BridgeManager(inputView, outputView)
 
     var position = 0
-    while (bridgeGame.isContinue && position < bridgeSize) {
+    while (bridgeManager.getBridgeGame().isContinue && position < bridgeManager.getBridgeSize()) {
         outputView.printMove()
         val move = inputView.readMoving()
-        bridgeGame.move(bridge, move, position)
-        val triple = Triple(position, bridgeGame.getUpBridge(), bridgeGame.getDownBridge())
+        bridgeManager.getBridgeGame().move(bridgeManager.getBridge(), move, position)
+        val triple = Triple(position, bridgeManager.getBridgeGame().getUpBridge(), bridgeManager.getBridgeGame().getDownBridge())
         outputView.printMap(triple)
         position++
 
         // 중간에 실패한 경우
-        if (!bridgeGame.isContinue && position < bridgeSize) {
+        if (!bridgeManager.getBridgeGame().isContinue && position < bridgeManager.getBridgeSize()) {
             outputView.printRetry()
             val retryCommand = inputView.readGameCommand()
             if (retryCommand == "R") {
-                bridgeGame.retry()
+                bridgeManager.getBridgeGame().retry()
                 position = 0
             } else {
                 outputView.printResult(triple, false)
-                outputView.printTryNumber(bridgeGame.getRetryCounter())
+                outputView.printTryNumber(bridgeManager.getBridgeGame().getRetryCounter())
                 return
             }
         }
     }
 
-    val triple = Triple(position-1, bridgeGame.getUpBridge(), bridgeGame.getDownBridge())
+    val triple = Triple(position-1, bridgeManager.getBridgeGame().getUpBridge(), bridgeManager.getBridgeGame().getDownBridge())
     outputView.printResult(triple, true)
-    outputView.printTryNumber(bridgeGame.getRetryCounter())
+    outputView.printTryNumber(bridgeManager.getBridgeGame().getRetryCounter())
 }
 
 fun main() {
     try {
-        println("다리 건너기 게임을 시작합니다.")
         gameManager()
     } catch (e: IllegalArgumentException) {
         println(e.message)
