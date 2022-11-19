f64daf8d30369ed5fc980eca8a001e62285c1f85
minju kim <62362753+alswn2348@users.noreply.github.com>
2022-11-20 08:59:59 +0900

    refactor(OutputView) : 메시지 분리
     message 오브젝트 추가

---
+++ src/main/kotlin/bridge/consol/Message.kt
@@ -0,0 +1,11 @@
+package bridge.consol
+
+object Message {
+    const val START_GAME_MESSAGE = "다리 건너기 게임을 시작합니다. \n"
+    const val INPUT_LENGTH_MESSAGE = "다리의 길이를 입력해주세요."
+    const val INPUT_MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
+    const val INPUT_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
+    const val RESULT_GUIDE_MESSAGE = "최종 게임 결과"
+    const val WHETHER_SUCCESSFUL_MESSAGE = "게임 성공 여부: "
+    const val NUMBER_OF_ATTEMPT_MESSAGE = "총 시도한 횟수: "
+}
--- src/main/kotlin/bridge/consol/OutputView.kt
+++ src/main/kotlin/bridge/consol/OutputView.kt
@@ -1,32 +1,25 @@
 package bridge.consol
 
-const val START_GAME_MESSAGE = "다리 건너기 게임을 시작합니다. \n"
-const val INPUT_LENGTH_MESSAGE = "다리의 길이를 입력해주세요."
-const val INPUT_MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
-const val INPUT_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
-const val RESULT_GUIDE_MESSAGE = "최종 게임 결과"
-const val WHETHER_SUCCESSFUL_MESSAGE = "게임 성공 여부: "
-const val NUMBER_OF_ATTEMPT_MESSAGE = "총 시도한 횟수: "
 
 class OutputView {
     fun printStartGame() {
-        println(START_GAME_MESSAGE)
+        println(Message.START_GAME_MESSAGE)
     }
 
     fun printInputLength() {
-        println(INPUT_LENGTH_MESSAGE)
+        println(Message.INPUT_LENGTH_MESSAGE)
     }
 
     fun printInputMove() {
-        println(INPUT_MOVE_MESSAGE)
+        println(Message.INPUT_MOVE_MESSAGE)
     }
 
     fun printInputGameCommand() {
-        println(INPUT_COMMAND_MESSAGE)
+        println(Message.INPUT_COMMAND_MESSAGE)
     }
 
     fun printResultGuide() {
-        println(RESULT_GUIDE_MESSAGE)
+        println(Message.RESULT_GUIDE_MESSAGE)
     }
 
     /**
@@ -46,7 +39,7 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printResult(gameResult: Pair<String, Int>) {
-        println(WHETHER_SUCCESSFUL_MESSAGE + gameResult.first)
-        println(NUMBER_OF_ATTEMPT_MESSAGE + gameResult.second.toString())
+        println(Message.WHETHER_SUCCESSFUL_MESSAGE + gameResult.first)
+        println(Message.NUMBER_OF_ATTEMPT_MESSAGE + gameResult.second.toString())
     }
 }
