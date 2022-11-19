3c60445d6c38a06c2c2146c653c78dc5ba36ad49
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-20 02:44:45 +0900

    feat(BridgeGame) : 다리 이동과 게임을 다시 시도하는 기능 구현

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -10,7 +10,11 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun move(direction: String): Int {
+        println("이동할 칸을 선택해주세요. (위: U, 아래: D)\n")
+
+        return direction.decideNextPosition()
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -18,5 +22,17 @@ class BridgeGame {
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry() {}
+    fun retry(retryFlag: String): Boolean {
+        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n")
+
+        return retryFlag.decideRetryOrQuit()
+    }
+
+    fun String.decideNextPosition() = if (this == "U") {
+        0
+    } else {
+        1
+    }
+
+    fun String.decideRetryOrQuit() = this == "R"
 }
