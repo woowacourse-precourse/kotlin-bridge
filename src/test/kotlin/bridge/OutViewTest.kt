cd9c933f557e34fd997543915508b83ed2a52b06
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-19 16:07:24 +0900

    [Refactor] (OutViewTest.kt) 입력값에 따라 결과값이 다른 경우 테스트
    ParameterizedTest: 매개변수 테스트
    CsvSource: 입력값 지정

--- src/test/kotlin/bridge/OutViewTest.kt
+++ src/test/kotlin/bridge/OutViewTest.kt
@@ -2,16 +2,18 @@ package bridge
 
 import bridge.validator.OutputValidator
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class OutViewTest {
-    @Test
-    fun `첫 라운드 U를 입력하여 건널 수 있을 때 올바른 출력 테스트`() {
-        assertThat("[ O ]\n[   ]").isEqualTo(OutputValidator.validatePrintMap(listOf("U","D","D"), 0, "U"))
-    }
+    private val bridge = listOf("U","D","D")
 
-    @Test
-    fun `첫 라운드 U를 입력하여 건널 수 없을 때 올바른 출력 테스트`() {
-        assertThat("[   ]\n[ X ]").isEqualTo(OutputValidator.validatePrintMap(listOf("U","D","D"), 0, "D"))
+    @ParameterizedTest
+    @CsvSource(
+        "[ O ],[   ], 0, U",
+        "[   ],[ X ], 0, D"
+    )
+    fun `첫번째 라운드 올바른 출력 테스트`(upPrint: String, downPrint: String, round: Int, direction: String) {
+        assertThat(upPrint + "\n" + downPrint).isEqualTo(OutputValidator.validatePrintMap(bridge, round, direction))
     }
 }
