ac054b30441c16530ff5071a7686e6f7050e4459
agfalcon <agfalcon12@gmail.com>
2022-11-21 00:44:07 +0900

    BridgeMakerTest 구현 완료

---
+++ src/test/kotlin/bridge/BridgeMakerTest.kt
@@ -0,0 +1,15 @@
+package bridge
+
+import camp.nextstep.edu.missionutils.test.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class BridgeMakeTest {
+    val bridgemaker = BridgeMaker(BridgeRandomNumberGenerator())
+    @Test
+    fun makeBridgeTest(){
+        Assertions.assertRandomNumberInRangeTest({
+            assertThat(bridgemaker.makeBridge(4)).isEqualTo(listOf("D","U","D","U"))
+        },0,1,0,1)
+    }
+}
