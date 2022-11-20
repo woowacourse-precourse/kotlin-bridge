8d2c8aba6e36322ce187ba0ad31415fef466f283
junhyungss <daba44@naver.com>
2022-11-21 00:38:53 +0900

    feat(OutputView): 게임 시작 메시지 출력 게임 시작 메시지 출력 함수 printStart()

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,10 @@
 package bridge
 
 fun main() {
-    // TODO: 프로그램 구현
+
+    val outputView = OutputView()
+
+    println(outputView.printStart())
+
+
 }
--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -4,6 +4,13 @@ package bridge
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 class OutputView {
+
+    val MESSAGE_START = "다리 건너기 게임을 시작합니다.\n"
+
+    fun printStart():String{
+        return MESSAGE_START
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      *
