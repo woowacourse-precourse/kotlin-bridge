04e94227e71d8e220953360197a5f171b1e1741e
SEO-J17 <qw4643987@naver.com>
2022-11-21 03:54:50 +0900

    feat: 사용자에게 print메시지 보여주는 object 생성함

---
+++ src/main/kotlin/bridge/utils/Print.kt
@@ -0,0 +1,36 @@
+package bridge.utils
+
+object Print {
+    fun showStartMessage() {
+        println("다리 건너기 게임을 시작합니다.\n")
+    }
+
+    fun showInputBridgeLength() {
+        println("다리의 길이를 입력해주세요.")
+    }
+
+    fun showInputMove() {
+        println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)")
+    }
+
+    fun showRestart() {
+        println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+    }
+
+    fun showGameResult() {
+        println("\n최종 게임 결과")
+    }
+
+    fun showIsSuccess(result: String) {
+        println("\n게임 성공 여부: $result")
+    }
+
+    fun showAttempt(gameCount: Int) {
+        println("총 시도한 횟수: $gameCount")
+    }
+
+    fun showBridgeMap(map: String) {
+        println(map)
+    }
+
+}
