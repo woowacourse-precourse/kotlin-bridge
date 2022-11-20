9f7553b4e7c9f99f2d180d08adb9d6f18a59f63f
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-20 23:24:46 +0900

    feat: add bridgeSizeIsDigitException function Add function that check bridge size input is in digit

---
+++ src/main/kotlin/bridge/utils/ExceptionChecker.kt
@@ -0,0 +1,11 @@
+package bridge.utils
+
+class ExceptionChecker {
+    fun bridgeSizeIsDigitException(size: String){
+        require(size.all { it.isDigit() }){ println(ERROR_PREFIX + "다리 길이는 3부터 20 사이의 숫자여야 합니다.") }
+    }
+
+    companion object {
+        const val ERROR_PREFIX = "[ERROR] "
+    }
+}
