bd4dcc6c37504beb909a8bb919be2aa497e4e975
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-20 16:42:25 +0900

    feat(OutputView) : 경로를 출력하는 기능 구현 리팩토링 필수

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -10,7 +10,7 @@ class BridgeGame() {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move(bridge: List<String>, direction: String, turn: Int): Boolean = direction == bridge[turn]
+    fun move(bridge: List<String>, direction: String, turn: Int) = bridge[turn] == direction
 
 
     /**
--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -9,7 +9,13 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(playerPath: List<String>, isPlayerAlive: Boolean) {
+        val upBridgePath = playerPath.pathToMap("U", isPlayerAlive)
+        val downBridgePath = playerPath.pathToMap("D", isPlayerAlive)
+
+        println(upBridgePath)
+        println(downBridgePath)
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
@@ -17,4 +23,34 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printResult() {}
+
+    /**
+     * @param 위 방향의 다리 경로를 위한 출력인지 아래 방향의 다리 경로를 위한 출력인지
+     */
+    fun List<String>.pathToMap(direction: String, isPlayerAlive: Boolean): String {
+        val path = StringBuilder().append("[")
+
+        for (turn in 0 until this.size - 1) {
+            when (this[turn]) {
+                direction -> path.append(" O |")
+                else -> path.append("   |")
+            }
+        }
+        if (this.last() == direction) {
+            if (isPlayerAlive) {
+                path.append(" O ]")
+            } else {
+                path.append(" X ]")
+            }
+        } else {
+            path.append("   ]")
+        }
+        return path.toString()
+    }
+
+    fun String.directionToInt() = if (this == "D") {
+        0
+    } else {
+        1
+    }
 }
