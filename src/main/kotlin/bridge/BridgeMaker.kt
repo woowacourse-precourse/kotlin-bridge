04daad3cdb9148625055104a9e1fad06af485887
[alsswl] <[alswl4223@naver.com]>
2022-11-21 02:06:01 +0900

    feat(BridgeMaker) : 랜덤으로 수 받고 U또는 D로 다리 만들어서 return

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,14 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+       val BridgeRandomNumberGenerator = BridgeRandomNumberGenerator()
+        var bridge = mutableListOf<String>()
+        var randomNum = BridgeRandomNumberGenerator.generate()
+       for(i in 0 until size) {
+           if (randomNum == 0) bridge.add("U")
+           else bridge.add("D")
+       }
+        var bridgeReturn = bridge.toList()
+        return bridgeReturn
     }
 }
