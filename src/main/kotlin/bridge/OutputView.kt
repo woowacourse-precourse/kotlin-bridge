563240ed6ba4989acfe413d202ad297ebd505a2f
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-20 14:29:42 +0900

    [Feat] (OutputView.kt) 최종 결과 출력 기능 추가

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -20,5 +20,13 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+    fun printResult(map: String, round: Int, isNotMatched: Boolean) {
+        var trialCount = round
+        trialCount++
+        println("최종 게임 결과")
+        println(map)
+        if (isNotMatched) println("게임 성공 여부: 실패")
+        if (!isNotMatched) println("게임 성공 여부: 성공")
+        println("총 시도한 횟수:${trialCount}")
+    }
 }
