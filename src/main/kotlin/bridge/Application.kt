b109ac393c9d4d5a93ad797f261559be610e0226
LHS0731 <gustjq2536@gmail.com>
2022-11-20 19:22:26 +0900

    refactor: main()함수 리팩토링 및 버그수정

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -4,28 +4,40 @@ fun main() {
     // TODO: 프로그램 구현
     val bridgeSize = InputView().readBridgeSize()
     val bridgeMade = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)
-
-    var caseList = mutableListOf<Int>()
-    var answer = true
+    var answer: Boolean
     var regame = ""
-    while(true){
-        for (i in bridgeMade.indices) {
-            val case = BridgeGame().move(bridgeMade[i], InputView().readMoving())
-            caseList.add(case)
-            OutputView().printMap(caseList)
-            if (case == 3 || case == 4) {
-                answer = false
-                break
 
-            }
-            if (i == bridgeMade.size - 1){
-                answer = true
-                break
-            }
-        }
+    while (true) {
+        var caseList = mutableListOf<Int>()
+        answer = caseMaker(bridgeMade, caseList)
+        //맞추기 틀린 경우
         if (!answer) {
             regame = InputView().readGameCommand()
-            println(regame)
+        }
+        //틀리고 재시작시 Q가 들어온 경우
+        if (!answer && BridgeGame().retry(regame)) {
+            break
+        }
+        //틀리고 재시작시 R이 들어온 경우
+        if (!answer && !BridgeGame().retry(regame)) {
+            regame = ""
+        }
+        //맞추기 성공한 경우
+        if (answer) {
+            break
         }
     }
+}
+
+fun caseMaker(bridge: List<String>, caseList: MutableList<Int>): Boolean {
+    for (i in bridge.indices) {
+        val case = BridgeGame().move(bridge[i], InputView().readMoving())
+        caseList.add(case)
+        OutputView().printMap(caseList)
+        if (case == 3 || case == 4)
+            break
+        if (i == bridge.size - 1)
+            return true
+    }
+    return false
 }
