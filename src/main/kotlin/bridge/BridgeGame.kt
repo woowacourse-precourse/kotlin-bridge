e9887272f5500ae6846e90d48248f4a061d9e3c7
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-11-20 16:49:59 +0900

    feat: [다리 건너기] 이동 및 이동에 따른 다리 그림 출력 기능 추가
    BridgeGame.move()
    .getMovingRecord()
    
    OutputView.printMap()
    .printOneSideBridge(), .makeBridgePicture(), addBridgeBeginning()
    .addBridgeMiddle(), addBridgeEnd(), addBlockBoundary()
    .addBlockRightAnswer(), addBlockWrongAnswer() 구현

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -4,13 +4,18 @@ package bridge
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+    private val movingRecord = mutableListOf<String>()
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun move(direction: String): String {
+        movingRecord.add(direction)
+
+        return direction
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -19,4 +24,6 @@ class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry() {}
+
+    fun getMovingRecord() = movingRecord.toList()
 }
--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -4,17 +4,122 @@ package bridge
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 class OutputView {
+    private val input = InputView()
+    private val randomNumberGenerator = BridgeRandomNumberGenerator()
+    private val bridgeMaker = BridgeMaker(randomNumberGenerator)
+    private val bridge = bridgeMaker.makeBridge(input.readBridgeSize())
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(movingRecord: List<String>) {
+        val bridgePicture = makeBridgePicture(movingRecord)
 
+        printOneSideBridge(bridgePicture[0])   // upsideBridge
+        printOneSideBridge(bridgePicture[1])   // downsideBridge
+    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printResult() {}
+
+    private fun printOneSideBridge(list: List<String>) {
+        for (i in list.indices) print(list[i])
+        println("")
+    }
+
+    private fun makeBridgePicture (movingRecord: List<String>): List<List<String>> {
+        val upsideBridge = mutableListOf<String>()
+        val downsideBridge = mutableListOf<String>()
+
+        addBridgeBeginning(upsideBridge, downsideBridge)
+        addBridgeMiddle(upsideBridge, downsideBridge, movingRecord)
+        addBridgeEnd(upsideBridge, downsideBridge)
+
+        return listOf(upsideBridge.toList(), downsideBridge.toList())
+    }
+
+    private fun addBridgeBeginning (
+            upsideBridge: MutableList<String>,
+            downsideBridge: MutableList<String>
+    ) {
+        upsideBridge.add(BRIDGE_BEGINNING)
+        downsideBridge.add(BRIDGE_BEGINNING)
+    }
+
+    private fun addBridgeMiddle(
+            upsideBridge: MutableList<String>,
+            downsideBridge: MutableList<String>,
+            movingRecord: List<String>
+    ) {
+        val round = movingRecord.size
+
+        for (count in 1..round) {
+            if (movingRecord[count - 1] == bridge[count - 1])
+                addBlockRightAnswer(movingRecord[count - 1], upsideBridge, downsideBridge)
+            else
+                addBlockWrongAnswer(movingRecord[count - 1], upsideBridge, downsideBridge)
+            addBlockBoundary(upsideBridge, round, count)
+            addBlockBoundary(downsideBridge, round, count)
+        }
+    }
+
+    private fun addBridgeEnd(
+            upsideBridge: MutableList<String>,
+            downsideBridge: MutableList<String>
+    ) {
+        upsideBridge.add(BRIDGE_END)
+        downsideBridge.add(BRIDGE_END)
+    }
+
+    private fun addBlockBoundary(block: MutableList<String>, round: Int, count: Int) {
+        if (round != 1 && count != round) block.add("|")
+    }
+
+    private fun addBlockRightAnswer(
+            movingDirection: String,
+            upsideBridge: MutableList<String>,
+            downsideBridge: MutableList<String>
+    ) {
+        when (movingDirection) {
+            "U" -> {
+                upsideBridge.add(RIGHT_ANSWER)
+                downsideBridge.add(NO_INPUT)
+            }
+
+            "D" -> {
+                upsideBridge.add(NO_INPUT)
+                downsideBridge .add(RIGHT_ANSWER)
+            }
+        }
+    }
+
+    private fun addBlockWrongAnswer(
+            movingDirection: String,
+            upsideBridge: MutableList<String>,
+            downsideBridge: MutableList<String>
+    ) {
+        when (movingDirection) {
+            "U" -> {
+                upsideBridge.add(WRONG_ANSWER)
+                downsideBridge.add(NO_INPUT)
+            }
+
+            "D" -> {
+                upsideBridge.add(NO_INPUT)
+                downsideBridge .add(WRONG_ANSWER)
+            }
+        }
+    }
+
+    companion object {
+        const val BRIDGE_BEGINNING = "["
+        const val BRIDGE_END = "]"
+        const val NO_INPUT = "   "
+        const val RIGHT_ANSWER = " o "
+        const val WRONG_ANSWER = " x "
+    }
 }
