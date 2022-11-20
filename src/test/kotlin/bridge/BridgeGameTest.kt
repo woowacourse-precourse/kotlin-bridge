d51c98f884c8f63bb2096d5321296704fc0b53c9
Doyeop Kim <dygames@naver.com>
2022-11-20 20:10:42 +0900

    TEST(BridgeGameTest): 도메인 로직에 대한 테스트 함수들 정의

---
+++ src/test/kotlin/bridge/BridgeGameTest.kt
@@ -0,0 +1,52 @@
+package bridge
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.io.ByteArrayInputStream
+
+class BridgeGameTest {
+    @Test
+    fun `다리 생성 테스트`() {
+
+    }
+
+    @Test
+    fun `다리 생성 길이 예외 테스트`() {
+
+    }
+
+
+    @Test
+    fun `유저 이동 테스트`() {
+
+    }
+
+
+    @Test
+    fun `유저 이동 예외 테스트`() {
+
+    }
+
+    @Test
+    fun `재시도 테스트`() {
+
+    }
+
+    @Test
+    fun `종료 테스트`() {
+
+    }
+
+    @Test
+    fun `게임 클리어 테스트`() {
+
+    }
+
+    @Test
+    fun `시도한 횟수 테스트`() {
+
+    }
+}
