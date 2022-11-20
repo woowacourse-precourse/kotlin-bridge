3442e8d4a615cedea1c4fc745c3473867dbd2f2d
Hyemin Song <hyemdooly@gmail.com>
2022-11-20 21:27:07 +0900

    feat (OutputView) : printMap 구현

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -9,7 +9,27 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+
+    private fun judge(route: String, bridge: String, direction: String): String {
+        return if (route == direction && bridge == direction) {
+            "O "
+        } else if (route == direction && bridge != direction) {
+            "X "
+        } else {
+            "  "
+        }
+    }
+    fun printMap(route: List<String>, bridge: List<String>) {
+        print("[ ")
+        for (i in route.indices) {
+            print(judge(route[i], bridge[i], "U")+ if (i != route.lastIndex) "| " else "")
+        }
+        print("]\n[ ")
+        for (i in route.indices) {
+            print(judge(route[i], bridge[i], "D") + if (i != route.lastIndex) "| " else "")
+        }
+        println("]")
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
