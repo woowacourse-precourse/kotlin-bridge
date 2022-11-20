0aeeb994b9c37a4f7e9e57a06a108011e091c3b0
joh9911 <joh9911@naver.com>
2022-11-21 02:27:11 +0900

    refactor(Constants): 성공 실패 심볼 string으로 변경

--- src/main/kotlin/bridge/utils/Constants.kt
+++ src/main/kotlin/bridge/utils/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     /**
      * 게임 진행 메시지
      */
-    const val GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다."
+    const val GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.\n"
     const val GAME_INPUT_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요."
     const val GAME_INPUT_MOVE_LANE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
     const val GAME_INPUT_RETRIAL_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
@@ -23,10 +23,10 @@ object Constants {
     /**
      * 게임 심볼
      */
-    const val UP_SYMBOL = "U"
-    const val DOWN_SYMBOL = "D"
-    const val RETRIAL_SYMBOL = "R"
-    const val QUIT_SYMBOL = "Q"
-    const val SUCCESS_SYMBOL = "O"
-    const val FAIL_SYMBOL = "X"
+    const val UP_SYMBOL = 'U'
+    const val DOWN_SYMBOL = 'D'
+    const val RETRIAL_SYMBOL = 'R'
+    const val QUIT_SYMBOL = 'Q'
+    const val SUCCESS_SYMBOL = " O "
+    const val FAIL_SYMBOL = " X "
 }
