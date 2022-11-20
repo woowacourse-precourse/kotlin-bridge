a3b1b43309befb93ae20b4ec4e380f6d0ce54f7a
otter <a33397397@gmail.com>
2022-11-20 17:27:15 +0900

    feat: 게임 재시도 여부에 따른 진행 기능 작성

--- src/main/kotlin/bridge/BridgeGameController.kt
+++ src/main/kotlin/bridge/BridgeGameController.kt
@@ -18,7 +18,12 @@ class BridgeGameController {
             println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
             bridgeGame.move(inputView.readMoving())
             outputView.printMap(bridgeGame.getGameProgress())
-            if(!bridgeGame.isSuccess()) break
+
+            if(!bridgeGame.isSuccess()) {
+                println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+                if(inputView.readGameCommand() == "R") bridgeGame.retry()
+                else break
+            }
         }
 
         outputView.printResult(bridgeGame)
