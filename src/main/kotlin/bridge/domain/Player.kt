b3b879e75be5dd719a07c9b36030aa8afee3087f
jiingom <vvankatam0@gmail.com>
2022-11-21 00:31:27 +0900

    refactor: 스테이지 결과를 열거형 객체 Stage로 추가하도록 변경

--- src/main/kotlin/bridge/domain/Player.kt
+++ src/main/kotlin/bridge/domain/Player.kt
@@ -2,8 +2,6 @@ package bridge.domain
 
 class Player(private val bridge: Bridge) {
     private var playerBridge = mutableListOf<Stage>()
-    private var upBridge = mutableListOf<String>()
-    private var downBridge = mutableListOf<String>()
     private var stage = 0
 
     fun updateStage(inputMoving: String): Boolean {
@@ -15,29 +13,12 @@ class Player(private val bridge: Bridge) {
 
     private fun moveToNext(inputUpOrDown: String, isClearStage: Boolean) {
         when (isClearStage) {
-            true -> updateBridge(inputUpOrDown, MOVE_CORRECT)
+            true -> playerBridge.add(Stage.getStageInform(inputUpOrDown, MOVE_CORRECT))
             false -> {
-                updateBridge(inputUpOrDown, MOVE_FAIL)
+                playerBridge.add(Stage.getStageInform(inputUpOrDown, MOVE_FAIL))
                 stage -= 1
             }
         }
-    }
-
-    private fun updateBridge(inputUpOrDown: String, stageResult: String) {
-        when (inputUpOrDown) {
-            MOVE_UP -> updateUpBridge(stageResult)
-            MOVE_DOWN -> updateDownBridge(stageResult)
-        }
-    }
-
-    private fun updateUpBridge(stageResult: String) {
-        upBridge.add(stageResult)
-        downBridge.add(NOT_MOVE_TO_HERE)
-    }
-
-    private fun updateDownBridge(stageResult: String) {
-        upBridge.add(NOT_MOVE_TO_HERE)
-        downBridge.add(stageResult)
     }
 
     fun isClear(): Boolean {
@@ -48,25 +29,13 @@ class Player(private val bridge: Bridge) {
         return playerBridge
     }
 
-    fun getUpBridge(): List<String> {
-        return upBridge
-    }
-
-    fun getDownBridge(): List<String> {
-        return downBridge
-    }
-
     fun initializePlayer() {
         stage = 0
-        upBridge = mutableListOf<String>()
-        downBridge = mutableListOf<String>()
+        playerBridge = mutableListOf()
     }
 
     companion object {
-        const val MOVE_UP = "U"
-        const val MOVE_DOWN = "D"
         const val MOVE_CORRECT = "O"
         const val MOVE_FAIL = "X"
-        const val NOT_MOVE_TO_HERE = " "
     }
 }
