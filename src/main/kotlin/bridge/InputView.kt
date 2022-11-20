844af3d1b95016a0c33969d6c6cb6302b5061ac9
vrexpert <vrexpert2302@gmail.com>
2022-11-20 20:35:12 +0900

    feat(InputView): 플레이어에게 요구사항 및 예시 출력
    플레이어에게 요구사항 및 예시를 출력한다

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,29 +1,21 @@
 package bridge
 import camp.nextstep.edu.missionutils.Console
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+
 class InputView {
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     fun readBridgeSize(): Int {
+        println("다리의 길이를 입력해주세요.")
         val bridgeSize = Console.readLine()
         return bridgeSize.toInt()
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     fun readMoving(): String {
+        println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
         val moving = Console.readLine()
         return moving
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     fun readGameCommand(): String {
+        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
         val gameCommand = Console.readLine()
         return gameCommand
     }
