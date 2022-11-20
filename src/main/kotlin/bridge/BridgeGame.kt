441601dd3c0dd6f6142c6cfb57fbf254881ba309
vrexpert <vrexpert2302@gmail.com>
2022-11-20 14:38:00 +0900

    feat(BridgeGame): 플레이어 이동 구현
    move(): 플레이어가 지금까지 이동한 결과를 리스트로 반환

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -1,16 +1,26 @@
 package bridge
+import bridge.data.Games
+import bridge.data.Moves
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     *
-     *
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    fun move() {}
+    companion object{
+        val UP_RIGHT = listOf("O"," ")
+        val UP_WRONG = listOf("X"," ")
+        val DOWN_RIGHT = listOf(" ","O")
+        val DOWN_WRONG = listOf(" ","X")
+    }
+    fun move(playerMoves:MutableList<List<String>>,upOrDown:String,alive:Boolean):MutableList<List<String>>{
+        var jump = listOf("")
+        when (upOrDown) {
+            Games.UP.game -> if(alive) jump = UP_RIGHT else UP_WRONG
+            Games.DOWN.game -> if(alive) jump = DOWN_RIGHT else DOWN_WRONG
+        }
+        playerMoves.add(jump)
+        return playerMoves
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
