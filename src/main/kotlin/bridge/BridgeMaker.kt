a42cde37ad8c6a8f05d0768946f7c77e718920c6
SEO-J17 <qw4643987@naver.com>
2022-11-21 03:57:31 +0900

    feat: 입력받은 0과 1에 따라 U인지 D인지 나타내는 다리 구현함

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,23 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        val bridge = mutableListOf<String>()
+        repeat(size) {
+            bridge.add(inputUpDown(bridgeNumberGenerator.generate()))
+        }
+        return bridge
+    }
+
+    private fun inputUpDown(number: Int): String {
+        return if (number == BridgeRandomNumberGenerator.RANDOM_UPPER_INCLUSIVE) {
+            UP_STAIR
+        } else {
+            DOWN_STAIR
+        }
+    }
+
+    companion object {
+        const val UP_STAIR = "U"
+        const val DOWN_STAIR = "D"
     }
 }
