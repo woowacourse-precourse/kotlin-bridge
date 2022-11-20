205d94ab2e808fc815fb1861027b31cfe8f6926a
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-20 23:39:57 +0900

    feat: add exception check in readGameCommand function Add exception check code to check exception about game command

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -22,8 +22,8 @@ class InputView {
 
     fun readGameCommand(): String {
         println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
-        val retry = readLine()!!
-        //todo: 예외처리 필요
-        return retry
+        val command = readLine()!!
+        exceptionChecker.gameCommandException(command)
+        return command
     }
 }
