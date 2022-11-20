ade20663c662e3417fca81d410ff59006ad721e6
choHarmony <jws0183@naver.com>
2022-11-21 00:44:40 +0900

    feat: 0, 1로 이루어진 정답 다리 배열 만들기

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,19 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
+        val row = size
+        val column = 2
+        // 0, 1로 이루어진 답지 만들기
+        val answer = Array(size, {IntArray(2, {0})})
+
+        for (c in 0 until column-1) {
+            for (r in 0 until row-1) {
+                answer[c][r] = bridgeNumberGenerator.generate()
+            }
+        }
+
+
+
         return listOf()
     }
 }
