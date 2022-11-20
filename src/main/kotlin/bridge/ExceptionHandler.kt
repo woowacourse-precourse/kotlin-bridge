5213f62dece883839c7a270f2287faf25c93015e
krrong <khk9664@naver.com>
2022-11-20 22:35:42 +0900

    feat(ExceptionHandler): 재시작 입력 시 예외 처리

--- src/main/kotlin/bridge/ExceptionHandler.kt
+++ src/main/kotlin/bridge/ExceptionHandler.kt
@@ -4,4 +4,8 @@ object ExceptionHandler {
     fun checkValidMoving(location: String) {
         require(location == "U" || location == "D") { Message.InputViewEnum.INVALID_MSG }
     }
+
+    fun checkValidRestart(restart: String) {
+        require(restart == "Q" || restart == "R") { Message.InputViewEnum.INVALID_MSG }
+    }
 }
