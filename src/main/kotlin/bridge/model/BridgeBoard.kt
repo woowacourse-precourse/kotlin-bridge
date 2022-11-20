3341554fd4f8698580aa044ea3d819deff0e6e60
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-20 23:53:16 +0900

    feat: add updateBoard function Add function that update BridgeBoard to latest state

--- src/main/kotlin/bridge/model/BridgeBoard.kt
+++ src/main/kotlin/bridge/model/BridgeBoard.kt
@@ -3,4 +3,24 @@ package bridge.model
 class BridgeBoard {
     var board = MutableList(2){ mutableListOf<String>() }
 
+    fun updateBoard(correct: Boolean, moving: String){
+        if(correct){
+            if(moving == "U") {
+                board[0].add("O")
+                board[1].add(" ")
+            }else{
+                board[0].add(" ")
+                board[1].add("O")
+            }
+        }else{
+            if(moving == "U"){
+                board[0].add("X")
+                board[1].add(" ")
+            }else{
+                board[0].add(" ")
+                board[1].add("X")
+            }
+        }
+    }
+
 }
