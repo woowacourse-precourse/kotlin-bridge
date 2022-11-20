f4479e69f7ce0e9dbfabd5a08114b8cf22e53313
no1msh <no1msh1217@gmail.com>
2022-11-20 15:30:17 +0900

    feat: 건널 수 있는 칸이 적힌 리스트를 반환하는 메소드 구현

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -13,6 +13,12 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        val bridge = mutableListOf<String>()
+        for (index in 1..size) {
+            val bridgeNumber = bridgeNumberGenerator.generate()
+            if (bridgeNumber == 0) bridge.add("D")
+            if (bridgeNumber == 1) bridge.add("U")
+        }
+        return bridge.toList()
     }
 }
