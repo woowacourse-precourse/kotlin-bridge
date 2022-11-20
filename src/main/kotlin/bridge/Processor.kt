54c8852a84d9602725b3608b52fb923fc3d21834
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-21 05:33:40 +0900

    feat(Processor) : 베이스볼 게임을 진행하는 객체인 processor 생성 및 사용자의 이동 경로 파악을 위한 map배열 생성

--- src/main/kotlin/bridge/Processor.kt
+++ src/main/kotlin/bridge/Processor.kt
@@ -1,4 +1,22 @@
 package bridge
 
-class Processor {
+class Processor(
+    val bridgeMaker: BridgeMaker,
+    val bridgeGame: BridgeGame,
+    val inputView: InputView,
+    val outputView: OutputView,
+) {
+    var playerPosition = 0
+    lateinit var bridgeMap: Array<String>
+    lateinit var bridge: List<String>
+
+    fun initBridge() {
+        val bridgeSize = inputView.readBridgeSize()
+        bridge = bridgeMaker.makeBridge(bridgeSize)
+        initBridgeMap()
+    }
+
+    fun initBridgeMap(){
+        bridgeMap = Array(2) { "[ N ]".repeat(bridge.size).replace("][", "|") }
+    }
 }
