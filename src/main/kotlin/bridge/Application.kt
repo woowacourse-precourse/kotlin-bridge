b2f91f36276a51495ddeef5e1a595ad6147bfc43
jiingom <vvankatam0@gmail.com>
2022-11-20 16:41:34 +0900

    refactor: 게임 시작 안내 문구 추가

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -5,5 +5,6 @@ import bridge.controller.BridgeGameController
 fun main() {
     // TODO: 프로그램 구현
     val bridgeGame = BridgeGameController()
+    println("다리 건너기 게임을 시작합니다.")
     bridgeGame.start()
 }
