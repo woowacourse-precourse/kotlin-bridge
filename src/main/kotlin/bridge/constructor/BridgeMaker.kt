fd7a176c8d1a68f0514ec590b280aec8b8aa3564
brian <briandr@naver.com>
2022-11-21 07:10:17 +0900

    refactor(OutputView): for문을 repeat으로 바꿈
    for문의 i가 필요 없다고 판단되어 repeat으로 바꿈

--- src/main/kotlin/bridge/constructor/BridgeMaker.kt
+++ src/main/kotlin/bridge/constructor/BridgeMaker.kt
@@ -10,7 +10,7 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      */
     fun makeBridge(size: Int): List<String> {
         val bridge = mutableListOf<String>()
-        for (i in 0 until size) {
+        repeat(size) {
             bridge.add(makeRandomBlock())
         }
         return bridge
