aaefabef170ac7b4bcd3829461de94ecd6408585
krrong <khk9664@naver.com>
2022-11-20 21:21:56 +0900

    feat(Controller): 필요 클래스 생성 및 게임 시작 기능 추가

---
+++ src/main/kotlin/bridge/Controller.kt
@@ -0,0 +1,14 @@
+package bridge
+
+class Controller {
+    /**
+     * 필요 클래스 생성 및 게임 시작
+     */
+    fun play() {
+        try {
+            InputView().readBridgeSize()
+        } catch (e: IllegalArgumentException) {
+            print(e)
+        }
+    }
+}
