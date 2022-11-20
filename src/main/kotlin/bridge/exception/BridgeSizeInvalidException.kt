0cdf557ec8e53a5afc4366c62f5ed32e17eb0627
yeongjin <dudwls901@naver.com>
2022-11-20 18:06:13 +0900

    [Feat] Custom Exception 정의 - BridgeSize, Moving

---
+++ src/main/kotlin/bridge/exception/BridgeSizeInvalidException.kt
@@ -0,0 +1,3 @@
+package bridge.exception
+
+class BridgeSizeInvalidException : BridgeGameException()
---
+++ src/main/kotlin/bridge/exception/MovingInvalidException.kt
@@ -0,0 +1,3 @@
+package bridge.exception
+
+class MovingInvalidException : BridgeGameException()
