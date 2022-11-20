277a98a54f283ba07e36bc269b9a51a5827fa181
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-21 00:54:55 +0900

    feat: add printMap function Add function that print current state of BridgeBoard

--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -1,24 +1,26 @@
 package bridge.view
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 class OutputView {
 
     fun printStartMessage() {
         println("다리 건너기 게임을 시작합니다.")
     }
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    fun printMap() {}
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    fun printMap(board: MutableList<MutableList<String>>) {
+        var upperBoard = "[ "
+        var lowerBoard = "[ "
+
+        for(i in board[0].indices){
+            if(i == board[0].size-1) {
+                upperBoard += "${board[0][i]} ]"
+                lowerBoard += "${board[1][i]} ]"
+            }else{
+                upperBoard += "${board[0][i]} | "
+                lowerBoard += "${board[1][i]} | "
+            }
+        }
+        println("$upperBoard\n$lowerBoard")
+    }
+
     fun printResult() {}
 }
