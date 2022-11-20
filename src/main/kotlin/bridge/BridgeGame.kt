d840d77ac3c8523ae0b7a5c00e5914e776bec1d6
mendel <mendel.urssu@gmail.com>
2022-11-20 20:28:44 +0900

    feat: 이동 입력 값에 대해 이동 결과 여부를 반환하는 move 메소드 구현

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -7,7 +7,13 @@ class BridgeGame(private val realBridges: List<String>) {
     private val bridges = mutableListOf<String>()
     private var curStep: Int = 0
     private var success: Boolean = false
-    private var tryCount: Int = 0
+    private var tryCount: Int = 1
+
+    init {
+        for (i in realBridges.indices) {
+            bridges.add("U")
+        }
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -15,7 +21,15 @@ class BridgeGame(private val realBridges: List<String>) {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun move(curMove: String): BridgeMoveResult {
+        bridges[curStep] = curMove
+        if (bridges[curStep] == realBridges[curStep] && curStep + 1 == realBridges.size) return BridgeMoveResult.MOVE_END
+        if (bridges[curStep] == realBridges[curStep]) {
+            curStep++
+            return BridgeMoveResult.MOVE_SUCCESS
+        }
+        return BridgeMoveResult.MOVE_FAILED
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -24,4 +38,8 @@ class BridgeGame(private val realBridges: List<String>) {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry() {}
+
+    enum class BridgeMoveResult {
+        MOVE_SUCCESS, MOVE_FAILED, MOVE_END
+    }
 }
