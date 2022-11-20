0cf5faba1e80c4fd4bcc541ffe05fc9ae9129b1d
LHS0731 <gustjq2536@gmail.com>
2022-11-20 16:42:55 +0900

    feat: inputView 클래스 구현

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,20 +10,27 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        println("다리 건너기 게임을 시작합니다.")
+        println("다리의 길이를 입력해주세요.")
+        val readSize = Console.readLine()
+        return readSize.toInt()
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
+        var step = Console.readLine()
+        return step
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+        var regame = Console.readLine()
+        return regame
     }
 }
