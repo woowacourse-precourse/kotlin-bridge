c301f727007f253546092612afdbb7515a5b500c
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-20 23:26:49 +0900

    feat: add movingException function Add function that check moving input is only "U" or "D"

--- src/main/kotlin/bridge/utils/ExceptionChecker.kt
+++ src/main/kotlin/bridge/utils/ExceptionChecker.kt
@@ -9,6 +9,10 @@ class ExceptionChecker {
         require(size in 3 .. 20){ println(ERROR_PREFIX + "다리 길이는 3부터 20 사이의 숫자여야 합니다.") }
     }
 
+    fun movingException(moving: String){
+        require(moving == "U" || moving == "D"){ println(ERROR_PREFIX + "이동할 칸은 U 나 D 로만 입력되어야 합니다.") }
+    }
+
     companion object {
         const val ERROR_PREFIX = "[ERROR] "
     }
