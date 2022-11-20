fb512ccf660a35e233b4dd701c9d874c936bcb4f
YuGyeong98 <kuk3097@naver.com>
2022-11-20 13:44:52 +0900

    feat: 예외 처리 후 입력 다시 받도록 메인에 적용

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,9 @@
 package bridge
 
+import exception.BridgeSize
+import exception.GameOver
+import exception.MovingSpace
+
 fun main() {
     val inputView = InputView()
     val numberGenerator = BridgeRandomNumberGenerator()
@@ -10,16 +14,24 @@ fun main() {
     var successOrFailure = ""
     val up = BridgeGame.getUpList()
     val down = BridgeGame.getDownList()
+    var size = 0
+    var space = ""
+    var retryOrQuit = ""
+    val bridgeSize = BridgeSize(inputView, size)
+    val movingSpace = MovingSpace(inputView, space)
+    val gameOver = GameOver(inputView, retryOrQuit)
 
     println("다리 건너기 게임을 시작합니다.\n")
-
     println("다리의 길이를 입력해주세요.")
-    val size = inputView.readBridgeSize()
+    bridgeSize.verify()
+    size = bridgeSize.getInput()
     val bridge = bridgeMaker.makeBridge(size)
 
     while (true) {
         println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
-        val space = inputView.readMoving()
+        movingSpace.verify()
+        space = movingSpace.getInput()
+
         val movingList = bridgeGame.move(space, bridge)
         val upList = movingList.filter { it.key == "U" }.values.flatten()
         val downList = movingList.filter { it.key == "D" }.values.flatten()
@@ -34,13 +46,14 @@ fun main() {
 
         if (BridgeGame.failure()) {
             println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
-            val isRetry = inputView.readGameCommand()
+            gameOver.verify()
+            retryOrQuit = gameOver.getInput()
 
             numberOfAttempts += 1
-            if (isRetry == "R") {
+            if (retryOrQuit == "R") {
                 bridgeGame.retry()
                 continue
-            } else if (isRetry == "Q") {
+            } else if (retryOrQuit == "Q") {
                 successOrFailure = "실패"
                 outputView.printResult(up, down, successOrFailure, numberOfAttempts)
                 break
