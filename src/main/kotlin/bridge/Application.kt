d0f151053e28d56da98b186d0015e810ad4602b2
dlwgnud <wngud980411@naver.com>
2022-11-20 18:23:02 +0900

    docs: 게임 시작 문구 출력 함수

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -2,4 +2,5 @@ package bridge
 
 fun main() {
     // TODO: 프로그램 구현
+    OutputView().gameStart()
 }
--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -3,7 +3,17 @@ package bridge
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
+
+enum class Output(val message: String){
+    GAME_START("다리 건너기 게임을 시작합니다."),
+}
+
 class OutputView {
+    fun gameStart() {
+        println(Output.GAME_START.message)
+        println()
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      *
