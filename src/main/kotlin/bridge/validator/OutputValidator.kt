113537cfa730f2cf78cbb6376a1329217c5307aa
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-20 21:29:13 +0900

    [Fit] (OutputValidator.kt) 첫 라운드 맞고, 틀림 출력 기능 수정

--- src/main/kotlin/bridge/validator/OutputValidator.kt
+++ src/main/kotlin/bridge/validator/OutputValidator.kt
@@ -3,9 +3,15 @@ package bridge.validator
 object OutputValidator {
 
     fun validatePrintMap(bridge: List<String>, round: Int, direction: String): String {
-        // 첫번째가 U일 때
-        if (bridge[round] == direction) return "[ O ]\n[   ]"
-        if (bridge[round] != direction) return "[   ]\n[ X ]"
+        if (bridge[round] == "U" && direction == "U") return "[ O ]\n[   ]"
+        if (bridge[round] == "U" && direction == "D") return "[   ]\n[ X ]"
+        if (bridge[round] == "D" && direction == "U") return "[ X ]\n[   ]"
+        if (bridge[round] == "D" && direction == "D") return "[   ]\n[ O ]"
+
         return ""
+    }
+
+    fun validatePrintResult(whether: Boolean, trialCount: Int): String {
+        TODO("Not yet implemented")
     }
 }
