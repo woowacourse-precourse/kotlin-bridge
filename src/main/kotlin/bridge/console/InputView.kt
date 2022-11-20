10b753aa16416954a5969c77602ec75a383eab39
joh9911 <joh9911@naver.com>
2022-11-21 03:34:11 +0900

    feat(InputView): 입력 받는 클래스 InputView 구현

--- src/main/kotlin/bridge/console/InputView.kt
+++ src/main/kotlin/bridge/console/InputView.kt
@@ -21,18 +21,20 @@ class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    fun readMoving(): String {
+    fun readMoving(): Char {
         val inputMoveLaneSymbol = readLine()
         validateMoveLaneSymbol(inputMoveLaneSymbol!!)
-        return inputMoveLaneSymbol
+        return inputMoveLaneSymbol[0]
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    fun readGameCommand(): String {
+    fun readGameCommand(): Char {
         val inputRetrialSymbol = readLine()
         validateRetrialSymbol(inputRetrialSymbol!!)
-        return inputRetrialSymbol
+        return inputRetrialSymbol[0]
     }
 }
+
+
