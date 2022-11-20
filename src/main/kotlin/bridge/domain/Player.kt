82c88e077b33a0aed5d0ade04aa61d61f6041a96
jiingom <vvankatam0@gmail.com>
2022-11-20 15:17:29 +0900

    refactor: 다리를 위쪽 다리와 아래쪽 다리로 나눠 이동할 수 있는 칸인지 저장하도록 변경

--- src/main/kotlin/bridge/domain/Player.kt
+++ src/main/kotlin/bridge/domain/Player.kt
@@ -1,28 +1,46 @@
 package bridge.domain
 
 class Player(private val bridge: Bridge) {
-    private var playerBridge = mutableListOf<String>()
+    private var upBridge = mutableListOf<String>()
+    private var downBridge = mutableListOf<String>()
     private var stage = 0
 
     fun updateStage(inputMoving: String): Boolean {
         val isClearStage = bridge.match(stage, inputMoving)
-        when(isClearStage) {
-            true -> playerBridge.add(inputMoving)
-            false -> playerBridge.add(failToClearStage(inputMoving))
+        when (isClearStage) {
+            true -> updateBridge(inputMoving, MOVE_CORRECT)
+            false -> updateBridge(inputMoving, MOVE_FAIL)
         }
         stage += 1
         return isClearStage
     }
 
-    fun isClear(): Boolean {
-        return bridge.finishCrossBridge(playerBridge.size)
+    private fun updateBridge(inputUpOrDown: String, stageResult: String) {
+        when (inputUpOrDown) {
+            MOVE_UP -> updateUpBridge(stageResult)
+            MOVE_DOWN -> updateDownBridge(stageResult)
+        }
     }
 
-    private fun failToClearStage(inputUpOrDown: String): String {
-        return inputUpOrDown + FAIL_TO_STAGE_CLEAR
+    private fun updateUpBridge(stageResult: String) {
+        upBridge.add(stageResult)
+        downBridge.add(NOT_MOVE_TO_HERE)
+    }
+
+    private fun updateDownBridge(stageResult: String) {
+        upBridge.add(NOT_MOVE_TO_HERE)
+        downBridge.add(stageResult)
+    }
+
+    fun isClear(): Boolean {
+        return bridge.finishCrossBridge(upBridge.size)
     }
 
     companion object {
-        const val FAIL_TO_STAGE_CLEAR = "F"
+        const val MOVE_UP = "U"
+        const val MOVE_DOWN = "D"
+        const val MOVE_CORRECT = "O"
+        const val MOVE_FAIL = "X"
+        const val NOT_MOVE_TO_HERE = " "
     }
 }
