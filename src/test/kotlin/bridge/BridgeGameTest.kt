5a5cb8b3f3150b10908ce04671b9e68f5a265729
Joonhoo <mnc5@naver.com>
2022-11-20 19:05:25 +0900

    test: 생성된 다리에 따라 사용자 이동 테스트 확인

---
+++ src/test/kotlin/bridge/BridgeGameTest.kt
@@ -0,0 +1,18 @@
+package bridge
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class BridgeGameTest {
+    private val bridgeGame = BridgeGame(listOf("U", "D", "U", "D"))
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 2, 3])
+    fun `사용자가 이동한 다리의 정답 여부 확인`(position: Int) {
+        assertThat(
+            bridgeGame.move("D", position)
+        ).isEqualTo(position % 2 != 0)
+    }
+
+}
