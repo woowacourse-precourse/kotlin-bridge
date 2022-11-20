24cebbeb31cdd4db4927942aa4dc5882397fdb5a
Joonhoo <mnc5@naver.com>
2022-11-20 21:28:44 +0900

    feat: 다리를 건너는 게임을 진행하는 함수 구현

--- src/main/kotlin/bridge/GameController.kt
+++ src/main/kotlin/bridge/GameController.kt
@@ -1,5 +1,6 @@
 package bridge
 
+import bridge.util.RESTART
 import bridge.util.START_GAME
 
 class GameController(
@@ -8,10 +9,38 @@ class GameController(
     private val bridgeMaker: BridgeMaker
 ) {
     private lateinit var bridgeGame: BridgeGame
+    private var position = 0
 
     fun start() {
         println(START_GAME)
         val size = inputView.readBridgeSize()
         bridgeGame = BridgeGame(bridgeMaker.makeBridge(size))
+        val result = gameProgress(size)
+        // 결과 출력
+        outputView.printResult()
+    }
+
+    // 다리 건너기 게임 진행
+    private fun gameProgress(size: Int): Boolean {
+        while (position < size) {
+            val key = inputView.readMoving()
+            val isCorrect = bridgeGame.move(key, position)
+            outputView.printMap(bridgeGame)
+            position++
+            if (isCorrect) continue
+            if (isQuit()) return false
+        }
+        return true
+    }
+
+    // 게임 종료
+    private fun isQuit(): Boolean {
+        val command = inputView.readGameCommand()
+        if (command == RESTART) {
+            position = 0
+            bridgeGame.retry()
+            return false
+        }
+        return true
     }
 }
