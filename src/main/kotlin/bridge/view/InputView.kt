10d15cc0c79cde304abafc9a9f54d237537720a3
dltkd1395 <dltkd13956@naver.com>
2022-11-20 21:11:54 +0900

    refactor(InputView) : 입력에 해당하는 출력 추가

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -6,18 +6,32 @@ import camp.nextstep.edu.missionutils.Console
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 class InputView {
+
+    private val outputView = OutputView()
     /**
      * 다리의 길이를 입력받는다.
      */
-    fun readBridgeSize(): String = Console.readLine()
+    fun readBridgeSize(): String {
+        outputView.printBridgeLength()
+        val size = Console.readLine()
+        return size
+    }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    fun readMoving(): String = Console.readLine()
+    fun readMoving(): String {
+        outputView.printBridgeDirection()
+        val direction = Console.readLine()
+        return direction
+    }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    fun readGameCommand(): String = Console.readLine()
+    fun readGameCommand(): String {
+        outputView.printGameRetry()
+        val command = Console.readLine()
+        return command
+    }
 }
