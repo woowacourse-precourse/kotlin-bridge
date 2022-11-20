1b566a0d23078ebafb3451fb67735042337b2168
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-20 15:21:17 +0900

    refactor(bridge): 중복되는 메소드 제거

--- src/main/kotlin/bridge/controller/BridgeGameController.kt
+++ src/main/kotlin/bridge/controller/BridgeGameController.kt
@@ -6,14 +6,17 @@ import bridge.domain.BridgeGameStatus
 import bridge.domain.BridgeGameStatus.FAILURE
 import bridge.domain.BridgeGameStatus.RETRY
 import bridge.domain.BridgeGameStatus.RUNNING
+import bridge.domain.BridgeMaker
+import bridge.domain.BridgeRandomNumberGenerator
 import bridge.domain.User
 import bridge.view.OutputView
 
 class BridgeGameController {
     private var status: BridgeGameStatus = RUNNING
-    private var info = BridgeGameInfo(BridgeGame().build(), emptyList(), 1)
+    private var info: BridgeGameInfo = BridgeGameInfo(emptyList(), emptyList(), 1)
 
     fun run() {
+        info.bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(User().inputBridgeSize())
         while (status == RUNNING) {
             info = BridgeGame().move(info, User().inputMoving())
             status = BridgeGameStatus.getStatus(info)
--- src/main/kotlin/bridge/domain/BridgeGame.kt
+++ src/main/kotlin/bridge/domain/BridgeGame.kt
@@ -30,10 +30,5 @@ class BridgeGame {
         }
     }
 
-    fun build(): List<String> {
-        val bridgeSize = User().inputBridgeSize()
-        return BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)
-    }
-
     private fun String.isSame(bridgeLocation: String) = this == bridgeLocation
 }
