dfdefa1323db5b29308b292f242cc2af059e271b
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-20 14:40:14 +0900

    [Feat] (BridgeGame.kt) 게임 시작, 진행 기능 추가
    start: 게임 시작
    progress: 게임 진행

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -6,15 +6,44 @@ import bridge.validator.BridgeGameValidator
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+
+    // 진행 메소드
+    fun start() {
+        progress()
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * 일치했을 때 (true) - 칸 이동 = 인덱스 증가
-     * 틀렸을 때 (false) - 칸 이동 X = 현재 인덱스
+     * 일치했을 때 (true) - 다음 라운드 반환
+     * 틀렸을 때 (false) - 현재 라운드 반환
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move(isMatched: Boolean, round: Int): Int {
-        return BridgeGameValidator.validateMoveOutput(isMatched, round)
+    fun move(isMatched: Boolean): Int {
+        return BridgeGameValidator.validateMoveOutput(isMatched)
+    }
+
+    /*
+    * 전체적인 게임 진행 과정
+    * */
+    fun progress(): Boolean {
+/*        val bridgeSize = InputView().readBridgeSize()
+        val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)*/
+        // 사용자 입력
+        val moving = InputView().readMoving()
+        // 현재 출력
+        val map = OutputView().printMap(listOf("U"), 0, moving)
+        println(map)
+        // 오답일 때
+        val isNotMatched = ("U" != moving)
+        if (isNotMatched) {
+            val gameCommand = InputView().readGameCommand()
+            if (gameCommand == "Q") {
+                OutputView().printResult(map, 0, isNotMatched)
+                return false
+            }
+        }
+        return true
     }
 
     /**
