706fcc7f9805174966a8b906266bd32666b491e6
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-20 20:43:47 +0900

    test[bridge]: 다리 이동결과 확인하는 테스트 수정

--- src/test/kotlin/bridge/BridgeTest.kt
+++ src/test/kotlin/bridge/BridgeTest.kt
@@ -29,25 +29,14 @@ class BridgeTest {
     }
 
     @Test
-    fun `이동가능한 칸이면 'O'인지 확인`() {
-        val result = "O"
-        assertThat(bridge.movePossibleMark(true)).isEqualTo(result)
-    }
-
-    @Test
-    fun `이동 불가능한 칸이면 'X'인지 확인`() {
-        val result = "X"
-        assertThat(bridge.movePossibleMark(false)).isEqualTo(result)
-    }
-
-    @Test
     fun `다리 이동 결과 확인하는 기능`() {
-        bridge.move(BridgeDirection.U, bridgeMap, 0)
-        bridge.move(BridgeDirection.D, bridgeMap, 1)
-        val upResult = "[ O |   ]"
-        val downResult = "[   | O ]"
-        assertThat(bridge.getUpBridgeResult()).isEqualTo(upResult)
-        assertThat(bridge.getDownBridgeResult()).isEqualTo(downResult)
+        var bridgeReulst = comBridge.move(listOf("D"))
+        assertThat(bridgeReulst.upBridgeResult()).isEqualTo("[   ]")
+        assertThat(bridgeReulst.downBridgeResult()).isEqualTo("[ X ]")
+
+        bridgeReulst = comBridge.move(listOf("U", "D", "U"))
+        assertThat(bridgeReulst.upBridgeResult()).isEqualTo("[ O |   | X ]")
+        assertThat(bridgeReulst.downBridgeResult()).isEqualTo("[   | O |   ]")
     }
 
 
