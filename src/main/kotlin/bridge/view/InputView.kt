b9e9ed93bf44d25b5c277274b8b73229823c207b
Sujin Kim <sujinkim919@gmail.com>
2022-11-21 01:51:38 +0900

    feat(InputView): 상수 추가

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -24,4 +24,15 @@ class InputView {
     fun readGameCommand(): String {
         return ""
     }
+
+    companion object {
+        const val MOVE_UP = "U"
+        const val MOVE_DOWN = "D"
+        const val GAME_RESTART = "R"
+        const val GAME_END = "Q"
+
+        const val INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요."
+        const val INPUT_MOVEMENT = "이동할 칸을 선택해주세요. (위: $MOVE_UP, 아래: $MOVE_DOWN)"
+        const val INPUT_RESTART = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: $GAME_RESTART, 종료: $GAME_END)"
+    }
 }
