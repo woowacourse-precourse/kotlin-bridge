e126eed27dd423db92d661f85bc5253157572943
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-20 20:18:57 +0900

    test[bridge]: 게임 성공여부를 확인하는 테스트 코드 추가

---
+++ src/test/kotlin/bridge/GameResultTest.kt
@@ -0,0 +1,38 @@
+package bridge
+
+import bridge.Result
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class GameResultTest {
+
+    val size = 3
+    lateinit var gameResult : GameResult
+
+    @BeforeEach
+    fun init() {
+         gameResult = GameResult()
+    }
+
+    @Test
+    fun `게임 실패를 확인하는 테스트`() {
+        gameResult.setGameResult(Move.POSSIBLE)
+        gameResult.setGameResult(Move.IMPOSSIBLE)
+        val result = Result.FAIL
+
+        assertThat(gameResult.checkGameSuccess(3)).isEqualTo(result)
+    }
+
+    @Test
+    fun `게임 성공을 확인하는 테스트`() {
+        gameResult.setGameResult(Move.POSSIBLE)
+        gameResult.setGameResult(Move.IMPOSSIBLE)
+        gameResult.setGameResult(Move.POSSIBLE)
+        gameResult.setGameResult(Move.POSSIBLE)
+        val result = Result.SUCCESS
+
+        assertThat(gameResult.checkGameSuccess(3)).isEqualTo(result)
+    }
+
+}
