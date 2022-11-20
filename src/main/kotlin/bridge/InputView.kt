51e548ac365aa223fe135427abad8a0a87624401
dohun_ <silver2679@gmail.com>
2022-11-20 18:18:13 +0900

    Feat(InputView): readBridgeSize 기능 추가, 예외처리 추가

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -7,10 +9,32 @@ class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    fun readBridgeSize(): Int {
-        return 0
+    fun chkNum(str: String): Boolean {
+        var temp: Char
+        var result = false
+        for (i in 0 until str.length) {
+            temp = str.elementAt(i)
+            if (temp.toInt() < 48 || temp.toInt() > 57) {
+                result = true
+            }
+        }
+        return result
     }
 
+    fun readBridgeSize(): Int {
+        println("다리의 길이를 입력해주세요.")
+        val bridgeSize = Console.readLine()
+        inputBridgeException(bridgeSize.toInt())
+        return bridgeSize.toInt()
+    }
+
+    fun inputBridgeException(bridgeLength: Int) {
+        when {
+            bridgeLength < 3 -> return throw IllegalArgumentException("[ERROR] 다리의 길이는 3보다 커야합니다.")
+            bridgeLength > 20 -> return throw IllegalArgumentException("[ERROR] 다리의 길이는 20보다 작아야합니다.")
+            chkNum(bridgeLength.toString()) -> return throw IllegalArgumentException("[ERROR] 다리의 길이는 숫자만 입력 가능합니다.")
+        }
+    }
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
