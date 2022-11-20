e3e6bd047b47e0c2cdcbb61392d214cba3c4607f
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-20 18:19:40 +0900

    test(CorrectBridgeTest) : add assertion that bridge size test

--- src/test/kotlin/bridge/CorrectBridgeTest.kt
+++ src/test/kotlin/bridge/CorrectBridgeTest.kt
@@ -1,6 +1,7 @@
 package bridge
 
 import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
 
 class CorrectBridgeTest {
     private val correctBridge = CorrectBridge
@@ -8,6 +9,6 @@ class CorrectBridgeTest {
     @Test
     fun `다리 생성시 사이즈 테스트`(){
         correctBridge.createBridge(3)
-
+        assertThat(correctBridge.correctBridge.size).isEqualTo(3)
     }
 }
