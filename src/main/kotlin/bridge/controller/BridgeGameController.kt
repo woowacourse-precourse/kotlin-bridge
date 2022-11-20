b22bffb439ec97c71f71cfe0a4b718d29a37f259
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-20 13:02:18 +0900

    feat(bridge): 게임을 시작하는 기능 구현

---
+++ src/main/kotlin/bridge/controller/BridgeGameController.kt
@@ -0,0 +1,24 @@
+package bridge.controller
+
+import bridge.domain.BridgeGame
+import bridge.domain.BridgeGameInfo
+import bridge.domain.BridgeGameStatus
+import bridge.domain.BridgeGameStatus.FAILURE
+import bridge.domain.BridgeGameStatus.RETRY
+import bridge.domain.BridgeGameStatus.RUNNING
+import bridge.domain.User
+import bridge.view.OutputView
+
+class BridgeGameController {
+    private var status: BridgeGameStatus = RUNNING
+    private var info = BridgeGameInfo(BridgeGame().build(), emptyList(), 1)
+
+    fun run() {
+        while (status == RUNNING) {
+            info = BridgeGame().move(info)
+            status = BridgeGameStatus.getStatus(info)
+            OutputView().printMap(info)
+        }
+        OutputView().printResult(info)
+    }
+}
