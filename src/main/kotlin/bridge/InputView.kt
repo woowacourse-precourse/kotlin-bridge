bbec47ead707a2241abe43675235441b2b2ce4c5
leehandsub <gustjq7027@naver.com>
2022-11-20 21:51:20 +0900

    feat(InputVIew): 다리의 길이 저장

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,14 +1,20 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
+ * InputView의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
+ * 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
  */
 class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    fun readBridgeSize(): Int {
-        return 0
+    fun readBridgeSize(): String {
+        println(BRIDGE_GAME_START_MESSAGE)
+        println(INPUT_BRIDGE_LENGTH_MESSAGE)
+        return Console.readLine()
     }
 
     /**
---
+++ src/main/kotlin/bridge/Resource.kt
@@ -0,0 +1,9 @@
+package bridge
+
+const val BRIDGE_GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다."
+const val INPUT_BRIDGE_LENGTH_MESSAGE = "다리의 길이를 입력해주세요."
+
+
+const val ERROR_NOT_NUMBER_MESSAGE = "[ERROR] 다리의 길이는 숫자로 입력해주세요."
+const val ERROR_BIG_NUMBER_MESSAGE = "[ERROR] 다리의 길이가 너무 큽니다."
+const val ERROR_RANGE_BRIDGE_SIZE_MESSAGE = "다리 길이는 3부터 20 사이의 숫자여야 합니다."
