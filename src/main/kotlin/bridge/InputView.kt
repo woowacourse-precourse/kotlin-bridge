c5139ac44645fc61dc3e19d89303950faa03a789
LHS0731 <gustjq2536@gmail.com>
2022-11-21 08:57:57 +0900

    feat: readMoving(), readGameCommand() 예외처리

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -28,17 +28,34 @@ class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
-        var step = Console.readLine()
-        return step
+//        println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
+//        var step = Console.readLine()
+//        return step
+
+        while (true){
+            println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
+            val step = Console.readLine()
+            if (!(step == "U" || step == "D"))
+                println("[ERROR]: 'U'(위) 혹은 'D'(아래) 값만 입력 가능합니다.")
+            if (step == "U" || step == "D")
+                return step
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
-        var regame = Console.readLine()
-        return regame
+//        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+//        var regame = Console.readLine()
+//        return regame
+        while (true){
+            println("이동할 칸을 선택해주세요. (재시도: R, 종료: Q)")
+            val regame = Console.readLine()
+            if (!(regame == "R" || regame == "Q"))
+                println("[ERROR]: 'R'(재시도) 혹은 'Q'(종료) 값만 입력 가능합니다.")
+            if (regame == "R" || regame == "Q")
+                return regame
+        }
     }
 }
