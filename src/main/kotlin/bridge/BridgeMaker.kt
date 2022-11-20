2ef0b2bf23fcb1657a35ec7f0bbe18d1b3656591
choHarmony <jws0183@naver.com>
2022-11-21 01:28:49 +0900

    refactor: 안 쓰는 변수 지우기

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,8 +9,6 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        val row = size
-        val column = 2
 
         // 0, 1로 이루어진 답지 만들기
         val answer = mutableListOf<String>()
