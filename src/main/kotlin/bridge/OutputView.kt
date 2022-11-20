8ee9f33af90b2a93d68be08c9b3f0c79d8fcf1a7
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-21 05:03:56 +0900

    refactor(OutputView) : map을 입력받으면 단순히 출력만 해주는 형식으로 바꾸기 위해 메소드들 삭제

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -9,12 +9,8 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap(playerPath: List<String>, isPlayerAlive: Boolean) {
-        val upBridgePath = playerPath.pathToMap("U", isPlayerAlive)
-        val downBridgePath = playerPath.pathToMap("D", isPlayerAlive)
-
-        println(upBridgePath)
-        println(downBridgePath)
+    fun printMap(bridgeMap:String) {
+        println(bridgeMap)
     }
 
     /**
@@ -24,33 +20,4 @@ class OutputView {
      */
     fun printResult() {}
 
-    /**
-     * @param 위 방향의 다리 경로를 위한 출력인지 아래 방향의 다리 경로를 위한 출력인지
-     */
-    fun List<String>.pathToMap(direction: String, isPlayerAlive: Boolean): String {
-        val path = StringBuilder().append("[")
-
-        for (turn in 0 until this.size - 1) {
-            when (this[turn]) {
-                direction -> path.append(" O |")
-                else -> path.append("   |")
-            }
-        }
-        if (this.last() == direction) {
-            if (isPlayerAlive) {
-                path.append(" O ]")
-            } else {
-                path.append(" X ]")
-            }
-        } else {
-            path.append("   ]")
-        }
-        return path.toString()
-    }
-
-    fun String.directionToInt() = if (this == "D") {
-        0
-    } else {
-        1
-    }
 }
