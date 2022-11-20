17553e5a5e6df953ab49b66dee930de6b99960c8
Sujin Kim <sujinkim919@gmail.com>
2022-11-21 02:38:52 +0900

    feat: 오류 상수 추가

---
+++ src/main/kotlin/bridge/domain/Const.kt
@@ -0,0 +1,6 @@
+package bridge.domain
+
+const val ERROR_FORMAT = "[ERROR] %s"
+const val ERROR_NOT_INTEGER = "숫자를 입력해야 합니다."
+const val ERROR_RANGE = "범위를 벗어난 값입니다."
+const val ERROR_OPTION = "잘못된 옵션을 입력했습니다."
---
+++ src/main/kotlin/bridge/domain/ErrorType.kt
@@ -0,0 +1,13 @@
+package bridge.domain
+
+enum class ErrorType(private val message: String) {
+    NOT_INTEGER(ERROR_FORMAT.format(ERROR_NOT_INTEGER)),
+    OUT_OF_RANGE(ERROR_FORMAT.format(ERROR_RANGE)),
+    INVALID_OPTION(ERROR_FORMAT.format(ERROR_OPTION));
+
+    companion object {
+        fun makeError(error: ErrorType) {
+            throw IllegalArgumentException(error.message)
+        }
+    }
+}
