5247d1332e47987512de35b14e32c4abb30daafc
윤동현 <donghyunyoon81@gmail.com>
2022-11-20 12:54:19 +0900

    refactor<BridgeGame> : getColumnsResult를 원래 기능으로 만들고 move 기능을 추가
    무엇을: getColumnsResult와 move를
    어떻게 : 다리의 위 칸 아래 칸을 사용자에게 입력받은 값과 비교하여 getColumnResult를 호출하도록 getColumnsResult를 다시 만들었습니다.
    다리의 결과를 모두 반환할 때까지 반복하는 for 문을 사용하여 각 다리의 결과를 사용자 텍스트와 비교하여 결과를 담아 print 하는 move 기능을 만들었습니다.
    왜 : print를 outputView에서 구현 하려 했으나 outputView에 게임을 진행할 때 다른 print 내용도 담게 하여야 코드를 보기도 정리하기도 좋기 때문입니다.

--- src/main/kotlin/bridge/domain/usecase/BridgeGame.kt
+++ src/main/kotlin/bridge/domain/usecase/BridgeGame.kt
@@ -13,21 +13,37 @@ class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     // val p = listOf<String>("U1,D0", "U0,D1", "U0,D1", "U0,D1")
-    fun move(bridgeColumns: String,userInputColumn:String):String {
+    fun move(bridge:List<String>) {
+        var p =""
+        var c = ""
+        var count =1
+        var line = ""
+        for(i in bridge) {
+            val a = Console.readLine()
+            if (count > 1) line = " | "
+            p += "$line${getColumnsResult(i, a).split(",").first()}"
+            c += "$line${getColumnsResult(i, a).split(",").last()}"
+
+            println("[ $p ]")
+            println("[ $c ]")
+            count++
+        }
+    }
+
+    private fun isPassable(passableNumber: Int): String {
+        return if (passableNumber == RANDOM_UPPER_INCLUSIVE) "O" else "X"
+    }
+
+    private fun getColumnResult(bridgeColumn: String, column: Char) =
+        if (bridgeColumn.first() == column) isPassable(bridgeColumn.last().digitToInt())
+        else " "
+
+    private fun getColumnsResult(bridgeColumns: String,userInputColumn:String):String {
         bridgeColumns.split(",").map { bridgeColumn ->
             if (bridgeColumn.first() == userInputColumn.first()) return "${getColumnResult(bridgeColumn,ONE_COLUMN_UP)},${getColumnResult(bridgeColumn,ONE_COLUMN_DOWN)}"
         }
         return " "
     }
-
-    private fun isPassable(passableNumber: Int): String {
-        return if (passableNumber == RANDOM_UPPER_INCLUSIVE) " O " else " X "
-    }
-
-    private fun getColumnResult(bridgeColumn: String, column: Char) =
-        if (bridgeColumn.first() == column) isPassable(bridgeColumn.last().digitToInt())
-        else "   "
-
 
 
 
