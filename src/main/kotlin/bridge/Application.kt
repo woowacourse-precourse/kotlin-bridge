f8451436f18dff5c637cb0cd8babf1ca1c21d0fe
DormunDoorlock <skrudgh2001@naver.com>
2022-11-21 01:04:48 +0900

    feat: 전체 기능 통합

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -3,4 +3,6 @@ package bridge
 fun main() {
     OutputView().printGameStartMessage()
     val size = InputView().readBridgeSize()
+    val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(size)
+    BridgeGame(bridge).move(0,1)
 }
