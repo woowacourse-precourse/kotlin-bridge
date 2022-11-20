1e82807e5c25e3eabc13230e067bc32a34cb9728
yeongjin <dudwls901@naver.com>
2022-11-20 20:44:20 +0900

    [Feat] 다리 만들기 구현

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -1,5 +1,9 @@
 package bridge
 
+import bridge.BridgeGame.Companion.MOVING_DIRECT_DOWN
+import bridge.BridgeGame.Companion.MOVING_DIRECT_UP
+import bridge.BridgeRandomNumberGenerator.Companion.RANDOM_LOWER_INCLUSIVE
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -9,6 +13,15 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        return List(size) {
+            parseToBridgeDirect(
+                bridgeNumberGenerator.generate()
+            )
+        }
+    }
+
+    private fun parseToBridgeDirect(num: Int): String = when (num) {
+        RANDOM_LOWER_INCLUSIVE -> MOVING_DIRECT_UP
+        else -> MOVING_DIRECT_DOWN
     }
 }
---
+++ src/test/kotlin/bridge/BridgeMakerTest.kt
@@ -0,0 +1,27 @@
+package bridge
+
+import bridge.BridgeGame.Companion.MOVING_DIRECT_DOWN
+import bridge.BridgeGame.Companion.MOVING_DIRECT_UP
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class BridgeMakerTest {
+
+    private val bridgeMaker = BridgeMaker(
+        bridgeNumberGenerator = BridgeRandomNumberGenerator()
+    )
+
+    @Test
+    fun `입력한 길이 만큼의 다리 만들기`() {
+        assert(bridgeMaker.makeBridge(3).size == 3)
+    }
+
+    @Test
+    fun `U와 D로 이루어진 다리 만들기`() {
+        bridgeMaker.makeBridge(100).forEach {
+            assertThat(
+                it == MOVING_DIRECT_UP || it == MOVING_DIRECT_DOWN
+            )
+        }
+    }
+}
