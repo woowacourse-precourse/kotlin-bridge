ff306902e8d82018299aacbcfdcac62e675871a4
phs <gustjd0808@pusan.ac.kr>
2022-11-20 22:21:11 +0900

    feat(sentence): add sentence for final result

--- src/main/kotlin/bridge/sentence/OutputSentence.kt
+++ src/main/kotlin/bridge/sentence/OutputSentence.kt
@@ -1,6 +1,7 @@
 package bridge.sentence
 
 enum class OutputSentence(private val sentence: String) {
+    RESULT("최종 게임 결과"),
     START("다리 건너기 게임을 시작합니다."),
     CHECK_SUCCESS("게임 성공 여부: "),
     TOTAL_TRIAL("총 시도한 횟수: ");
