96bc92f521293f3291c13d799cbe5ae130ae914c
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-20 18:40:30 +0900

    refactor[bridge]: 상수 생성

--- src/main/kotlin/bridge/BridgeResult.kt
+++ src/main/kotlin/bridge/BridgeResult.kt
@@ -1,14 +1,14 @@
 package bridge
 
-class BridgeResult(private val size : Int) {
+class BridgeResult(private val size: Int) {
 
     private val upBridge = mutableListOf<String>()
     private val downBridge = mutableListOf<String>()
     private val gameResult = GameResult()
-    private lateinit var movePossible : Move
+    private lateinit var movePossible: Move
 
-    fun setMoveResult(input : BridgeDirection, movePossible : Move) {
-        when(input) {
+    fun setMoveResult(input: BridgeDirection, movePossible: Move) {
+        when (input) {
             BridgeDirection.U -> moveUp(movePossible.getMark())
             BridgeDirection.D -> moveDown(movePossible.getMark())
         }
@@ -16,9 +16,9 @@ class BridgeResult(private val size : Int) {
         this.movePossible = movePossible
     }
 
-    fun getMovePossible() : Move = movePossible
+    fun getMovePossible(): Move = movePossible
 
-    fun moveUp(mark : String) {
+    fun moveUp(mark: String) {
         upBridge.add(mark)
         downBridge.add(" ")
     }
@@ -28,15 +28,23 @@ class BridgeResult(private val size : Int) {
         upBridge.add(" ")
     }
 
-    fun upBridgeResult() : String {
-        return upBridge.joinToString(prefix = "[ ", separator = " | ", postfix = " ]")
+    fun upBridgeResult(): String {
+        return upBridge.joinToString(
+            prefix = BRIDGE_PREFIX, separator = BRIDGE_SEPARATOR, postfix = BRIDGE_POSTFIX
+        )
     }
 
-    fun downBridgeResult() : String {
-        return downBridge.joinToString(prefix = "[ ", separator = " | ", postfix = " ]")
+    fun downBridgeResult(): String {
+        return downBridge.joinToString(prefix = BRIDGE_PREFIX, separator = BRIDGE_SEPARATOR, postfix = BRIDGE_POSTFIX)
     }
 
-    fun getGameResult() : Result {
+    fun getGameResult(): Result {
         return gameResult.checkGameSuccess(size)
+    }
+
+    companion object {
+        const val BRIDGE_PREFIX = "[ "
+        const val BRIDGE_SEPARATOR = " | "
+        const val BRIDGE_POSTFIX = " ]"
     }
 }
