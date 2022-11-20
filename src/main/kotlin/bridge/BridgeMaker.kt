e51dfacce4d7ba252a496cfeaa715b71e9ad01a8
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-20 14:38:23 +0900

    [Fix] (BridgeMaker.kt) 올바르지 않은 U, D 반환 수정

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,16 +9,19 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
     /**
      * @param size   다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+     * TODO: else 없애야 하는데 ?
      */
     fun makeBridge(size: Int): List<String> {
-        val bridgeNumberList = MutableList(size) { index -> bridgeNumberGenerator.generate() }
-        val bridge = bridgeNumberList.map { index ->
-            val bridgeNumber = bridgeNumberList[index].toString()
-            when (bridgeNumberList[index]) {
-                UP_INT_NUMBER_ONE -> bridgeNumber.replace(UP_STRING_NUMBER_ONE, UP_SIDE_EXPRESSION)
-                else -> bridgeNumber.replace(DOWN_STRING_NUMBER_ZERO, DOWN_SIDE_EXPRESSION)
+        val bridgeNumberList = List(size) { index -> bridgeNumberGenerator.generate() }
+        println(bridgeNumberList)
+        val bridge =bridgeNumberList.map { number ->
+            val bridgeNumber = number.toString()
+            when (number) {
+                UP_INT_NUMBER_ONE -> bridgeNumber.replace(UP_STRING_NUMBER_ONE, UP_DIRECTION)
+                else -> bridgeNumber.replace(DOWN_STRING_NUMBER_ZERO, DOWN_DIRECTION)
             }
         }
+        println(bridge)
         return bridge
     }
 }
