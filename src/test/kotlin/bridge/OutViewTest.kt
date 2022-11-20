f1490911684f358efb4f1a77c5ac6e3b752151d7
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-20 10:07:42 +0900

    [Refactor] (OutViewTest.kt) 첫번째 라운드 중복 테스트 제거
    MethodSource 활용한 테스트

--- src/test/kotlin/bridge/OutViewTest.kt
+++ src/test/kotlin/bridge/OutViewTest.kt
@@ -2,18 +2,39 @@ package bridge
 
 import bridge.validator.OutputValidator
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
 
 class OutViewTest {
-    private val bridge = listOf("U","D","D")
+
+    companion object {
+        @JvmStatic
+        fun bridge() = listOf(
+            Arguments.of("[ O ]", "[   ]", listOf("U", "D", "D"), 0, "U"),
+            Arguments.of("[   ]", "[ O ]", listOf("D", "D", "D"), 0, "D"),
+        )
+
+    }
 
     @ParameterizedTest
-    @CsvSource(
-        "[ O ],[   ], 0, U",
-        "[   ],[ X ], 0, D"
-    )
-    fun `첫번째 라운드 올바른 출력 테스트`(upPrint: String, downPrint: String, round: Int, direction: String) {
+    @MethodSource("bridge")
+    fun `첫번째 라운드 올바른 출력 테스트`(
+        upPrint: String,
+        downPrint: String,
+        bridge: List<String>,
+        round: Int,
+        direction: String,
+    ) {
         assertThat(upPrint + "\n" + downPrint).isEqualTo(OutputValidator.validatePrintMap(bridge, round, direction))
+    }
+
+    @Test
+    fun `첫번째 라운드 오답일 때 최종 게임 결과 출력 테스트`() {
+        assertThat("최종 게임 결과" + "\n" + "[   ]" + "\n" + "[ X ]" + "\n" + "게임 성공 여부: 실패" + "\n" + "총 시도한 횟수: 1"
+        ).isEqualTo(OutputValidator.validatePrintResult(false, 1))
+
     }
 }
