0ccb320646d4ab464a1318ec18db5f4693793aeb
Gloria <ippnsj@gmail.com>
2022-11-20 20:48:31 +0900

    feat: 이동할 칸이 U 또는 D 인지 확인하는 기능 구현

--- src/main/kotlin/bridge/utils/MovementValidator.kt
+++ src/main/kotlin/bridge/utils/MovementValidator.kt
@@ -1,5 +1,13 @@
 package bridge.utils
 
 class MovementValidator : Validator {
-    override fun validate(input: String) {}
+    private val options = listOf("U", "D")
+
+    override fun validate(input: String) {
+        require(options.contains(input)) { ERROR_MESSAGE }
+    }
+
+    companion object {
+        const val ERROR_MESSAGE = "이동할 칸은 U 또는 D 여야 합니다."
+    }
 }
---
+++ src/test/kotlin/bridge/utils/MovementValidatorTest.kt
@@ -0,0 +1,31 @@
+package bridge.utils
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.lang.IllegalArgumentException
+
+class MovementValidatorTest {
+    private lateinit var validator: MovementValidator
+
+    @BeforeEach
+    private fun setUp() {
+        validator = MovementValidator()
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["U", "D"])
+    fun `U 또는 D 이면 예외가 발생하지 않는다`(input: String) {
+        assertDoesNotThrow { validator.validate(input) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["u", "d", "a", "", " "])
+    fun `U 또는 D 가 아니면 예외가 발생한다`(input: String) {
+        val exception = assertThrows<IllegalArgumentException> { validator.validate(input) }
+        assertThat(exception.message).isEqualTo(MovementValidator.ERROR_MESSAGE)
+    }
+}
