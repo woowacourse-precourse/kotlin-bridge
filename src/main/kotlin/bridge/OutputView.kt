b6f73fefe72079e5928d307cc27f29fa648b5c0a
LHS0731 <gustjq2536@gmail.com>
2022-11-20 19:43:18 +0900

    feat: OutputView클래스 printResult()함수 구현

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -27,7 +27,15 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+    fun printResult(answer: Boolean, caseList: List<Int>, tryCount: Int) {
+        println("최종 게임 결과")
+        printMap(caseList)
+        if (answer)
+            println("게임 성공 여부: 성공")
+        if (!answer)
+            println("게임 성공 여부: 실패")
+        println("총 시도한 횟수: $tryCount")
+    }
 
 
     fun correctAnswer(case: Int) {
