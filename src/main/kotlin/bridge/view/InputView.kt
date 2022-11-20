27e9ad7f12d625e83a6053263ff4f9acf19c8b5e
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-20 23:35:19 +0900

    feat: add exception check in readBridgeSize function Add exception check code to check exception about bridge size

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -1,22 +1,18 @@
 package bridge.view
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import bridge.utils.ExceptionChecker
+
 class InputView {
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+    private val exceptionChecker = ExceptionChecker()
+
     fun readBridgeSize(): Int {
         println("다리의 길이를 입력해주세요.")
-        val bridgeSize = readLine()!!.toInt()
-        //todo: 예외처리 필요
-        return bridgeSize
+        val bridgeSize = readLine()!!
+        exceptionChecker.bridgeSizeIsDigitException(bridgeSize)
+        exceptionChecker.bridgeSizeIsInRangeException(bridgeSize.toInt())
+        return bridgeSize.toInt()
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     fun readMoving(): String {
         println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
         val userMove = readLine()!!
@@ -24,9 +20,6 @@ class InputView {
         return userMove
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     fun readGameCommand(): String {
         println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
         val retry = readLine()!!
