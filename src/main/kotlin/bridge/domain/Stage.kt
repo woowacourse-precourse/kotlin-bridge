752ead00774d46cbd76dfc9ae131cab42b236ff3
jiingom <vvankatam0@gmail.com>
2022-11-21 00:24:40 +0900

    feat: 스테이지 진행 결과를 열거형 객체 Stage로 반환하는 기능 구현

--- src/main/kotlin/bridge/domain/Stage.kt
+++ src/main/kotlin/bridge/domain/Stage.kt
@@ -5,4 +5,11 @@ enum class Stage(val upOrDown: String, val isClearStage: String) {
     DOWN("D", "O"),
     UP_FAIL("U","X"),
     DOWN_FAIL("D", "X");
+
+    companion object {
+        fun getStageInform(moving: String, isClearStage: String): Stage {
+            return Stage.values().filter { stage -> stage.upOrDown == moving }
+                .first() { stage -> isClearStage == stage.isClearStage }
+        }
+    }
 }
