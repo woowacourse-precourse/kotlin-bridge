1e9b8a5da73a3d87862e5c7a2e0bb2ea96ae409c
choHarmony <jws0183@naver.com>
2022-11-20 16:09:06 +0900

    feat: 다리의 길이를 입력받는다

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,7 @@
 package bridge
 
 fun main() {
-    // TODO: 프로그램 구현
+    println("다리 건너기 게임을 시작합니다.")
+
+
 }
--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,7 +10,10 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        println("다리의 길이를 입력해주세요.")
+        val bridgeSize: Int = Console.readLine().toInt()
+
+        return bridgeSize
     }
 
     /**
