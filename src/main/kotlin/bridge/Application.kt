a02e10a91467f9922d27080a32832e35ce32141a
LHS0731 <gustjq2536@gmail.com>
2022-11-20 19:10:55 +0900

    feat: main()함수 클래스 기능들 종합 구현

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -6,10 +6,26 @@ fun main() {
     val bridgeMade = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)
 
     var caseList = mutableListOf<Int>()
-    for (i in bridgeMade.indices) {
-        val case = BridgeGame().move(bridgeMade[i], InputView().readMoving())
-        caseList.add(case)
-        OutputView().printMap(caseList)
-    }
+    var answer = true
+    var regame = ""
+    while(true){
+        for (i in bridgeMade.indices) {
+            val case = BridgeGame().move(bridgeMade[i], InputView().readMoving())
+            caseList.add(case)
+            OutputView().printMap(caseList)
+            if (case == 3 || case == 4) {
+                answer = false
+                break
 
+            }
+            if (i == bridgeMade.size - 1){
+                answer = true
+                break
+            }
+        }
+        if (!answer) {
+            regame = InputView().readGameCommand()
+            println(regame)
+        }
+    }
 }
