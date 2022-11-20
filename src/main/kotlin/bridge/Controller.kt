9d81a9322afc0afb2dc3805e16e9fd0cfafa3bf6
mendel <mendel.urssu@gmail.com>
2022-11-20 22:43:41 +0900

    feat: 게임의 입력을 받아서 BridgeGame에게 주입해주는 컨트롤로 추가

---
+++ src/main/kotlin/bridge/Controller.kt
@@ -0,0 +1,84 @@
+package bridge
+
+import java.lang.IllegalArgumentException
+
+class Controller() {
+    private lateinit var bridgeGame: BridgeGame
+    private val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
+    private val inputView = InputView()
+    private val outputView = OutputView()
+    private val inputInvalidCheck = InputInvalidCheck()
+    private val inputConverter = InputConverter()
+
+    fun run() {
+        outputView.gameStartMessage()
+        inputGameBridgeSizeStep()
+        while (inputMoveStep()) {
+        }
+        endGame()
+    }
+
+    private fun inputGameBridgeSizeStep() {
+        while (true) {
+            try {
+                outputView.bridgeLengthInputPleaseMessage()
+                val bridgeSize = inputView.readBridgeSize(inputInvalidCheck, inputConverter)
+                val random = bridgeMaker.makeBridge(bridgeSize)
+                //println("정답: " + random + "\n")
+                bridgeGame = BridgeGame(random)
+                break
+            } catch (e: IllegalArgumentException) {
+                OutputView.errorMessage() // 다리 개수 입력 시 에러 처리
+            }
+        }
+    }
+
+    private fun inputMoveStep(): Boolean { // 이동 입력 그만 받고 종료하고 싶으면 false를 반환해야 함.
+        var curMoveResult: BridgeGame.BridgeMoveResult
+        try {
+            outputView.moveInputPleaseMessage()
+            curMoveResult = bridgeGame.move(inputView.readMoving(inputInvalidCheck))
+            outputView.printMap(bridgeGame.realBridges(), bridgeGame.gameBridges(), bridgeGame.curStep())
+        } catch (e: IllegalArgumentException) {
+            OutputView.errorMessage() // 이동 입력 시 에러 처리
+            return true
+        }
+        return moveResultStep(curMoveResult)
+    }
+
+    private fun moveResultStep(curMoveResult: BridgeGame.BridgeMoveResult): Boolean { // 이동 입력 그만 받고 싶으면 false 리턴.
+        return when (curMoveResult) {
+            BridgeGame.BridgeMoveResult.MOVE_FAILED -> moveFailedStep()
+            BridgeGame.BridgeMoveResult.MOVE_SUCCESS -> true
+            else -> false
+        }
+    }
+
+    private fun moveFailedStep(): Boolean {
+        while (true) {
+            try {
+                return inputRetryStep()
+            } catch (e: IllegalArgumentException) {
+                OutputView.errorMessage()
+            }
+        }
+    }
+
+    private fun inputRetryStep(): Boolean {
+        outputView.retryInputPleaseMessage()
+        if (inputView.readGameCommand(inputInvalidCheck) == "R") {
+            bridgeGame.retry()
+            return true
+        }
+        return false
+    }
+
+    private fun endGame() {
+        outputView.finalGameResult()
+        outputView.printMap(bridgeGame.realBridges(),
+            bridgeGame.gameBridges(),
+            bridgeGame.curStep())
+        outputView.printResult(bridgeGame.isGameSuccess(), bridgeGame.tryCount())
+    }
+
+}
