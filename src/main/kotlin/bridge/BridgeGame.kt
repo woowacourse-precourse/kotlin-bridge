007c2a7e5fbf23cfe00083b9c7870efc00bf8120
choHarmony <jws0183@naver.com>
2022-11-21 03:20:05 +0900

    feat: 사용자가 이동할 때 사용하는 함수 기능 추가

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -10,7 +10,24 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    var upBridge = "["
+    var downBridge = "["
+    fun move(bridgeAns: String, userMovingInput: String) {
+        if (bridgeAns == "U" && userMovingInput == "U") {
+            upBridge += "O"
+            upBridge += "|"
+        }
+        else if (bridgeAns == "D" &&userMovingInput == "D") {
+            downBridge += "O"
+            downBridge += "|"
+        }
+        else if (bridgeAns == "U" && userMovingInput == "D"){
+            downBridge += "X"
+        }
+        else {
+            upBridge += "X"
+        }
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
