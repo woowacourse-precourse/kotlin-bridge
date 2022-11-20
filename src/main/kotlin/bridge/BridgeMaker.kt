ee08f12243b455e1a0fdb3b12add9818fd3cf993
otter <a33397397@gmail.com>
2022-11-20 15:42:46 +0900

    fix: 다리 만들기 기능이 작동하지 않는 문제 수정 - map을 사용해 반환된 값을 저장하지 않아 발생한 문제 해결

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,10 +9,10 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        val bridge: List<String> = List(size) { "" }
-
-        bridge.map {
-            if (bridgeNumberGenerator.generate() == RANDOM_LOWER_INCLUSIVE) {
+        val bridge: List<String> = List(size) { "" }.map {
+            val bridgeRandomNumberGenerator = BridgeRandomNumberGenerator()
+            val randomNumber = bridgeRandomNumberGenerator.generate()
+            if (randomNumber == RANDOM_LOWER_INCLUSIVE) {
                 "D"
             } else {
                 "U"
