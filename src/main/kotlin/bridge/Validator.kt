f14c5cb763fb626e68df19e36bc4d96de7dc01c3
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-11-21 20:18:03 +0900

    fix: 사용자가 잘못된 값을 입력할 경우 오류 수정

--- src/main/kotlin/bridge/Validator.kt
+++ src/main/kotlin/bridge/Validator.kt
@@ -1,44 +1,70 @@
 package bridge
 
+import kotlin.IllegalArgumentException as IllegalArgumentException1
+
 class Validator {
     fun validateBridgeSize(bridgeSize: String?): Int {
-        require(validatorNumeric(bridgeSize)){
+        try{ validatorNumeric(bridgeSize)
+        }catch (e: IllegalArgumentException){
             println(ERROR_BRIDGE_SIZE_STRING)
-            InputView().readBridgeSize()
-        }
+            return InputView().readBridgeSize() }
+
         val bridgeLength= bridgeSize!!.toInt()
-        require(bridgeLength in 3..20){
+        try{ validatorRange(bridgeLength)
+        }catch (e: IllegalArgumentException){
             println(ERROR_BRIDGE_SIZE)
-            InputView().readBridgeSize()
-        }
+            return InputView().readBridgeSize() }
         return bridgeLength
     }
 
     fun validatorMove(move: String?): String {
         val moving = move.toString()
-        require(moving == "U" || moving == "D"){
+        try{
+            validatorUpDown(moving)
+        }catch (e: IllegalArgumentException){
             println(ERROR_MOVING)
-            InputView().readMoving()
+            return InputView().readMoving()
         }
         return moving
     }
 
     fun validatorGameCommand(command: String?): String {
         val gameCommand = command.toString()
-        require(gameCommand == "R" || gameCommand == "Q"){
+        try{
+            validatorResetQuit(gameCommand)
+        }catch (e: IllegalArgumentException){
             println(ERROR_GAME_COMMAND)
-            InputView().readGameCommand()
+            return InputView().readGameCommand()
         }
         return gameCommand
-
     }
 
-    private fun validatorNumeric(input: String?): Boolean {
-        return try{
-            input!!.toInt()
-            true
+    private fun validatorNumeric(input: String?): Int {
+        try{
+            return input!!.toInt()
         }catch (e: IllegalArgumentException){
-            false
+            throw e
         }
+    }
+
+    private fun validatorRange(input: Int): Boolean {
+        if(input in 3..20){
+            return true
+        }
+        throw IllegalArgumentException()
+    }
+
+    private fun validatorUpDown(input: String): Boolean {
+        if(input == "U" || input == "D"){
+            return true
+        }
+        throw IllegalArgumentException()
+    }
+
+    private fun validatorResetQuit(input: String): Boolean {
+        if(input == "R" || input == "Q"){
+            return true
+        }
+        throw IllegalArgumentException()
     }
 }
