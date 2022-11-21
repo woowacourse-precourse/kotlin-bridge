11715d8c05ee6e9662691d7f4b5b05f7dae76283
[alsswl] <[alswl4223@naver.com]>
2022-11-21 15:41:22 +0900

    refactor(BridgeMarker) : 줄띄움 수정

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,7 +9,6 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-
         var bridge = mutableListOf<String>()
         var randomNum = 1
        for(i in 0 until size) {
