62a4e2551cc5191ed5a9c0393e5c84e0353acfeb
jklom0326 <jklom0326@gmail.com>
2022-11-22 07:59:31 +0900

    refactor(OutputView.kt): 문자열 수정

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -12,8 +12,12 @@ class OutputView {
     fun printMap(bridgeGame: BridgeGame) {
         val upperBridge = bridgeGame.upperBridge.toList()
         val downBridge = bridgeGame.downBridge.toList()
-        println("[ ${upperBridge.joinToString(" | ")} ]")
-        println("[ ${downBridge.joinToString(" | ")} ]")
+        printBridge(upperBridge)
+        printBridge(downBridge)
+    }
+
+    private fun printBridge(bridge: List<String>) {
+        println("$FRONT_BRACKET ${bridge.joinToString(SEPARATOR)} $LAST_BRACKET")
     }
 
     /**
@@ -22,12 +26,27 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printResult(bridgeGame: BridgeGame, isSuccess: Boolean, tryCounter: Int) {
-        println("최종 게임 결과")
+        printGameResultMessage()
         this.printMap(bridgeGame)
         when (isSuccess) {
-            true -> println("게임 성공 여부: 성공")
-            false -> println("게임 성공 여부: 실패")
+            true -> printSuccessGameMessage()
+            false -> printFailureGameMessage()
         }
-        println("총 시도한 횟수: $tryCounter")
+        printTryCountMessage(tryCounter)
+    }
+    
+    private fun printGameResultMessage() = println(GAME_RESULT_MESSAGE)
+    private fun printSuccessGameMessage() = println(SUCCESS_GAME_MESSAGE)
+    private fun printFailureGameMessage() = println(FAILURE_GAME_MESSAGE)
+    private fun printTryCountMessage(tryCounter: Int) = println("$TRY_COUNT_MESSAGE$tryCounter")
+
+    companion object {
+        const val FRONT_BRACKET = "["
+        const val LAST_BRACKET = "]"
+        const val SEPARATOR = " | "
+        const val GAME_RESULT_MESSAGE = "최종 게임 결과"
+        const val SUCCESS_GAME_MESSAGE = "게임 성공 여부: 성공"
+        const val FAILURE_GAME_MESSAGE = "게임 성공 여부: 실패"
+        const val TRY_COUNT_MESSAGE = "총 시도한 횟수: "
     }
 }
