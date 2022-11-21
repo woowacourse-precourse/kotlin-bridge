18144864600bedfec5834d700a6a9af98083f800
Sujin Kim <sujinkim919@gmail.com>
2022-11-21 18:00:37 +0900

    feat(Application, BridgeGame): 게임 실행 구현

--- src/main/kotlin/bridge/domain/BridgeGame.kt
+++ src/main/kotlin/bridge/domain/BridgeGame.kt
@@ -1,9 +1,14 @@
 package bridge.domain
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
-class BridgeGame {
+class BridgeGame(
+    private val gameManager: BridgeGameManager,
+    private val bridgeMaker: BridgeMaker
+) {
+    fun runGame() {
+        gameManager.startGame()
+        val bridgeSize = gameManager.getBridgeSize()
+        val bridge = bridgeMaker.makeBridge(bridgeSize)
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
