90148d3db8cd8903c4b8a6847874a46fba304c51
Sujin Kim <sujinkim919@gmail.com>
2022-11-21 15:19:25 +0900

    feat(OutputView): 상수 추가

--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -17,4 +17,14 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printResult() {}
+
+    companion object {
+        const val MAP_START = "[ "
+        const val MAP_MIDDLE = " | "
+        const val MAP_END = " ]"
+
+        const val GAME_RESULT = "최종 게임 결과"
+        const val GAME_SUCCESS = "게임 성공 여부: "
+        const val GAME_TRY = "총 시도한 횟수: "
+    }
 }
