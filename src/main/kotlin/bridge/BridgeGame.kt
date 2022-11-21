11155180c9f59ade64e82b46cdb361f8ca590141
Hyemin Song <hyemdooly@gmail.com>
2022-11-21 13:40:44 +0900

    feat (BridgeGame) : move, retry 구현

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -3,14 +3,20 @@ package bridge
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
+class BridgeGame(val bridge: List<String>) {
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    var route = mutableListOf<String>()
+    var tryNumber = 1
+
+    fun move(direction: String): Boolean {
+        route.add(direction)
+        return route[route.lastIndex] == bridge[route.lastIndex]
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -18,5 +24,8 @@ class BridgeGame {
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry() {}
+    fun retry() {
+        route.clear()
+        tryNumber++
+    }
 }
