1ba8fe8c12e1beada10002df9459a8132a781ce1
Hyeongseon Jeon <hsun5163@gmail.com>
2022-11-21 21:25:27 +0900

    feat : addUserBridge 함수 구현
    움직일 칸을 입력하면 userBridge에 칸 추가

--- src/main/kotlin/bridge/Bridge.kt
+++ src/main/kotlin/bridge/Bridge.kt
@@ -2,4 +2,8 @@ package bridge
 
 class Bridge (size : Int = 0) {
     val bridge : MutableList<String> = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(size).toMutableList()
+
+    fun addUserBridge(movingKey : String) {
+        bridge.add(movingKey)
+    }
 }
