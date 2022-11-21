e30bf94ea8a5d2780027b2939fece1c8eaea932b
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-21 14:09:43 +0900

    refactor(controller): main 메서드에서 사용하는 변수들 전역 변수로 변경
    inputView, bridgeMaker 같은 설정해야 하는 변수들을 전역 변수로 변경하고 config 메서드를
    통해 초기화하도록 변경
    애플리케이션의 로직과는 크게 상관없기 때문

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -3,11 +3,19 @@ package bridge
 import bridge.model.BridgeGame
 import bridge.view.InputView
 
+private lateinit var inputView: InputView
+private lateinit var bridgeMaker: BridgeMaker
+private lateinit var bridgeNumberGenerator: BridgeNumberGenerator
+
 fun main() {
-    val inputView = InputView()
     val bridgeSize = inputView.askBridgeSizeToUser()
-    val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
     val bridge = bridgeMaker.makeBridge(bridgeSize)
     val bridgeGame = BridgeGame(bridge)
     val moving = inputView.askMovingToUser()
+}
+
+private fun config() {
+    inputView = InputView()
+    bridgeNumberGenerator = BridgeRandomNumberGenerator()
+    bridgeMaker = BridgeMaker(bridgeNumberGenerator)
 }
