222491d4cd02f2916ca3f2edfb24cf2a7e32d770
phs <gustjd0808@pusan.ac.kr>
2022-11-21 14:10:42 +0900

    feat(domain): add condition in method that check movable bridge block

--- src/main/kotlin/bridge/domain/Bridge.kt
+++ src/main/kotlin/bridge/domain/Bridge.kt
@@ -4,7 +4,8 @@ import bridge.sentence.ErrorSentence
 
 class Bridge(private val blocks: List<String>) {
     fun isMovable(index: Int, direction: Direction): Boolean {
-        return blocks[index] == direction.hotKey()
+        return index < blocks.size &&
+                blocks[index] == direction.hotKey()
     }
 
     fun size() = blocks.size
