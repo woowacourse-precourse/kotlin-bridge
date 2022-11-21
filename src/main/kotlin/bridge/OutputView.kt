9ae9e88a5ccd89afa02cbef9a7d52016c73fd720
jklom0326 <jklom0326@gmail.com>
2022-11-22 07:39:23 +0900

    feat(OutputView.kt): 게임 결과 출력 함수 구현
    - printResult()로 게임 결과 출력

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -21,5 +21,13 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+    fun printResult(bridgeGame: BridgeGame, isSuccess: Boolean, position: Int) {
+        println("최종 게임 결과")
+        this.printMap(bridgeGame)
+        when (isSuccess) {
+            true -> println("게임 성공 여부: 성공")
+            false -> println("게임 성공 여부: 실패")
+        }
+    }
+
 }
