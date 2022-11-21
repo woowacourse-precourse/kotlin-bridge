6dec32bd8e7a9d3edc80757651f600356e2f6dee
healthykim <bsbs8645@snu.ac.kr>
2022-11-21 17:28:35 +0900

    feat(BridgeGame): Implement move function

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -3,14 +3,18 @@ package bridge
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
+class BridgeGame(private val bridge: List<String>) {
+
+    private val moves: MutableList<String> = mutableListOf()
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     *
-     *
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun move(idx: Int, moving: String): Boolean {
+        moves.add(moving)
+        if(moving == bridge[idx]) return true
+        return false
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
