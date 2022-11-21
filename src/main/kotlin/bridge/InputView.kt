3468546e1066f4435c92a9776615c4c18859c191
dev-worthwhile <dev.lboram@gmail.com>
2022-11-21 21:32:42 +0900

    feat<InputView>: read game command until user enter valid command

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -40,7 +40,14 @@ class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        var validFlag = true
+        var gameCommand = ""
+        while (validFlag) {
+            println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+            gameCommand = cs.readLine()
+            validFlag = needToAskMoveAgain(gameCommand)
+        }
+        return gameCommand
     }
 
     private fun getBridgeSize(): Int {
