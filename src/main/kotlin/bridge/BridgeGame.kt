047ec8c9b863db593d2df1e6182f3547238c7afd
healthykim <bsbs8645@snu.ac.kr>
2022-11-21 17:46:45 +0900

    chore(BridgeGame): Add comments to methods in BridgeGame class

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -8,7 +8,10 @@ class BridgeGame(private val bridge: List<String>) {
     private val moves: MutableList<String> = mutableListOf()
 
     /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
+     * @brief 사용자가 칸을 이동할 때 사용하는 메서드
+     * @param idx 현재 사용자의 위치
+     * @param moving 사용자가 입력한 이동 방향
+     * @return 사용자가 이동한 방향에 다리가 있으면 true, 없으면 false를 반환한다.
      */
     fun move(idx: Int, moving: String): Boolean {
         moves.add(moving)
@@ -17,16 +20,23 @@ class BridgeGame(private val bridge: List<String>) {
     }
 
     /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     * @brief 사용자가 게임을 다시 시도할 때 사용하는 메서드
      */
     fun retry() {
         moves.clear()
     }
 
+    /**
+     * @return game 상황을 string으로 변환한 결과를 반환한다.
+     */
     fun gameToString(): String {
         return gameToStringLine(true) + "\n" + gameToStringLine(false);
     }
 
+    /**
+     * @param isUpper 변환할 줄을 나타내는 변수. true면 윗줄, false면 아랫줄을 의미한다.
+     * @return game 상황의 윗줄 혹은 아랫줄을 string으로 변환한 결과를 반환한다.
+     */
     private fun gameToStringLine(isUpper: Boolean): String {
         var result = "[ "
         val direction = if(isUpper) "U" else "D"
@@ -37,6 +47,12 @@ class BridgeGame(private val bridge: List<String>) {
         return result
     }
 
+    /**
+     * @brief 사용자의 이동 결과를 string으로 변환한다.
+     * @param index 사용자의 위치를 나타낸다.
+     * @param direction 어느 줄을 변환 중인지 나타낸다. U면 윗줄, D면 아랫줄이다.
+     * @return 변환 결과를 반환한다.
+     */
     private fun moveToString(index: Int, direction: String): String {
         if(moves[index] != direction) return " "
         else if(moves[index] == direction && bridge[index] == moves[index]) return "O"
