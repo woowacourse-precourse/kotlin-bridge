e2c08ce25394116004f905574891722255a9eb4e
Yuhee Kim <dbgml4509@naver.com>
2022-11-21 23:46:06 +0900

    test : 입력 값 NULL 값인지 테스트

--- src/test/kotlin/bridge/InputViewTest.kt
+++ src/test/kotlin/bridge/InputViewTest.kt
@@ -1,4 +1,17 @@
 package bridge
 
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
 class InputViewTest {
+
+    @Test
+    fun `입력 값에 아무 것도 없을 경우 테스트`() {
+        assertThrows<IllegalArgumentException> {
+            InputException().checkInputIsNull("")
+        }
+    }
+
 }
