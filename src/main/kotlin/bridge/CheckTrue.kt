c1156db8d8647f2e23d269cca3f9bcb3b3049c1e
[alsswl] <[alswl4223@naver.com]>
2022-11-21 11:06:22 +0900

    feat(CheckTrue) : 사용자에게 위 아래를 입력받은 것이 맞는지 확인

--- src/main/kotlin/bridge/CheckTrue.kt
+++ src/main/kotlin/bridge/CheckTrue.kt
@@ -1,10 +1,17 @@
 package bridge
 
 class CheckTrue {
-    fun checkBridgeLength(length : String){
+    fun checkBridgeLength(length : String) :Int{
        var length = length.toIntOrNull();
         if(length == null) throw IllegalArgumentException()
         if(length <3 || length>20) throw IllegalArgumentException()
+        return length
     }
+
+    fun checkMove(way : String) : String{
+        if(way != "U" && way != "D") throw IllegalArgumentException()
+        return way
+    }
+
 
 }
