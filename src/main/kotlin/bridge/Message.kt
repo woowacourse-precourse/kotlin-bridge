5d9895b2fc8731595f66798c63c840d96e7a29e4
krrong <khk9664@naver.com>
2022-11-21 14:42:42 +0900

    feat(Message): OutputViewEnum 클래스 생성

--- src/main/kotlin/bridge/Message.kt
+++ src/main/kotlin/bridge/Message.kt
@@ -6,7 +6,12 @@ object Message {
 
     enum class InputViewEnum(private val msg: String) {
         INVALID_MSG("올바른 값을 입력해주세요.");
-        
+
+        override fun toString(): String = msg.applyPrefix()
+    }
+
+    enum class OutputViewEnum(private val msg: String) {
+        INVALID_STATE("0 또는 1이 아닙니다.");
         override fun toString(): String = msg.applyPrefix()
     }
 }
