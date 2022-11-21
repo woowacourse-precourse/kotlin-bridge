949dda3ecaead5d97c7e8c764a2e30202ac31201
joh9911 <joh9911@naver.com>
2022-11-21 18:13:22 +0900

    refactor: 다리 자료형으로 list<String>으로 수정

--- src/main/kotlin/bridge/console/InputView.kt
+++ src/main/kotlin/bridge/console/InputView.kt
@@ -15,25 +15,25 @@ class InputView {
         val inputBridgeSize = readLine()
         validateIntType(inputBridgeSize!!)
         validateInRange(inputBridgeSize.toInt())
-        return inputBridgeSize.toInt()
+        return inputBridgeSize!!.toInt()
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    fun readMoving(): Char {
+    fun readMoving(): String {
         val inputMoveLaneSymbol = readLine()
         validateMoveLaneSymbol(inputMoveLaneSymbol!!)
-        return inputMoveLaneSymbol[0]
+        return inputMoveLaneSymbol
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    fun readGameCommand(): Char {
+    fun readGameCommand(): String {
         val inputRetrialSymbol = readLine()
         validateRetrialSymbol(inputRetrialSymbol!!)
-        return inputRetrialSymbol[0]
+        return inputRetrialSymbol
     }
 }
 
