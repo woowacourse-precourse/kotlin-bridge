b9f7744d9bf9b664786478c1344fe04de90a4273
dlgocks1 <cksgodl1@naver.com>
2022-11-21 13:02:58 +0900

    feat(Application.kt) : 게임 시작 및 플로우 코드 작성

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,42 @@
 package bridge
 
+import bridge.model.BridgeResult
+import bridge.view.InputView
+import bridge.view.OutputView
+
+
 fun main() {
-    // TODO: 프로그램 구현
+    val bridgeGame = BridgeGame()
+    val inputView = InputView()
+    val outputView = OutputView()
+
+    initBridgeGame(inputView, bridgeGame)
+    with(bridgeGame) {
+        play(inputView, outputView)
+    }
+}
+
+private fun BridgeGame.play(inputView: InputView, outputView: OutputView) {
+    do {
+        val result = move(inputView.readMoving())
+        outputView.printMap()
+        calResult(result, outputView, inputView)
+    } while (true)
+}
+
+private fun BridgeGame.calResult(
+    result: BridgeResult,
+    outputView: OutputView,
+    inputView: InputView
+) {
+    when (result) {
+        BridgeResult.FINISH -> outputView.printResult()
+        BridgeResult.SUCCESS -> return
+        BridgeResult.FAIL -> retry(inputView.readGameCommand())
+    }
+}
+
+private fun initBridgeGame(inputView: InputView, bridgeGame: BridgeGame) {
+    inputView.gameStart()
+    bridgeGame.setBridge(bridgeGame.generateRandomBridge(inputView.readBridgeSize()))
 }
