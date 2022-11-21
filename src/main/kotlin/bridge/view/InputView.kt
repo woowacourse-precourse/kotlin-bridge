3d9292bc7bfe24a8687b730927641a869862bf98
brian <briandr@naver.com>
2022-11-22 01:53:43 +0900

    feat(InputView): 게임의 모든 입력받는 기능 구현

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -1,5 +1,13 @@
 package bridge.view
 
+import bridge.common.GameCommand
+import bridge.common.UpDown
+import camp.nextstep.edu.missionutils.Console.readLine
+
+private const val NUMBER_NOT_IN_RANGE = "[ERROR] 다리 길이가 범위 밖입니다."
+private const val NOT_NUMBER = "[ERROR] 숫자가 아닙니다."
+private const val INVALID_DIRECTION = "[ERROR] 잘못된 방향입니다"
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,20 +16,57 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        val bridgeSize = readLine()
+        checkBridgeSizeInputValid(bridgeSize)
+        return bridgeSize.toInt()
+    }
+
+    private fun checkBridgeSizeInputValid(input: String) {
+        isNumber(input)
+        isNumberInRange(input.toInt())
+    }
+
+    private fun isNumber(input: String) {
+        if (!input.all { Character.isDigit(it) }) {
+            throwException(NOT_NUMBER)
+        }
+    }
+
+    private fun isNumberInRange(number: Int) {
+        if (number !in 3..20) {
+            throwException(NUMBER_NOT_IN_RANGE)
+        }
+    }
+
+    private fun throwException(message: String) {
+        throw IllegalArgumentException(message)
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        val direction = readLine()
+        checkMovingInputValid(direction)
+        return direction
+    }
+
+    private fun checkMovingInputValid(input: String) {
+        UpDown.values().forEach {
+            if (input == it.name) return
+        }
+        throwException(INVALID_DIRECTION)
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    fun readGameCommand(): String {
-        return ""
+    fun readGameCommand(): GameCommand {
+        val command = readLine()
+        return checkGameCommandValid(command)
+    }
+
+    private fun checkGameCommandValid(input: String): GameCommand {
+        return GameCommand.inGameCommand(input)
     }
 }
