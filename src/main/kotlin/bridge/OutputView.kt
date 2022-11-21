30ab3f96e5084842f8765a3f43e09475c154efe2
nayoung511 <nayoungbae511@gmail.com>
2022-11-22 03:44:41 +0900

    feat (OutputView.kt): 다리 출력 기능 추가
    fun printMap: 현재 단계에 맞춰 윗 줄 스텝 출력
    fun printMapLast: 마지막 결과값에 따른 윗 줄 출력
    fun printMapBottom: 현재 단계에 맞춰 아랫 줄 스텝 출력
    fun printMapBottomLast: 마지막 결과값에 따른 아랫 줄 출력

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -9,7 +9,43 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(bridge: List<String>, result: String, step: Int){
+        for ( i in 0..step-1 ) {
+            if ( i == 0 ) print("[")
+            if ( bridge[i] == "U" ) print(" O ")
+            else print("   ")
+            print("|")
+        }
+        printMapLast(bridge, result, step)
+    }
+    fun printMapLast(bridge: List<String>, result: String, step: Int): Boolean {
+        if ( bridge[step] == "U") {
+            if (result == "U") print(" O ]")
+            else print(" X ]")
+        } else {
+            print("   ]")
+            printMapBottom(bridge, result, step)
+        }
+        return if ( bridge[step] == result ) true else false
+    }
+    fun printMapBottom(bridge: List<String>, result: String, step: Int) {
+        for ( i in 0..step-1 ){
+            if ( i == 0 ) print("\n[")
+            if ( bridge[i] == "D" ) print(" O ")
+            else print("   ")
+            print("|")
+        }
+        printMapBottomLast(bridge, result, step)
+    }
+
+    fun printMapBottomLast(bridge: List<String>, result: String, step: Int): Boolean {
+        if ( bridge[step] == "D") {
+            if (result == "D") print(" O ]")
+            else print(" X ]")
+        } else print("   ]")
+        return if ( bridge[step] == result ) true else false
+    }
+
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
