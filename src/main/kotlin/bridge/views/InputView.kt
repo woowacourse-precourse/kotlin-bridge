9d799bdd6fc85835c50f721a459f980a0bdfd5dc
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-22 04:09:20 +0900

    feat(InputView): 재시작 또는 종료 여부 입력 기능 구현

--- src/main/kotlin/bridge/views/InputView.kt
+++ src/main/kotlin/bridge/views/InputView.kt
@@ -2,6 +2,7 @@ package bridge.views
 
 import bridge.constants.Exception.Companion.EXCEPTION_INVALID_MOVING
 import bridge.constants.Exception.Companion.EXCEPTION_INVALID_RANGE_NUMBER
+import bridge.constants.Exception.Companion.EXCEPTION_INVALID_RETRY
 import bridge.constants.Exception.Companion.EXCEPTION_IS_NOT_NUMBER
 import camp.nextstep.edu.missionutils.Console
 import java.lang.IllegalArgumentException
@@ -51,7 +52,16 @@ class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    fun readGameCommand(): String {
-        return ""
+    fun readGameRetry(): String {
+        outputView.printInputRetry()
+        val command = Console.readLine()
+        return command.uppercase()
+    }
+
+    fun checkGameRetry(command: String) {
+        val valid = listOf<String>("R", "r", "Q", "q")
+        if(!valid.contains(command)) {
+            throw IllegalArgumentException(EXCEPTION_INVALID_RETRY)
+        }
     }
 }
