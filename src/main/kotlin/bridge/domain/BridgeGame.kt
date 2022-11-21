139e3e727159e795585672972ee2ecdce986abf5
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-11-22 00:09:35 +0900

    feat(BridgeGame): 다리 결과 구하는 기능 구현

--- src/main/kotlin/bridge/domain/BridgeGame.kt
+++ src/main/kotlin/bridge/domain/BridgeGame.kt
@@ -3,7 +3,18 @@ package bridge.domain
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
+
+private enum class Bridge(val message: String) {
+    RIGHT_ANSWER("O"),
+    WRONG_ANSWER("X"),
+    NOT_SELECT(" ")
+}
+
 class BridgeGame(private var bridge: List<String>) {
+
+    private val upMapResult = mutableListOf<String>()
+    private val downMapResult = mutableListOf<String>()
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
@@ -19,6 +30,23 @@ class BridgeGame(private var bridge: List<String>) {
         }
         return false
     }
+
+    private fun moveUpMap(movable: Boolean) {
+        val addValue = if (movable) Bridge.RIGHT_ANSWER.message else Bridge.WRONG_ANSWER.message
+        upMapResult.add(addValue)
+        downMapResult.add(Bridge.NOT_SELECT.message)
+    }
+
+    private fun moveDownMap(movable: Boolean) {
+        val addValue = if (movable) Bridge.RIGHT_ANSWER.message else Bridge.WRONG_ANSWER.message
+        upMapResult.add(Bridge.NOT_SELECT.message)
+        downMapResult.add(addValue)
+    }
+
+    fun getMapResult(): Pair<List<String>, List<String>> {
+        return Pair(upMapResult, downMapResult)
+    }
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
