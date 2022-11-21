878b667d3773323b179b1f19bd16cbc14d155cff
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-22 01:17:21 +0900

    feat(view): OutputView.printMap() 함수 기능 구현

--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -1,6 +1,7 @@
 package bridge.view
 
 import bridge.constants.Message
+import bridge.data.GameHistory
 
 object OutputView {
 
@@ -20,12 +21,10 @@ object OutputView {
         println("\n${Message.SelectRetryOrFinishGame}")
     }
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    fun printMap() {}
+    fun printMap(history: GameHistory) {
+        println(buildHistoryOf(history.upstairs))
+        println(buildHistoryOf(history.downstairs))
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
@@ -36,5 +35,9 @@ object OutputView {
 
     fun printError(message: String) {
         println("[ERROR] $message")
+    }
+
+    private fun buildHistoryOf(data: List<Char>): String {
+        return data.joinToString(prefix = "[", postfix = "]", separator = "|") { " $it " }
     }
 }
