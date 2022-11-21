30111acc0654b3fbbb9dc185a5825ee2dead70f0
username <ieejo716@naver.com>
2022-11-21 15:09:18 +0900

    [FEAT] : 입력값 형식 예외 처리

--- src/main/kotlin/util/InputException.kt
+++ src/main/kotlin/util/InputException.kt
@@ -1,7 +1,21 @@
 package util
 
 class InputException {
-    private fun printException(message : String) {
+
+    fun checkInputLength(input: String) {
+        try {
+            val length = input.toInt()
+        } catch (e: Exception) {
+            printException(TYPE_ERROR)
+        }
+    }
+
+    private fun printException(message: String) {
         throw IllegalArgumentException("[ERROR] $message")
+    }
+
+    companion object {
+        const val RANGE_ERROR = "3~20의 범위가 아닙니다."
+        const val TYPE_ERROR = "올바른 형식이 아닙니다."
     }
 }
