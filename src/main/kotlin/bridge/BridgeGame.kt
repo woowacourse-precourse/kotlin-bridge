0f7f70136f59608d2af210a650bd7960afae9af7
Jongwon-0518 <77ha@naver.com>
2022-11-21 19:14:44 +0900

    fix: 이동할 칸 입력시 예외 처리 수정

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -11,6 +11,10 @@ class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun move(move: String, bridge: MutableList<String>): MutableList<String> {
+        if (move!="U" && move!="D") {
+            println("[ERROR] 이동할 칸은 U와 D만 입력 가능합니다.")
+            throw IllegalArgumentException()
+        }
         bridge.add(move)
         return bridge
     }
@@ -31,4 +35,6 @@ class BridgeGame {
         }
         return true
     }
+
+
 }
