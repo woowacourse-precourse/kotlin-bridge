945052ea869da7203cde414b04ace8f481fe8245
dlwgnud <wngud980411@naver.com>
2022-11-22 00:55:13 +0900

    feat(printMap): 선택한 칸에 대한 다리 결과를 출력하는 함수

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -29,7 +29,13 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(bridgeGame: BridgeGame, now: Int) {
+        val upBridge = bridgeGame.getUpBridge()
+        val downBridge = bridgeGame.getDownBridge()
+        println(upBridge.slice(0..now).joinToString(" | ", "[ ", " ]"))
+        println(downBridge.slice(0..now).joinToString(" | ", "[ ", " ]"))
+        println()
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
