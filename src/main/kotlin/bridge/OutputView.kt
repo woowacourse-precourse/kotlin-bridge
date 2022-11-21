9747da75554700373a01ec2a72669d901d17943c
Hyeongseon Jeon <hsun5163@gmail.com>
2022-11-21 11:47:57 +0900

    feat : printSelectMoveToSpace 함수 구현

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -27,4 +27,9 @@ class OutputView {
     fun printBridgeLength() {
         println("다리의 길이를 입력해주세요.")
     }
+
+    /** <출력> 이동할 칸을 선택해주세요. (위: U, 아래: D) */
+    fun printSelectMoveToSpace() {
+        println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
+    }
 }
