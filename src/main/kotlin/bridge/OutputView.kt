b10d9b53d62fb27008b8f66ba2b243ebf06fadf4
dev-worthwhile <dev.lboram@gmail.com>
2022-11-22 01:23:48 +0900

    feat<OutputView>: check user crossed the bridge or not

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -62,4 +62,13 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printResult() {}
+
+    fun successOrFailure(moves: List<String>, answers: List<String>): String {
+        for (attempt in moves.indices) {
+            if (moves[attempt] != answers[attempt]) {
+                return "실패"
+            }
+        }
+        return "성공"
+    }
 }
