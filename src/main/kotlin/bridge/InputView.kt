3fcf2d30a8fc428e933d3fe2010831161d75286a
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-11-22 04:13:42 +0900

    fix: 요구사항에 맞게 Consol.readLine 변경 및 검증관련 수정

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,6 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -10,8 +11,14 @@ class InputView {
     private val validator =  Validator()
     fun readBridgeSize(): Int {
         println(BRIDGE_SIZE_MESSAGE)
-        val bridgeSize = readLine()
-        return validator.validateBridgeSize(bridgeSize)
+        val bridgeSize = Console.readLine()
+        try{
+            validator.validateBridgeSize(bridgeSize)
+        }catch (e: IllegalArgumentException){
+            println(e.message)
+            return readBridgeSize()
+        }
+        return bridgeSize.toInt()
     }
 
     /**
@@ -19,15 +26,27 @@ class InputView {
      */
     fun readMoving(): String {
         println(BRIDGE_MOVING_MESSAGE)
-        val move = readLine()
-        return validator.validatorMove(move)
+        val move = Console.readLine()
+        try{
+            validator.validatorMove(move)
+        }catch (e: IllegalArgumentException){
+            println(e.message)
+            return readMoving()
+        }
+        return move
     }
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
         println(GAME_COMMAND_MESSAGE)
-        val command = readLine()
-        return validator.validatorGameCommand(command)
+        val command = Console.readLine()
+        try{
+            validator.validatorGameCommand(command)
+        }catch (e: IllegalArgumentException){
+            println(e.message)
+            return readGameCommand()
+        }
+        return command
     }
 }
