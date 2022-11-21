650b75508726fdef2a0498c6beb05fa83cd8b4b8
Jaeheon Jeong <drdd1120@naver.com>
2022-11-22 04:39:10 +0900

    <feat> (1)에서 입력한 길이에 따라 다리를 생성한다.

--- src/main/kotlin/bridge/Game.kt
+++ src/main/kotlin/bridge/Game.kt
@@ -4,5 +4,10 @@ class Game {
     fun gameStart() {
         val inputView = InputView()
         val size = inputView.readBridgeSize()
+
+        val bridgeRandomNumberGenerator = BridgeRandomNumberGenerator()
+        val bridgeMaker = BridgeMaker(bridgeRandomNumberGenerator)
+        val bridge = bridgeMaker.makeBridge(size)
+        println(bridge.toString())
     }
 }
