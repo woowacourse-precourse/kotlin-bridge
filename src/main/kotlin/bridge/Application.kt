0a9f42b67c7f588d7ee3b3054238db480b705592
lgy15 <lgy159@naver.com>
2022-11-21 10:16:14 +0900

    refactor: outputView 수정중

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,18 +1,50 @@
 package bridge
 
-import bridge.resources.INPUT_BRIDGE_GAME_START
-import bridge.resources.START_LOCATION
-import bridge.resources.START_TRY
+import bridge.resources.*
 import bridge.view.InputView
+import bridge.view.OutputView
 import java.lang.IllegalArgumentException
 
 fun main() {
     println(INPUT_BRIDGE_GAME_START)
     try {
         val bridgeLength = InputView().readBridgeSize()
-        val bridgeGame = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeLength)
-
-        BridgeGame(bridgeGame, START_TRY).move(START_LOCATION)
+        val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeLength)
+        val tryCount = START_TRY
+        val game = BridgeGame(bridge, bridgeLength)
+        println(bridge)
+        gameStart(game, tryCount)
     } catch (_: IllegalArgumentException) {
     }
 }
+
+fun shortBridge(game: BridgeGame, location: Int): MutableList<String> {
+    val reduceList = mutableListOf<String>()
+    for (idx in 0..location) {
+        reduceList.add(game.bridge[idx])
+    }
+    return reduceList
+}
+
+fun gameStart(game: BridgeGame, tryCount: Int) {
+    for (location in 0 until game.bridgeLength) {
+        val answer = game.move(location, inputMove())
+        if (answer && location < game.bridgeLength - BRIDGE_PADDING) OutputView().printMap(game.bridge, location, true)
+        if (answer && location == game.bridgeLength - BRIDGE_PADDING) {
+            OutputView().printResult(game.bridge, location, true)
+            OutputView().printEnd(true, tryCount)
+        }
+        if (!answer) {
+            if (game.retry(inputRetry())) gameStart(game, tryCount + NEXT_TRY)
+            else {
+                OutputView().printResult(game.bridge, location, false)
+                OutputView().printEnd(false, tryCount)
+            }
+            break
+        }
+    }
+}
+
+fun inputMove() = InputView().readMoving()
+
+fun inputRetry() = InputView().readGameCommand()
--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -1,32 +1,21 @@
 package bridge
 
 import bridge.resources.*
-import bridge.view.InputView
 import bridge.view.OutputView
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame(val bridge: List<String>, private val retry: Int) {
+class BridgeGame(val bridge: List<String>, val bridgeLength: Int) {
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move(location: Int) {
-        val maxLocation = bridge.size - BRIDGE_PADDING // size - 1
-        val answer = bridge[location] == inputMove()
-        OutputView().printMap(bridge, location, answer)
-        if (location < maxLocation && answer) move(location + NEXT_MOVE) // 범위 안에서 재귀
-        if (location == maxLocation && answer) { // 끝까지 건넜을 때
-            OutputView().printResult(bridge, location, true)
-            OutputView().printEnd(true, this.retry)
-        }
-        if (!answer) retry(location)
+    fun move(location: Int, inputMove: String): Boolean {
+        return bridge[location] == inputMove
     }
-
-    private fun inputMove() = InputView().readMoving()
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -34,15 +23,8 @@ class BridgeGame(val bridge: List<String>, private val retry: Int) {
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry(location: Int) {
-        val retryCheck = inputRetry()
-
-        if (retryCheck == RETRY) BridgeGame(this.bridge, this.retry + NEXT_TRY).move(START_LOCATION)
-        if (retryCheck == QUIT) {
-            OutputView().printResult(bridge, location, false)
-            OutputView().printEnd(false, this.retry)
-        }
+    fun retry(inputRetry: String): Boolean {
+        if (inputRetry == RETRY) return true
+        return false
     }
-
-    private fun inputRetry() = InputView().readGameCommand()
 }
