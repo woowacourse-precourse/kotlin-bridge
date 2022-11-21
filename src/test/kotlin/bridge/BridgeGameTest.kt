47b2d767cf867610774ac44daa938a631d83d0b2
WoeunJung <leena09123@naver.com>
2022-11-21 18:53:44 +0900

    test: print map test

--- src/test/kotlin/bridge/BridgeGameTest.kt
+++ src/test/kotlin/bridge/BridgeGameTest.kt
@@ -9,6 +9,10 @@ class BridgeGameTest: NsTest() {
     fun `건널 수 있는 다리인지 판별하는 테스트`() {
         val bridgeGame = BridgeGame(listOf("U", "D", "D", "U", "U"))
         bridgeGame.move("U")
+        bridgeGame.move("U")
+        val lastBridge = bridgeGame.up.last()
+        Assertions.assertThat(lastBridge).isEqualTo(" X ")
+    }
         bridgeGame.move("D")
         val isMovable = bridgeGame.move("U")
         Assertions.assertThat(isMovable).isEqualTo(false)
