70b69c31ec4df842368f8b50e254d5c92149d9e3
dohun_ <silver2679@gmail.com>
2022-11-21 22:49:54 +0900

    Refactor(Application): 예외 처리를 위한 try, catch 추가

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,11 +1,15 @@
 package bridge
 
 fun main() {
-    val bridgeMaker: BridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
-    val bridgeSize: Int = InputView().readBridgeSize()
-    val bridge: List<String> = bridgeMaker.makeBridge(bridgeSize)
-    val bridgeGame: BridgeGame = BridgeGame(bridge)
-    gameStart(bridgeGame, bridgeSize)
+    val bridgeMaker: BridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())//다리 생성
+    try {
+        val bridgeSize: Int = InputView().readBridgeSize()//다리 길이 입력
+        val bridge: List<String> = bridgeMaker.makeBridge(bridgeSize)//
+        val bridgeGame: BridgeGame = BridgeGame(bridge)
+        gameStart(bridgeGame, bridgeSize)
+    } catch (e: IllegalArgumentException) {
+        println("${e.message}")
+    }
 }
 
 fun gameStart(bridgeGame: BridgeGame, bridgeSize: Int) {
