4fdce69f70aaa5bd5adec058394b33a22269ddda
phs <gustjd0808@pusan.ac.kr>
2022-11-21 16:24:11 +0900

    test: test execption when argument is illegal for making bridge

--- src/test/kotlin/bridge/domain/BridgeMakerTest.kt
+++ src/test/kotlin/bridge/domain/BridgeMakerTest.kt
@@ -1,5 +1,8 @@
 package bridge.domain
 
+import bridge.ApplicationTest
+import bridge.ApplicationTest.*
+import bridge.BridgeNumberGenerator
 import bridge.BridgeRandomNumberGenerator
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Assertions.*
@@ -15,6 +18,15 @@ class BridgeMakerTest {
         val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
         assertThrows<IllegalArgumentException> {
             bridgeMaker.makeBridge(input)
+        }
+    }
+
+    @Test
+    fun `makeBridge 메서드로 다리 생성 시 지정한 방향을 나타내는 숫자 이외의 숫자로 생성하면 예외 발생`() {
+        val numberGenerator: BridgeNumberGenerator = TestNumberGenerator(listOf(-1, 0, 1))
+        val bridgeMaker = BridgeMaker(numberGenerator)
+        assertThrows<IllegalArgumentException> {
+            val bridge: List<String> = bridgeMaker.makeBridge(3)
         }
     }
 }
