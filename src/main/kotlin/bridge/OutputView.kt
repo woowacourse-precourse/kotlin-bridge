e424c5af7d551ed433eb07a8e6b198bae828f105
lsakee <lsls4868@gmail.com>
2022-11-22 02:18:48 +0900

    feat : 결과 값 출력 기능 추가

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -23,11 +23,22 @@ class OutputView {
     fun printRestartMsg(){
         println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
     }
+    fun printResult(){
 
+        println("게임 성공 여부: 성공")
+        println("최종 게임 결과")
+
+    }
+
+    fun printResult2() {
+        println("게임 성공 여부: 실패")
+        println("최종 게임 결과")
+
+    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+
 }
