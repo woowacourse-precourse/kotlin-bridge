b5d778a521d719d2e66b574765c7af8c4474c2c7
joh9911 <joh9911@naver.com>
2022-11-21 18:15:11 +0900

    feat(Bridge): 다리 끝까지 이동했는지 확인해주는 함수 구현

--- src/main/kotlin/bridge/model/Bridge.kt
+++ src/main/kotlin/bridge/model/Bridge.kt
@@ -1,17 +1,30 @@
 package bridge.model
 
 class Bridge(private val bridgeLaneShape: List<String>) {
+    init {
+        println(bridgeLaneShape)
+    }
     var laneCount = 0
 
     /**
      * 랜덤 생성한 다리 형태와 입력받은 이동 칸의 일치 여부를 boolean 으로 반환하는 함수
      */
-    fun isLaneAvailable(inputMoveLaneSymbol: Char): Boolean{
-        if (inputMoveLaneSymbol == bridgeLaneShape[0][laneCount]){
+    fun isLaneAvailable(inputMoveLaneSymbol: String): Boolean{
+        if (inputMoveLaneSymbol == bridgeLaneShape[laneCount]){
             laneCount += 1
             return true
         }
         laneCount += 1
         return false
+    }
+
+    fun isMoveUntilEnd(): Boolean {
+        if (laneCount == bridgeLaneShape.size)
+            return true
+        return false
+    }
+
+    fun initializeLaneCount(){
+        laneCount = 0
     }
 }
