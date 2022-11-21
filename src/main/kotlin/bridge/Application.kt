fa4595d5934bf42b6251537976380b9c030cfd55
healthykim <bsbs8645@snu.ac.kr>
2022-11-21 21:05:57 +0900

    feat(Application): Add function for playing multiple games

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -23,3 +23,19 @@ fun playGame(size: Int, game: BridgeGame): Boolean {
     }
     return !isGameOver
 }
+
+/**
+ * @brief 브릿지 게임을 여러 번 플레이한다
+ * @param bridge 이어진 다리의 정보
+ * @param game 브릿지 게임
+ */
+fun playGames(size: Int, game: BridgeGame) {
+    var success: Boolean
+    var tries = 0
+    do {
+        game.retry()
+        success = playGame(size, game)
+        tries += 1
+    } while (!success && input.readGameCommand() == "R")
+    output.printResult(success, tries, game)
+}
