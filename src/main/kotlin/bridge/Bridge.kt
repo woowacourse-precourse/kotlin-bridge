8b893912362eee02dd8d00d5f8ebdef974d8bf7a
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-22 00:14:42 +0900

    refactor(bridge): bridge 클래스 수정
    에러 검사 추가, 변수 명 변경

--- src/main/kotlin/bridge/Bridge.kt
+++ src/main/kotlin/bridge/Bridge.kt
@@ -1,9 +1,23 @@
 package bridge
 
+import bridge.util.BridgeMoving
+
 data class Bridge(val steps: List<String>) {
 
-    fun matchUserChoice(moving: String, position: Int): Boolean {
-        if (steps[position] == moving) return true
+    init {
+        require(steps.size in 3..20) { BRIDGE_SIZE_ERROR_MESSAGE }
+        require(steps.all { it == BridgeMoving.UP.character || it == BridgeMoving.DOWN.character }) { BRIDGE_LOCATION_ERROR_MESSAGE }
+    }
+
+    fun isMatchMoving(userMoving: String, userPosition: Int): Boolean {
+        if (steps[userPosition] == userMoving) return true
         return false
+    }
+
+    fun length() = steps.size
+
+    companion object {
+        const val BRIDGE_SIZE_ERROR_MESSAGE = "[ERROR] 다리의 길이는 3 이상 20 이하여야 합니다."
+        const val BRIDGE_LOCATION_ERROR_MESSAGE = "[ERROR] 다리의 위치는 위 또는 아래여야 합니다."
     }
 }
