99cf79dd45d369d9d09b1ee9750bc91434a31417
dltkd1395 <dltkd13956@naver.com>
2022-11-22 01:04:24 +0900

    test(BridgeControllerTest) : 게임 실패 테스트 추가

--- src/test/kotlin/bridge/BridgeControllerTest.kt
+++ src/test/kotlin/bridge/BridgeControllerTest.kt
@@ -24,6 +24,23 @@ class BridgeControllerTest : NsTest() {
         }, 1, 0, 0)
     }
 
+    @Test
+    fun `게임 실패 테스트`() {
+        assertRandomNumberInRangeTest({
+            run("3", "U", "D", "U", "Q")
+            assertThat(output()).contains(
+                "최종 게임 결과",
+                "[ O |   | X ]",
+                "[   | O |   ]",
+                "게임 성공 여부: 실패",
+                "총 시도한 횟수: 1"
+            )
+            val upSideIndex = output().indexOf("[ O |   | X ]")
+            val downSideIndex = output().indexOf("[   | O |   ]")
+            assertThat(upSideIndex).isLessThan(downSideIndex)
+        }, 1, 0, 0)
+    }
+
     override fun runMain() {
         BridgeController().start()
     }
