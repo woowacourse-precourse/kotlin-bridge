b5793cc57719bb44730aebefee23e32299bffda5
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-11-22 00:06:45 +0900

    feat(BridgeGame): 이동한 칸을 건널 수 있는지 확인하는 기능 구현

--- src/main/kotlin/bridge/domain/BridgeGame.kt
+++ src/main/kotlin/bridge/domain/BridgeGame.kt
@@ -3,7 +3,7 @@ package bridge.domain
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
+class BridgeGame(private var bridge: List<String>) {
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
@@ -12,6 +12,13 @@ class BridgeGame {
      */
 
     fun move() {}
+
+    private fun checkMovable(moving: String, step: Int): Boolean {
+        if (moving == bridge[step]) {
+            return true
+        }
+        return false
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
