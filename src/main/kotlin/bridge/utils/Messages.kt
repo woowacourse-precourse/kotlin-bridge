f2ee66fdcc3627bc80c6db48a2a8b05551381149
jwson <sonjungwoo9@gmail.com>
2022-11-21 14:55:45 +0900

    [feat] Messages.kt 게임에 사용될 String 메세지 오브젝트 클래스로 정리

---
+++ src/main/kotlin/bridge/utils/Messages.kt
@@ -0,0 +1,13 @@
+package bridge.utils
+
+object Messages{
+    const val GameStart = "다리 건너기 게임을 시작합니다."
+    const val InputLength = "다리의 길이를 입력해주세요."
+    const val InputUpAndDown = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
+    const val GameRestart = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
+    const val GameLastResult = "최종 게임 결과"
+    const val GameSuccess = "게임 성공 여부: 성공"
+    const val GameFail = "게임 성공 여부: 실패"
+    const val GameTryTimes = "총 시도한 횟수: "
+}
+
