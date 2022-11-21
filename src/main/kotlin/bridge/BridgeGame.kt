927ba81bd4a623b0f2f6195e4cb4b1bb9a09ec36
Hyeongseon Jeon <hsun5163@gmail.com>
2022-11-22 01:40:03 +0900

    feat : Bridge 클래스 필드 추가 및 initBridge 함수 구현

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -4,6 +4,14 @@ package bridge
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+    private var comBridge: Bridge? = null
+    private var userBridge = Bridge()
+    private var gameCount = 1
+
+    fun initBridge(size : Int){
+        comBridge = Bridge(size)
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
