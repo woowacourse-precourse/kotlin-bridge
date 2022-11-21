bda158d98235a9ad512580831851f396b9bef21e
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-21 19:28:56 +0900

    refactor(OutputView) : 사용되는 문자열들을 companion object의 멤버로 변경 및 플레이어의 경로에 대한 기록을 관리 하는 기능 추가

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -4,13 +4,36 @@ package bridge
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 class OutputView {
+    init{
+        println("다리 건너기 게임을 시작합니다.")
+    }
+    private lateinit var playerPathMap: Array<String>
+
+    fun initPathMap(bridgeSize: Int) {
+        playerPathMap = Array(2) { NOT_INITIALIZED.repeat(bridgeSize) }
+    }
+
+    /**
+     * 이동 경로를 최신화 하기 위한 함수이다.
+     */
+    fun recordPlayerMoving(playerStatus: PlayerStatus) {
+        var recordSign = AVAILABLE_PATH
+        if (!playerStatus.isPlayerAlive) {
+            recordSign = UNAVAILABLE_PATH
+        }
+        playerPathMap[playerStatus.playerDirection] =
+            playerPathMap[playerStatus.playerDirection].replaceFirst("N", recordSign)
+        playerPathMap[playerStatus.playerDirection.oppositeDirection()] =
+            playerPathMap[playerStatus.playerDirection.oppositeDirection()].replaceFirst("N", " ")
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap(bridgeMap: String) {
-        println(bridgeMap.replace("| N ", ""))
+    fun printMap() = playerPathMap.forEach { eachMap ->
+        println(eachMap.formattingMap())
     }
 
     /**
@@ -18,19 +41,26 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult(bridgeMap: Array<String>, numberOfTry: Int, isSucceeded: Boolean) {
-        println("최종 게임 결과")
-        bridgeMap.forEach {
-            println(it)
-        }
-
-        if (isSucceeded) {
-            println("게임 성공 여부: 성공")
-        } else {
-            println("게임 성공 여부: 실패")
-        }
-        println("총 시도한 횟수: %d".format(numberOfTry))
-
+    fun printResult(numberOfTry: Int, gameStatus: GameData) {
+        println(GAME_RESULT)
+        printMap()
+        println(GAME_RESULT_FLAG.format(gameStatus.gameMsg))
+        println(THE_NUMBER_OF_TRY.format(numberOfTry))
     }
 
+    private fun String.formattingMap() =
+        this.replace(NOT_INITIALIZED, "")
+            .replace(SUCCESSIVE_PARENTHESIS, CONNECT_PARENTHESIS)
+
+    private fun Int.oppositeDirection() = if (this == 0) {
+        1
+    } else {
+        0
+    }
+
+    companion object{
+        const val GAME_RESULT = "최종 게임 결과"
+        const val GAME_RESULT_FLAG = "게임 성공 여부: %s"
+        const val THE_NUMBER_OF_TRY = "총 시도한 횟수: %d"
+    }
 }
