aea53ac39f0618af82d57473fc234686e0d68973
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-21 19:32:42 +0900

    feat(GameData) : 게임을 위한 데이터 클래스들을 모으기 및 게임 상태 이외에도 플레이어의 상태를 나타내는 데이터 클래스 추가

--- src/main/kotlin/bridge/GameData.kt
+++ src/main/kotlin/bridge/GameData.kt
@@ -1,5 +1,15 @@
 package bridge
 
-enum class GameStatus(val gameMsg:String) {
+enum class GameData(val gameMsg:String) {
     CONTINUING(""), SUCCEEDED("성공"), FAILED("실패")
+}
+
+data class PlayerStatus(
+    val playerDirection: Int,
+    val isPlayerAlive: Boolean
+)
+
+enum class Direction(val direction: String, val directionNumber:Int){
+     UP("U", 0),
+    DOWN("D", 1)
 }
