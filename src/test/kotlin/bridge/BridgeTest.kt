63f7597e2dbb616fb388958e6be585d37a06cd21
pingu244 <69796976+pingu244@users.noreply.github.com>
2022-11-22 00:47:13 +0900

    test: 테스트 추가
    - 다리 만들기 테스트
    - 입력값과 정답따라 다리 결정하는 테스트
    - 재시도 테스트

--- src/test/kotlin/bridge/BridgeTest.kt
+++ src/test/kotlin/bridge/BridgeTest.kt
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.assertThrows
 
 
 class BridgeTest {
-
     class InputException {
         @Test
         fun `숫자가 아닌 값 입력했을 경우`() {
@@ -38,18 +37,42 @@ class BridgeTest {
         }
     }
 
-    @Test
-    fun `칸 이동할 때 입력과 정답 판단 테스트`() {
-        assertTrue(BridgeGame().move("U", "U"))
+    class MoveTest() {
+        @Test
+        fun `칸 이동할 때 입력과 정답이 맞았을 경우`() {
+            assertTrue(BridgeGame().move("U", "U"))
+        }
+
+        @Test
+        fun `칸 이동할 때 입력과 정답이 틀렸을 경우`() {
+            assertFalse(BridgeGame().move("D", "U"))
+        }
+    }
+
+    class RetryTest() {
+        @Test
+        fun `종료 테스트`() {
+            assertFalse(BridgeGame().retry("Q"))
+        }
+
+        @Test
+        fun `재시도 테스트`() {
+            assertTrue(BridgeGame().retry("R"))
+        }
     }
 
     @Test
-    fun `재시도 혹은 종료 테스트`() {
-        assertFalse(BridgeGame().retry("Q"))
+    fun `다리 만들기 테스트`() {
+        val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(5)
+        assertTrue(bridge.size == 5)
     }
 
-    // 다리 만들기 테스트
-
-    // 정답 입력 처리 테스트 2개(맞은 경우, 틀린 경우)
-
+    @Test
+    fun `입력값과 정답따라 다리 결정 테스트`() {
+        val bridge = listOf("U", "D", "U")
+        val userStep = listOf(true, false)
+        val printValue = BridgeGame().determineMap(bridge, userStep)
+        val answer = mutableListOf(listOf(" ", "O"), listOf(" ", "X"))
+        assertTrue(printValue == answer)
+    }
 }
