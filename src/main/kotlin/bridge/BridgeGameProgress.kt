070621a665b9914cacec511f773f4f3c9b500467
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-11-22 04:23:44 +0900

    feat: 다리 게임 진행 및 관리 메소드

---
+++ src/main/kotlin/bridge/BridgeGameProgress.kt
@@ -0,0 +1,38 @@
+package bridge
+
+var count:Int = 1
+class BridgeGameProgress {
+
+    fun bridgeGameProgress(bridge: List<String>){
+        val result = bridgeGameStart(bridge)
+        if(result[result.size-1][1]==FAIL){
+            count += 1
+            gameCommand(result, bridge)
+        }
+        return OutputView().printResult(result, SUCCESS, count)
+    }
+
+    private fun bridgeGameStart(bridge: List<String>): MutableList<List<String>> {
+        val result:MutableList<List<String>> = mutableListOf()
+        for(step in bridge){
+            val moving = InputView().readMoving()
+            if(!BridgeGame().move(step, moving, result)){
+                OutputView().printMap(result)
+                break
+            }
+            OutputView().printMap(result)
+        }
+        return result
+    }
+
+    private fun gameCommand(result: MutableList<List<String>>, bridge: List<String>){
+        val gameCommand = InputView().readGameCommand()
+        if(gameCommand== QUIT){
+            return OutputView().printResult(result, LOSE, count)
+        }
+        if(gameCommand == RETRY){
+            BridgeGame().retry(bridge)
+            return
+        }
+    }
+}
