9149aa07dcf2ee7763382023f3d318fca717dfd1
joh9911 <joh9911@naver.com>
2022-11-21 12:16:08 +0900

    feat(OutputView): 게임 진행 메시지를 출력하는 함수 구현

--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -44,5 +44,29 @@ class OutputView() {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printResult() {}
-    
+
+    fun printGameStartMessage(){
+        println(Constants.GAME_START_MESSAGE)
+    }
+
+    fun printGameInputBridgeSizeMessage(){
+        println(Constants.GAME_INPUT_BRIDGE_SIZE_MESSAGE)
+    }
+
+    fun printGameInputMoveLaneMessage(){
+        println(Constants.GAME_INPUT_MOVE_LANE_MESSAGE)
+    }
+
+    fun printGameInputRetrialMessage(){
+        println(Constants.GAME_INPUT_RETRIAL_MESSAGE)
+    }
+
+    fun printGameInitializeProcess(){
+        printGameStartMessage()
+        printGameInputBridgeSizeMessage()
+    }
+
+    fun printGameMovingProcess(){
+        printGameInputMoveLaneMessage()
+    }
 }
