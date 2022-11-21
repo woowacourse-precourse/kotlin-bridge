df272b33d05ebaf2e7b9fa1be5d62f45b6ddd452
Hyeongseon Jeon <hsun5163@gmail.com>
2022-11-22 00:21:15 +0900

    feat : bridgeComparison 함수 구현
    targetBridge와 자신의 bridge가 값 비교

--- src/main/kotlin/bridge/Bridge.kt
+++ src/main/kotlin/bridge/Bridge.kt
@@ -1,9 +1,18 @@
 package bridge
 
 class Bridge (size : Int = 0) {
-    val bridge : MutableList<String> = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(size).toMutableList()
+    private val bridge : MutableList<String> = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(size).toMutableList()
 
     fun addUserBridge(movingKey : String) {
         bridge.add(movingKey)
+    }
+
+    fun bridgeComparison(targetBridge: Bridge) : Boolean{
+        for(i in 0 until bridge.size){
+            if(targetBridge.bridge[i] != bridge[i]){
+                return false
+            }
+        }
+        return true
     }
 }
