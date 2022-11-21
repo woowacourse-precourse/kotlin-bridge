d31d00efd442f35b1cef5f6eb435c0da45286580
JEGHTNER <jeheon0717@naver.com>
2022-11-21 13:26:51 +0900

    <fix> : InputView 내의 입력가이드 문구 출력

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -10,6 +10,7 @@ class InputView {
          * 다리의 길이를 입력받는다.
          */
         fun readBridgeSize(): Int {
+            OutputView.messageInputBridgeSize()
             val bridgeSize = Console.readLine()
             ExceptionCheck.checkBridgeSize(bridgeSize)
             return bridgeSize.toInt()
@@ -19,6 +20,7 @@ class InputView {
          * 사용자가 이동할 칸을 입력받는다.
          */
         fun readMoving(): String {
+            OutputView.messageInputMove()
             val movingDirection = Console.readLine()
             ExceptionCheck.checkDirection(movingDirection)
             return movingDirection
@@ -28,6 +30,7 @@ class InputView {
          * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
          */
         fun readGameCommand(): String {
+            OutputView.messageInputGameCommand()
             val gameCommand = Console.readLine()
             ExceptionCheck.checkGameCommand(gameCommand)
             return ""
