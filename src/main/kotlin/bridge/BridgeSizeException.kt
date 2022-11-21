2a948c942acf5e307ba6009de0842cd06be0a11b
inseonyun <yuninseon@naver.com>
2022-11-21 16:42:28 +0900

    feat: 다리 길이 예외 클래스 생성
    1. isNotInteger - 입력된 값이 정수인지 check
    2. isNotBridgeNumber - 입력된 값이 3부터 20사이의 숫자인지 check

--- src/main/kotlin/bridge/BridgeSizeException.kt
+++ src/main/kotlin/bridge/BridgeSizeException.kt
@@ -7,10 +7,12 @@ class BridgeSizeException(inputData: String) {
     }
 
     private fun isNotInteger(inputData: String) {
-        TODO("기능구현")
+        if(!inputData.chars().allMatch { Character.isDigit(it) })
+            throw IllegalArgumentException("[ERROR] 입력한 다리 길이는 정수가 아닙니다.")
     }
 
     private fun isNotBridgeNumber(inputData: String) {
-        TODO("기능구현")
+        if(inputData.toInt() < 3 || inputData.toInt() > 20)
+            throw IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.")
     }
 }
