75cb8a7609e36e3e76ec3c07eace1d6c18c4fc9c
dlgocks1 <cksgodl1@naver.com>
2022-11-21 13:07:00 +0900

    feat(Constants.kt) : Input 텍스트 상수로 추출

---
+++ src/main/kotlin/bridge/utils/Constants.kt
@@ -0,0 +1,7 @@
+package bridge.utils
+
+// Input Text
+const val BRIDGE_GAME_START = "다리 건너기 게임을 시작합니다."
+const val CHOOSE_BRIDGE_LENGTH = "다리의 길이를 입력해주세요."
+const val CHOOSE_BRIDGE_DIRECTION = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
+const val CHOOSE_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
