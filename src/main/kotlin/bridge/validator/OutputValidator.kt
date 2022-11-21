febc60ef4e6bd7c729f3952d6f7f5ab73cf2e539
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-21 10:55:05 +0900

    [Refactor] (OutputValidator) 위, 아래 출력 메소드 분리
    upSideMap, downSideMap

--- src/main/kotlin/bridge/validator/OutputValidator.kt
+++ src/main/kotlin/bridge/validator/OutputValidator.kt
@@ -3,12 +3,27 @@ package bridge.validator
 object OutputValidator {
 
     fun validatePrintMap(bridge: List<String>, round: Int, direction: String): String {
-        if (bridge[round] == "U" && direction == "U") return "[ O ]\n[   ]"
-        if (bridge[round] == "U" && direction == "D") return "[   ]\n[ X ]"
-        if (bridge[round] == "D" && direction == "U") return "[ X ]\n[   ]"
-        if (bridge[round] == "D" && direction == "D") return "[   ]\n[ O ]"
 
-        return ""
+        return validateUpSideMap(bridge, round, direction) + "\n" + validateDownSideMap(bridge, round, direction)
+    }
+
+    // TODO: 리팩토링
+    private fun validateUpSideMap(bridge: List<String>, round: Int, direction: String): String {
+        val truncatedUpList: MutableList<String> = mutableListOf()
+        if (bridge[round] == "U" && direction == "U") truncatedUpList.add(" O ")
+        if (bridge[round] == "U" && direction == "D") truncatedUpList.add("   ")
+        if (bridge[round] == "D" && direction == "U") truncatedUpList.add(" X ")
+        if (bridge[round] == "D" && direction == "D") truncatedUpList.add("   ")
+        return truncatedUpList.toString().replace(", ", "|")
+    }
+
+    private fun validateDownSideMap(bridge: List<String>, round: Int, direction: String): String {
+        val truncatedDownList: MutableList<String> = mutableListOf()
+        if (bridge[round] == "U" && direction == "U") truncatedDownList.add("   ")
+        if (bridge[round] == "U" && direction == "D") truncatedDownList.add(" X ")
+        if (bridge[round] == "D" && direction == "U") truncatedDownList.add("   ")
+        if (bridge[round] == "D" && direction == "D") truncatedDownList.add(" O ")
+        return truncatedDownList.toString().replace(", ", "|")
     }
 
     fun validatePrintResult(whether: Boolean, trialCount: Int): String {
