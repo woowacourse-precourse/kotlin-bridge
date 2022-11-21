d49c1bb1ab81a5177f240956b5ffd3fae5cbd4c1
brian <briandr@naver.com>
2022-11-22 02:01:22 +0900

    feat(GameController): bridgeGame과 view들을 이용해서 전체적인 게임을 진행하는 로직 구현

---
+++ src/main/kotlin/bridge/GameController.kt
@@ -0,0 +1,63 @@
+package bridge
+
+import bridge.common.GameCommand
+import bridge.common.GameResult
+import bridge.common.GameState
+import bridge.domain.BridgeGame
+import bridge.domain.constructor.BridgeMaker
+import bridge.view.InputView
+import bridge.view.OutputView
+
+class GameController(
+    private val inputView: InputView,
+    private val outputView: OutputView
+) {
+    private lateinit var bridgeGame: BridgeGame
+    private lateinit var gameState: GameState
+    private var tryCount: Int = 1
+
+    fun standByPhase(bridgeMaker: BridgeMaker) {
+        outputView.printGameStart()
+        outputView.printBridgeSizeRequest()
+        val bridgeSize = inputView.readBridgeSize()
+        bridgeGame = BridgeGame(bridgeMaker.makeBridge(bridgeSize))
+        gameState = GameState.IN_GAME
+    }
+
+    fun mainPhase() {
+        while (gameState == GameState.IN_GAME) {
+            play()
+            checkRetry()
+        }
+    }
+
+    fun endPhase() {
+        val gameResult = GameResult(
+            bridgeGame.currentUpperBridge,
+            bridgeGame.currentDownerBridge,
+            gameState,
+            tryCount
+        )
+        outputView.printResult(gameResult)
+    }
+
+    private fun play() {
+        outputView.printUpDownRequest()
+        gameState = bridgeGame.move(inputView.readMoving())
+        outputView.printMap(bridgeGame.currentUpperBridge, bridgeGame.currentDownerBridge)
+    }
+
+    private fun checkRetry() {
+        if (gameState != GameState.FAILURE) return
+        outputView.printRetryRequest()
+        if (inputView.readGameCommand() == GameCommand.R) {
+            retryGame()
+        }
+    }
+
+    private fun retryGame() {
+        bridgeGame.retry()
+        gameState = GameState.IN_GAME
+        tryCount++
+    }
+}
