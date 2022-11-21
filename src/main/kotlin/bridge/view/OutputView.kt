e1372ace646bf3a1f841cb25c5888b8ee6f2560b
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-22 01:15:42 +0900

    feat(view): OutputView.printStartGame() 함수 기능 구현

--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -1,9 +1,13 @@
 package bridge.view
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+import bridge.constants.Message
+
 object OutputView {
+
+    fun printStartGame() {
+        println("\n${Message.StartGame}")
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      *
