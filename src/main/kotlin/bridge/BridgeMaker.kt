2317970bd520d604443d221c0997952905908f3d
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-22 04:27:47 +0900

    feat(BridgeMaker): 랜덤 다리를 생성하는 makeBridge 메소드 구현

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import bridge.constants.Constant.Companion.START_INDEX
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -9,6 +11,13 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        val bridge = mutableListOf<String>()
+        for (count in START_INDEX until size) {
+            when (bridgeNumberGenerator.generate()) {
+                0 -> bridge.add("D")
+                1 -> bridge.add("U")
+            }
+        }
+        return bridge
     }
 }
