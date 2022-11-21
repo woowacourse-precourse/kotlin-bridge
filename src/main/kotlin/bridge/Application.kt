a5faa99bdbe892e47dc73a023fb775bbdcccc962
healthykim <bsbs8645@snu.ac.kr>
2022-11-21 21:05:28 +0900

    feat(Application): Add function for playing one game

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,25 @@
 package bridge
 
+val input = InputView()
+val output = OutputView()
+val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
+
 fun main() {
     // TODO: 프로그램 구현
+}
+
+/**
+ * @param bridge 이어진 다리의 정보
+ * @param game 브릿지 게임
+ * @return 브릿지 게임 플레이 결과
+ */
+fun playGame(size: Int, game: BridgeGame): Boolean {
+    var isGameOver = false
+    var index = 0
+    while(index != size && !isGameOver) {
+        var moving = input.readMoving()
+        isGameOver = !game.move(index++, moving)
+        output.printMap(game)
+    }
+    return !isGameOver
 }
