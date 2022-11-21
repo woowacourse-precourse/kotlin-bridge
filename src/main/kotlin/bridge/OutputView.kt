53e5c19d1642453554b14fb4f1481333fb1798eb
inseonyun <yuninseon@naver.com>
2022-11-21 21:07:41 +0900

    feat: 게임 진행 출력 함수 구현
    1. printStartGame - 터미널에 "다리 건너기 게임을 시작합니다." 출력
    2. printInputBridgeSize - 터미널에 "다리의 길이를 입력해주세요." 출력
    3. printInputMoveBridge - 터미널에 "이동할 칸을 선택해주세요. (위: U, 아래: D)" 출력
    4. printInputRetryOrExit - 터미널에 "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)" 출력

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -4,6 +4,22 @@ package bridge
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 class OutputView {
+    fun printStartGame() {
+        println("다리 건너기 게임을 시작합니다.")
+    }
+
+    fun printInputBridgeSize() {
+        println("다리의 길이를 입력해주세요.")
+    }
+
+    fun printInputMoveBridge() {
+        println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
+    }
+
+    fun printInputRetryOrExit() {
+        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      *
