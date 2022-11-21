ae65e5d98c56b9ebe41f4d6d84c5f92df7499285
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-22 02:43:24 +0900

    feat(OutputView): 입력 관련 메세지를 출력하는 메소드들 구현

--- src/main/kotlin/bridge/views/OutputView.kt
+++ src/main/kotlin/bridge/views/OutputView.kt
@@ -1,15 +1,14 @@
 package bridge.views
 
+import bridge.constants.String.Companion.STRING_INPUT_LENGTH
+import bridge.constants.String.Companion.STRING_INPUT_MOVING
+import bridge.constants.String.Companion.STRING_INPUT_RETRY
 import bridge.constants.String.Companion.STRING_START_GAME
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 class OutputView {
-    fun printStart() {
-        println(STRING_START_GAME)
-    }
-
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      *
@@ -23,4 +22,20 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printResult() {}
+
+    fun printStart() {
+        println(STRING_START_GAME)
+    }
+
+    fun printInputBridgeLength() {
+        println(STRING_INPUT_LENGTH)
+    }
+
+    fun printInputMoving() {
+        println(STRING_INPUT_MOVING)
+    }
+
+    fun printInputRetry() {
+        println(STRING_INPUT_RETRY)
+    }
 }
