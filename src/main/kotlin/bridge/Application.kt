682060f162c8fa7ef01417b719a25a178e1ddd68
dlgocks1 <cksgodl1@naver.com>
2022-11-22 02:53:48 +0900

    refactor(Application.kt) : BridgeGameController를 중간에 사용하는 방식으로 구조 변경

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,42 +1,20 @@
 package bridge
 
-import bridge.model.BridgeResult
+import bridge.controller.BridgeGameController
 import bridge.view.InputView
 import bridge.view.OutputView
-
+import java.lang.Exception
 
 fun main() {
-    val bridgeGame = BridgeGame()
-    val inputView = InputView()
-    val outputView = OutputView()
-
-    initBridgeGame(inputView, bridgeGame)
-    with(bridgeGame) {
-        play(inputView, outputView)
+    try {
+        val bridgeGameController = BridgeGameController(
+            bridgeGame = BridgeGame(),
+            inputView = InputView(),
+            outputView = OutputView()
+        )
+        bridgeGameController.play()
+    } catch (error: Exception) {
+        println(error.message)
     }
 }
 
-private fun BridgeGame.play(inputView: InputView, outputView: OutputView) {
-    do {
-        val result = move(inputView.readMoving())
-        outputView.printMap()
-        calResult(result, outputView, inputView)
-    } while (true)
-}
-
-private fun BridgeGame.calResult(
-    result: BridgeResult,
-    outputView: OutputView,
-    inputView: InputView
-) {
-    when (result) {
-        BridgeResult.FINISH -> outputView.printResult()
-        BridgeResult.SUCCESS -> return
-        BridgeResult.FAIL -> retry(inputView.readGameCommand())
-    }
-}
-
-private fun initBridgeGame(inputView: InputView, bridgeGame: BridgeGame) {
-    inputView.gameStart()
-    bridgeGame.setBridge(bridgeGame.generateRandomBridge(inputView.readBridgeSize()))
-}
