8ab99a5ca6648eb34e5bd7ebdf7efa3f375a256b
swjw1110 <swjw1110@naver.com>
2022-11-22 02:17:44 +0900

    feat: add gaming logic
    what is added:
    - make movement logic
      - input: prevList, movementInput
      - output: newList(which will be current state)
    - make retry logic
      - input: command player write
      - output: true if retry / false if not / exception else input

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -10,7 +10,10 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun move(
+        prevBridge: List<String>,
+        movement: String
+    ): List<String> = prevBridge + movement
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -18,5 +21,13 @@ class BridgeGame {
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry() {}
+    fun retry(
+        command: String
+    ): Boolean {
+        return when (command) {
+            "R" -> true
+            "Q" -> false
+            else -> throw java.lang.IllegalArgumentException("[ERROR] 잘못된 종료 입력입니다.")
+        }
+    }
 }
