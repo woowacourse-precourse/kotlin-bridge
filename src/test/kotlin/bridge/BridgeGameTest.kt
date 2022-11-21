f974dcf6a36380a9e9f780b4a724d055fd107c55
Hyemin Song <hyemdooly@gmail.com>
2022-11-21 13:45:01 +0900

    test (BridgeGameTest) : move, retry 구현

---
+++ src/test/kotlin/bridge/BridgeGameTest.kt
@@ -0,0 +1,54 @@
+package bridge
+
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class BridgeGameTest : NsTest() {
+    @Test
+    fun `move true 테스트`() {
+        val numberGenerator: BridgeNumberGenerator = TestNumberGenerator(listOf(1, 0, 0))
+        val bridgeMaker = BridgeMaker(numberGenerator)
+        val bridge: List<String> = bridgeMaker.makeBridge(3)
+        val bridgeGame = BridgeGame(bridge)
+        assertThat(bridgeGame.move("U")).isEqualTo(true)
+    }
+
+    @Test
+    fun `move false 테스트`() {
+        val numberGenerator: BridgeNumberGenerator = TestNumberGenerator(listOf(1, 0, 0))
+        val bridgeMaker = BridgeMaker(numberGenerator)
+        val bridge: List<String> = bridgeMaker.makeBridge(3)
+        val bridgeGame = BridgeGame(bridge)
+        bridgeGame.move("U")
+        assertThat(bridgeGame.move("U")).isEqualTo(false)
+    }
+
+    @Test
+    fun `retry 테스트`() {
+        val numberGenerator: BridgeNumberGenerator = TestNumberGenerator(listOf(1, 0, 0))
+        val bridgeMaker = BridgeMaker(numberGenerator)
+        val bridge: List<String> = bridgeMaker.makeBridge(3)
+        val bridgeGame = BridgeGame(bridge)
+        bridgeGame.move("U")
+        bridgeGame.move("U")
+        bridgeGame.retry()
+        assertThat(bridgeGame.tryNumber == 2 && bridgeGame.route.isEmpty()).isEqualTo(true)
+    }
+
+    override fun runMain() {
+        main()
+    }
+
+    class TestNumberGenerator(numbers: List<Int>) : BridgeNumberGenerator {
+        private val numbers: MutableList<Int> = numbers.toMutableList()
+
+        override fun generate(): Int {
+            return numbers.removeAt(0)
+        }
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE = "[ERROR]"
+    }
+}
