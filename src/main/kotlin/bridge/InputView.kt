3edf277e5a267b839813c7f2a1b5438e8d737de4
최재훈 <be_clever@naver.com>
2022-11-22 01:32:00 +0900

    feat(input): 게임 재시작/종료 여부를 확인하는 기능
    게임 재시작/종료 여부를 문자열로 입력 받고, 올바른 값이 아니면 예외를 발생시키는 기능 작성

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -36,6 +36,12 @@ class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        val input = readLine() ?: throw IllegalArgumentException()
+
+        if(input != "R" && input != "Q") {
+            throw IllegalArgumentException()
+        }
+
+        return input
     }
 }
