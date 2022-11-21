3aae825f5ed01e70fefd6be249c2da4d1c782717
Sujin Kim <sujinkim919@gmail.com>
2022-11-21 17:54:55 +0900

    feat(BridgeGameManager): 게임의 입출력과 예외 처리 위한 클래스 생성

---
+++ src/main/kotlin/bridge/domain/BridgeGameManager.kt
@@ -0,0 +1,11 @@
+package bridge.domain
+
+import bridge.view.InputView
+import bridge.view.OutputView
+
+class BridgeGameManager(
+    private val inputView: InputView,
+    private val outputView: OutputView
+) {
+
+}
