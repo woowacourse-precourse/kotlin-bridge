1d99017816ca9e3c71652edce4ed007a0dbaa285
inseonyun <yuninseon@naver.com>
2022-11-21 17:28:47 +0900

    feat: 게임 재시작/종료 입력 예외 클래스 생성
    isNotRetryOrExitCharacter - 입력된 값이 U 또는 D인지 check

--- src/main/kotlin/bridge/GameCommandException.kt
+++ src/main/kotlin/bridge/GameCommandException.kt
@@ -6,6 +6,7 @@ class GameCommandException(inputData: String) {
     }
 
     private fun isNotRetryOrExitCharacter(inputData: String) {
-        TODO("기능구현")
+        if(!(inputData == "R" || inputData == "Q"))
+            throw IllegalArgumentException("[ERROR] 입력한 값은 'R' 또는 'Q'가 아닙니다.")
     }
 }
