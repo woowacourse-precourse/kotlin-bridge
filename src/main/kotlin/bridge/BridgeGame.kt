b9fc55ded507183ec96bc3a29cd4182c2fea9146
Hyeongseon Jeon <hsun5163@gmail.com>
2022-11-22 01:40:31 +0900

    feat : move 함수 구현

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -18,7 +18,13 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun move(movingKey : String) : Boolean{
+        userBridge.addUserBridge(movingKey)
+        if(comBridge?.let { userBridge.bridgeComparison(it) } == true){
+            return true
+        }
+        return false
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
