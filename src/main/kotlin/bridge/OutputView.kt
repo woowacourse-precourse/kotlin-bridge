5d0e9501ce7bbe1b7945b862a947579c46126db5
leehandsub <gustjq7027@naver.com>
2022-11-22 02:34:30 +0900

    feat(OutputView) : 선택한 칸에 따라 결과에 따른 출력

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -2,6 +2,8 @@ package bridge
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ * OutputView의 메서드의 이름은 변경할 수 없고, 인자와 반환 타입은 필요에 따라 추가하거나 변경할 수 있다.
+ * 값 출력을 위해 필요한 메서드를 추가할 수 있다.
  */
 class OutputView {
     /**
@@ -9,7 +11,33 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(compareBridge: List<Pair<String, Boolean>>) {
+        var printMapBridge = "["
+        printMapBridge += nowBridgeResult(compareBridge, "U")
+        printMapBridge += "]\n["
+        printMapBridge += nowBridgeResult(compareBridge, "D")
+        printMapBridge += "]"
+        println(printMapBridge)
+    }
+
+    private fun nowBridgeResult(compareBridge: List<Pair<String, Boolean>>, nowState: String): String {
+        var nowPrintMapBridge = ""
+        for (compareBridgeIndex in compareBridge.indices) {
+            nowPrintMapBridge += stateBridge(compareBridge[compareBridgeIndex], nowState)
+            if (compareBridgeIndex != compareBridge.size - 1)
+                nowPrintMapBridge += "|"
+        }
+        return nowPrintMapBridge
+    }
+
+    private fun stateBridge(nowCompareBridge: Pair<String, Boolean>, nowState: String): String {
+        if (nowCompareBridge.first == nowState && nowCompareBridge.second)
+            return " O "
+        else if (nowCompareBridge.first == nowState)
+            return " X "
+        return "   "
+    }
+
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
