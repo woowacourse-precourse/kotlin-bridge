de44bd7a3dbfe4372bea8e8ecb418d2eb0eb683f
dev-worthwhile <dev.lboram@gmail.com>
2022-11-22 01:56:31 +0900

    fix<InputView>: check game command

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -45,7 +45,7 @@ class InputView {
         while (validFlag) {
             println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
             gameCommand = cs.readLine()
-            validFlag = needToAskMoveAgain(gameCommand)
+            validFlag = needToAskCommandAgain(gameCommand)
         }
         return gameCommand
     }
