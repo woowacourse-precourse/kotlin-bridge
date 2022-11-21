9c97c7ed693443b2a39d93ecc99e0ad11a0d395e
lkeonwoo94 <lkeonw0o@naver.com>
2022-11-21 18:25:59 +0900

    feat,test($OutputView) : create $OutputView
    - ⌨️ Service Logic : OutputView
    Added new testsuite, function($printGameStart) to $OutputView:
    1. 게임시작 문구 `printGameStart()`
       - [x] 콘솔출력: `다리 건너기 게임을 시작합니다.`
    Added new testsuite, function($printMap) to $OutputView:
    2. 현재까지 이동한 다리 출력 `printMap()`
       - [x] 콘솔출력
    Added new testsuite, function($printResult) to $OutputView:
    3. 게임 종료 문구 `printGameResult()`
       - [x] 콘솔출력: `최종 게임 결과`
       - [x] 콘솔출력: 이동한 다리 출력 `printMap()`
       - [x] 콘솔출력: `게임 성공 여부 : ${isSuccess}`
       - [x] 콘솔출력: `총 시도한 횟수: ${round}`
    
    functional requirements:
    - 제공된 `OutputView` 클래스를 활용해 구현해야 한다.
    - `OutputView`의 패키지는 변경할 수 있다.
    - `OutputView`의 메서드의 이름은 변경할 수 없고, 인자와 반환 타입은
       필요에 따라 추가하거나 변경할 수 있다.
    - 값 출력을 위해 필요한 메서드를 추가할 수 있다.
    
    Non-functional requirements:
    - indent(인덴트, 들여쓰기) depth를 3이 넘지 않도록 구현한다.
    - 함수(또는 메서드)의 길이가 10라인을 넘어가지 않도록 구현한다.
    - 함수(또는 메서드)가 한 가지 일만 잘 하도록 구현한다.
    - 메서드의 파라미터 개수는 최대 3개까지만 허용한다.

--- src/test/kotlin/bridge/UI/UITest.kt
+++ src/test/kotlin/bridge/UI/UITest.kt
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions.assertThat
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 open class InputViewTest: NsTest() {
     class readBridgeSizeTest: InputViewTest() {
@@ -127,5 +129,201 @@ open class InputViewTest: NsTest() {
     }
     companion object {
         private const val ERROR_MESSAGE = "[ERROR]"
+    }
+}
+
+open class OutputViewTest: NsTest() {
+    class printGameStartTest: OutputViewTest() {
+        @Test
+        @DisplayName("게임 시작 메시지를 출력한다.")
+        fun `print GameStart Message`() {
+            assertSimpleTest {
+                OutputView().printGameStart()
+                assertThat(output()).contains(GAME_START_MESSAGE)
+            }
+        }
+    }
+
+    class printMapTest: OutputViewTest() {
+        @ParameterizedTest
+        @ValueSource(strings = ["UDU"])
+        @DisplayName("맵을 출력한다. 0")
+        fun `print Map Test 0 `(Moving: String) {
+            assertSimpleTest {
+                val numberGenerator: BridgeNumberGenerator =
+                    BrigeGameTest.TestNumberGenerator(listOf(1, 0, 1))
+                val bridgeGame = BridgeGame(BridgeMaker(numberGenerator),3)
+                Moving.forEachIndexed { index, it -> bridgeGame.move(it.toString(), index)}
+                OutputView().printMap(bridgeGame.getCurBridge())
+                assertThat(output()).contains(
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                )
+            }
+        }
+        @ParameterizedTest
+        @ValueSource(strings = ["UDU"])
+        @DisplayName("맵을 출력한다. 1")
+        fun `print Map Test 1 `(Moving: String) {
+            assertSimpleTest {
+                val numberGenerator: BridgeNumberGenerator =
+                    BrigeGameTest.TestNumberGenerator(listOf(1, 0, 0))
+                val bridgeGame = BridgeGame(BridgeMaker(numberGenerator),3)
+                Moving.forEachIndexed { index, it -> bridgeGame.move(it.toString(), index)}
+                OutputView().printMap(bridgeGame.getCurBridge())
+                assertThat(output()).contains(
+                    "[ O |   | X ]",
+                    "[   | O |   ]",
+                )
+            }
+        }
+        @ParameterizedTest
+        @ValueSource(strings = ["U"])
+        @DisplayName("맵을 출력한다. 2")
+        fun `print Map Test 2 `(Moving: String) {
+            assertSimpleTest {
+                val numberGenerator: BridgeNumberGenerator =
+                    BrigeGameTest.TestNumberGenerator(listOf(1, 0, 0))
+                val bridgeGame = BridgeGame(BridgeMaker(numberGenerator),3)
+                Moving.forEachIndexed { index, it -> bridgeGame.move(it.toString(), index)}
+                OutputView().printMap(bridgeGame.getCurBridge())
+                assertThat(output()).contains(
+                    "[ O ]",
+                    "[   ]",
+                )
+            }
+        }
+        @ParameterizedTest
+        @ValueSource(strings = ["D"])
+        @DisplayName("맵을 출력한다. 3")
+        fun `print Map Test 3 `(Moving: String) {
+            assertSimpleTest {
+                val numberGenerator: BridgeNumberGenerator =
+                    BrigeGameTest.TestNumberGenerator(listOf(1, 0, 0))
+                val bridgeGame = BridgeGame(BridgeMaker(numberGenerator),3)
+                Moving.forEachIndexed { index, it -> bridgeGame.move(it.toString(), index)}
+                OutputView().printMap(bridgeGame.getCurBridge())
+                assertThat(output()).contains(
+                    "[   ]",
+                    "[ X ]",
+                )
+            }
+        }
+        @ParameterizedTest
+        @ValueSource(strings = ["UD"])
+        @DisplayName("맵을 출력한다. 4")
+        fun `print Map Test 4 `(Moving: String) {
+            assertSimpleTest {
+                val numberGenerator: BridgeNumberGenerator =
+                    BrigeGameTest.TestNumberGenerator(listOf(1, 0, 0))
+                val bridgeGame = BridgeGame(BridgeMaker(numberGenerator),3)
+                Moving.forEachIndexed { index, it -> bridgeGame.move(it.toString(), index)}
+                OutputView().printMap(bridgeGame.getCurBridge())
+                assertThat(output()).contains(
+                    "[ O |   ]",
+                    "[   | O ]"
+                )
+            }
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = ["UU"])
+        @DisplayName("맵을 출력한다. 5")
+        fun `print Map Test 5 `(Moving: String) {
+            assertSimpleTest {
+                val numberGenerator: BridgeNumberGenerator =
+                    BrigeGameTest.TestNumberGenerator(listOf(1, 0, 0))
+                val bridgeGame = BridgeGame(BridgeMaker(numberGenerator),3)
+                Moving.forEachIndexed { index, it -> bridgeGame.move(it.toString(), index)}
+                OutputView().printMap(bridgeGame.getCurBridge())
+                assertThat(output()).contains(
+                    "[ O | X ]",
+                    "[   |   ]"
+                )
+            }
+        }
+    }
+
+    class printResultTest: OutputViewTest() {
+        @ParameterizedTest
+        @ValueSource(strings = ["UDU"])
+        @DisplayName("최종 결과을 출력한다. 0")
+        fun `print Result Test 0 `(Moving: String) {
+            assertSimpleTest {
+                val numberGenerator: BridgeNumberGenerator =
+                    BrigeGameTest.TestNumberGenerator(listOf(1, 0, 1))
+                val bridgeGame = BridgeGame(BridgeMaker(numberGenerator),3)
+                Moving.forEachIndexed { index, it -> bridgeGame.move(it.toString(), index)}
+                OutputView().printResult(bridgeGame.getCurBridge(), bridgeGame)
+                assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+                )
+            }
+        }
+        @ParameterizedTest
+        @ValueSource(strings = ["UU"])
+        @DisplayName("최종 결과을 출력한다. 1")
+        fun `print Result Test 1 `(Moving: String) {
+            assertSimpleTest {
+                val numberGenerator: BridgeNumberGenerator =
+                    BrigeGameTest.TestNumberGenerator(listOf(1, 0, 1))
+                val bridgeGame = BridgeGame(BridgeMaker(numberGenerator),3)
+                Moving.forEachIndexed { index, it -> bridgeGame.move(it.toString(), index)}
+                OutputView().printResult(bridgeGame.getCurBridge(), bridgeGame)
+                assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+                )
+            }
+        }
+
+        @Test
+        @DisplayName("최종 결과을 출력한다. 2")
+        fun `print Result Test 2 `() {
+            assertSimpleTest {
+                var Moving = "UU"
+                val numberGenerator: BridgeNumberGenerator =
+                    BrigeGameTest.TestNumberGenerator(listOf(1, 0, 1))
+                val bridgeGame = BridgeGame(BridgeMaker(numberGenerator),3)
+                Moving.forEachIndexed { index, it -> bridgeGame.move(it.toString(), index)}
+                bridgeGame.retry("R")
+                Moving = "UDU"
+                Moving.forEachIndexed { index, it -> bridgeGame.move(it.toString(), index)}
+                OutputView().printResult(bridgeGame.getCurBridge(), bridgeGame)
+                assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+                )
+            }
+        }
+    }
+    override fun runMain() {
+        printGameStartTest()
+        printMapTest()
+        printResultTest()
+    }
+
+    class TestNumberGenerator(numbers: List<Int>) : BridgeNumberGenerator {
+        private val numbers: MutableList<Int> = numbers.toMutableList()
+
+        override fun generate(): Int {
+            return numbers.removeAt(0)
+        }
+    }
+    companion object {
+        private const val GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다."
+        private const val GAME_RESULT_MESSAGE = "최종 게임 결과"
+        private const val GAME_SUCCESS_MESSAGE = "게임 성공 여부 : "
+        private const val GAME_TRY_MESSAGE = "총 시도한 횟수 : "
     }
 }
