9cb1485f89556a9fe6ee9748ce23c57e70ac2251
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-21 13:46:00 +0900

    refactor(controller): 다리 생성 클래스 리팩터링
    makeBridge 메서드 단일식으로 변경
    원시값을 상수로 변경

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -10,15 +10,21 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    fun makeBridge(size: Int): List<String> {
-        return (1..size).map { createBridgeElement(bridgeNumberGenerator.generate()) }.toList()
-    }
+    fun makeBridge(size: Int): List<String> =
+        (1..size).map { createBridgeElement(bridgeNumberGenerator.generate()) }.toList()
 
     private fun createBridgeElement(bridgeNumber: Int): String {
-        return when(bridgeNumber) {
-            0 -> "D"
-            1 -> "U"
-            else -> { throw IllegalArgumentException("다리의 칸을 생성하려면 0또는 1만 입력해야 합니다.") }
+        return when (bridgeNumber) {
+            RANDOM_LOWER_INCLUSIVE -> "D"
+            RANDOM_UPPER_INCLUSIVE -> "U"
+            else -> {
+                throw IllegalArgumentException("다리의 칸을 생성하려면 0또는 1만 가능합니다.")
+            }
         }
+    }
+
+    companion object {
+        private const val RANDOM_LOWER_INCLUSIVE = 0
+        private const val RANDOM_UPPER_INCLUSIVE = 1
     }
 }
