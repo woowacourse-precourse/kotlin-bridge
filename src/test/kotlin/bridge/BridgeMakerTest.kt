1bb05a5f89aa001f3cf822a71d94434a7dd74256
changhwan <chang5hustle@gmail.com>
2022-11-21 20:09:22 +0900

    test(bridge) : 다리생성 추가 테스트

--- src/test/kotlin/bridge/BridgeMakerTest.kt
+++ src/test/kotlin/bridge/BridgeMakerTest.kt
@@ -7,8 +7,20 @@ import org.junit.jupiter.api.Test
 class BridgeMakerTest {
 
     @Test
-    fun `다리생성 함수 테스트`() {
+    fun `다리생성 함수 테스트 1`() {
         val bridgeMaker = BridgeMaker(StubBridgeNumberGenerator(mutableListOf(0, 1, 1, 1, 0, 1)))
         assertThat(bridgeMaker.makeBridge(6)).isEqualTo(listOf("D", "U", "U", "U", "D", "U"))
+    }
+
+    @Test
+    fun `다리생성 함수 테스트 2`() {
+        val bridgeMaker = BridgeMaker(StubBridgeNumberGenerator(mutableListOf(0, 0, 1, 0, 0, 1, 1, 0)))
+        assertThat(bridgeMaker.makeBridge(8)).isEqualTo(listOf("D", "D", "U", "D", "D", "U", "U", "D"))
+    }
+
+    @Test
+    fun `다리생성 함수 테스트 3`() {
+        val bridgeMaker = BridgeMaker(StubBridgeNumberGenerator(mutableListOf(1, 0, 1)))
+        assertThat(bridgeMaker.makeBridge(3)).isEqualTo(listOf("U", "D", "U"))
     }
 }
