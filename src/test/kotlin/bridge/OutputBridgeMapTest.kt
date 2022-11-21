d1a16f214b5937685443f45086c681d2435aa13d
DormunDoorlock <skrudgh2001@naver.com>
2022-11-21 21:57:13 +0900

    refactor: OutputView() 구조 바뀜에 test parameter 수정

--- src/test/kotlin/bridge/OutputBridgeMapTest.kt
+++ src/test/kotlin/bridge/OutputBridgeMapTest.kt
@@ -9,42 +9,42 @@ class OutputBridgeMapTest : NsTest(){
     @Test
     fun `Up 정답 다리 출력 테스트`() {
         assertSimpleTest {
-            OutputView().printMapTrue(2,listOf("U","D","D"), "U")
+            OutputView(listOf("U","D","D")).printMapTrue(2, "U")
             Assertions.assertThat(output()).contains("[ O |   |   ]")
         }
     }
     @Test
     fun `Down 정답 다리 출력 테스트`() {
         assertSimpleTest {
-            OutputView().printMapTrue(2,listOf("U","D","D"), "D")
+            OutputView(listOf("U","D","D")).printMapTrue(2, "D")
             Assertions.assertThat(output()).contains("[   | O | O ]")
         }
     }
     @Test
     fun `정답 다리 출력 테스트`() {
         assertSimpleTest {
-            OutputView().printMap(2,listOf("U","D","D"), true)
+            OutputView(listOf("U","D","D")).printMap(2, true)
             Assertions.assertThat(output()).contains("[ O |   |   ]\n[   | O | O ]")
         }
     }
     @Test
     fun `Up 오답 다리 출력 테스트`() {
         assertSimpleTest {
-            OutputView().printMapFalse(2,listOf("U","D","D"), "U")
+            OutputView(listOf("U","D","D")).printMapFalse(2, "U")
             Assertions.assertThat(output()).contains("[ O |   | X ]")
         }
     }
     @Test
     fun `Down 오답 다리 출력 테스트`() {
         assertSimpleTest {
-            OutputView().printMapFalse(2,listOf("U","D","D"), "D")
+            OutputView(listOf("U","D","D")).printMapFalse(2, "D")
             Assertions.assertThat(output()).contains("[   | O |   ]")
         }
     }
     @Test
     fun `오답 다리 출력 테스트`() {
         assertSimpleTest {
-            OutputView().printMap(2,listOf("U","D","D"), false)
+            OutputView(listOf("U","D","D")).printMap(2, false)
             Assertions.assertThat(output()).contains("[ O |   | X ]\n[   | O |   ]")
         }
     }
