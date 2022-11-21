fdf0a61e1465dbe1a0601beaebe800b38b0c057a
2taezeat <xorudfl772@naver.com>
2022-11-21 17:58:00 +0900

    feat(OutputView): 현재까지 이동한 다리의 상태를 출력하는 기능 구현

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -3,10 +3,31 @@ package bridge
 fun main() {
     printStartGame()
     val inputView = InputView()
-    inputView.readBridgeSize()
+    val bridgeSize = inputView.readBridgeSize()
+    val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
+    val bridge = bridgeMaker.makeBridge(bridgeSize)
+    println(bridge)
 
-    inputView.readMoving()
-    inputView.readGameCommand()
+
+    var moveCount = 0
+    val userMoveDirections = mutableListOf<String>()
+
+    while (moveCount < bridgeSize) {
+        val userMoveDirection = inputView.readMoving()
+        userMoveDirections.add(userMoveDirection)
+
+        val outputView = OutputView(bridge, userMoveDirections, moveCount)
+        outputView.printMap()
+
+        moveCount += 1
+
+    }
+//    inputView.readMoving()
+//    val outputView = OutputView(bridge)
+
+    //inputView.readMoving()
+    //inputView.readGameCommand()
+
 }
 
 
--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -3,18 +3,131 @@ package bridge
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
-class OutputView {
+class OutputView(
+    private val bridge: List<String>,
+    private val moveDirections: List<String>,
+    private val moveCount: Int
+) {
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap() {
+        for (i in moveDirections.indices) {
+            printTopLine(bridge[i], moveDirections[i], i)
+        }
+        for (i in moveDirections.indices) {
+            printBottomLine(bridge[i], moveDirections[i], i)
+        }
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+    fun printResult() {
+
+    }
+
+
+    private fun printTopLine(bridgeDirection: String, moveDirection: String, index: Int) {
+        if (moveDirection == Moving.UP.getMovDirection()) {
+            printBridgeOneLine(bridgeDirection, moveDirection, index)
+        } else if (moveDirection == Moving.DOWN.getMovDirection()) {
+            printBridgeOneLine(bridgeDirection, BLANK, index)
+        }
+    }
+
+    private fun printBottomLine(bridgeDirection: String, moveDirection: String, index: Int) {
+        if (moveDirection == Moving.DOWN.getMovDirection()) {
+            printBridgeOneLine(bridgeDirection, moveDirection, index)
+        } else if (moveDirection == Moving.UP.getMovDirection()) {
+            printBridgeOneLine(bridgeDirection, BLANK, index)
+        }
+    }
+
+
+    private fun printBridgeOneLine(bridgeDirection: String, userDirection: String, count: Int) {
+        if (count == ZERO_VALUE) {
+            printBridgeHead(bridgeDirection, userDirection)
+            if (moveDirections.size == ONE_VALUE) printBridgeEnd()
+        } else {
+            printBridgeMiddle(bridgeDirection, userDirection)
+            if (moveDirections.size - ONE_VALUE == count) printBridgeEnd()
+        }
+    }
+
+
+    private fun printBottomBridge(bridgeDirection: String, userDirection: String, count: Int) {
+        if (count == ZERO_VALUE) {
+            printBridgeHead(bridgeDirection, userDirection)
+            if (moveDirections.size == ONE_VALUE) printBridgeEnd()
+        } else {
+            printBridgeMiddle(bridgeDirection, userDirection)
+            if (moveDirections.size - ONE_VALUE == count) printBridgeEnd()
+        }
+    }
+
+
+    private fun printBridgeHead(bridgeDirection: String, userDirection: String) {
+        printBridgeStart()
+        printBridgeBlank()
+        when (userDirection) {
+            bridgeDirection -> printMovingPossible()
+            BLANK -> printBridgeBlank()
+            else -> printMovingNotPossible()
+        }
+        printBridgeBlank()
+    }
+
+    private fun printBridgeMiddle(bridgeDirection: String, userDirection: String) {
+        printBridgeDivision()
+        printBridgeBlank()
+        when (userDirection) {
+            bridgeDirection -> printMovingPossible()
+            BLANK -> printBridgeBlank()
+            else -> printMovingNotPossible()
+        }
+        printBridgeBlank()
+    }
+
+
+    private fun printBridgeStart() {
+        print(BRIDGE_START)
+    }
+
+    private fun printBridgeEnd() {
+        print(BRIDGE_END)
+        println()
+    }
+
+    private fun printBridgeDivision() {
+        print(BRIDGE_DIVISION)
+    }
+
+    private fun printBridgeBlank() {
+        print(BLANK)
+    }
+
+    private fun printMovingPossible() {
+        print(MOVING_POSSIBLE)
+    }
+
+    private fun printMovingNotPossible() {
+        print(MOVING_NOT_POSSIBLE)
+    }
+
+
+    companion object {
+        const val BRIDGE_START = "["
+        const val BRIDGE_END = "]"
+        const val BRIDGE_DIVISION = "|"
+        const val BLANK = " "
+        const val MOVING_POSSIBLE = "O"
+        const val MOVING_NOT_POSSIBLE = "X"
+        const val ZERO_VALUE = 0
+        const val ONE_VALUE = 1
+    }
 }
