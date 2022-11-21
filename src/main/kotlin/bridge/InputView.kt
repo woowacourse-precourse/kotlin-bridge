e1a94c3d0e3630f0356d3d567696246eba40d095
lsakee <lsls4868@gmail.com>
2022-11-21 19:12:58 +0900

    feat : 게임을 다시 시도할지 종료할지 선택하는 기능 -선택의 입력을 받는 기능

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -32,21 +32,29 @@ class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        val userInputNumber = inputUser()
+        val userInputMoving = inputUser()
         try {
-            bridge.util.InputVaildator().checkNullBridgeString(userInputNumber)
-            bridge.util.InputVaildator().checkSelectBridgePatten(userInputNumber)
+            bridge.util.InputVaildator().checkNullBridgeString(userInputMoving)
+            bridge.util.InputVaildator().checkSelectBridgePatten(userInputMoving)
         }catch (e:IllegalArgumentException){
             println("[ERROR]")
             return readMoving()
         }
-        return userInputNumber
+        return userInputMoving
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        val userInputRetry = inputUser()
+        try {
+            bridge.util.InputVaildator().checkNullBridgeString(userInputRetry)
+            bridge.util.InputVaildator().checkRetryPatten(userInputRetry)
+        }catch (e:IllegalArgumentException){
+            println("[ERROR]")
+            return readGameCommand()
+        }
+        return userInputRetry
     }
 }
