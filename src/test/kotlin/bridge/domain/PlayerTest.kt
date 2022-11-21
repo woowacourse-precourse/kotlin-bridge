2de70db4fb98cb02a110936a58c597f93055c1e2
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-11-21 16:19:36 +0900

    test(PlayerTest) : 테스트 코드 실패된 메소드 수정

--- src/test/kotlin/bridge/domain/PlayerTest.kt
+++ src/test/kotlin/bridge/domain/PlayerTest.kt
@@ -11,19 +11,24 @@ import org.junit.jupiter.params.provider.CsvSource
 internal class PlayerTest {
 
     private lateinit var numberGenerator: BridgeNumberGenerator
+    private lateinit var bridgeGame: BridgeGame
 
     @BeforeEach
     fun setUp() {
         numberGenerator = ApplicationTest.TestNumberGenerator(listOf(1, 0, 0))
+        bridgeGame = BridgeGame(numberGenerator, 3, player)
     }
 
     @ParameterizedTest
     @CsvSource("U,0,true", "D,1,true", "U,2,false")
     @DisplayName("이동할 명령어대로 이동하고, 기록을 잘하는 지 확인 및 다리 칸이랑 비교")
     fun `플레이어가_지나간_칸과_다리_비교_확인`(move: String, index: Int, flag: Boolean) {
-        val player = Player()
-        val bridgeGame = BridgeGame(numberGenerator, 3, player)
+
         player.go(move)
         Assertions.assertThat(bridgeGame.getBridge()[index] == player.getRecord()[index]).isEqualTo(flag)
+    }
+
+    companion object {
+        private val player = Player()
     }
 }
