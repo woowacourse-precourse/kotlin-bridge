98ec3e2ec8f2b026a41c36fba06b243b1aff2fb0
jklom0326 <jklom0326@gmail.com>
2022-11-22 08:22:32 +0900

    feat(Application.kt): 게임 시작

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,11 @@
 package bridge
 
 fun main() {
-    // TODO: 프로그램 구현
+    try {
+        val gameManager = GameManager()
+        gameManager.play()
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+        return
+    }
 }
