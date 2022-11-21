faf5798cacc3d41397a9748e2da070733e80bc31
dltkd1395 <dltkd13956@naver.com>
2022-11-22 00:59:55 +0900

    refactor(BridgeControllerTest) : 다리 길이, 다리 이동, 게임 재시작 여부 재입력 테스트 중복 수정

--- src/test/kotlin/bridge/BridgeControllerTest.kt
+++ src/test/kotlin/bridge/BridgeControllerTest.kt
@@ -7,45 +7,10 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class BridgeControllerTest : NsTest() {
-
     @Test
-    fun `다리 길이 잘못 입력 후 재입력`() {
+    fun `다리 길이, 다리 이동, 게임 재시작 여부 재입력 테스트`() {
         assertRandomNumberInRangeTest({
-            run("1", "", "U", "3", "U", "D", "D")
-            assertThat(output()).contains(
-                "최종 게임 결과",
-                "[ O |   |   ]",
-                "[   | O | O ]",
-                "게임 성공 여부: 성공",
-                "총 시도한 횟수: 1"
-            )
-            val upSideIndex = output().indexOf("[ O |   |   ]")
-            val downSideIndex = output().indexOf("[   | O | O ]")
-            assertThat(upSideIndex).isLessThan(downSideIndex)
-        }, 1, 0, 0)
-    }
-
-    @Test
-    fun `다리 이동 잘못 입력 후 재입력`() {
-        assertRandomNumberInRangeTest({
-            run("3", "", "u", "3", "U", "D", "D")
-            assertThat(output()).contains(
-                "최종 게임 결과",
-                "[ O |   |   ]",
-                "[   | O | O ]",
-                "게임 성공 여부: 성공",
-                "총 시도한 횟수: 1"
-            )
-            val upSideIndex = output().indexOf("[ O |   |   ]")
-            val downSideIndex = output().indexOf("[   | O | O ]")
-            assertThat(upSideIndex).isLessThan(downSideIndex)
-        }, 1, 0, 0)
-    }
-
-    @Test
-    fun `게임 재시작 여부 잘못 입력 후 재입력`() {
-        assertRandomNumberInRangeTest({
-            run("3", "U", "D", "U", "", "r", "1", "R", "U", "D", "D")
+            run("1", "", "U", "3", "1", "", "u", "U", "D", "U", "", "r", "1", "R", "U", "D", "D")
             assertThat(output()).contains(
                 "최종 게임 결과",
                 "[ O |   |   ]",
