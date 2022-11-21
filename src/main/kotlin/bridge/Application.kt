88e1210f2462855bda51c76225c7078fd7ae49b7
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2022-11-22 03:23:05 +0900

    feat: 이동한 결과를 계산하는 기능 구현

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -3,7 +3,9 @@ package bridge
 fun main() {
     OutputView().printStart()
     println()
+
     val bridgeSize = InputView().readBridgeSize()
     val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)
-    val moving = InputView().readMoving()
+
+    val bridgeGame = BridgeGame(bridge)
 }
--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -1,16 +1,44 @@
 package bridge
 
+import constant.Message
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
+class BridgeGame(val bridge: List<String>) {
+
+    private val records = mutableListOf<Pair<String, String>>()
+
+    init {
+        move()
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    private fun move() {
+        var attempt = 0
+        for (i in bridge.indices) {
+            val success = calcResult()
+            attempt++
+            if (!success) {
+                break
+            }
+        }
+    }
+
+    private fun calcResult(): Boolean {
+        val moving = InputView().readMoving()
+        if (bridge[records.size] == moving) {
+            records.add(Pair(moving, Message.SUCCESS.message))
+            return true
+        }
+        records.add(Pair(moving, Message.FAIL.message))
+        return false
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
