134fe93ae6d2bfbe86ecc2f7cb2cc05ed537d6f6
dlgocks1 <cksgodl1@naver.com>
2022-11-21 12:31:49 +0900

    style(BridgeMarker.kt) : 오타수정

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -11,7 +11,7 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        require(size in BRIDGE_MIN_LENGTH..BRIDGE_MaX_LENGTH)
+        require(size in BRIDGE_MIN_LENGTH..BRIDGE_MAX_LENGTH)
         return List(size) {
             if (bridgeNumberGenerator.generate() == RANDOM_UPPER_INCLUSIVE) "U" else "D"
         }
@@ -19,7 +19,7 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
 
     companion object {
         private const val BRIDGE_MIN_LENGTH = 3
-        private const val BRIDGE_MaX_LENGTH = 20
+        private const val BRIDGE_MAX_LENGTH = 20
     }
 
 }
