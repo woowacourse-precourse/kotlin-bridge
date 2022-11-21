11dec462bc08bea13ac1ab76689e1346da2c1af1
2taezeat <xorudfl772@naver.com>
2022-11-21 20:04:07 +0900

    feat(BridgeGame): 사용자가 게임을 다시 시도하는 기능

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -25,6 +25,7 @@ class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry(userGameCommand: String): Boolean {
+        return userGameCommand != GameCommand.QUIT.getGameCommand()
 
     }
 }
