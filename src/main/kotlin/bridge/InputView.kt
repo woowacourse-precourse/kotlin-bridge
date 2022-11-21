e2a7db7f0e3efad7a49fb9fd0cf4f5e985653b86
최재훈 <be_clever@naver.com>
2022-11-22 01:31:28 +0900

    feat(input): 플레이어가 이동할 칸을 입력 받는 기능
    플레이어가 이동할 칸을 문자열로 입력 받고, 올바른 값이 아니면 예외를 발생시키는 기능 작성

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -23,7 +23,13 @@ class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        val input = readLine() ?: throw IllegalArgumentException()
+
+        if(input != "U" && input != "D") {
+            throw IllegalArgumentException()
+        }
+
+        return input
     }
 
     /**
