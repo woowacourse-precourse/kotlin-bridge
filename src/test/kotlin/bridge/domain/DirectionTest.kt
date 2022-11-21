e4760648d93c7350b2480804ae404a78df445981
phs <gustjd0808@pusan.ac.kr>
2022-11-21 15:31:25 +0900

    test(domain): test to get Direction by number

--- src/test/kotlin/bridge/domain/DirectionTest.kt
+++ src/test/kotlin/bridge/domain/DirectionTest.kt
@@ -23,4 +23,19 @@ class DirectionTest {
             Direction.getDirection(input)
         }
     }
+
+    @Test
+    fun `getDirection 메서드에 방향을 나타내는 숫자를 인자로 주었을 때 그에 맞는 Direction 객체를 반환`() {
+        val input = listOf(0, 1)
+        val result = input.map { Direction.getDirection(it) }
+        assertThat(result).isEqualTo(listOf(Direction.DOWN, Direction.UP))
+    }
+
+    @ValueSource(ints = [-1, 2])
+    @ParameterizedTest
+    fun `getDirection 메서드에 방향을 나타내는 숫자가 아닌 것을 인자로 주었을 때 예외 발생`(input: Int) {
+        assertThrows<IllegalArgumentException> {
+            Direction.getDirection(input)
+        }
+    }
 }
