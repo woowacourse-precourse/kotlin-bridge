944946e19e35eaaff1e3b49673cc149e558be6f0
inseonyun <yuninseon@naver.com>
2022-11-21 17:31:15 +0900

    feat: 다리 길이 입력 함수 구현
    readBridgeSize - 다리 길이 입력 받고, 예외처리 후 return

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,7 +10,10 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        val bridgeSize = Console.readLine()
+        BridgeSizeException(bridgeSize)
+
+        return bridgeSize.toInt()
     }
 
     /**
