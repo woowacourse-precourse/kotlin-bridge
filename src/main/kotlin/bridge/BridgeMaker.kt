f219ecce722747bab1ece8889fbc708d6d7aa793
dlgocks1 <cksgodl1@naver.com>
2022-11-21 10:17:27 +0900

    feat(BridgeMaker.kt) : 브릿시 생성 기능 구현

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -1,5 +1,8 @@
 package bridge
 
+import bridge.BridgeRandomNumberGenerator.Companion.RANDOM_UPPER_INCLUSIVE
+import bridge.model.Bridge
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -9,6 +12,15 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        require(size in BRIDGE_MIN_LENGTH..BRIDGE_MaX_LENGTH)
+        return List(size) {
+            if (bridgeNumberGenerator.generate() == RANDOM_UPPER_INCLUSIVE) "U" else "D"
+        }
     }
+
+    companion object {
+        private const val BRIDGE_MIN_LENGTH = 3
+        private const val BRIDGE_MaX_LENGTH = 20
+    }
+
 }
