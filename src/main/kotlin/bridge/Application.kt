b70c040daf1e00fde1443709fd9247132d1809cf
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-11-22 00:59:56 +0900

    feat(Application): 게임을 시작하는 기능 구현

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -11,6 +11,20 @@ private val bridgeGameController = BridgeGameController()
 fun main() {
     outputView.printStartMent()
     val bridge = generateBridge(readBridgeSize())
+    bridgeGameController.play(bridge)
+}
+
+private fun readBridgeSize(): Int {
+    outputView.printReadBridgeSizeMent()
+    do {
+        try {
+            return inputView.readBridgeSize()
+        } catch (e: IllegalArgumentException) {
+            outputView.printErrorMessage(e.message.toString())
+        }
+    } while (true)
+}
+
 private fun generateBridge(size: Int): List<String> {
     val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
     return bridgeMaker.makeBridge(size)
