700a95f3a8d8eb08cc72aaefb082d7d37f6705e9
Hyuna Kim <akxmcse@gmail.com>
2022-11-22 00:26:44 +0900

    feat(Error): 에러 출력 관련 Enum class 구현

---
+++ src/main/kotlin/util/Error.kt
@@ -0,0 +1,9 @@
+package util
+
+enum class Error (val message: String) {
+    BRIDGE_LENGTH_INPUT_TYPE_ERROR("[ERROR] 다리 길이는 숫자여야 합니다."),
+    MOVE_INPUT_TYPE_ERROR("[ERROR] 이동할 칸은 문자여야 합니다."),
+    BRIDGE_LENGTH_INPUT_RANGE_ERROR("[ERROR] 다리 길이는 3 이상 20 이하의 숫자여야 합니다."),
+    MOVE_INPUT_LETTER_ERROR("[ERROR] 이동할 칸은 U 또는 D의 문자여야 합니다."),
+    INPUT_NUMBER_ERROR("[ERROR] 입력값은 하나여야 합니다."),
+}
