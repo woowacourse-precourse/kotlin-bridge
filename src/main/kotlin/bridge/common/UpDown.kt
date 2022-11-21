6e1141ddd36b14d54e58cf89cd8a07c12cdc38c5
brian <briandr@naver.com>
2022-11-21 23:35:19 +0900

    feat(UpDown): 틀렸을 때에 대한 변수도 enum클래스에 추가

--- src/main/kotlin/bridge/common/UpDown.kt
+++ src/main/kotlin/bridge/common/UpDown.kt
@@ -1,8 +1,8 @@
 package bridge.common
 
-enum class UpDown(val up: String, val down: String) {
-    D(" ", "O"),
-    U("O", " ");
+enum class UpDown(val up: String, val down: String, val upMismatch: String, val downMismatch: String) {
+    D(" ", "O", " ", "X"),
+    U("O", " ", "X", " ");
 
     companion object {
         private const val SHOULD_ZERO_OR_ONE = "[ERROR] 랜덤 생성 번호는 0 혹은 1이어야합니다."
