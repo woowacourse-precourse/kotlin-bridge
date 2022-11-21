99c26f4896fdba59b33f389ca92d8e562a403426
WoeunJung <leena09123@naver.com>
2022-11-21 14:28:44 +0900

    feat: make bridge

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -3,4 +3,5 @@ package bridge
 fun main() {
     OutputView().printStartGame()
     val bridgeSize = InputView().readBridgeSize()
+    val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)
 }
