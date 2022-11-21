907111d0adcdfa711c77e8515bfe7a5b7e6d6b19
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-21 17:41:43 +0900

    refactor[bridge]: userbridge 초기화하는 과정 함수 생성

--- src/main/kotlin/bridge/model/BridgeGame.kt
+++ src/main/kotlin/bridge/model/BridgeGame.kt
@@ -39,8 +39,12 @@ class BridgeGame(private val size : Int) {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry(){
-        userInputBridge = mutableListOf()
+        initUserBridge()
         gameCount++
+    }
+
+    fun initUserBridge() {
+        userInputBridge = mutableListOf()
     }
 
     fun addUserBridge(input: String) {
