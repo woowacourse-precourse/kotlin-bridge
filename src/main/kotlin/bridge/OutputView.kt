b078b98e3469c9a60e0f2da457abd7bb03d6533e
bluecat0823 <star4_you@naver.com>
2022-11-22 01:23:03 +0900

    [feat] 다리 위 아래 모양을 만드는 기능

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -9,7 +9,32 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(bridgeAnswer : List<String>, currentMove : Boolean, turnCount : Int) {
+    }
+
+    fun upperSide(bridgeAnswer : List<String>, currentMove : Boolean, turnCount : Int): List<String> {
+        val bridgeUpperSide = mutableListOf<String>()
+        for (turn in 0 until  turnCount){
+            if (bridgeAnswer[turn] == "U") bridgeUpperSide.add("O")
+            if (bridgeAnswer[turn] == "D") bridgeUpperSide.add(" ")
+            bridgeUpperSide.add(" | ")
+        }
+        if ((bridgeAnswer[turnCount] == "U") && currentMove) bridgeUpperSide.add("O")
+        if ((bridgeAnswer[turnCount] == "U") && !currentMove) bridgeUpperSide.add("X")
+        return bridgeUpperSide.toList()
+    }
+
+    fun downSide (bridgeAnswer : List<String>, currentMove : Boolean, turnCount : Int): List<String> {
+        val bridgeDownSide = mutableListOf<String>()
+        for (turn in 0 .. turnCount){
+            if (bridgeAnswer[turn] == "D") bridgeDownSide.add("O")
+            if (bridgeAnswer[turn] == "U") bridgeDownSide.add(" ")
+            bridgeDownSide.add(" | ")
+        }
+        if ((bridgeAnswer[turnCount] == "D") && currentMove) bridgeDownSide.add("O")
+        if ((bridgeAnswer[turnCount] == "D") && !currentMove) bridgeDownSide.add("X")
+        return bridgeDownSide.toList()
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
