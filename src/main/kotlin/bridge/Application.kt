c6646cf55581ee891fc63a4361c745682431e6b0
brian <briandr@naver.com>
2022-11-22 02:37:53 +0900

    feat(Application): Application에서 gameController를 만들어주고 게임의 각 페이즈를 진행한다.

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,13 @@
 package bridge
 
+import bridge.domain.constructor.BridgeMaker
+import bridge.domain.constructor.BridgeRandomNumberGenerator
+import bridge.view.InputView
+import bridge.view.OutputView
+
 fun main() {
-    // TODO: 프로그램 구현
+    val gameController = GameController(InputView(), OutputView())
+    gameController.standByPhase(BridgeMaker(BridgeRandomNumberGenerator()))
+    gameController.mainPhase()
+    gameController.endPhase()
 }
