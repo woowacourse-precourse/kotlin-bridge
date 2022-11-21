9f1b3b438684b73ebe6d72da8506ef12c23fd3b2
username <ieejo716@naver.com>
2022-11-21 15:44:27 +0900

    [FEAT] : 입력 받은 길이의 다리 생성

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,15 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        val numbers = mutableListOf<Int>()
+        val bridge = mutableListOf<String>()
+        for (cnt in 1..size) numbers.add(bridgeNumberGenerator.generate())
+        numbers.forEach { number ->
+            when (number) {
+                0 -> bridge.add("U")
+                1 -> bridge.add("D")
+            }
+        }
+        return bridge
     }
 }
