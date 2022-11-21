8437587de6db8005ed8d88c9bacf49e31ec92127
lsakee <lsls4868@gmail.com>
2022-11-21 19:16:03 +0900

    feat : 재시작 문구 메시지 출력 기능

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -20,6 +20,9 @@ class OutputView {
     fun printMap(bridge:String) {
         println("["+bridge+"]")
     }
+    fun printRestartMsg(){
+        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
