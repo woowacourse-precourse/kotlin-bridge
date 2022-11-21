e4d4a374ee1a3a86141498dacd0df3ef22b7ce88
dlgocks1 <cksgodl1@naver.com>
2022-11-21 13:06:33 +0900

    feat(InputView.kt) : 사용자 정보 입력 뷰 구현

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -1,27 +1,44 @@
 package bridge.view
 
+import bridge.utils.BRIDGE_GAME_START
+import bridge.utils.CHOOSE_BRIDGE_DIRECTION
+import bridge.utils.CHOOSE_BRIDGE_LENGTH
+import bridge.utils.CHOOSE_RETRY
+import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 class InputView {
+
+    fun gameStart() {
+        println(BRIDGE_GAME_START)
+    }
+
     /**
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        println(CHOOSE_BRIDGE_LENGTH)
+        return Console.readLine().toIntOrNull() ?: throw IllegalArgumentException("")
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        println(CHOOSE_BRIDGE_DIRECTION)
+        return Console.readLine()
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        println(CHOOSE_RETRY)
+        return Console.readLine()
     }
+
+
 }
