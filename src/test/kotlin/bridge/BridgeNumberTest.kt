49c9cf48849a206d9807f2cb7a3dc428701affd8
dltkd1395 <dltkd13956@naver.com>
2022-11-22 00:59:12 +0900

    refactor(BridgeNumberTest) : 중복되는 코드 수정

--- src/test/kotlin/bridge/BridgeNumberTest.kt
+++ src/test/kotlin/bridge/BridgeNumberTest.kt
@@ -1,32 +1,16 @@
 package bridge
 
 import bridge.domain.BridgeNumber
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class BridgeNumberTest {
     @ParameterizedTest
-    @ValueSource(strings = ["1,2,21,22,100"])
-    fun `다리 길이의 범위 예외 테스트`(size: String) {
+    @ValueSource(strings = ["1", "2", "21", "22", "100", "a", "12a", "ab12", "+", ""])
+    fun `다리 길이 예외 테스트`(size: String) {
         assertThrows<IllegalArgumentException> {
             BridgeNumber(size)
-        }
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["a", "12a", "ab12", "+"])
-    fun `다리 길이가 숫자가 아닌 경우 예외 테스트`(size: String) {
-        assertThrows<IllegalArgumentException> {
-            BridgeNumber(size)
-        }
-    }
-
-    @Test
-    fun `다리 길이를 입력하지 않은 경우 예외 테스트`() {
-        assertThrows<IllegalArgumentException> {
-            BridgeNumber("")
         }
     }
 }
