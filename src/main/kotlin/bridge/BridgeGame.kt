a446e60f8712e7d7eaa7aac226c5c7230c106f87
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-11-22 04:22:15 +0900

    feat: 사용자가 다음 칸으로 이동하는 메소드

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -10,7 +10,14 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun move(step:String, moving:String, result:MutableList<List<String>>): Boolean {
+        if(!BridgeGame().isCorrect(step, moving)){
+            result.add(listOf(moving, FAIL))
+            return false
+        }
+        result.add(listOf(moving, PASS))
+        return true
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -18,5 +25,14 @@ class BridgeGame {
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry() {}
+    fun retry() {
+    }
+
+    private fun isCorrect(step: String, moving:String): Boolean {
+        if(step == moving){
+            return true
+        }
+        return false
+    }
+
 }
