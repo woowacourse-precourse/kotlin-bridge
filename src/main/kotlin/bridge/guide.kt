3004744645b82f816aa5786acb6d8af1c1a16aef
lsakee <lsls4868@gmail.com>
2022-11-22 02:17:32 +0900

    feat : 게임을 안내하는 기능

---
+++ src/main/kotlin/bridge/guide.kt
@@ -0,0 +1,33 @@
+package bridge
+
+class guide {
+    fun moveresult(){
+        OutputView().printResult()
+    }
+    fun retryresult(){
+        OutputView().printResult()
+    }
+    fun bridgeList(bridgecheck: String, bridgeUpList: MutableList<String>, bridgeDownList: MutableList<String>){
+        val bridgechoose = guide().BridgeChoose()
+        bridgeUpList.add(BridgeGame().BridgUp(bridgecheck, bridgechoose))
+        bridgeDownList.add(BridgeGame().BridgDown(bridgecheck, bridgechoose))
+        OutputView().printMap(bridgeUpList.joinToString("|"))
+        OutputView().printMap(bridgeDownList.joinToString("|"))
+    }
+
+    fun Guide(): Int {
+        OutputView().printGuide()
+        return InputView().readBridgeSize()
+    }
+    fun restart(): String {
+        OutputView().printRestartMsg()
+        return InputView().readGameCommand()
+    }
+
+    fun BridgeChoose(): String {
+        OutputView().printBridgeChoose()
+        val a= InputView().readMoving()
+        println(a)
+        return a
+    }
+}
