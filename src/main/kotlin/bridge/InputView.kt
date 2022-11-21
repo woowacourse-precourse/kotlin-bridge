bf5fa6f0b1d9c4426a42119f97aa27638d4b5ab6
jklom0326 <jklom0326@gmail.com>
2022-11-22 07:32:30 +0900

    refactor(InputView.kt): 게임 출력 함수 예외처리
    - readBridgeSize() 함수 예외 추가 및 수정
    - readMoving() 함수 예외 추가 및 수정
    - readGameCommand() 함수 예외 추가 및 수정

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -10,18 +10,28 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
+        println("\n다리의 길이를 입력해주세요.")
         val bridgeSize = Console.readLine()
-            .toInt()
-        // 예외 처리
-        return bridgeSize
+        if (bridgeSize.toIntOrNull() == null) {
+            throw IllegalArgumentException("[ERROR] 3 부터 20 사이의 숫자만 입력해주세요.")
+        }
+        if (bridgeSize.toInt() !in 3..20) {
+            throw IllegalArgumentException("[ERROR] 3 부터 20 사이의 숫자만 입력해주세요.")
+        }
+
+        return bridgeSize.toInt()
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
+        println("\n이동할 칸을 선택해 주세요. (위: U, 아래: D)")
         val moveSize = Console.readLine()
-        // 예외 처리
+
+        if (moveSize.length != 1 || moveSize !in listOf("U", "D")) {
+            throw IllegalArgumentException()
+        }
 
         return moveSize
     }
@@ -30,8 +40,11 @@ class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
+        println("\n게임을 다시 시도할지 여부를입력해주세요. (재시도:R, 종료:Q)")
         val gameCommand = Console.readLine()
-        // 예외 처리
+        if (gameCommand.length != 1 || gameCommand !in listOf("R", "Q")) {
+            throw IllegalArgumentException()
+        }
         return gameCommand
     }
 }
