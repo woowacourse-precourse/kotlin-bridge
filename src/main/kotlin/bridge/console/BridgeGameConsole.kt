966c129d992c95be49004c024df8b7b7472f7713
joh9911 <joh9911@naver.com>
2022-11-21 15:55:05 +0900

    feat(BridgeGameConsole): while문 수정 및 재시도, 성공 시 게임 진행 함수 추가

--- src/main/kotlin/bridge/console/BridgeGameConsole.kt
+++ src/main/kotlin/bridge/console/BridgeGameConsole.kt
@@ -7,23 +7,51 @@ import bridge.model.BridgeGame
 import bridge.view.OutputView
 
 class BridgeGameConsole {
+    private val inputView = InputView()
     private val outputView = OutputView()
     private val bridgeGame = BridgeGame()
+    private lateinit var bridge: Bridge
+
 
     fun startGame(){
         outputView.printGameInitializeProcess()
-        val bridge = Bridge(BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(InputView().readBridgeSize()))
-        processGame(bridge)
+        bridge = Bridge(BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(inputView.readBridgeSize()))
+        processMovingPart(bridge)
     }
 
-    fun processGame(bridge: Bridge){
+    fun processMovingPart(bridge: Bridge){
         do {
             outputView.printGameInputMoveLaneMessage()
-            bridgeGame.move(InputView().readMoving(), bridge)
+            bridgeGame.move(inputView.readMoving(), bridge)
             outputView.printMap(bridgeGame.upLane, bridgeGame.downLane)
-        }while(bridgeGame.isMovingSuccess)
+        }while(bridgeGame.isMovingSuccess && !bridge.isFinished())
+        decideGamePart()
+    }
+
+    fun decideGamePart(){
+        if (!bridgeGame.isMovingSuccess)
+            processGameCommandPart()
+        if (bridge.isFinished())
+            outputView.printResult()
+    }
+
+    fun processGameCommandPart(){
+        outputView.printGameInputGameCommandMessage()
+        when (inputView.readGameCommand()){
+            'R' -> {
+                setForRetrial()
+                processMovingPart(bridge)
+            }
+            'Q' -> outputView.printResult()
+        }
+    }
+
+    fun setForRetrial(){
+        bridge.initializeLaneCount()
+        bridgeGame.retry()
     }
 }
+
 
 fun main(){
     BridgeGameConsole().startGame()
