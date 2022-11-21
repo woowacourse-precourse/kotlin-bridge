6d758b65ecec0d01b3b716ec7de53b0f458c8c8d
DormunDoorlock <skrudgh2001@naver.com>
2022-11-21 20:19:50 +0900

    refactor: BridgeGame 클래스에서 제외된 InputView(), OutputView() 기능 추가

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -4,5 +4,31 @@ fun main() {
     OutputView().printGameStartMessage()
     val size = InputView().readBridgeSize()
     val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(size)
-    BridgeGame(bridge).move(0,1)
+    val bridgeGame = BridgeGame(bridge)
+    var index = 0
+    var tryCount = 1
+    while(true){
+        if(index == bridge.size){
+            OutputView().printResult(index,bridge,tryCount)
+            break
+        }
+        val userInput = InputView().readMoving()
+        val result = bridgeGame.move(index,userInput)
+        if (result == "Correct") {
+            OutputView().printMap(index,bridge,true)
+            index++
+        }
+        if (result == "Wrong"){
+            OutputView().printMap(index,bridge,false)
+            val userGameCommand = InputView().readGameCommand()
+            if(userGameCommand=="R") {
+                tryCount = bridgeGame.retry(tryCount)
+                index = 0
+            }
+            if(userGameCommand=="Q"){
+                OutputView().printResult(index,bridge,tryCount)
+                break
+            }
+        }
+    }
 }
