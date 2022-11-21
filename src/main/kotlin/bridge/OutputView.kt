a0d196a36a9b0b30f5b027fc55f1e1960a8df213
2taezeat <xorudfl772@naver.com>
2022-11-21 20:46:51 +0900

    feat(OutputView): 게임 성공 여부에 따라 빈줄 출력 구현

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -7,6 +7,7 @@ class OutputView(
     private val bridge: List<String>,
 ) {
     private var moveDirections = listOf<String>()
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      *
@@ -26,9 +27,11 @@ class OutputView(
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {
+    fun printResult(isGameSuccess: Boolean) {
+        if (isGameSuccess) println()
         println(FINAL_GAME_RESULT)
         printMap()
+        println()
     }
 
     fun setMoveDirections(userMoveDirections: List<String>) {
