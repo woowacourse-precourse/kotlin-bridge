c50e3f48b84ca25b484db404a9a5249ec431c014
Sujin Kim <sujinkim919@gmail.com>
2022-11-21 21:15:37 +0900

    chore: 줄바꿈 추가

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -36,7 +36,7 @@ class InputView {
 
     companion object {
         const val INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요."
-        const val INPUT_MOVEMENT = "이동할 칸을 선택해주세요. (위: ${MOVE_UP}, 아래: ${MOVE_DOWN})"
-        const val INPUT_RESTART = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: $GAME_RESTART, 종료: $GAME_END)"
+        const val INPUT_MOVEMENT = "\n이동할 칸을 선택해주세요. (위: ${MOVE_UP}, 아래: ${MOVE_DOWN})"
+        const val INPUT_RESTART = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: $GAME_RESTART, 종료: $GAME_END)"
     }
 }
