0ab1d016b8135f97290cd17dad7d9612a2c95c61
Jongwon-0518 <77ha@naver.com>
2022-11-21 20:53:23 +0900

    feat: 게임 종료 및 결과 출력 기능 추가

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -3,22 +3,33 @@ package bridge
 fun main() {
     val bridgeSize = InputView().readBridgeSize()
     val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)
+    var finish: Boolean
+    var nowBridge: MutableList<String>
+    var tryCount = 0
 
     do {
-        var finish = playing(bridge)
+        tryCount += 1
+        val pair = playing(bridge)
+        finish = pair.first
+        nowBridge = pair.second
         if (!finish) { finish = BridgeGame().retry(InputView().readGameCommand()) }
     } while (!finish)
+
+    println("\n최종 게임 결과")
+    OutputView().printMap(bridge, nowBridge)
+
+    OutputView().printResult(BridgeGame().isRight(bridge, nowBridge), tryCount)
 }
 
-fun playing(bridge: List<String>): Boolean {
+fun playing(bridge: List<String>): Pair<Boolean, MutableList<String>> {
     var nowBridge = mutableListOf<String>()
 
     do {
-        if (bridge == nowBridge) { return true }
+        if (bridge == nowBridge) { return Pair(true, nowBridge) }
         nowBridge = BridgeGame().move(InputView().readMoving(), nowBridge)
         OutputView().printMap(bridge, nowBridge)
         val state = BridgeGame().isRight(bridge, nowBridge)
     } while (state)
 
-    return false
+    return Pair(false, nowBridge)
 }
