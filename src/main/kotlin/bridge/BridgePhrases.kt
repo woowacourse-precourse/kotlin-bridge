f3143bb4fabc12b3ba16020b0f7bb7e3e46b4064
changhwan <chang5hustle@gmail.com>
2022-11-21 17:01:56 +0900

    feat(bridge) : 게임 재시작 종료여부 예외처리 함수 작성

--- src/main/kotlin/bridge/BridgePhrases.kt
+++ src/main/kotlin/bridge/BridgePhrases.kt
@@ -5,4 +5,5 @@ object BridgePhrases {
     const val BRIDGE_SIZE_INPUT_NOT_IN_CORRECT_RANGE_ERROR = "[ERROR] 입력하신 숫자의 사이즈가 3~20 사이의 숫자가 아닙니다."
     const val INPUT_IS_NOT_ALPHABET_ERROR = "[ERROR] 입력하신 내용이 알파벳이 아닙니다."
     const val MOVING_CODE_INCORRECT_ERROR = "[ERROR] 입력하신 문자가 올바른 코드인 U,D가 아닙니다."
+    const val GAME_COMMEND_CODE_INCORRECT_ERROR = "[ERROR] 입력하신 문자가 올바른 코드인 R,Q가 아닙니다."
 }
--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -4,6 +4,7 @@ import bridge.BridgeGameRule.BRIDGE_MAXIMUM_LENGTH
 import bridge.BridgeGameRule.BRIDGE_MINIMUM_LENGTH
 import bridge.BridgePhrases.BRIDGE_SIZE_INPUT_NOT_IN_CORRECT_RANGE_ERROR
 import bridge.BridgePhrases.BRIDGE_SIZE_INPUT_TYPE_ERROR
+import bridge.BridgePhrases.GAME_COMMEND_CODE_INCORRECT_ERROR
 import bridge.BridgePhrases.INPUT_IS_NOT_ALPHABET_ERROR
 import bridge.BridgePhrases.MOVING_CODE_INCORRECT_ERROR
 import camp.nextstep.edu.missionutils.Console.*
@@ -37,11 +38,13 @@ class InputView {
     private fun checkCorrectMovingCode(input: String): Boolean =
         input == BridgeType.UP.charCode.toString() || input == BridgeType.DOWN.charCode.toString()
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    fun readGameCommand(): String {
-        return ""
+    fun readGameCommand(): String = readLine()
+
+    fun getValidateGameCommend(userInput: String): String {
+        require(isAlphabet(userInput)) { INPUT_IS_NOT_ALPHABET_ERROR }
+        val processedUserInput = userInput.uppercase()
+        require(checkCorrectGameCommendCode(processedUserInput)) { GAME_COMMEND_CODE_INCORRECT_ERROR }
+        return processedUserInput
     }
 
     private fun checkCorrectGameCommendCode(input: String): Boolean =
