58cedbef9d8266023d25bb82cb8dd05fb934fb01
Gloria <ippnsj@gmail.com>
2022-11-21 17:51:12 +0900

    feat: 명령어가 R 또는 Q 가 아니면 예외를 발생시키는 기능 구현

--- src/main/kotlin/bridge/utils/CommandValidator.kt
+++ src/main/kotlin/bridge/utils/CommandValidator.kt
@@ -1,5 +1,13 @@
 package bridge.utils
 
 class CommandValidator : Validator {
-    override fun validate(input: String) {}
+    private val options = listOf("R", "Q")
+
+    override fun validate(input: String) {
+        require(options.contains(input)) { ERROR_MESSAGE }
+    }
+
+    companion object {
+        const val ERROR_MESSAGE = "명령어는 R 또는 Q 여야 합니다."
+    }
 }
