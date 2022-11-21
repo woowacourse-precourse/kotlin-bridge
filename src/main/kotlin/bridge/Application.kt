9bd1a54263c4cf8249481c41c5787a21e519ccf3
[alsswl] <[alswl4223@naver.com]>
2022-11-21 15:19:38 +0900

    feat(Application) : 성공과 실패에 따른 종료

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -31,20 +31,26 @@ fun BridgeGameMain() {
     val inputView = InputView()
     val outputView = OutputView()
     var size = -1
-    while(size ==-1) {
-        size = InputView.readBridgeSize()
+    while (size == -1) {
+        size = inputView.readBridgeSize()
     }
     var bridgeNumberGenerator = BridgeRandomNumberGenerator()
     var bridgeMaker = BridgeMaker(bridgeNumberGenerator)
     var bridge = bridgeMaker.makeBridge(size)
-    for(i in 0 until size){
-        var way = "E"
-        while(way == "E") {
-            way = InputView.readMoving()
-        }
-
+    var finish = 1
+    var count = 0
+    while (finish == 1) {
+        count++
+        finish = BridgeGameing(size, bridge)
+    }
+    if (finish == 0) {
+        outputView.printResult(false,bridge,size-1,count)
+    }else{
+        outputView.printResult(true,bridge,size-1,count)
     }
 }
+
+
 
 fun main() {
     println("다리건너기 게임을 시작합니다.")
