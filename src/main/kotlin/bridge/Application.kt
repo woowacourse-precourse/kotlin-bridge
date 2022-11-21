8edbec4f9fc59fcc0711a9d0bde7f62640ebc6c2
[alsswl] <[alswl4223@naver.com]>
2022-11-21 15:35:05 +0900

    refactor(Application) : BridgeGameing 함수 depth수정

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,28 +1,26 @@
 package bridge
 
-fun BridgeGameing(size : Int,bridge : List<String>) : Int {
+fun printBridgeMap(i : Int,bridge:List<String>):Int{
     val inputView = InputView()
     val outputView = OutputView()
+    var way = "E"
+    while (way == "E") way = inputView.readMoving()
+    val bridgeGame = BridgeGame()
+    if (bridgeGame.move(i, bridge, way)) {
+        outputView.printMap(true, bridge, i)
+    } else {
+        outputView.printMap(false, bridge, i)
+        var again = inputView.readGameCommand()
+        if (again == "R") return 1
+        else return 0
+    }
+    return 2
+}
+
+fun BridgeGameing(size : Int,bridge : List<String>) : Int {
     for (i in 0 until size) {
-        var way = "E"
-        while (way == "E") {
-            way = inputView.readMoving()
-        }
-        val bridgeGame = BridgeGame()
-
-
-        if (bridgeGame.move(i, bridge, way)) {
-            outputView.printMap(true, bridge, i)
-        } else {
-            outputView.printMap(false, bridge, i)
-            var again = inputView.readGameCommand()
-            if (again == "R") {
-                return 1
-            }
-            else {
-                return 0
-            }
-        }
+        var printBridgeMapReturn = printBridgeMap(i,bridge)
+        return printBridgeMapReturn
     }
     return 2
 }
