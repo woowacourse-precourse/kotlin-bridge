3dd89e5c7cc0337510b32268574ce135ba96e044
dlwgnud <wngud980411@naver.com>
2022-11-21 18:15:14 +0900

    refactor: 유효성 검사 InputView에서 하도록 리팩토링

--- src/main/kotlin/bridge/Controller.kt
+++ src/main/kotlin/bridge/Controller.kt
@@ -3,7 +3,6 @@ package bridge
 class Controller {
     private val inputView = InputView()
     private val outputView = OutputView()
-    private val inputCheck = InputCheck()
 
     fun run() {
         gameStartStep()
@@ -19,14 +18,12 @@ class Controller {
         outputView.printInputBridgeLength()
         val bridgeLength = inputView.readBridgeSize()
         println()
-        inputCheck.checkBridgeLength(bridgeLength)
         return bridgeLength.toInt()
     }
 
     private fun chooseSpaceToMoveStep(length: Int) {
         outputView.printChooseSpaceToMove()
         val moveSpace = inputView.readMoving()
-        inputCheck.checkChooseSpace(moveSpace)
     }
 
 }
--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -6,18 +6,23 @@ import camp.nextstep.edu.missionutils.Console
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 class InputView {
+    private val inputCheck = InputCheck()
     /**
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): String {
-        return Console.readLine()
+        val input = Console.readLine()
+        inputCheck.checkBridgeLength(input)
+        return input
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return Console.readLine()
+        val input = Console.readLine()
+        inputCheck.checkChooseSpace(input)
+        return input
     }
 
     /**
