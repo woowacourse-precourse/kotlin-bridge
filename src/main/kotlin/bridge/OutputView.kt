d5639717ad207ff288da256cd165f2b7f820a83d
healthykim <bsbs8645@snu.ac.kr>
2022-11-21 17:47:54 +0900

    feat(OutputView): Implement printMap function

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -6,10 +6,10 @@ package bridge
 class OutputView {
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(game: BridgeGame) {
+        println(game.gameToString())
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
