ea195103c5aeb38820adf0d8c654b765e5afbaf8
dohun_ <silver2679@gmail.com>
2022-11-21 22:47:01 +0900

    Feat(Application): fun gameEnd 기능 추가

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -11,10 +11,16 @@ fun main() {
 fun gameStart(bridgeGame: BridgeGame, bridgeSize: Int) {
     var count = 0
     while (bridgeGame.endGame()) {
-        if(bridgeSize == count) break
+        if(bridgeSize == count) return gameEnd(bridgeGame)
         val readMoving: String = InputView().readMoving()// 위칸 아래칸
         bridgeGame.move(readMoving)
         bridgeGame.printMap()
         count += 1
     }
+}
+
+fun gameEnd(bridgeGame: BridgeGame) {
+    println("\n최종 게임 결과")
+    bridgeGame.printMap()
+    bridgeGame.printResult()
 }
