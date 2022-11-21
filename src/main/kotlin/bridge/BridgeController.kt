18bbe508f4f99711f5f22a68a07da106f18c15a8
inseonyun <yuninseon@naver.com>
2022-11-21 22:59:33 +0900

    feat: 게임 진행 클래스 구현
    1. startBridgeGame - move 입출력 후 플레이어 위치 이동 후 맵 출력
    2. isFinished - 플레이어가 다리 건너기가 끝났는지(실패, 성공, 다리 다 건넘) return
    3. isSuccess - 플레이어가 다리를 다 건넜고, 성공했는지 return
    4. setRetry - retry를 입력했을 때, 기본 값 초기화
    5. retryOrExit - 다리 건너기 실패 후 retry 할 건지 exit 할 건지 수행 후 bool return
    6. gameOver - 게임이 모두 종료되면 결과 출력

--- src/main/kotlin/bridge/BridgeController.kt
+++ src/main/kotlin/bridge/BridgeController.kt
@@ -1,11 +1,70 @@
 package bridge
 
 class BridgeController {
-    init {
+    private val inputView = InputView()
+    private val outputView = OutputView()
+    private val bridgeGame = BridgeGame()
 
+    private val bridgeSize: Int
+    private val bridge: List<String>
+
+    private val playerBridge = mutableListOf<Pair<String, String>>()
+
+    private var nowPlace = 0
+    private var tryCount = 1
+    private var gameResult = false
+
+    init {
+        outputView.printStartGame()
+        outputView.printInputBridgeSize()
+        bridgeSize = inputView.readBridgeSize()
+        bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)
     }
 
     fun startBridgeGame() {
+        outputView.printInputMoveBridge()
+        val moveData = inputView.readMoving()
 
+        playerBridge.add(Pair(moveData, bridgeGame.move(moveData, bridge[nowPlace])))
+        nowPlace += 1
+        outputView.printMap(playerBridge)
+    }
+
+    fun isFinished(): Boolean {
+        if(playerBridge.size == bridgeSize)
+            return true
+        if(playerBridge[playerBridge.size - 1].second == "X")
+            return true
+        return false
+    }
+
+    fun isSuccess(): Boolean {
+        if(playerBridge.size == bridgeSize && playerBridge[playerBridge.size - 1].second == "O") {
+            gameResult = true
+            gameOver()
+            return true
+        }
+        return false
+    }
+
+    private fun setRetry() {
+        playerBridge.clear()
+        nowPlace = 0
+        tryCount += 1
+        gameResult = false
+    }
+
+    fun retryOrExit(): Boolean {
+        outputView.printInputRetryOrExit()
+        if(bridgeGame.retry(inputView.readGameCommand())) {
+            setRetry()
+            return true
+        }
+        gameOver()
+        return false
+    }
+
+    private fun gameOver() {
+        outputView.printResult(gameResult, tryCount, playerBridge)
     }
 }
