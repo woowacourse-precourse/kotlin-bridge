a3f64e2928d34673e6f912bbb89c4f6399daad75
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-11-22 00:38:57 +0900

    feat(BridgeGameController): 사용자에게 이동할 칸 입력받는 기능 구현

--- src/main/kotlin/bridge/domain/BridgeGameController.kt
+++ src/main/kotlin/bridge/domain/BridgeGameController.kt
@@ -1,6 +1,26 @@
 package bridge.domain
 
+import bridge.view.InputView
+import bridge.view.OutputView
+
 
 class BridgeGameController {
+
+    private val inputView = InputView()
+    private val outputView = OutputView()
+    private fun readMoving(): String {
+        outputView.printReadMovingMent()
+        return repeatInputProcess { inputView.readMoving() } as String
+    }
+
+    private fun repeatInputProcess(inputOperation: () -> Any): Any {
+        do {
+            try {
+                return inputOperation()
+            } catch (e: IllegalArgumentException) {
+                outputView.printErrorMessage(e.message.toString())
+            }
+        } while (true)
+    }
 
 }
