3735a78a2a88edfcb6c2bef50f0e44508b844095
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-22 02:34:52 +0900

    feat(String): String 클래스 생성 및 문자열 상수 정의

---
+++ src/main/kotlin/bridge/constants/String.kt
@@ -0,0 +1,15 @@
+package bridge.constants
+
+class String {
+    companion object {
+        const val STRING_START_GAME = "다리 건너기 게임을 시작합니다."
+        const val STRING_INPUT_LENGTH = "다리의 길이를 입력해주세요."
+        const val STRING_INPUT_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
+        const val STRING_INPUT_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
+        const val STRING_PRINT_RESULT = "최종 게임 결과"
+        const val STRING_PRINT_SUCCESS_OR_FAIL = "게임 성공 여부: "
+        const val STRING_PRINT_SUCCESS = "성공"
+        const val STRING_PRINT_FAIL = "실패"
+        const val STRING_PRINT_TRY_NUMBER = "총 시도한 횟수: "
+    }
+}
