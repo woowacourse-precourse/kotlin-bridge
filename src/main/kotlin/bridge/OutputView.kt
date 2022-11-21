410fa7901472f602dd734fa77803aa978f488e50
[alsswl] <[alswl4223@naver.com]>
2022-11-21 11:43:40 +0900

    feat(OutputView) : 현재까지 이도한 다리의 상태를 출력하는 함수
    해당 순서까지 왔다는 것은 앞에까지 다 맞았다는 것이니까 지금 고른게 맞던 틀리던 그 전 것 까지는 그대로 하고 마지막 것만 바꾸는 형식으로 진행

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -9,7 +9,54 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(correct : Boolean, bridge : List<String>,number : Int) {
+        var bridgePrintU = mutableListOf<String>()
+        var bridgePrintD = mutableListOf<String>()
+        bridgePrintU.add("[")
+        bridgePrintD.add("[")
+        if(correct){
+            for(i in 0 until number){
+                if(bridge[i] == "U"){
+                    bridgePrintU.add(" O ")
+                    bridgePrintD.add("   ")
+                } else{
+                    bridgePrintU.add("   ")
+                    bridgePrintD.add(" O ")
+                }
+                if(i != number-1) {
+                    bridgePrintD.add("|")
+                    bridgePrintU.add("|")
+                }
+            }
+            bridgePrintU.add("]")
+            bridgePrintD.add("]")
+            println(bridgePrintU)
+            println(bridgePrintD)
+        }else{
+            for(i in 0 until number-1){
+                if(bridge[i] == "U"){
+                    bridgePrintU.add(" O ")
+                    bridgePrintD.add("   ")
+                } else{
+                    bridgePrintU.add("   ")
+                    bridgePrintD.add(" O ")
+                }
+                bridgePrintD.add("|")
+                bridgePrintU.add("|")
+            }
+            if(bridge[number-1] == "U"){
+                bridgePrintU.add("   ")
+                bridgePrintD.add(" X ")
+            } else{
+                bridgePrintU.add(" X ")
+                bridgePrintD.add("   ")
+            }
+            bridgePrintU.add("]")
+            bridgePrintD.add("]")
+            println(bridgePrintU)
+            println(bridgePrintD)
+        }
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
