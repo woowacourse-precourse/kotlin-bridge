97c6c869c1345ccf9db28f6d57cf83128f1d04bf
DormunDoorlock <skrudgh2001@naver.com>
2022-11-21 22:14:36 +0900

    refactor: 고정 문자열 상수화

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,39 +1,57 @@
 package bridge
 
-fun checkUserInput(index: Int, bridgeGame: BridgeGame,outputView: OutputView): Boolean {
-    val userInput = InputView().readMoving()
-    val result =  bridgeGame.move(index, userInput)
-    return if(result == "Correct") {
+fun checkUserInput(index: Int, bridgeGame: BridgeGame,outputView: OutputView): Int {
+    val result =  bridgeGame.move(index, InputView().readMoving())
+    if(result == Result.USER_INPUT_CORRECT.message) {
         outputView.printMap(index, true)
-        true
-    } else {
-        outputView.printMap(index,false)
-        false
+        return Constant.CORRECT.params
     }
+    if(result == Result.USER_INPUT_WRONG.message) {
+        outputView.printMap(index,false)
+        return Constant.WRONG.params
+    }
+    return Constant.ERROR.params
 }
 fun checkUserGameCommand(bridge: List<String>,tryCount: Int,idx:Int): Int {
     val userGameCommand = InputView().readGameCommand()
-    return if (userGameCommand== "R")
-        playGame(bridge, tryCount+1)
-    else {
+    if (userGameCommand==Result.USER_COMMAND_RETRY.message)
+        return playGame(bridge, BridgeGame(bridge).retry(tryCount))
+    if (userGameCommand==Result.USER_COMMAND_QUIT.message){
         OutputView(bridge).printResult(idx, tryCount)
-        0
+        return Constant.WRONG.params
     }
+    return Constant.ERROR.params
 }
 
 fun playGame(bridge:List<String>,tryCount:Int): Int {
     for(idx in bridge.indices) {
-        if (!checkUserInput(idx, BridgeGame(bridge), OutputView(bridge))) {
+        if (checkUserInput(idx, BridgeGame(bridge), OutputView(bridge))==Constant.WRONG.params) {
             return checkUserGameCommand(bridge,tryCount,idx)
         }
     }
     return tryCount
 }
 fun main() {
-    println("다리 건너기 게임을 시작합니다.")
+    println(OutputView.Message.GAMESTART.message)
     val size = InputView().readBridgeSize()
     val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(size)
-    val finalTryCount = playGame(bridge,1)
-    if (finalTryCount!=0)
+    val finalTryCount = playGame(bridge,Constant.TRY_COUNT.params)
+    if (finalTryCount !=Constant.CORRECT.params || finalTryCount !=Constant.ERROR.params)
         OutputView(bridge).printResult(bridge.size,finalTryCount)
+}
+enum class Constant(
+        val params:Int
+){
+    CORRECT(1),
+    WRONG(0),
+    ERROR(-1),
+    TRY_COUNT(1)
+}
+enum class Result(
+        val message:String
+){
+    USER_INPUT_CORRECT("Correct"),
+    USER_INPUT_WRONG("Wrong"),
+    USER_COMMAND_RETRY("R"),
+    USER_COMMAND_QUIT("Q")
 }
