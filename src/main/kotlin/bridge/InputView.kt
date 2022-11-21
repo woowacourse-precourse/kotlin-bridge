0ddc983c341323ad304e75f8927e659242c4118d
Jaehun-Jo <chojh922@gmail.com>
2022-11-22 00:46:07 +0900

    [Feat] InputErrorCheck : 입력에 대한 예외처리 메서드 추가

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -13,6 +13,7 @@ class InputView {
     fun readBridgeSize(): Int {
         println("다리의 길이를 입력해주세요.")
         val bridgeSize = Console.readLine()
+        inputErrorCheck.checkValidBridgeSize(bridgeSize)
         println()
         return bridgeSize.toInt()
     }
@@ -23,6 +24,7 @@ class InputView {
     fun readMoving(): String {
         println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
         val moving = Console.readLine()
+        inputErrorCheck.throwMovingException(moving)
         return moving
     }
 
@@ -32,6 +34,7 @@ class InputView {
     fun readGameCommand(): String {
         println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
         val gameCommand = Console.readLine()
+        inputErrorCheck.throwGameCommandException(gameCommand)
         return gameCommand
     }
 }
