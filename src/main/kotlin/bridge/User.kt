839d3982c4fd041ce3200b048340e2b33dd5c158
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-21 22:16:22 +0900

    feat(bridge): 유저 데이터 클래스 추가

---
+++ src/main/kotlin/bridge/User.kt
@@ -0,0 +1,14 @@
+package bridge
+
+data class User(
+    val movingResults: MutableList<Pair<String, Boolean>> = mutableListOf<Pair<String, Boolean>>(),
+    var moving: String = "",
+    var presentPosition: Int = INITIALIZE_TO_ZERO,
+    var tryCount: Int = INITIALIZE_TO_ONE,
+){
+
+    companion object {
+        const val INITIALIZE_TO_ZERO = 0
+        const val INITIALIZE_TO_ONE = 1
+    }
+}
