d74507139b578aa383ce269f3cceccb7b6e3188b
jiingom <vvankatam0@gmail.com>
2022-11-22 00:03:32 +0900

    style: 객체의 프로퍼티 선언 부분에 띄어쓰기 추가

--- src/main/kotlin/bridge/domain/Stage.kt
+++ src/main/kotlin/bridge/domain/Stage.kt
@@ -3,7 +3,7 @@ package bridge.domain
 enum class Stage(val upOrDown: String, val isClearStage: String) {
     UP("U", "O"),
     DOWN("D", "O"),
-    UP_FAIL("U","X"),
+    UP_FAIL("U", "X"),
     DOWN_FAIL("D", "X");
 
     companion object {
