cf4c5f68de3e0461a0d88896b55e6a4c2f02bb69
dltkd1395 <dltkd13956@naver.com>
2022-11-21 20:44:38 +0900

    test(BridgeControllerTest) : 다리 이동 잘못 입력 후 재입력 테스트 추가

--- src/test/kotlin/bridge/BridgeControllerTest.kt
+++ src/test/kotlin/bridge/BridgeControllerTest.kt
@@ -25,6 +25,22 @@ class BridgeControllerTest : NsTest() {
         }, 1, 0, 0)
     }
 
+    @Test
+    fun `다리 이동 잘못 입력 후 재입력`() {
+        assertRandomNumberInRangeTest({
+            run("3", "", "u", "3", "U", "D", "D")
+            assertThat(output()).contains(
+                "최종 게임 결과",
+                "[ O |   |   ]",
+                "[   | O | O ]",
+                "게임 성공 여부: 성공",
+                "총 시도한 횟수: 1"
+            )
+            val upSideIndex = output().indexOf("[ O |   |   ]")
+            val downSideIndex = output().indexOf("[   | O | O ]")
+            assertThat(upSideIndex).isLessThan(downSideIndex)
+        }, 1, 0, 0)
+    }
     override fun runMain() {
         BridgeController().start()
     }
