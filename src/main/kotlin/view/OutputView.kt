6f1640b265ca8a18d76c5b353110eb1710627ee1
username <ieejo716@naver.com>
2022-11-22 01:56:51 +0900

    [CHORE] : 띄어쓰기 추가

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -14,6 +14,7 @@ class OutputView {
         printUpDecide(bridge, input)
         println()
         printDownDecide(bridge, input)
+        println()
     }
 
     private fun printDownDecide(bridge: List<String>, input: List<String>) {
