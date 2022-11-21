03cfae148acec7f5bfd74b9e18345c2026906aa0
dohun_ <silver2679@gmail.com>
2022-11-21 22:26:36 +0900

    Feat(Application): 필요한  함수 선언

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,4 +1,8 @@
 package bridge
 
 fun main() {
+    val bridgeMaker: BridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
+    val bridgeSize: Int = InputView().readBridgeSize()
+    val bridge: List<String> = bridgeMaker.makeBridge(bridgeSize)
+    val bridgeGame: BridgeGame = BridgeGame(bridge)
 }
