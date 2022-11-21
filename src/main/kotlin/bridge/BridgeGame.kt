95430fc70f57ab413f06dfb280dd7e2c0a7591de
dohun_ <silver2679@gmail.com>
2022-11-21 21:35:43 +0900

    Feat(BridgeGame): 이동하는 칸을 입력 받으면 이동하는 기능 추가

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -3,14 +3,54 @@ package bridge
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
+class BridgeGame(private val bridge: List<String>) {
+    var upCase: MutableList<String> = mutableListOf<String>()
+    var downCase: MutableList<String> = mutableListOf<String>()
+    private var index: Int = 0
+    private var isSuccess: Boolean = true
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun isUpCase(isUp: Boolean) {
+        if(isUp) {
+            upCase.add("O")
+            downCase.add(" ")
+        } else {
+            upCase.add("X")
+            downCase.add(" ")
+        }
+    }
+    fun isDownCase(isDown: Boolean) {
+        if(isDown) {
+            upCase.add(" ")
+            downCase.add("O")
+        } else {
+            upCase.add(" ")
+            downCase.add("X")
+        }
+    }
+    fun move(readMoving: String) {
+        when (bridge[index]) {
+            "U" -> if(readMoving == "U") {
+                isUpCase(true)
+                isSuccess = true
+            } else if(readMoving == "D") {
+                isDownCase(false)
+                isSuccess = false
+            }
+            "D" -> if(readMoving == "D") {
+                isDownCase(true)
+                isSuccess = true
+            } else if(readMoving == "U") {
+                isUpCase(false)
+                isSuccess = false
+            }
+        }
+        index += 1
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
