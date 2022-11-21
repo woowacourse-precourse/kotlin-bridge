922c3e4a6a8a41ae55bfd7c44fa7191d8837d0ae
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-22 04:25:22 +0900

    feat(Constant): 상수 추가

--- src/main/kotlin/bridge/constants/Constant.kt
+++ src/main/kotlin/bridge/constants/Constant.kt
@@ -1,4 +1,9 @@
 package bridge.constants
 
 class Constant {
+    companion object {
+        const val START_INDEX = 0
+        const val MIN_BRIDGE_LENGTH = 3
+        const val MAX_BRIDGE_LENGTH = 20
+    }
 }
