06398e80a427ce071eba2dbd751694fd9a8b30d3
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-22 02:58:46 +0900

    [Refactor] (OutputView.kt) printMap, printResult 필요없는 매개변수 제거
    printMap: 현재 출력
    printResult: 현재 출력 + 결과

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -1,39 +1,21 @@
 package bridge
 
 import bridge.util.FINAL_GAME_RESULT_MESSAGE
-import bridge.util.GAME_FAILURE_MESSAGE
-import bridge.util.GAME_SUCCESS_MESSAGE
-import bridge.validator.OutputValidator
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 class OutputView {
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    fun printMap(
-        upSideList: MutableList<String>,
-        downSideList: MutableList<String>,
-        round: Int,
-    ): String {
-        return OutputValidator.validatePrintMap(upSideList, downSideList, round)
+
+    fun printMap(bridge: List<String>) {
+        bridge.forEach { currentMap -> print(currentMap) }
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    fun printResult(map: String, round: Int, isNotMatched: Boolean) {
-        var trialCount = round
-        trialCount++
-        println(FINAL_GAME_RESULT_MESSAGE + "\n" + map)
-        if (isNotMatched) println(GAME_FAILURE_MESSAGE)
-        if (!isNotMatched) println(GAME_SUCCESS_MESSAGE)
-        println("총 시도한 횟수:${trialCount}")
+    fun printResult(bridge: List<String>, trialCount: Int, whether: String) {
+        println(FINAL_GAME_RESULT_MESSAGE)
+        printMap(bridge)
+        println("게임 성공 여부: $whether")
+        println("총 시도한 횟수: $trialCount")
     }
 }
 
