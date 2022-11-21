5c459aafc2fad20d89e0b441bded26cb8042ab5e
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-21 19:36:25 +0900

    refactor(Const) : 특정 문자열들을 const로 관리

--- src/main/kotlin/bridge/Const.kt
+++ src/main/kotlin/bridge/Const.kt
@@ -1,2 +1,11 @@
 package bridge
 
+const val NOT_INITIALIZED = "[ N ]"
+const val SUCCESSIVE_PARENTHESIS = "]["
+const val CONNECT_PARENTHESIS = "|"
+
+const val AVAILABLE_PATH = "O"
+const val UNAVAILABLE_PATH = "X"
+
+const val RETRY_INPUT = "R"
+const val QUIT_INPUT = "Q"
