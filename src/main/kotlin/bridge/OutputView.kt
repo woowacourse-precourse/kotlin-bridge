6ad674efb91b031cd72bc8249cc07ba6420dc362
healthykim <bsbs8645@snu.ac.kr>
2022-11-21 17:49:56 +0900

    chore(OutputView): Add comments to methods in OutputView class

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -5,14 +5,18 @@ package bridge
  */
 class OutputView {
     /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     * @brief 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     * @param game 현재 플레이 중인 게임
      */
     fun printMap(game: BridgeGame) {
         println(game.gameToString())
     }
 
     /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * @brief 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * @param success 게임 성공 여부
+     * @param tries 게임 시도 횟수
+     * @param game 플레이 완료된 게임
      */
     fun printResult(success: Boolean, tries: Int, game: BridgeGame) {
         println("최종 게임 결과")
