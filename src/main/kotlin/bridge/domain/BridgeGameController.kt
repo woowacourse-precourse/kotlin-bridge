b4bec69c8572e5e3b26ff2389e9d9afe9b9d8135
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-11-22 00:41:17 +0900

    feat(BridgeGameController): 게임 한 라운드를 진행하는 기능 구현

--- src/main/kotlin/bridge/domain/BridgeGameController.kt
+++ src/main/kotlin/bridge/domain/BridgeGameController.kt
@@ -6,8 +6,20 @@ import bridge.view.OutputView
 
 class BridgeGameController {
 
+    private lateinit var bridgeGame: BridgeGame
     private val inputView = InputView()
     private val outputView = OutputView()
+
+    private fun playOneGame(bridgeSize: Int): Boolean {
+        for (step in 0 until bridgeSize) {
+            val moving = readMoving()
+            val isSuccess = bridgeGame.move(moving, step)
+            outputView.printMap(bridgeGame.getMapResult())
+            if (!isSuccess) return false
+        }
+        return true
+    }
+
     private fun readMoving(): String {
         outputView.printReadMovingMent()
         return repeatInputProcess { inputView.readMoving() } as String
