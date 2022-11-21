a2b47a4185c321311bca408d1dc0ce84ba17dcc7
2taezeat <xorudfl772@naver.com>
2022-11-21 14:07:45 +0900

    feat(Application): 게임 시작 문자열 출력 기능 구현

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,6 +1,10 @@
 package bridge
 
 fun main() {
-    // TODO: 프로그램 구현
+    startGame()
 }
 
+
+fun startGame() {
+    println(BrideGameConstValue.GAME_START_MESSAGE)
+}
---
+++ src/main/kotlin/bridge/BrideGameConstValue.kt
@@ -0,0 +1,6 @@
+package bridge
+
+object BrideGameConstValue {
+    const val GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.\n"
+
+}
