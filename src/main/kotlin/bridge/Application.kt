da09aa8bbab3111d1538143a1070934e50d8d869
[alsswl] <[alswl4223@naver.com]>
2022-11-21 15:08:51 +0900

    feat(Application) : 클래스에 있는 기능 모아서 구현

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,7 +1,35 @@
 package bridge
 
-fun BridgeGameMain(){
-    var InputView = InputView()
+fun BridgeGameing(size : Int,bridge : List<String>) : Int {
+    val inputView = InputView()
+    val outputView = OutputView()
+    for (i in 0 until size) {
+        var way = "E"
+        while (way == "E") {
+            way = inputView.readMoving()
+        }
+        val bridgeGame = BridgeGame()
+
+
+        if (bridgeGame.move(i, bridge, way)) {
+            outputView.printMap(true, bridge, i)
+        } else {
+            outputView.printMap(false, bridge, i)
+            var again = inputView.readGameCommand()
+            if (again == "R") {
+                return 1
+            }
+            else {
+                return 0
+            }
+        }
+    }
+    return 2
+}
+
+fun BridgeGameMain() {
+    val inputView = InputView()
+    val outputView = OutputView()
     var size = -1
     while(size ==-1) {
         size = InputView.readBridgeSize()
