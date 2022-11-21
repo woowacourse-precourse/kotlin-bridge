f007d7069f7ce255c3bd002746a0b5811c6e6c7b
lsakee <lsls4868@gmail.com>
2022-11-22 02:53:41 +0900

    refactor : Output 클래스 사용

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -2,9 +2,9 @@ package bridge
 
 fun main() {
     // TODO: 프로그램 구현
-    gamestart()
+    startGame()
 
 }
-fun gamestart(){
-    print( "총 시도한 횟수: "+BridgeGame().move(BridgeGame().guideSet()))
+fun startGame(){
+    OutputView().printGameEnd()
 }
