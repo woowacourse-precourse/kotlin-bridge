c614576eb0fface93301d6a6b15cd44cbc7bb776
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-22 03:10:34 +0900

    feat(Exception): Exception 클래스 생성 및 에러 메세지 문자열 상수 정의

---
+++ src/main/kotlin/bridge/constants/Exception.kt
@@ -0,0 +1,10 @@
+package bridge.constants
+
+class Exception {
+    companion object {
+        const val EXCEPTION_IS_NOT_NUMBER = "[ERROR] 숫자만 입력해주세요."
+        const val EXCEPTION_INVALID_RANGE_NUMBER = "[ERROR] 3 ~ 20 까지의 숫자를 입력해주세요."
+        const val EXCEPTION_INVALID_MOVING = "[ERROR] U 또는 D만 입력해주세요."
+        const val EXCEPTION_INVALID_RETRY = "[ERROR] R 또는 Q만 입력해주세요."
+    }
+}
