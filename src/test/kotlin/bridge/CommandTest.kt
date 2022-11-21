a29c3c4fa3b99bdf2a5eed49b5da9223dc7d576b
dltkd1395 <dltkd13956@naver.com>
2022-11-22 00:16:25 +0900

    refactor(CommandTest) : 중복되는 코드 수정

--- src/test/kotlin/bridge/CommandTest.kt
+++ src/test/kotlin/bridge/CommandTest.kt
@@ -7,32 +7,8 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class CommandTest {
     @ParameterizedTest
-    @ValueSource(strings = ["1", "100", "2"])
-    fun `명령어가 숫자인 경우 예외 테스트`(execution: String) {
-        assertThrows<IllegalArgumentException> {
-            Command(execution)
-        }
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["r", "q"])
-    fun `명령어가 소문자인 경우 예외 테스트`(execution: String) {
-        assertThrows<IllegalArgumentException> {
-            Command(execution)
-        }
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["RR", "QQ"])
-    fun `명령어가 1글자가 아닌 경우 예외 테스트`(execution: String) {
-        assertThrows<IllegalArgumentException> {
-            Command(execution)
-        }
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["U", "D", "A", "B"])
-    fun `명령어가 R 또는 Q가 아닌 경우 예외 테스트`(execution: String) {
+    @ValueSource(strings = ["1", "100", "2", "r", "q", "RR", "QQ", "U", "D", "A", "B"])
+    fun `명령어 예외 테스트`(execution: String) {
         assertThrows<IllegalArgumentException> {
             Command(execution)
         }
