a04c4830fe0c1fdcae85be7c36497c59be4bc005
nayoung511 <nayoungbae511@gmail.com>
2022-11-21 18:38:46 +0900

    feat (InputView.kt): BridgeSize, Moving, Gamecommand 메서드 추가

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -8,21 +8,23 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        val BridgeSize = readLine()!!.toInt()
-        return BridgeSize
+        val bridgeSize = readLine()!!.toInt()
+        return bridgeSize
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        val movingDirection = readLine()!!.toString()
+        return movingDirection
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        val gameCommand = readLine()!!.toString()
+        return gameCommand
     }
 }
