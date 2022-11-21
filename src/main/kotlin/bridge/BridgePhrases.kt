168e0ce12d3b94c3c9c84b2e33ade6cd0522bead
changhwan <chang5hustle@gmail.com>
2022-11-21 18:04:23 +0900

    feat(bridge) : OutputView 게임 진행 대사 출력 함수 작성

--- src/main/kotlin/bridge/BridgePhrases.kt
+++ src/main/kotlin/bridge/BridgePhrases.kt
@@ -6,4 +6,9 @@ object BridgePhrases {
     const val INPUT_IS_NOT_ALPHABET_ERROR = "[ERROR] 입력하신 내용이 알파벳이 아닙니다."
     const val MOVING_CODE_INCORRECT_ERROR = "[ERROR] 입력하신 문자가 올바른 코드인 U,D가 아닙니다."
     const val GAME_COMMEND_CODE_INCORRECT_ERROR = "[ERROR] 입력하신 문자가 올바른 코드인 R,Q가 아닙니다."
+
+    const val GAME_START_PHRASE = "다리 건너기 게임을 시작합니다."
+    const val BRIDGE_SIZE_PHRASE = "다리의 길이를 입력해주세요."
+    const val MOVING_BRIDGE_SELECT_PHRASE = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
+    const val RESTART_OR_QUIT_SELECT_PHRASE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
 }
--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -1,5 +1,10 @@
 package bridge
 
+import bridge.BridgePhrases.BRIDGE_SIZE_PHRASE
+import bridge.BridgePhrases.GAME_START_PHRASE
+import bridge.BridgePhrases.MOVING_BRIDGE_SELECT_PHRASE
+import bridge.BridgePhrases.RESTART_OR_QUIT_SELECT_PHRASE
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -17,4 +22,20 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printResult() {}
+
+    fun printGameStartPhrase() {
+        println(GAME_START_PHRASE)
+    }
+
+    fun printBridgeSizePhrase() {
+        println(BRIDGE_SIZE_PHRASE)
+    }
+
+    fun printMovingBridgeSelectPhrase() {
+        println(MOVING_BRIDGE_SELECT_PHRASE)
+    }
+
+    fun printRestartOrQuitSelectPhrase() {
+        println(RESTART_OR_QUIT_SELECT_PHRASE)
+    }
 }
