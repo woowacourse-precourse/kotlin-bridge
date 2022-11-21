a6d6fa9f882b9f315e99d190824dbc198910a430
phs <gustjd0808@pusan.ac.kr>
2022-11-21 17:48:28 +0900

    test: test to reset player bridge blocks

---
+++ src/test/kotlin/bridge/domain/PlayerTest.kt
@@ -0,0 +1,19 @@
+package bridge.domain
+
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class PlayerTest {
+    @Test
+    fun `resetBlocks 메서드를 사용해 사용자가 선택했던 다리의 blocks를 초기화하고 시도 횟수를 늘린다`() {
+        val player = Player(mutableListOf(
+            PlayerBlock(Direction.DOWN, true),
+            PlayerBlock(Direction.UP, true),
+            PlayerBlock(Direction.DOWN, false)), 1)
+        player.resetBlocks()
+        assertThat(player.getPlayerBlocks()).isEmpty()
+        assertThat(player.getTries()).isEqualTo(2)
+    }
+}
