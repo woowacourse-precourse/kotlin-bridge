e33b752494980c484c4e52b33f0210aa418438ff
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-22 01:23:08 +0900

    feat(view): View.printMap() 함수 기능 구현

--- src/main/kotlin/bridge/view/View.kt
+++ src/main/kotlin/bridge/view/View.kt
@@ -1,8 +1,12 @@
 package bridge.view
 
+import bridge.data.GameHistory
+
 object View {
 
     fun printStartGame() = OutputView.printStartGame()
+
+    fun printMap(history: GameHistory) = OutputView.printMap(history)
 
 
 }
