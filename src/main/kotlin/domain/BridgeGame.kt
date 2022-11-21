e87857882bc7b04836d137a5b3ec95a06697fb78
junhyungss <daba44@naver.com>
2022-11-21 17:43:27 +0900

    refactor(BridgeGame): 게임 다시 시작 메시지 함수 분리 retyr()함수에서 게임 다시 시작 여부를 입력 받은 후 처리

--- src/main/kotlin/domain/BridgeGame.kt
+++ src/main/kotlin/domain/BridgeGame.kt
@@ -9,7 +9,6 @@ class BridgeGame {
     private val outputView = OutputView()
     private var playMap = Pair<List<String>, List<String>>(arrayListOf(), arrayListOf())
 
-
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
@@ -24,17 +23,14 @@ class BridgeGame {
         for (i in 1.. bridgeSize){
             val move = OutputView().printMoving()
             if (!isMoving(i, bridgeMaker, move)) {
-                val retry = retry()
-                if (retry == "R") return play(bridgeSize,bridgeMaker, numberOfGames+1)
-                if(retry == "Q"){
-                    outputView.printResult("실패", numberOfGames)
-                    break
-                }
+                retry(bridgeSize,bridgeMaker, numberOfGames)
+                break
             }
         }
         outputView.printResultMap(playMap)
         outputView.printResult("성공", numberOfGames)
     }
+
 
     // 현재 입력 값과 다리의 값이 같은지, 들린지
     private fun isMoving(bridgeToNow: Int, bridge: List<String>, nowMove:String): Boolean{
@@ -47,7 +43,11 @@ class BridgeGame {
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    private fun retry(): String {
-        return outputView.printRetry()
+    private fun retry(bridgeSize: Int, bridgeMaker: List<String>, numberOfGames:Int) {
+        val retry = outputView.printRetry()
+        if (retry == "R") return play(bridgeSize,bridgeMaker, numberOfGames+1)
+        if(retry == "Q"){
+            outputView.printResult("실패", numberOfGames)
+        }
     }
 }
