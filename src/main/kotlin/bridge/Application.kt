d22882f0e5f5eb955c37c6e340661046ff1de8d7
lanpia <95679243+lanpia@users.noreply.github.com>
2022-11-22 13:24:33 +0900

    feat,(Application) : creat the main & subfucntion 1. 랜덤으로 생성된 다리와 사용자가 입력한 U & D 비교함수 2. 성공 & 실패 Count 함수 3. 게임 횟수 Count하고 재시작 또는 종료하는 함수 4. main 함수 와 playgame 함수 호출로 게임을 진행

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,54 @@
 package bridge
 
-fun main() {
-    // TODO: 프로그램 구현
+fun playinggame (playround : Int, size : Int, gamemap: List<String>) {
+    val output = OutputView()
+    val input = InputView()
+    var ox  = mutableListOf<Pair<String, String>>()
+    for(i in 0..size-1 step 1) {
+        ox = comparelist(gamemap, ox, i)
+        if (ox[i].first.contains("X") || ox[i].second.contains("X")) break
+    }
+    var wingame = countdone(ox, size)
+    var quit = ""
+    if (ox.size != size ) { quit = restartgame(playround, size, gamemap)}
+    if (quit == "Q" || wingame == "성공") {
+        output.printResult1(ox, size)
+        output.printResult2(wingame, playround)
+    }
 }
+fun comparelist(gamemap: List<String>, output : MutableList<Pair<String, String>> ,size : Int) : MutableList<Pair<String, String>>{ // 입력인자랑 랜덤 맵비교
+    val input = InputView().readMoving()
+    if (gamemap[size] == input && input == "U")  {output += Pair(" O ", "   ") }
+    else if (gamemap[size] != input && input == "U")   {output += Pair(" X ", "   ")}
+    else if (gamemap[size] == input && input == "D")  {output += Pair("   ", " O ")  }
+    else if (gamemap[size] != input && input == "D")  {output += Pair("   ", " X ") }
+    OutputView().printMap(output, size)
+    return output
+}
+
+fun countdone(output: MutableList<Pair<String, String>>, size: Int) : String{
+    if (output.size != size || size == 0) return "실패"
+    if (output.size == size) return "성공"
+    return ""
+}
+
+fun restartgame (playround: Int, size: Int, gamemap: List<String>) : String{
+    val input = InputView()
+    var playround = playround
+    when (input.readGameCommand()) {
+        "R" -> { playround += 1 ; playinggame(playround, size, gamemap); return ""}
+        "Q" -> return "Q"
+        else -> return ""
+    }
+}
+
+fun main() {
+    // TODO: 맵 재활용 // 입력 업다운이랑 맵비교 // 성공실패//게임종료가 한번에 안됨
+    val input = InputView()
+    val playround = 1
+    OutputView().startgame()
+    var size = input.readBridgeSize()
+    var gamemap = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(size.toInt())
+    playinggame(playround, size, gamemap)
+}
+
