0b0052792f03598e8aa0420517d39960d003fc01
dlgocks1 <cksgodl1@naver.com>
2022-11-22 13:59:16 +0900

    refactor(BridgeResult) : 기존 enum class에서 결과 값을 가지는 결과클래스로 변경

--- src/main/kotlin/bridge/model/BridgeResult.kt
+++ src/main/kotlin/bridge/model/BridgeResult.kt
@@ -1,8 +1,20 @@
 package bridge.model
 
-enum class BridgeResult {
-    FINISH,
-    SUCCESS,
-    FAIL;
+class BridgeResult {
+    private val bridgeMap: BridgeMap = BridgeMap()
+    private var totalCount: Int = 1
+
+    fun restart() {
+        bridgeMap.clear()
+        totalCount++
+    }
+
+    fun getTotalCount(): Int = totalCount
+
+    fun getBridgeMap(): String = bridgeMap.toString()
+
+    fun update(result: BridgeStatus) {
+        bridgeMap.update(result)
+    }
 
 }
