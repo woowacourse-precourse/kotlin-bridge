b5af80c976a4729229046f95cbb7734a5f571dae
no1msh <no1msh1217@gmail.com>
2022-11-22 19:36:37 +0900

    test: 기능 테스트 추가

--- src/test/kotlin/bridge/ApplicationTest.kt
+++ src/test/kotlin/bridge/ApplicationTest.kt
@@ -35,6 +35,40 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
+    fun `중간에 한번 잘못된 값 넣어도 잘 동작 하는지 확인`() {
+        assertRandomNumberInRangeTest({
+            run("3", "U", "D", "q", "D", "Q")
+            assertThat(output()).contains(
+                "최종 게임 결과",
+                "[ O |   |   ]",
+                "[   | O | X ]",
+                "게임 성공 여부: 실패",
+                "총 시도한 횟수: 1"
+            )
+            val upSideIndex = output().indexOf("[ O |   |   ]")
+            val downSideIndex = output().indexOf("[   | O | X ]")
+            assertThat(upSideIndex).isLessThan(downSideIndex)
+        }, 1, 0, 1)
+    }
+
+    @Test
+    fun `두번 시도하고 성공결과가 잘나오는지 확인`() {
+        assertRandomNumberInRangeTest({
+            run("5", "U", "U", "R", "U", "D", "U", "D", "D")
+            assertThat(output()).contains(
+                "최종 게임 결과",
+                "[ O |   | O |   |   ]",
+                "[   | O |   | O | O ]",
+                "게임 성공 여부: 성공",
+                "총 시도한 횟수: 2"
+            )
+            val upSideIndex = output().indexOf("[ O |   | O |   |   ]")
+            val downSideIndex = output().indexOf("[   | O |   | O | O ]")
+            assertThat(upSideIndex).isLessThan(downSideIndex)
+        }, 1, 0, 1, 0, 0)
+    }
+
+    @Test
     fun `예외 테스트`() {
         assertSimpleTest {
             runException("a")
