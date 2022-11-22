5c5b37b1620ec750f79c78190e0a3fe77d606523
lee989898 <wnsrud1352@gmail.com>
2022-11-22 15:45:17 +0900

    [refactor](OutputView) 불필요한 주석 제거

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -9,20 +9,10 @@ class OutputView {
         println("다리 건너기 게임을 시작합니다.\n")
     }
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     fun printMap(ox: List<MutableList<String>>) {
         printGameResult(ox)
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     fun printResult(ox: List<MutableList<String>>, count: Int, isSuccesss: String) {
         var isSuccess = "성공"
         if (isSuccesss == "FAIL") {
@@ -44,6 +34,3 @@ class OutputView {
         println()
     }
 }
-
-// OutputView의 메서드의 이름은 변경할 수 없고, 인자와 반환 타입은 필요에 따라 추가하거나 변경할 수 있다.
-// 값 출력을 위해 필요한 메서드를 추가할 수 있다.
