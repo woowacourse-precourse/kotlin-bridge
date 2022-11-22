daa0ef566231c0880c601635edd7c8bb357ad825
최재훈 <be_clever@naver.com>
2022-11-22 20:53:27 +0900

    feat(InputView): 예외 처리 기능 추가
    - 다리 길이가 올바르지 않은 경우 예외 처리 하는 기능 추가
    - 플레이어가 입력한 이동 칸이 올바르지 않은 경우 예외 처리 하는 기능 추가
    - 게임 재시작/종료 값이 올바르지 않은 경우 예외 처리 하는 기능 추가
    - 예외 사항 발생 시 에러 메시지를 출력하는 기능 추가

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import kotlin.system.exitProcess
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,14 +10,31 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        println("다리 건너기 게임을 시작합니다.\n다리의 길이를 입력해주세요.")
-        val input = readLine() ?: throw IllegalArgumentException()
+        println("다리 건너기 게임을 시작합니다.\n\n다리의 길이를 입력해주세요.")
+        while(true) {
+            try {
+                val input = readLine()
+                checkBridgeSize(input)
+                return input!!.toInt()
+            } catch (e: IllegalArgumentException) {
+                println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.")
+            }
+        }
+    }
+
+    private fun checkBridgeSize(input: String?) {
+        if(input == null) {
+            exitProcess(0)
+        }
+        var result = 0
         try {
-            input.toInt()
+            result = input.toInt()
         } catch (e: Exception) {
             throw IllegalArgumentException()
         }
-        return input.toInt()
+        if(result < 3 || result > 20) {
+            throw IllegalArgumentException()
+        }
     }
 
     /**
@@ -24,13 +43,24 @@ class InputView {
     fun readMoving(): String {
         println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
 
-        val input = readLine() ?: throw IllegalArgumentException()
+        while(true) {
+            try {
+                val input = readLine()
+                checkMoving(input)
+                return input!!
+            } catch (e: IllegalArgumentException) {
+                println("[ERROR] 이동할 칸은 U 또는 D로 입력해야 합니다.")
+            }
+        }
+    }
 
+    private fun checkMoving(input: String?) {
+        if(input == null) {
+            exitProcess(0)
+        }
         if(input != "U" && input != "D") {
             throw IllegalArgumentException()
         }
-
-        return input
     }
 
     /**
@@ -39,12 +69,23 @@ class InputView {
     fun readGameCommand(): String {
         println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
 
-        val input = readLine() ?: throw IllegalArgumentException()
+        while(true) {
+            try {
+                val input = readLine()
+                checkCommand(input)
+                return input!!
+            } catch (e: IllegalArgumentException) {
+                println("[ERROR] 재시도 여부는 R 또는 Q로 입력해야 합니다.")
+            }
+        }
+    }
 
+    private fun checkCommand(input: String?) {
+        if(input == null) {
+            exitProcess(0)
+        }
         if(input != "R" && input != "Q") {
             throw IllegalArgumentException()
         }
-
-        return input
     }
 }
