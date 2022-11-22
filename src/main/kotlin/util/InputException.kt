24045b5648ae9b5655f687344fdfc3cd37647cc2
username <ieejo716@naver.com>
2022-11-22 16:03:02 +0900

    [FEAT] : 재시도 예외 구현

--- src/main/kotlin/util/InputException.kt
+++ src/main/kotlin/util/InputException.kt
@@ -17,6 +17,15 @@ class InputException {
         }
     }
 
+    fun checkRetryCommand(input: String?) {
+        when {
+            checkNullException(input) -> printException(NULL_ERROR)
+            checkRetryException(input!!) -> printException(COMMAND_ERROR)
+        }
+    }
+
+    private fun checkRetryException(input: String): Boolean = !listOf("R", "Q").contains(input)
+
     private fun checkCommandException(input: String): Boolean = !listOf("U", "D").contains(input)
 
     private fun checkNullException(input: String?): Boolean = input.isNullOrEmpty()
@@ -36,10 +45,12 @@ class InputException {
         throw IllegalArgumentException("[ERROR] $message")
     }
 
+
     companion object {
         const val NULL_ERROR = "입력 값이 없습니다."
         const val RANGE_ERROR = "3~20의 범위가 아닙니다."
         const val TYPE_ERROR = "올바른 형식이 아닙니다."
         const val COMMAND_ERROR = "U, D 만 입력 가능합니다."
+        const val REQ_ERROR = "R, Q 만 입력 가능합니다."
     }
 }
