83b8efb1c1cf529b7846ec2e029b6d1856d094bd
KimDongHyun0907 <ehdgus8397@naver.com>
2022-11-22 23:46:36 +0900

    [make] InputView fun readMoving getMoving

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -22,7 +22,15 @@ class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-
+    fun readMoving(): String {
+        println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
+        return try{
+            getMoving()
+        }catch (e:IllegalArgumentException){
+            println(e.message)
+            readMoving()
+        }
+    }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
@@ -37,6 +45,12 @@ class InputView {
             throw IllegalArgumentException("[ERROR] 숫자를 입력하세요.")
         }
         return size
+    }
+
+    private fun getMoving(): String{
+        val moving: String = Console.readLine().trim()
+        if(moving!="U" && moving !="D") throw IllegalArgumentException("[ERROR] 이동하기 위해서는 U, D 중 하나를 입력하세요")
+        return moving
     }
 
 
