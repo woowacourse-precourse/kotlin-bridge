66f63fa9da6eb082df30d24a04c5baf44d0b190e
s9hn <tnghqn@naver.com>
2022-11-22 20:08:32 +0900

    [FEAT(InputViewTest)] 방향 입력 관련 테스트코드 추가

--- src/test/kotlin/bridge/InputViewTest.kt
+++ src/test/kotlin/bridge/InputViewTest.kt
@@ -1,33 +1,61 @@
 package bridge
 
+import bridge.ui.InputChecker
 import camp.nextstep.edu.missionutils.test.Assertions
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
-
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class InputViewTest : NsTest() {
-    @Test
-    fun `입력문 - 문자열 입력테스트`() {
-        Assertions.assertSimpleTest {
-            runException("a","ㅈㅇㄹㅈㅇ")
-            assertThat(output()).contains(InputViewTest.ERROR_MESSAGE)
+    @Nested
+    inner class BridgeLengthTest {
+        @Test
+        fun `다리 길이 입력문 - 문자열 입력테스트`() {
+            Assertions.assertSimpleTest {
+                runException("a", "ㅈㅇㄹㅈㅇ")
+                assertThat(output()).contains(ERROR_MESSAGE)
+            }
+        }
+
+        @Test
+        fun `다리 길이 입력문 - 범위 외 값 입력테스트`() {
+            Assertions.assertSimpleTest {
+                runException("2", "55")
+                assertThat(output()).contains(ERROR_MESSAGE)
+            }
+        }
+
+        @Test
+        fun `다리 길이 입력문 - 널값 입력테스트`() {
+            Assertions.assertSimpleTest {
+                runException("", "  ")
+                assertThat(output()).contains(ERROR_MESSAGE)
+            }
         }
     }
 
-    @Test
-    fun `입력문 - 범위 외 값 입력테스트`() {
-        Assertions.assertSimpleTest {
-            runException("2","55")
-            assertThat(output()).contains(InputViewTest.ERROR_MESSAGE)
+    @Nested
+    inner class DirectionTest {
+        @Test
+        fun `방향 입력문 - 널값 입력테스트`() {
+            Assertions.assertSimpleTest {
+                assertThrows<IllegalArgumentException> { InputChecker().checkDirection("") }
+            }
         }
-    }
+        @Test
+        fun `방향 입력문 - "U", "D"가 아닐 경우`() {
+            Assertions.assertSimpleTest {
+                assertThrows<IllegalArgumentException> { InputChecker().checkDirection("3") }
+            }
+        }
 
-    @Test
-    fun `입력문 - 널값 입력테스트`() {
-        Assertions.assertSimpleTest {
-            runException("","  ")
-            assertThat(output()).contains(InputViewTest.ERROR_MESSAGE)
+        @Test
+        fun `방향 입력문 - "U", "D"가 아닐 경우2`() {
+            Assertions.assertSimpleTest {
+                assertThrows<IllegalArgumentException> { InputChecker().checkDirection("a") }
+            }
         }
     }
 
