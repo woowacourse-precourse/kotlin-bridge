b5602fdc0234a34bf03cf126271192a57735689a
hyungmin <ramkid91@naver.com>
2022-11-22 18:50:46 +0900

    feat: 다리 생성 로직 구현
    - BridgeMaker의  makeBridge 구현
    - size만큼 반복하며 BridgeNumberGenerator의 genereate 함수 호출
    - generate 값에 따라 다리 리스트에 U or D 삽입
    - 결과 리스트 리턴

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,16 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
-    }
+        val res = mutableListOf<String>()
+        for(i in 0 until size){
+            val bridgeNumber = bridgeNumberGenerator.generate()
+            if(bridgeNumber==1){
+                res.add("U")
+            }
+            else{
+                res.add("D")
+            }
+        }
+        return res
+     }
 }
