0f2b25e43e1b2077369bc68c1f005f6bbe19eb16
changhwan <chang5hustle@gmail.com>
2022-11-22 16:51:05 +0900

    feat(bridge) : 입력 오류시 에러메시지 발생후 다시 로직 시작하도록 함수 변경

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -1,5 +1,7 @@
 package bridge.view
 
+import bridge.BridgeType
+import bridge.GameState
 import bridge.constants.BridgeGameRule.BRIDGE_MAXIMUM_LENGTH
 import bridge.constants.BridgeGameRule.BRIDGE_MINIMUM_LENGTH
 import bridge.constants.BridgePhrases.BRIDGE_SIZE_INPUT_NOT_IN_CORRECT_RANGE_ERROR
@@ -7,13 +9,18 @@ import bridge.constants.BridgePhrases.BRIDGE_SIZE_INPUT_TYPE_ERROR
 import bridge.constants.BridgePhrases.GAME_COMMEND_CODE_INCORRECT_ERROR
 import bridge.constants.BridgePhrases.INPUT_IS_NOT_ALPHABET_ERROR
 import bridge.constants.BridgePhrases.MOVING_CODE_INCORRECT_ERROR
-import bridge.BridgeType
-import bridge.GameState
 import camp.nextstep.edu.missionutils.Console.*
 import java.util.regex.Pattern
 
 class InputView {
-    fun readBridgeSize(): Int = getValidateBridgeSize(readLine())
+    fun readBridgeSize(): Int {
+        return try {
+            getValidateBridgeSize(readLine())
+        } catch (error: IllegalArgumentException) {
+            println(error.message)
+            readBridgeSize()
+        }
+    }
 
     fun getValidateBridgeSize(userInput: String): Int {
         require(isNumber(userInput)) { BRIDGE_SIZE_INPUT_TYPE_ERROR }
@@ -28,7 +35,14 @@ class InputView {
 
     private fun convertToInt(input: String): Int = input.toInt()
 
-    fun readMoving(): String = getValidateMovingCode(readLine())
+    fun readMoving(): String {
+        return try {
+            getValidateMovingCode(readLine())
+        } catch (error: IllegalArgumentException) {
+            println(error.message)
+            readMoving()
+        }
+    }
 
     fun getValidateMovingCode(userInput: String): String {
         require(isAlphabet(userInput)) { INPUT_IS_NOT_ALPHABET_ERROR }
@@ -40,7 +54,14 @@ class InputView {
     private fun checkCorrectMovingCode(input: String): Boolean =
         input == BridgeType.UP.stringCode || input == BridgeType.DOWN.stringCode
 
-    fun readGameCommand(): String = getValidateGameCommend(readLine())
+    fun readGameCommand(): String {
+        return try {
+            getValidateGameCommend(readLine())
+        } catch (error: IllegalArgumentException) {
+            println(error.message)
+            readGameCommand()
+        }
+    }
 
     fun getValidateGameCommend(userInput: String): String {
         require(isAlphabet(userInput)) { INPUT_IS_NOT_ALPHABET_ERROR }
