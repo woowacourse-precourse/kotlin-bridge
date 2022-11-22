21e00d414c65291c1f15ef96787043514f54d9b4
최재훈 <be_clever@naver.com>
2022-11-22 16:59:38 +0900

    feat(input): 현재까지 다리의 상태를 출력하는 기능 추가
    현재 입력받은 위치까지의 다리의 상태를 확인하고 O 또는 X로 확인하는 기능 추가

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -9,7 +9,45 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(bridge: List<String>, root: List<String>): Boolean {
+        val upBridge = getUpBridge(bridge, root)
+        val downBridge = getDownBridge(bridge, root)
+
+        println(upBridge)
+        println(downBridge)
+
+        return upBridge.contains('X') || downBridge.contains('X')
+    }
+
+    private fun getUpBridge(bridge: List<String>, root: List<String>): String {
+        var result = "["
+        for (i in root.indices) {
+            result += getState(bridge[i], root[i], "U")
+            result += if(i != root.size - 1) "|" else ""
+        }
+        result += "]"
+        return result
+    }
+
+    private fun getDownBridge(bridge: List<String>, root: List<String>): String {
+        var result = "]"
+        for(i in root.indices) {
+            result += getState(bridge[i], root[i], "D")
+            result += if(i != root.size - 1) "|" else ""
+        }
+        result += "]"
+        return result
+    }
+
+    private fun getState(bridgeState: String, rootState: String, position: String): String {
+        if(bridgeState == position && bridgeState == rootState) {
+            return " O "
+        }
+        if(bridgeState != position) {
+            return "   "
+        }
+        return " X "
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
