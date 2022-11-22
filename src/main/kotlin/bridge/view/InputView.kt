c0a94b59cf42432ffcfbab4fb9c51bab01173025
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-22 18:33:01 +0900

    refactor(bridge): 입력이 잘못되면 입력이 잘못된 부분부터 다시 입력을 받을 수 있도록 수정

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -3,6 +3,7 @@ package bridge.view
 import bridge.util.Converter
 import bridge.validation.InputValidation
 import camp.nextstep.edu.missionutils.Console
+import kotlin.IllegalArgumentException
 
 class InputView {
 
@@ -10,34 +11,47 @@ class InputView {
     private val converter = Converter()
 
     fun readBridgeSize(): Int {
-        println(INPUT_BRIDGE_SIZE_MESSAGE)
-        val input = Console.readLine()
-        inputValidation.checkInputInteger(input)
-        val bridgeSize = converter.convertStringToInt(input)
-        inputValidation.checkBridgeSizeRange(bridgeSize)
-        println()
-        return bridgeSize
+        return try{
+            println(INPUT_BRIDGE_SIZE_MESSAGE)
+            val input = Console.readLine()
+            inputValidation.checkInputInteger(input)
+            inputValidation.checkBridgeSizeRange(converter.convertStringToInt(input))
+            converter.convertStringToInt(input)
+        }catch (e:IllegalArgumentException){
+            println(e.message)
+            readBridgeSize()
+        }
     }
 
     fun readMoving(): String {
         println(INPUT_MOVING_MESSAGE)
         val moving = Console.readLine()
-        inputValidation.checkInputLength(moving)
-        inputValidation.checkUserMovingValidation(moving)
-        return moving
+        return try {
+            inputValidation.checkInputLength(moving)
+            inputValidation.checkUserMovingValidation(moving)
+            moving
+        }catch (e:IllegalArgumentException){
+            println(e.message)
+            readMoving()
+        }
     }
 
     fun readGameCommand(): String {
-        println(INPUT_GAME_COMMAND_MESSAGE)
-        val gameCommand = Console.readLine()
-        inputValidation.checkInputLength(gameCommand)
-        inputValidation.checkUserRestartValidation(gameCommand)
-        return gameCommand
+        return try {
+            println(INPUT_GAME_COMMAND_MESSAGE)
+            val gameCommand = Console.readLine()
+            inputValidation.checkInputLength(gameCommand)
+            inputValidation.checkUserRestartValidation(gameCommand)
+            gameCommand
+        }catch (e:IllegalArgumentException){
+            println(e.message)
+            readGameCommand()
+        }
     }
 
     companion object {
         const val INPUT_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요."
-        const val INPUT_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
+        const val INPUT_MOVING_MESSAGE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)"
         const val INPUT_GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
     }
 }
