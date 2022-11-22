5d012fb48954ba357950aee7659754159c50fe2d
username <ieejo716@naver.com>
2022-11-22 19:48:33 +0900

    [FEAT] : companion object 분리

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,14 +1,27 @@
 package view
 
+import data.BridgeStore.BRIDGE_DOWN
+import data.BridgeStore.BRIDGE_END
+import data.BridgeStore.BRIDGE_MID
+import data.BridgeStore.BRIDGE_UP
+import data.BridgeStore.BRIDGE_START
+import data.BridgeStore.FAIL
+import data.BridgeStore.FINAL_RESULT
+import data.BridgeStore.INPUT_BRIDGE_LENGTH
+import data.BridgeStore.MOVE_BRIDGE
+import data.BridgeStore.RETRY_GAME
+import data.BridgeStore.SPACE
+import data.BridgeStore.START_GAME
+import data.BridgeStore.SUCCESS
+import data.BridgeStore.SUCCESS_GAME_CHECK
+import data.BridgeStore.TRY_COUNT
+import data.BridgeStore.USER_ALIVE
+import data.BridgeStore.USER_DIE
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 class OutputView {
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
 
     fun printMap(bridge: List<String>, input: List<String>) {
         printUpDecide(bridge, input)
@@ -53,11 +66,6 @@ class OutputView {
         return SPACE
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     fun printResult() = println(FINAL_RESULT)
 
     fun printStartGame() = println(START_GAME)
@@ -75,24 +83,4 @@ class OutputView {
     }
 
     fun printTryCount(count: Int) = println("$TRY_COUNT $count")
-
-    companion object {
-        const val START_GAME = "다리 건너기 게임을 시작합니다."
-        const val INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요."
-        const val MOVE_BRIDGE = "이동할 칸을 선택해주세요. (위 : U, 아래 : D)"
-        const val RETRY_GAME = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
-        const val FINAL_RESULT = "최종 게임 결과"
-        const val SUCCESS_GAME_CHECK = "게임 성공 여부:"
-        const val TRY_COUNT = "총 시도한 횟수:"
-        const val SUCCESS = "성공"
-        const val FAIL = "실패"
-        const val USER_ALIVE = "O"
-        const val USER_DIE = "X"
-        const val SPACE = " "
-        const val BRIDGE_START = "["
-        const val BRIDGE_END = "]"
-        const val BRIDGE_MID = "|"
-        const val BRIDGE_UP = "U"
-        const val BRIDGE_DOWN = "D"
-    }
 }
