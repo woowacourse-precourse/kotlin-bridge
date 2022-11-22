116ea10f177f8f0a25f27742d1d480bc0c67b35b
윤동현 <donghyunyoon81@gmail.com>
2022-11-22 19:54:56 +0900

    feat<BridgeResource> : 사용할 숫자를 BridgeNumber enum class로 구현
    어떻게 : 각 상수에 상수와 관련된 number를  저장합니다.
    왜 :     enum class로 상수 선언하면 코드 가독성 이점과 타입 안전성 보장을 위해서

--- src/main/kotlin/bridge/domain/data/BridgeResource.kt
+++ src/main/kotlin/bridge/domain/data/BridgeResource.kt
@@ -1,11 +1,18 @@
 package bridge.domain.data
 
-enum class BridgeKeyword(val keyword:String){
+enum class BridgeKeyword(val keyword: String) {
     PASS("O"),
     FAIL("X"),
     UP("U"),
     DOWN("D"),
-    BLANK( " "),
+    BLANK(" "),
     RETRY("R"),
     QUIT("Q")
+}
+
+enum class BridgeNumber(val number: Int) {
+    ONE(1),
+    ZERO(0),
+    MAX_BRIDGE_SIZE(20),
+    MIN_BRIDGE_SIZE(3)
 }
