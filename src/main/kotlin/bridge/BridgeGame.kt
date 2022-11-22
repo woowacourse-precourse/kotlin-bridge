f405cb1d2ffbc763054b9a41b4bfcadf21083fbc
bluecat0823 <star4_you@naver.com>
2022-11-22 12:50:39 +0900

    [refactor] 재시작 여부와 최종 결과 출력 리팩토링

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -4,16 +4,17 @@ package bridge
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move(bridgeAnswer : List<String>, spaceTogo : String, turnCount : Int): Boolean {
-        return when (spaceTogo){
-            bridgeAnswer[turnCount] -> true
-            else -> false
+    fun move(currentSpace : String, currentMove : String): String {
+        return when (currentMove){
+            currentSpace -> "O"
+            else -> "X"
         }
     }
 
@@ -23,11 +24,8 @@ class BridgeGame {
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry(gameCommand : String): Boolean {
-        return when (gameCommand){
-            "R" -> true
-            "Q" -> false
-            else -> throw IllegalArgumentException(ErrorMessage.FORMATION.message)
-        }
+    fun retry(bridgeAnswer : List<String>){
+        val movedResult = gameCycleSecond(bridgeAnswer)
+        gameCycleThird(bridgeAnswer, movedResult)
     }
 }
--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -52,11 +52,15 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult(bridgeAnswer: List<String>, gameCommand : String, retryCount : Int) {
+
+    fun printResult(bridgeAnswer: List<String>, moveResult: List<String>, retryCount : Int) {
+        println("최종 게임 결과")
+        OutputView().printMap(bridgeAnswer, moveResult)
         print("게임 성공 여부: ")
-        when (gameCommand){
-            "R" -> println("성공")
-            "Q" -> println("실패")
+        if ("X" in moveResult){
+            println("실패")
+        } else {
+            println("성공")
         }
         print("총 시도한 횟수: $retryCount")
     }
