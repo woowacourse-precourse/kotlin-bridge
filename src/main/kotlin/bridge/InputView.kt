31843f8858f6f8b2d7c6ee517ca133ad07b7f18a
hyungmin <ramkid91@naver.com>
2022-11-22 19:20:41 +0900

    feat:  입력 처리에 대한 안내메시지 출력 추가

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -10,6 +10,7 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
+        println("다리 길이를 입력해주세요.")
         val input = Console.readLine()
         InputValidator.checkOnlyNumber(input)
         InputValidator.checkInBridgeRange(input)
@@ -20,6 +21,7 @@ class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
+        println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
         val input = Console.readLine()
         InputValidator.checkOnlyCharacter(input)
         InputValidator.checkUorD(input)
@@ -30,6 +32,7 @@ class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
+        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
         val input= Console.readLine()
         InputValidator.checkOnlyCharacter(input)
         InputValidator.checkQorR(input)
