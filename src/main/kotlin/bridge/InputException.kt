60c76fed9fcb3434c11dd2b8f4af6b88aeb2eba8
Yuhee Kim <dbgml4509@naver.com>
2022-11-22 23:07:23 +0900

    refactor : InputException 내 Constants 패키지 경로 변경

--- src/main/kotlin/bridge/InputException.kt
+++ src/main/kotlin/bridge/InputException.kt
@@ -1,36 +1,36 @@
 package bridge
 
-import bridge.Constants.Companion.DOWN
-import bridge.Constants.Companion.QUIT
-import bridge.Constants.Companion.RETRY
-import bridge.Constants.Companion.UP
+import bridge.Model.Constants.Companion.DOWN
+import bridge.Model.Constants.Companion.QUIT
+import bridge.Model.Constants.Companion.RETRY
+import bridge.Model.Constants.Companion.UP
 import java.lang.IllegalArgumentException
 import java.lang.NumberFormatException
 
 class InputException {
 
-    fun checkInputIsNull(input : String){
+    fun checkInputIsNull(input: String) {
         require(input.isNotEmpty()) { ERROR_HEADER + ERROR_NULL_INPUT }
     }
 
-    fun checkBridgeLenInputIsNumeric(input : String){
+    fun checkBridgeLenInputIsNumeric(input: String) {
         try {
             input.toInt()
-        } catch (e : NumberFormatException){
+        } catch (e: NumberFormatException) {
             throw IllegalArgumentException(ERROR_HEADER + ERROR_BRIDGE_LEN_INPUT)
         }
     }
 
-    fun checkDirectionInput(input : String){
+    fun checkDirectionInput(input: String) {
         require(input.contains(UP) || input.contains(DOWN)) { ERROR_HEADER + ERROR_BRIDGE_DIRECTION_INPUT }
     }
 
-    fun checkRetryCommandInput(input : String){
+    fun checkRetryCommandInput(input: String) {
         require(input.contains(RETRY) || input.contains(QUIT)) { ERROR_HEADER + ERROR_RETRY_COMMAND_INPUT }
     }
 
-    companion object{
-        const val ERROR_HEADER = "[ERROR]"
+    companion object {
+        const val ERROR_HEADER = "[ERROR] "
 
         const val ERROR_NULL_INPUT = "입력된 값이 없습니다."
         const val ERROR_BRIDGE_LEN_INPUT = "다리 길이는 숫자만 가능합니다."
