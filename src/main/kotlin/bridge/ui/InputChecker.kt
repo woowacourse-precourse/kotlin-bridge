6e6a9c59b6ea945b67f0c2d10d19798fbf2e72b2
s9hn <tnghqn@naver.com>
2022-11-22 20:07:37 +0900

    [FEAT(InputChecker)] 방향 입력 예외처리
    1. 널값 체크
    2. U, D 가 아닌 입력값 체크

--- src/main/kotlin/bridge/ui/InputChecker.kt
+++ src/main/kotlin/bridge/ui/InputChecker.kt
@@ -1,17 +1,23 @@
 package bridge.ui
 
 import bridge.data.ERROR_DIGIT
+import bridge.data.ERROR_DIRECTION
 import bridge.data.ERROR_NULL
 import bridge.data.ERROR_RANGE
 
 class InputChecker {
-    fun except(size: String) {
-        if (size.isEmpty()) throw IllegalArgumentException(ERROR_NULL)
-        if (!isNumber(size)) throw IllegalArgumentException(ERROR_DIGIT)
-        if (size.toInt() !in 3..20) throw java.lang.IllegalArgumentException(ERROR_RANGE)
+    fun checkBridgeLength(input: String) {
+        if (input.isEmpty()) throw IllegalArgumentException(ERROR_NULL)
+        if (!isNumber(input)) throw IllegalArgumentException(ERROR_DIGIT)
+        if (input.toInt() !in 3..20) throw java.lang.IllegalArgumentException(ERROR_RANGE)
     }
 
-    fun isNumber(input: String): Boolean {
-        return if (input.isNullOrEmpty()) false else input.all { Character.isDigit(it) }
+    fun checkDirection(input: String) {
+        if (input.isEmpty()) throw IllegalArgumentException(ERROR_NULL)
+        if (!((input == "U") or (input == "D"))) throw IllegalArgumentException(ERROR_DIRECTION)
+    }
+
+    private fun isNumber(input: String): Boolean {
+        return if (input.isEmpty()) false else input.all { Character.isDigit(it) }
     }
 }
