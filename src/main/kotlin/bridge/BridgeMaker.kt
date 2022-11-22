55db6d7eb4aa6d2ab6b6b2d2c12c3f1a45f21997
jjw0071 <jjw0071@naver.com>
2022-11-22 16:43:11 +0900

    feat : 다리 생성함수

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -9,6 +9,16 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        var bridge : MutableList<String> = mutableListOf()
+        for(i : Int in 1..size)
+            addBridge(bridge)
+        return bridge
+    }
+
+    private fun addBridge(bridge: MutableList<String>) {
+        if(bridgeNumberGenerator.generate() == 0)
+            bridge.add("D")
+        if(bridgeNumberGenerator.generate() == 1)
+            bridge.add("U")
     }
 }
