d9c2654c41109b330fc66b0db4de154e190c5bc8
최재훈 <be_clever@naver.com>
2022-11-22 17:14:47 +0900

    fix(InputView): 다리의 길이를 입력받는 기능에서 안내 문구 추가
    사용자로부터 다리의 길이를 입력받을 때, 게임 시작 및 입력 안내 문구를 출력하도록 수정

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -8,14 +8,13 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
+        println("다리 건너기 게임을 시작합니다.\n다리의 길이를 입력해주세요.")
         val input = readLine() ?: throw IllegalArgumentException()
-
         try {
             input.toInt()
         } catch (e: Exception) {
             throw IllegalArgumentException()
         }
-
         return input.toInt()
     }
 
