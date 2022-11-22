9586ed1fffbf2fea9825b0676a041bd46bcbb360
no1msh <no1msh1217@gmail.com>
2022-11-22 17:15:49 +0900

    feat: BridgeGame 클래스 중 임시 구현 된 move() retry() 메소드 완성

--- src/main/kotlin/bridge/domain/BridgeGame.kt
+++ src/main/kotlin/bridge/domain/BridgeGame.kt
@@ -1,29 +1,36 @@
-package bridge
+package bridge.domain
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
+ *
  * 요구사항 ⬇
+ *
  * 제공된 BridgeGame 클래스를 활용해 구현해야 한다.
+ *
  * BridgeGame에 필드를 추가할 수 있다.
+ *
  * BridgeGame의 패키지는 변경할 수 있다.
+ *
  * BridgeGame의 메서드의 이름은 변경할 수 없고, 인자와 반환 타입은 필요에 따라 추가하거나 변경할 수 있다.
+ *
  * BridgeGame의 클래스에서 InputView, OutputView 를 사용하지 않는다.
+ *
  * 게임 진행을 위해 필요한 메서드를 추가하거나 변경 할 수 있다.
  */
-class BridgeGame {
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     *
-     *
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    fun move() {}
+class BridgeGame(private val answerBridge: List<String>) {
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     *
-     *
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    fun retry() {}
+    private var position = 0
+    private var tryCount = 1
+
+    fun move(direction: String): Boolean {
+        if (answerBridge[position] == direction) {
+            return true
+        }
+        return false
+    }
+
+    fun retry() {
+        position = 0
+        tryCount++
+    }
 }
