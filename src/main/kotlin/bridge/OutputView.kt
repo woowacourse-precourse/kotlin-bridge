9c70482dcbf8274c8c1f5bb3b549336f6506cf29
dlwgnud <wngud980411@naver.com>
2022-11-22 15:16:47 +0900

    feat(printRetry): 게임 재시작/종료 여부 문구 출력 함수

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -8,6 +8,12 @@ enum class Output(val message: String) {
     GAME_START("다리 건너기 게임을 시작합니다."),
     BRIDGE_LENGTH("다리의 길이를 입력해주세요."),
     CHOOSE_SPACE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    RETRY_GAME("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    GAME_RESULT("최종 게임 결과"),
+    SUCCESS_OR_NOT("게임 성공 여부: "),
+    GAME_CLEAR("성공"),
+    GAME_FAIL("실패"),
+    TOTAL_ATTEMPTS("총 시도한 횟수: "),
 }
 
 class OutputView {
@@ -22,6 +28,10 @@ class OutputView {
 
     fun printChooseSpaceToMove() {
         println(Output.CHOOSE_SPACE.message)
+    }
+
+    fun printRetry() {
+        println(Output.RETRY_GAME.message)
     }
 
     /**
