526d8c165d213314102213433351eaba9185003c
KimDongHyun0907 <ehdgus8397@naver.com>
2022-11-22 23:51:43 +0900

    [make] Application main

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,14 @@
 package bridge
 
 fun main() {
-    // TODO: 프로그램 구현
+    val input=InputView()
+    val output=OutputView()
+
+    println("다리 건너기 게임을 시작합니다.\n")
+    val bridgeGame = BridgeGame(BridgeMaker(BridgeRandomNumberGenerator()), input.readBridgeSize())
+
 }
+
+
+
+
