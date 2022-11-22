42a27c0a63c68f31ee2f25f112ec7fa8e50a3477
s9hn <tnghqn@naver.com>
2022-11-22 18:18:25 +0900

    [FEAT(Message)] ENTER_GAME_RETRY 변수 추가

--- src/main/kotlin/bridge/data/Message.kt
+++ src/main/kotlin/bridge/data/Message.kt
@@ -9,10 +9,11 @@ const val BLANK = " "
 const val GAME_START = "다리 건너기 게임을 시작합니다.\n"
 const val ENTER_BRIDGE_SIZE = "다리의 길이를 입력해주세요."
 const val ENTER_DIRECTION = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
+const val ENTER_GAME_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
 
 const val GAME_RESULT = "최종 게임 결과"
-const val GAME_BOOLEAN = "게임 성공 여부"
-const val COUNT_RETRY = "총 시도한 횟수"
+const val GAME_BOOLEAN = "게임 성공 여부: "
+const val COUNT_RETRY = "총 시도한 횟수: "
 
 const val SUCCESS = "성공"
 const val FAIL = "실패"
