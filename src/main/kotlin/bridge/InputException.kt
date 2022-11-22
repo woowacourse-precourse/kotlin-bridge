ef52533343854b92312d3ba0c9232b19b2f6d072
Yuhee Kim <dbgml4509@naver.com>
2022-11-22 18:47:44 +0900

    feat : 재시도 여부 입력 예외 처리 추가

--- src/main/kotlin/bridge/InputException.kt
+++ src/main/kotlin/bridge/InputException.kt
@@ -23,12 +23,17 @@ class InputException {
         require(input.contains(UP) || input.contains(DOWN)) { ERROR_HEADER + ERROR_BRIDGE_DIRECTION_INPUT }
     }
 
+    fun checkRetryCommandInput(input : String){
+        require(input.contains("R") || input.contains("Q")) { ERROR_HEADER + ERROR_RETRY_COMMAND_INPUT }
+    }
+
     companion object{
         const val ERROR_HEADER = "[ERROR]"
 
         const val ERROR_NULL_INPUT = "입력된 값이 없습니다."
         const val ERROR_BRIDGE_LEN_INPUT = "다리 길이는 숫자만 가능합니다."
         const val ERROR_BRIDGE_DIRECTION_INPUT = "이동할 칸은 U, D로만 입력 가능합니다."
+        const val ERROR_RETRY_COMMAND_INPUT = "재시도 여부는 R, Q로만 답할 수 있습니다."
     }
 
 }
