0a7c2c3f6dfc2e91d291304fe38cfa8da819f39f
s9hn <tnghqn@naver.com>
2022-11-22 21:24:57 +0900

    [FEAT(OutputView)] 출력문 로직 구현
    1. 브릿지 맵 출력 설계
    2. 최종결과 출력
    3. 에러메시지 반환 출력

--- src/main/kotlin/bridge/ui/OutputView.kt
+++ src/main/kotlin/bridge/ui/OutputView.kt
@@ -1,16 +1,10 @@
 package bridge.ui
 
-import bridge.data.ENTER_BRIDGE_SIZE
-import bridge.data.ENTER_DIRECTION
-import bridge.data.ENTER_GAME_RETRY
-import bridge.data.GAME_START
+import bridge.data.*
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 class OutputView {
     fun printOpening() {
-        println(GAME_START)
+        println(GAME_START + NEXT_LINE)
         println(ENTER_BRIDGE_SIZE)
     }
 
@@ -22,20 +16,20 @@ class OutputView {
         println(ENTER_GAME_RETRY)
     }
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    fun printMap(Bridge: String) {
-        println(Bridge)
+    fun printMap(bridgeContainer: BridgeContainer) {
+        println(bridgeContainer.getUpperBridge)
+        println(bridgeContainer.getLowerBridge)
     }
 
+    fun printResult(bridgeContainer: BridgeContainer, boolean: Boolean, coin: Int) {
+        println(GAME_RESULT)
+        println(bridgeContainer.getUpperBridge)
+        println(bridgeContainer.getLowerBridge)
+        println(NEXT_LINE+GAME_BOOLEAN + if (boolean) SUCCESS else FAIL)
+        println(COUNT_RETRY + coin)
+    }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    fun printResult() {}
+    fun printErrorMessage(message: String?) {
+        println(message)
+    }
 }
