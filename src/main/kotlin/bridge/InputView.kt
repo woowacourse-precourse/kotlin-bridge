628039edfab4b67c0958988d0c694b1c83cd42fb
hyungmin <utopia96@hanmail.net>
2022-11-22 22:14:52 +0900

    feat: 사용자 입력값에 대한 예외처리 로직 추가
    - 입력함수를 다시 호출하는것이 아닌 입력값이 적합한지 boolean값으로 저장
    - 부적절하면 while문을 계속 돌며 계속 입력값을 입력받음
    - 예러 catch시 어떤 에러인지 메시지 출력

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -9,33 +9,72 @@ class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
+    private var inputValidator = false
     fun readBridgeSize(): Int {
-        println("다리 길이를 입력해주세요.")
-        val input = Console.readLine()
-        InputValidator.checkOnlyNumber(input)
-        InputValidator.checkInBridgeRange(input)
+        inputValidator = false
+        var input = ""
+        while (!inputValidator) {
+            println("다리 길이를 입력해주세요.")
+            input = Console.readLine()
+            validateBridgeSizeInput(input)
+        }
         return input.toInt()
+    }
+
+    private fun validateBridgeSizeInput(input: String) {
+        try {
+            InputValidator.checkOnlyNumber(input)
+            InputValidator.checkInBridgeRange(input)
+            inputValidator = true
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
-        val input = Console.readLine()
-        InputValidator.checkOnlyCharacter(input)
-        InputValidator.checkUorD(input)
+        inputValidator = false
+        var input = ""
+        while (!inputValidator) {
+            println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
+            input = Console.readLine()
+            validateMovingInput(input)
+        }
         return input
+    }
+
+    private fun validateMovingInput(input: String) {
+        try {
+            InputValidator.checkOnlyCharacter(input)
+            InputValidator.checkUorD(input)
+            inputValidator = true
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
-        val input= Console.readLine()
-        InputValidator.checkOnlyCharacter(input)
-        InputValidator.checkQorR(input)
+        inputValidator = false
+        var input = ""
+        while (!inputValidator) {
+            println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+            input = Console.readLine()
+            validateGameCommandInput(input)
+        }
         return input
+    }
+
+    private fun validateGameCommandInput(input: String) {
+        try {
+            InputValidator.checkOnlyCharacter(input)
+            InputValidator.checkQorR(input)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
     }
 }
