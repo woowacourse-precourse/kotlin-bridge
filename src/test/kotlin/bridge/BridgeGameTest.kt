c7b6e3b6f1398d202d6ff33799f1bbf35adc42b0
krrong <khk9664@naver.com>
2022-11-22 20:03:04 +0900

    test(BridgeGameTest): 테스트 코드 수정

--- src/test/kotlin/bridge/BridgeGameTest.kt
+++ src/test/kotlin/bridge/BridgeGameTest.kt
@@ -6,52 +6,52 @@ import org.junit.jupiter.api.Test
 class BridgeGameTest {
     @Test
     fun `종료 테스트1`() {
-        val bridgeGame = BridgeGame()
         val numberGenerator: BridgeNumberGenerator = ApplicationTest.TestNumberGenerator(listOf(1, 0, 1))
         val bridgeMaker = BridgeMaker(numberGenerator)
         val bridge: List<String> = bridgeMaker.makeBridge(3)
+        val bridgeGame = BridgeGame(bridge)
         bridgeGame.move("U")
         bridgeGame.move("D")
         bridgeGame.move("U")
-        val result = bridgeGame.isFail(bridge)
+        val result = bridgeGame.isFail()
 
         assertThat(result).isEqualTo(false)
     }
 
     @Test
     fun `종료 테스트2`() {
-        val bridgeGame = BridgeGame()
         val numberGenerator: BridgeNumberGenerator = ApplicationTest.TestNumberGenerator(listOf(1, 1, 1))
         val bridgeMaker = BridgeMaker(numberGenerator)
         val bridge: List<String> = bridgeMaker.makeBridge(3)
+        val bridgeGame = BridgeGame(bridge)
         bridgeGame.move("U")
         bridgeGame.move("U")
         bridgeGame.move("U")
-        val result = bridgeGame.isFail(bridge)
+        val result = bridgeGame.isFail()
 
         assertThat(result).isEqualTo(false)
     }
 
     @Test
     fun `종료 테스트3`() {
-        val bridgeGame = BridgeGame()
         val numberGenerator: BridgeNumberGenerator = ApplicationTest.TestNumberGenerator(listOf(1, 1, 1))
         val bridgeMaker = BridgeMaker(numberGenerator)
         val bridge: List<String> = bridgeMaker.makeBridge(3)
+        val bridgeGame = BridgeGame(bridge)
         bridgeGame.move("U")
         bridgeGame.move("D")
         bridgeGame.move("U")
-        val result = bridgeGame.isFail(bridge)
+        val result = bridgeGame.isFail()
 
         assertThat(result).isEqualTo(true)
     }
 
     @Test
     fun `재시작 테스트1`() {
-        val bridgeGame = BridgeGame()
         val numberGenerator: BridgeNumberGenerator = ApplicationTest.TestNumberGenerator(listOf(1, 1, 1))
         val bridgeMaker = BridgeMaker(numberGenerator)
         val bridge: List<String> = bridgeMaker.makeBridge(3)
+        val bridgeGame = BridgeGame(bridge)
         bridgeGame.move("U")
         bridgeGame.move("D")
         bridgeGame.move("U")
@@ -62,10 +62,10 @@ class BridgeGameTest {
 
     @Test
     fun `재시작 테스트2`() {
-        val bridgeGame = BridgeGame()
         val numberGenerator: BridgeNumberGenerator = ApplicationTest.TestNumberGenerator(listOf(1, 1, 1))
         val bridgeMaker = BridgeMaker(numberGenerator)
         val bridge: List<String> = bridgeMaker.makeBridge(3)
+        val bridgeGame = BridgeGame(bridge)
         bridgeGame.move("U")
         bridgeGame.move("D")
         bridgeGame.move("U")
