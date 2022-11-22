254186b56cec6c0bd7b1a448899f6757a13fe034
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-22 13:47:32 +0900

    feat(Constant): 상수 추가

--- src/main/kotlin/bridge/constants/Constant.kt
+++ src/main/kotlin/bridge/constants/Constant.kt
@@ -5,5 +5,7 @@ class Constant {
         const val START_INDEX = 0
         const val MIN_BRIDGE_LENGTH = 3
         const val MAX_BRIDGE_LENGTH = 20
+        const val UP_LINE = 0
+        const val DOWN_LINE = 1
     }
 }
