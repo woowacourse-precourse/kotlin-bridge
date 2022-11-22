e85d7ce8280f6cf1b95f9b82c2d43630d56c0b22
no1msh <no1msh1217@gmail.com>
2022-11-22 17:40:47 +0900

    feat: 에러 메시지로 사용될 상수를 정의하는 객체 생성

---
+++ src/main/kotlin/bridge/exceptions/ErrorMessage.kt
@@ -0,0 +1,9 @@
+package bridge.exceptions
+
+object ErrorMessage {
+    const val ERROR = "[ERROR] "
+    const val ONLY_INPUT_NUMBER = "숫자로만 입력해주세요."
+    const val ILLEGAL_BRIDGE_SIZE_RANGE = "3 ~ 20 사이의 크기로 입력해주세요."
+    const val ILLEGAL_MOVE_KEY = "U 또는 D를 입력해주세요."
+    const val ILLEGAL_RESTART_KEY = "R 또는 Q를 입력해주세요."
+}
