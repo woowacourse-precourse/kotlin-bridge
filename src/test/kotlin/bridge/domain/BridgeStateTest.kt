907ed6036915649d90b6a11c7861a9ac331191ac
vrexpert <vrexpert2302@gmail.com>
2022-11-22 15:08:49 +0900

    test(BridgeStateTest): BridgeStateTest 작성
    다리 건너기 성공 및 실패 테스트

---
+++ src/test/kotlin/bridge/domain/BridgeStateTest.kt
@@ -0,0 +1,49 @@
+package bridge.domain
+
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class BridgeStateTest:NsTest(){
+    private val bridgeState = BridgeState()
+
+    @Test
+    fun `다리 건너기 성공 테스트`() {
+        camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest {
+            run("U", "D", "D", "U")
+            Assertions.assertThat(output()).contains(
+                    "[ O ]",
+                    "[   ]",
+                    "[ O |   ]",
+                    "[   | O ]",
+                    "[ O |   |   ]",
+                    "[   | O | O ]",
+                    "[ O |   |   | O ]",
+                    "[   | O | O |   ]"
+            )
+        }
+    }
+
+    @Test
+    fun `다리 건너기 실패 테스트`() {
+        camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest {
+            run("U", "D", "U")
+            Assertions.assertThat(output()).contains(
+                    "[ O ]",
+                    "[   ]",
+                    "[ O |   ]",
+                    "[   | O ]",
+                    "[ O |   | X ]",
+                    "[   | O |   ]"
+            )
+        }
+    }
+
+    override fun runMain() {
+        bridgeState.compare(testBridge)
+    }
+
+    companion object {
+        val testBridge = Bridge(listOf("U","D","D","U"))
+    }
+}
