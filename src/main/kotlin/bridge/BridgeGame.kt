4e0e26b477d26fd1030f77dd747ea9fba1b526d0
[alsswl] <[alswl4223@naver.com]>
2022-11-22 15:17:51 +0900

    refector(BridgeGame) : retry 함수 사용

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -21,5 +21,13 @@ class BridgeGame {
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry() {}
+    fun retry(size : Int,bridge: List<String>) : Pair<Int,Int>{
+        var finish = 1
+        var count = 0
+        while (finish == 1) {
+            count++
+            finish = BridgeGameing(size, bridge)//다시시작
+        }
+        return Pair(finish,count)
+    }
 }
