daa8ad4b7f0f68e9c130139d6948ea99747bdc83
changhwan <chang5hustle@gmail.com>
2022-11-22 12:55:32 +0900

    feat(bridge) : 다리 만들기 함수 호출

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,14 +1,25 @@
 package bridge
 
+import bridge.view.InputView
 import bridge.view.OutputView
 
 val outputView = OutputView()
+val inputView = InputView()
 
 fun main() {
     startGame()
+    val bridge = makeBridge()
 }
 
 private fun startGame() {
     outputView.printGameStartPhrase()
     println()
 }
+
+private fun makeBridge(): List<String> {
+    outputView.printBridgeSizePhrase()
+    val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(inputView.readBridgeSize())
+    println()
+    return bridge
+}
+
