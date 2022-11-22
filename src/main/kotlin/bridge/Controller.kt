5cc6d6dd2ffe0606d6a37685b1c44f0827f6e8d8
krrong <khk9664@naver.com>
2022-11-22 11:19:17 +0900

    feat(Controller): 게임 기능 구현
    play - 게임 실행
    setUpGame - 게임에 필요한 클래스 생성 및 입력
    endGame - 게임 결과 출력
    mainLogic - 게임의 메인 로직
    runGame - 예외를 확인하며 실제로 게임 진행

--- src/main/kotlin/bridge/Controller.kt
+++ src/main/kotlin/bridge/Controller.kt
@@ -1,16 +1,57 @@
 package bridge
 
+import java.lang.IllegalArgumentException
+
+private typealias StateEnum = Message.ControllerEnum
+
 class Controller {
-    /**
-     * 필요 클래스 생성 및 게임 시작
-     */
+    private var isPlaying = StateEnum.RUNNING.state
+    private lateinit var inputView: InputView
+    private lateinit var outputView: OutputView
+    private lateinit var bridgeGame: BridgeGame
+    private lateinit var bridge: List<String>
+
     fun play() {
+        setUpGame()
+        runGame()
+        endGame()
+    }
+
+    private fun setUpGame() {
+        inputView = InputView()
+        outputView = OutputView()
+        bridgeGame = BridgeGame()
+        inputView.startGame()
+        val bridgeSize = inputView.readBridgeSize()
+        bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)
+    }
+
+    private fun endGame() {
+        outputView.printResult(bridgeGame.getResult(bridge))
+        outputView.printResult(bridgeGame.getAnalysis(isPlaying))
+    }
+
+    private fun mainLogic() {
+        do {
+            bridgeGame.move(inputView.readMoving())                         // U/D 를 입력받아 moves에 저장
+            outputView.printMap(bridgeGame.getState(bridge))                // 결과 출력
+
+            if (bridgeGame.isFail(bridge)) {                                // 건널 수 없는 경우
+                isPlaying = bridgeGame.retry(inputView.readGameCommand())   // 재시작 여부 확인
+            }
+            if (bridgeGame.isSuccess(bridge)) {                             // 정상 종료
+                break
+            }
+        } while (isPlaying)
+    }
+
+    private fun runGame() {
         try {
-            InputView().readBridgeSize()
-            InputView().readMoving()
-            InputView().readGameCommand()
+            mainLogic()
         } catch (e: IllegalArgumentException) {
-            print(e)
+            println(e)
+        } catch (e: IllegalStateException) {
+            println(e)
         }
     }
 }
