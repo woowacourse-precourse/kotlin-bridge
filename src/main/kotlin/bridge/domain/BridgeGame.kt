34266f1a74e476cb75409c59d9eedd74b18eb853
no1msh <no1msh1217@gmail.com>
2022-11-22 17:17:01 +0900

    feat: BridgeGame 클래스 필드 게터 구현

--- src/main/kotlin/bridge/domain/BridgeGame.kt
+++ src/main/kotlin/bridge/domain/BridgeGame.kt
@@ -22,6 +22,15 @@ class BridgeGame(private val answerBridge: List<String>) {
     private var position = 0
     private var tryCount = 1
 
+    fun getPosition(): Int = this.position
+    fun getTryCount(): Int = this.tryCount
+
+    fun nextPosition() {
+        position += 1
+    }
+
+    fun getAnswerBridge() = answerBridge
+
     fun move(direction: String): Boolean {
         if (answerBridge[position] == direction) {
             return true
