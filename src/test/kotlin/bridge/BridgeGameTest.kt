af9e3fd3bfbbb5364116dbc0602e8e67ac7a4534
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-22 17:25:17 +0900

    [Test] (BridgeGameTest.kt) 종료, 재시작 테스트 및 불필요 테스트 제거
    BridgeGameValidator, OutputValidator, OutputViewTest 파일 제거

--- src/test/kotlin/bridge/BridgeGameTest.kt
+++ src/test/kotlin/bridge/BridgeGameTest.kt
@@ -1,18 +1,59 @@
 package bridge
 
-import bridge.view.validator.BridgeGameValidator
+import bridge.view.main
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
+import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.api.Test
 
-class BridgeGameTest {
+class BridgeGameTest : NsTest() {
 
-    @ParameterizedTest
-    @CsvSource(
-        "1, true",
-        "0, false"
-    )
-    fun `라운드 이동 기능 테스트`(nextRound: Int, isMatched: Boolean) {
-        assertThat(nextRound).isEqualTo(BridgeGameValidator.validateMoveOutput(isMatched))
+    @Test
+    fun `종료 테스트`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("3", "D", "Q")
+                assertThat(output()).contains(
+                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+                    "최종 게임 결과",
+                    "[   ]",
+                    "[ X ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+                )
+                val upSideIndex = output().indexOf("[   ]")
+                val downSideIndex = output().indexOf("[ X ]")
+                assertThat(upSideIndex).isLessThan(downSideIndex)
+            }, 1, 0, 1)
+    }
+
+    @Test
+    fun `재시작 테스트`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("3", "D", "R", "U", "D", "U")
+                assertThat(output()).contains(
+                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+                    "[   ]",
+                    "[ X ]",
+                    "[ O ]",
+                    "[   ]",
+                    "[ O |   ]",
+                    "[   | O ]",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "최종 게임 결과",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+                )
+                val upsideIndex = output().indexOf("[ O |   | O ]")
+                val downSideIndex = output().indexOf("[   | O |   ]")
+                assertThat(upsideIndex).isLessThan(downSideIndex)
+            }, 1, 0, 1
+        )
+    }
+
+    override fun runMain() {
+        main()
     }
 }
