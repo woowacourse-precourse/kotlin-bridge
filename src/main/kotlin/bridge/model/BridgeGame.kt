d08723ebf6dc3d52b854c7eacb903455e9ed05a7
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-22 17:30:07 +0900

    style(model): BridgeGame클래스의 backing 속성들 속성명 변경
    앞에 언더스코어 붙힘

--- src/main/kotlin/bridge/model/BridgeGame.kt
+++ src/main/kotlin/bridge/model/BridgeGame.kt
@@ -7,21 +7,21 @@ import bridge.QuitEventManager
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame(private val movingEventManager: MovingEventManager, private val quitEventManager: QuitEventManager) {
-    private lateinit var bridge: List<String>
-    private var userHistory = mutableListOf<String>()
-    private lateinit var gameStatus: GameStatus
-    private var attempts: Int = 0
+    private lateinit var _bridge: List<String>
+    private var _userHistory = mutableListOf<String>()
+    private lateinit var _gameStatus: GameStatus
+    private var _attempts: Int = 0
 
     fun start(bridge: List<String>) {
         require(!started()) { "[ERROR] 게임은 한 번만 시작할 수 있습니다." }
         validateBridge(bridge)
-        this.bridge = bridge
-        userHistory.clear()
-        gameStatus = GameStatus.RUNNING
-        attempts++
+        this._bridge = bridge
+        _userHistory.clear()
+        _gameStatus = GameStatus.RUNNING
+        _attempts++
     }
 
-    private fun started(): Boolean = ::bridge.isInitialized
+    private fun started(): Boolean = ::_bridge.isInitialized
 
     private fun validateBridge(bridge: List<String>) =
         require(bridge.size in 3..20 && bridge.all { it == "U" || it == "D" }) { "다리의 길이는 3 이상 20 이하여야 하고 U 또는 D만 포함할 수 있습니다." }
@@ -34,28 +34,28 @@ class BridgeGame(private val movingEventManager: MovingEventManager, private val
      */
     fun move(moving: String) {
         check(running()) { "게임이 실행되었을 때만 움직일 수 있습니다." }
-        userHistory += moving
-        movingEventManager.notify(GameMapStatus(bridge, userHistory))
+        _userHistory += moving
+        movingEventManager.notify(GameMapStatus(_bridge, _userHistory))
         if (moveFailed())
-            gameStatus = GameStatus.STOPPED
+            _gameStatus = GameStatus.STOPPED
         if (successed()) quit()
     }
 
-    private fun moveFailed() = userHistory.last() != bridge[userHistory.size - 1]
+    private fun moveFailed() = _userHistory.last() != _bridge[_userHistory.size - 1]
 
     fun successed(): Boolean {
         check(started()) { "게임이 시작되어야 성공했는지 판단할 수 있습니다." }
-        if (bridge.size != userHistory.size)
+        if (_bridge.size != _userHistory.size)
             return false
-        return (0 until userHistory.size).all { bridge[it] == userHistory[it] }
+        return (0 until _userHistory.size).all { _bridge[it] == _userHistory[it] }
     }
 
     fun quit() {
-        quitEventManager.notify(GameMapStatus(bridge, userHistory), GameResult(successed(), attempts))
-        gameStatus = GameStatus.FINISHED
+        quitEventManager.notify(GameMapStatus(_bridge, _userHistory), GameResult(successed(), _attempts))
+        _gameStatus = GameStatus.FINISHED
     }
 
-    fun running(): Boolean = gameStatus == GameStatus.RUNNING
+    fun running(): Boolean = _gameStatus == GameStatus.RUNNING
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -65,8 +65,8 @@ class BridgeGame(private val movingEventManager: MovingEventManager, private val
      */
     fun retry() {
         check(started() && !running()) { "게임이 중지 상태일 때만 재시작할 수 있습니다." }
-        userHistory.clear()
-        gameStatus = GameStatus.RUNNING
-        attempts++
+        _userHistory.clear()
+        _gameStatus = GameStatus.RUNNING
+        _attempts++
     }
 }
