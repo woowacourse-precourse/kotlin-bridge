d27f4cd7f7e2c8829a8e9981f2b8d561d34b10cf
changhwan <chang5hustle@gmail.com>
2022-11-22 11:32:47 +0900

    test(bridge) retry 함수 정상 작동 테스트

--- src/test/kotlin/bridge/BridgeGameTest.kt
+++ src/test/kotlin/bridge/BridgeGameTest.kt
@@ -2,18 +2,35 @@ package bridge
 
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class BridgeGameTest {
     val bridgeGame = BridgeGame()
 
-    @Test
-    fun `move 함수 정상작동 test 결과 true`() {
-        assertThat(bridgeGame.move("U", 3, listOf("U", "D", "U", "U", "D"))).isTrue
+    @Nested
+    inner class MoveTest {
+        @Test
+        fun `move 함수 정상작동 test 결과 true`() {
+            assertThat(bridgeGame.move("U", 3, listOf("U", "D", "U", "U", "D"))).isTrue
+        }
+
+        @Test
+        fun `move 함수 정상작동 test 결과 false`() {
+            assertThat(bridgeGame.move("D", 3, listOf("U", "D", "U", "U", "D"))).isFalse
+        }
     }
 
-    @Test
-    fun `move 함수 정상작동 test 결과 false`() {
-        assertThat(bridgeGame.move("D", 3, listOf("U", "D", "U", "U", "D"))).isFalse
+    @Nested
+    inner class RetryTest {
+        @Test
+        fun `retry 함수 정상작동 test 입력 Q`() {
+            assertThat(bridgeGame.retry("Q")).isEqualTo(GameState.QUIT)
+        }
+
+        @Test
+        fun `retry 함수 정상작동 test 입력 R`() {
+            assertThat(bridgeGame.retry("R")).isEqualTo(GameState.RESTART)
+        }
     }
 }
