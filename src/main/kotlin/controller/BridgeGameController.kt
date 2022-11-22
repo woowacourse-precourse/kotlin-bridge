7b78d9c97cfbaa6d681a67b08fca372dc22cb285
lee989898 <wnsrud1352@gmail.com>
2022-11-22 14:57:06 +0900

    [feat](BridgeGameController) 다리 게임 시작하는 메서드 구현

--- src/main/kotlin/controller/BridgeGameController.kt
+++ src/main/kotlin/controller/BridgeGameController.kt
@@ -1,9 +1,20 @@
 package controller
 
+import bridge.BridgeGame
 import view.InputView
+import view.OutputView
 
 class BridgeGameController {
+    private lateinit var bridgeGame: BridgeGame
     private val inputView = InputView()
+    private val outputView = OutputView()
+    private var bridgeSize: Int = 0
+
+    fun startBridgeGame() {
+        outputView.printGameStartNotification()
+        bridgeSize = inputBridgeSize()
+        bridgeGame = BridgeGame(bridgeSize)
+    }
 
     private fun inputBridgeSize(): Int {
         return inputView.inputValidBridgeSize()
