cf4d7669659db4ddeb8cf8db0570551710f1be38
Jaehun-Jo <chojh922@gmail.com>
2022-11-22 16:14:49 +0900

    [Feat] play 메서드 구현

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -9,5 +9,36 @@ fun main() {
 }
 
 fun play() {
+    println("다리 건너기 게임을 시작합니다.\n")
+    val bridge = generateBridge()
+    val bridgeGame = BridgeGame()
+    val outputView = OutputView(bridge)
+    progressBridgeGame(bridgeGame, outputView, bridge)
+}
 
+fun generateBridge(): List<String> {
+    val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
+    val bridgeSize = InputView().readBridgeSize()
+    return bridgeMaker.makeBridge(bridgeSize)
+}
+
+fun progressBridgeGame(bridgeGame: BridgeGame, outputView: OutputView, bridge: List<String>) {
+    var count = 0
+    var isSuccess = false
+    while (!isSuccess) {
+        bridgeGame.clearMovings()
+        isSuccess = true
+        count++
+        for (position in bridge.indices) {
+            if (!bridgeGame.move(InputView().readMoving(), position, bridge)) {
+                outputView.printMap(bridgeGame.movings)
+                isSuccess = false
+                break
+            }
+            outputView.printMap(bridgeGame.movings)
+        }
+        if (!isSuccess)
+            if (!bridgeGame.retry(InputView().readGameCommand())) break
+    }
+    outputView.printResult(bridgeGame.movings, isSuccess, count)
 }
