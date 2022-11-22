f32e01553d6aece9d636704ec3b59b17ec32dce3
username <ieejo716@naver.com>
2022-11-22 19:48:12 +0900

    [FEAT] : OutputView에 쓰던 companion object 따로 Store에 저장

---
+++ src/main/kotlin/data/BridgeStore.kt
@@ -0,0 +1,21 @@
+package data
+
+object BridgeStore {
+    const val START_GAME = "다리 건너기 게임을 시작합니다."
+    const val INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요."
+    const val MOVE_BRIDGE = "이동할 칸을 선택해주세요. (위 : U, 아래 : D)"
+    const val RETRY_GAME = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
+    const val FINAL_RESULT = "최종 게임 결과"
+    const val SUCCESS_GAME_CHECK = "게임 성공 여부:"
+    const val TRY_COUNT = "총 시도한 횟수:"
+    const val SUCCESS = "성공"
+    const val FAIL = "실패"
+    const val USER_ALIVE = "O"
+    const val USER_DIE = "X"
+    const val SPACE = " "
+    const val BRIDGE_START = "["
+    const val BRIDGE_END = "]"
+    const val BRIDGE_MID = "|"
+    const val BRIDGE_UP = "U"
+    const val BRIDGE_DOWN = "D"
+}
