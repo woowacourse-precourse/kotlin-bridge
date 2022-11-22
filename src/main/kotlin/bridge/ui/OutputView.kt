afade41bf022743bb6fc6d783e3056ce500f5156
윤동현 <donghyunyoon81@gmail.com>
2022-11-22 10:45:43 +0900

    feat<OutputView> : 현재까지 이동한 다리의 상태의 결과값을 구분해줄 " | " 를 반환하는 getLineOrBlank 기능 구현
    어떻게 : BridgeMaker에서 만든 bridge의 index가 0보다 클 경우에 " | " 를 반환하도록 구현
    왜:	 사용자가 다리의 상태를 확인하기 편하도록 구분선을 출력하기 위해서

--- src/main/kotlin/bridge/ui/OutputView.kt
+++ src/main/kotlin/bridge/ui/OutputView.kt
@@ -12,6 +12,12 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printMap() {}
+
+    private fun getLineOrBlank(bridgeIndex: Int): String {
+        if (bridgeIndex > 0) return " | "
+        return ""
+    }
+
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      *
