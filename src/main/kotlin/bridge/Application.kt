f1a376d9ffb2f1b0772ab35e465e95cf4da9b02f
leehandsub <gustjq7027@naver.com>
2022-11-22 23:51:19 +0900

    refact(main): 10줄 넘는 메소드 수정

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -6,22 +6,26 @@ import bridge.view.OutputView
 
 fun main() {
     val showInputView = InputView()
+    val outputView = OutputView()
     val bridgeGame = BridgeGame()
-
     val bridgeSize = showInputView.readBridgeSize()
     val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)
+    playGame(showInputView,bridgeGame,bridge)
+
+    outputView.printResult(bridgeGame.userBridge, bridge)
+    outputView.printSuccess(bridgeGame.gameSuccess)
+    outputView.printTryGameNumber(bridgeGame.tryNumber)
+}
+
+fun playGame(showInputView: InputView, bridgeGame: BridgeGame, bridge: List<String>) {
     var gameOver = true
     while (gameOver) {
         val moveDirection = showInputView.readMoving()
         if (!bridgeGame.move(moveDirection, bridge))
             gameOver = bridgeGame.retry(showInputView.readGameCommand())
-
         if (bridge.size == bridgeGame.userBridge.size) {
             gameOver = false
             bridgeGame.gameSuccess = true
         }
     }
-    OutputView().printResult(bridgeGame.userBridge, bridge)
-    OutputView().printSuccess(bridgeGame.gameSuccess)
-    OutputView().printTryGameNumber(bridgeGame.tryNumber)
 }
