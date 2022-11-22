b7d57f59136499dad2ebcee429854af07faeeb03
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-11-22 11:25:13 +0900

    feat: 게임 실행해주는 함수 작성

--- src/main/kotlin/bridge/Control.kt
+++ src/main/kotlin/bridge/Control.kt
@@ -3,11 +3,23 @@ package bridge
 import output.Output
 
 class Control {
-
+    private val bridgeGame = BridgeGame()
     private val inputView = InputView()
+    private val outputView = OutputView()
 
     fun gameStart() {
         println(Output.GAME_START.output)
         val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(inputView.readBridgeSize())
+        markingBridge(bridge)
+    }
+
+    private fun markingBridge(bridge: List<String>) {
+        val markUp = mutableListOf<String>()
+        val markDown = mutableListOf<String>()
+        for (index in bridge.indices) {
+            val mark = bridgeGame.markBridge(inputView.readMoving(), bridge, index)
+            bridgeGame.move(mark, markUp, markDown)
+            outputView.printMap(markUp, markDown)
+        }
     }
 }
