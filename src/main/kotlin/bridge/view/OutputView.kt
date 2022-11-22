c86cacffeea80b1185d04b2bb12bfcac0d67c2bd
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-22 21:21:49 +0900

    feat: add printReadGameCommandMessage function Add function that print require input message in InputView.readGameCommandMessage

--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -42,6 +42,10 @@ class OutputView {
         println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
     }
 
+    fun printReadGameCommandMessage(){
+        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+    }
+
     fun printErr(errMessage: String){
         println(errMessage)
     }
