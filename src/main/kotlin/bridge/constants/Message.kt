b4828645fe6715c6f6e16b4e543e91cc700161f1
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-22 14:59:44 +0900

    style(constants): StartGame 메시지 문자열 수정

--- src/main/kotlin/bridge/constants/Message.kt
+++ src/main/kotlin/bridge/constants/Message.kt
@@ -4,7 +4,7 @@ import bridge.data.Bridge
 
 enum class Message(vararg message: String) {
 
-    StartGame("다리 건너기 게임을 시작합니다"),
+    StartGame("다리 건너기 게임을 시작합니다."),
     InputBridgeSize("다리의 길이를 입력해주세요."),
     SelectNextFloor("이동할 칸을 선택해주세요. (위: ${Bridge.Floor.UP.command}, 아래: ${Bridge.Floor.DOWN.command})"),
     SelectRetryOrFinishGame("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
