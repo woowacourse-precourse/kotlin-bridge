9138cd30332b64fe0917c780a3d7ffbdb171272b
lee989898 <wnsrud1352@gmail.com>
2022-11-22 16:32:52 +0900

    [refactor](BridgeMaker) 불필요한 브라켓 제거

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -6,11 +6,8 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
         val upDownBridge = mutableListOf<String>()
         repeat(size) {
             val bridgeNumber = bridgeNumberGenerator.generate()
-            if (bridgeNumber == 0) {
-                upDownBridge.add("D")
-            } else if (bridgeNumber == 1) {
-                upDownBridge.add("U")
-            }
+            if (bridgeNumber == 0) upDownBridge.add("D")
+            else if (bridgeNumber == 1) upDownBridge.add("U")
         }
         return upDownBridge
     }
