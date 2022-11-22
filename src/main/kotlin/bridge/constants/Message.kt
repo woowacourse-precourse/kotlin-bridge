04edc902a72134584a26b11d813e2b897452f9a8
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-22 15:55:00 +0900

    refactor(view): 메시지 상수를 문자열 템플릿으로 변경

--- src/main/kotlin/bridge/constants/Message.kt
+++ src/main/kotlin/bridge/constants/Message.kt
@@ -3,24 +3,21 @@ package bridge.constants
 import bridge.data.Bridge
 import bridge.view.InputView
 
-enum class Message(vararg message: String) {
+enum class Message(private val message: String) {
 
-    StartGame("다리 건너기 게임을 시작합니다."),
+    StartGame("다리 건너기 게임을 시작합니다.\n"),
     InputBridgeSize("다리의 길이를 입력해주세요."),
     SelectNextFloor("이동할 칸을 선택해주세요. (위: ${Bridge.Floor.UP.command}, 아래: ${Bridge.Floor.DOWN.command})"),
     SelectRetryOrFinishGame("게임을 다시 시도할지 여부를 입력해주세요. (재시도: ${InputView.RETRY}, 종료: ${InputView.QUIT})"),
     Success("성공"),
     Failure("실패"),
-    FinishGame(
-        "최종 게임 결과",
-        "게임 성공 여부: %s",
-        "총 시도한 횟수: %d",
-    ),;
+    FinishGame("""
+        최종 게임 결과
+        %s
+        게임 성공 여부: %s
+        총 시도한 횟수: %d
+    """.trimIndent()),;
 
-    private val messages: List<String> = message.toList()
-
-    operator fun get(index: Int): String = messages[index]
-
-    override fun toString(): String = messages.joinToString("\n")
+    override fun toString(): String = message
 
 }
