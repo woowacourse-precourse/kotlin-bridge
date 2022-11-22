60e322f1cd33671c97ff7c62ddb9d245532baf44
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-22 23:15:53 +0900

    test: Add BridgeMaker Test

---
+++ src/test/kotlin/bridge/domain/BridgeMakerTest.kt
@@ -0,0 +1,24 @@
+package bridge.domain
+
+import bridge.ApplicationTest
+import bridge.BridgeNumberGenerator
+import bridge.BridgeRandomNumberGenerator
+import bridge.resources.BRIDGE_BOTTOM_LINE_CHAR
+import bridge.resources.BRIDGE_TOP_LINE_CHAR
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class BridgeMakerTest {
+
+    @Test
+    fun `다리 생성 테스트 1, 입력 길이대로 다리 생성`() {
+    }
+
+    @Test
+    fun `다리 생성 테스트 2, U 및 D로만 이루어진 다리 생성`() {
+    }
+
+    @Test
+    fun `다리 생성 테스트 3, 정상적 다리 생성`() {
+    }
+}
