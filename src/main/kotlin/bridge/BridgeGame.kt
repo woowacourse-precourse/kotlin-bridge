0e517186ad74714a45c2ea96672b70730cf34866
Lim-Joosung <kinglim1224@naver.com>
2022-11-22 20:30:48 +0900

    comment: BridgeGame.kt move() 메소드 주석 추가

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -6,9 +6,10 @@ package bridge
 class BridgeGame {
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
+     * A,B,C,D 각각의 의미는 다음과 같다.
      *
-     *
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * A: [ O ] B: [   ] C: [ X ] D: [   ]
+     *    [   ]    [ O ]    [   ]    [ X ]
      */
     fun move(upDown : String,bridge :String):String {
         if(upDown==bridge){
@@ -23,9 +24,6 @@ class BridgeGame {
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     *
-     *
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry(readGame:String,bridge: MutableList<String>):Boolean {
         if(readGame == "R") {
