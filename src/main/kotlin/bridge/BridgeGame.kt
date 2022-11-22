e1f923514d8bf17d4616b57ca982715ed1e129a7
username <ieejo716@naver.com>
2022-11-22 19:39:08 +0900

    [CHORE] : 불필요한 코드 제거

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -6,6 +6,25 @@ package bridge
 class BridgeGame(
     private val state: BridgeState,
 ) {
+
+    init {
+        state.inputBridgeLengthPart()
+    }
+
+    fun play() {
+        val bridge = initialBridge()
+        var isAlive: Boolean
+        while (true) {
+            isAlive = move(bridge)
+            val isRetry = retry(isAlive)
+            if (!isRetry) break
+        }
+        state.gameFinish(bridge, isAlive)
+    }
+
+    private fun initialBridge() =
+        BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(state.bridgeLength)
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
@@ -13,21 +32,12 @@ class BridgeGame(
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
 
-    init {
-        state.bridgeLengthState()
-    }
-
-    fun play() {
-        val bridge = initialBridge()
-        move(bridge)
-        retry()
-    }
-
-    private fun initialBridge() =
-        BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(state.bridgeLength)
-
-    fun move(bridge: List<String>) {
-        state.bridgeMovePart(bridge)
+    fun move(bridge: List<String>): Boolean {
+        for (cnt in 1..state.bridgeLength) {
+            state.bridgeMovePart(bridge)
+            if (!state.isUserLive(bridge)) return false
+        }
+        return true
     }
 
     /**
@@ -36,7 +46,10 @@ class BridgeGame(
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry() {
-
+    fun retry(isAlive: Boolean): Boolean {
+        if (!isAlive) {
+            return state.gameRetry()
+        }
+        return false
     }
 }
