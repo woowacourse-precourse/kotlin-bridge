d57218bf253c834be31cd9ad6c7afdd1dc275614
최재훈 <be_clever@naver.com>
2022-11-22 18:01:57 +0900

    feat(BridgeGameUtils): 다리를 생성하는 기능 추가
    - 다리를 생성하는 기능 추가
    - BridgeMaker 의 makeBridge 메서드의 반환형 변경

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -10,6 +10,7 @@ class BridgeGame {
 
     fun start() {
         bridgeSize = bridgeUtils.start()
+        bridgeUtils.setBridge(bridgeSize)
         run()
     }
 
@@ -38,7 +39,7 @@ class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry() {
-        if(bridgeUtils.restart()) {
+        if(!bridgeUtils.restart()) {
             return
         }
         run()
--- src/main/kotlin/bridge/BridgeGameUtils.kt
+++ src/main/kotlin/bridge/BridgeGameUtils.kt
@@ -1,17 +1,24 @@
 package bridge
 
 class BridgeGameUtils {
-    var inputView = InputView()
-    var outputView = OutputView()
-    private val bridge = ArrayList<String>()
-    private val root = ArrayList<String>()
+    private var inputView = InputView()
+    private var outputView = OutputView()
+    private val bridge = mutableListOf<String>()
+    private val root = mutableListOf<String>()
+    private val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
 
     fun start(): Int {
         return inputView.readBridgeSize()
     }
 
-    fun executeTurn(): Boolean {
+    fun setBridge(size: Int) {
+        bridge.addAll(bridgeMaker.makeBridge(size))
+    }
 
+    fun executeTurn(): Boolean {
+        while(true) {
+
+        }
     }
 
     fun restart(): Boolean {
