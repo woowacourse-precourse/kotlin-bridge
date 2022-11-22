d2e41b983c02c6c241c16bb7fac6fac695d70e7a
dohun_ <silver2679@gmail.com>
2022-11-22 20:01:17 +0900

    Test: ExceptionTest 추가

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -22,8 +22,9 @@ class InputView {
         return bridgeSize.toInt()
     }
 
-    private fun inputBridgeException(bridgeSize: String) {
+    fun inputBridgeException(bridgeSize: String) {
         when {
+            bridgeSize.isBlank() -> throw IllegalArgumentException(ConstantMessage.INPUT_TYPE_EXCEPTION)
             chkNum(bridgeSize) -> throw IllegalArgumentException(ConstantMessage.INPUT_TYPE_EXCEPTION)
             bridgeSize.toInt() < 3 -> throw IllegalArgumentException(ConstantMessage.INPUT_MORE_RANGE_EXCEPTION)
             bridgeSize.toInt() > 20 -> throw IllegalArgumentException(ConstantMessage.INPUT_LESS_RANGE_EXCEPTION)
@@ -39,7 +40,7 @@ class InputView {
         return inputMoving
     }
 
-    private fun inputMovingException(inputMoving: String) {
+    fun inputMovingException(inputMoving: String) {
         when {
             inputMoving != "U" && inputMoving != "D" -> return throw IllegalArgumentException(ConstantMessage.INPUT_MOVE_EXCEPTION)
         }
@@ -52,7 +53,7 @@ class InputView {
         return inputGameCommand
     }
 
-    private fun inputGameCommandException(inputGameCommand: String) {
+    fun inputGameCommandException(inputGameCommand: String) {
         when {
             inputGameCommand != "Q" && inputGameCommand != "R" -> return throw IllegalArgumentException(ConstantMessage.INPUT_GAME_COMMAND_EXCEPTION)
         }
---
+++ src/test/kotlin/bridge/ExceptionTest.kt
@@ -0,0 +1,35 @@
+package bridge
+
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class ExceptionTest {
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "21", "", " "])
+    fun `다리의 길이가 3이상 20이하 숫자가 아니고 숫자가 아니면 예외 발생`(bridgeSize: String) {
+        assertThatIllegalArgumentException().isThrownBy {
+            InputView().inputBridgeException(bridgeSize)
+        }
+            .withMessageContaining("[ERROR]")
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["u", "d", "a", " ", ""])
+    fun `플레이어가 이동할 칸 U와 D가 아니면 예외 발생`(inputMoving: String) {
+        assertThatIllegalArgumentException().isThrownBy {
+            InputView().inputMovingException(inputMoving)
+        }
+            .withMessageContaining("[ERROR]")
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["r", "q", "a", " ", ""])
+    fun `게임 재시작,종료 R과 Q가 아니면 예외 발생`(inputGameCommand: String) {
+        assertThatIllegalArgumentException().isThrownBy {
+            InputView().inputMovingException(inputGameCommand)
+        }
+            .withMessageContaining("[ERROR]")
+    }
+
+}
