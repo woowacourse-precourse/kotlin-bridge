4c200756b1d1bc0464d4c93106ba22060da6b624
jklom0326 <jklom0326@gmail.com>
2022-11-22 09:23:10 +0900

    refactor(GameManager.kt): 함수 분리, input 함수들 다시 시도하도록 수정 및 문자열 수정

--- src/main/kotlin/bridge/GameManager.kt
+++ src/main/kotlin/bridge/GameManager.kt
@@ -6,30 +6,72 @@ class GameManager {
     private var bridgeGame: BridgeGame
     private var bridge = listOf<String>()
     private var bridgeSize: Int
-    private var position = 0
+    private var position = INIT
 
     init {
         printStartMessage()
-        bridgeSize = inputView.readBridgeSize()
+        bridgeSize = setBridgeSize()
         bridgeGame = BridgeGame(bridgeSize)
         bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)
     }
 
-
     fun play() {
         while (bridgeGame.isSuccess && position < bridgeSize) {
-            val move = inputView.readMoving()
-            bridgeGame.move(bridge, move, position)
-            outputView.printMap(bridgeGame)
-            if (!bridgeGame.isSuccess) {
-                val retryCommand = inputView.readGameCommand()
-                if (retryCommand == "R") {
-                    restartGame()
-                }
-            }
+            val move = setMove()
+            checkBridge(move)
+            checkGameEnd()
             position++
         }
         printEndGame(bridgeGame.isSuccess)
+    }
+
+    private fun checkBridge(move: String) {
+        bridgeGame.move(bridge, move, position)
+        outputView.printMap(bridgeGame)
+    }
+
+    private fun checkGameEnd() {
+        if (!bridgeGame.isSuccess) {
+            val retryCommand = setGameCommand()
+            checkRetry(retryCommand)
+        }
+    }
+
+    private fun checkRetry(retryCommand: String) {
+        if (retryCommand == RETRY) restartGame()
+    }
+
+    private fun setBridgeSize(): Int {
+        while (true) {
+            try {
+                return inputView.readBridgeSize()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+                continue
+            }
+        }
+    }
+
+    private fun setMove(): String {
+        while (true) {
+            try {
+                return inputView.readMoving()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+                continue
+            }
+        }
+    }
+
+    private fun setGameCommand(): String {
+        while (true) {
+            try {
+                return inputView.readGameCommand()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+                continue
+            }
+        }
     }
 
     private fun printEndGame(isSuccess: Boolean) {
@@ -38,12 +80,15 @@ class GameManager {
 
     private fun restartGame() {
         bridgeGame.retry()
-        position = -1
+        position = INIT_RETRY
     }
 
     private fun printStartMessage() = println(START_MESSAGE)
 
     companion object {
         const val START_MESSAGE = "다리 건너기 게임을 시작합니다."
+        const val RETRY = "R"
+        const val INIT = 0
+        const val INIT_RETRY = -1
     }
 }
