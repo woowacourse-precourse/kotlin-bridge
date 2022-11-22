fa14f3e4d3e13cc50a6e5fc7b045c1bba36c6006
bmlsj <gaga2353@gmail.com>
2022-11-22 20:57:05 +0900

    feat(OutputView): 방향 입력시 결과 Map을 출력하는 함수 printMap 구현

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -21,7 +21,22 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
 
-    fun printMap() {
+    private var able1 = mutableListOf<String>()
+    private var able2 = mutableListOf<String>()
+
+    fun printMap(route: List<String>, bridge: List<String>) {
+
+        if (route[route.lastIndex] == "U") {
+            able1.add(oxJudge(route[route.lastIndex], bridge[route.lastIndex], "U"))
+            able2.add("  ")
+        } else if (route[route.lastIndex] == "D") {
+            able1.add("  ")
+            able2.add(oxJudge(route[route.lastIndex], bridge[route.lastIndex], "D"))
+        }
+
+        println(able1.joinToString(prefix = "[ ", separator = "| ", postfix = "]"))
+        println(able2.joinToString(prefix = "[ ", separator = "| ", postfix = "]"))
+
     }
 
     /**
