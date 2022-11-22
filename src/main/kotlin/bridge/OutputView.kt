2e74292979775bb60dc0764dade0b28fe3e7e946
bmlsj <gaga2353@gmail.com>
2022-11-22 21:35:41 +0900

    feat(OutputView): 게임 최종 결과 출력하는 printResult 함수

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -21,15 +21,17 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
 
-    private var able1 = mutableListOf<String>()
-    private var able2 = mutableListOf<String>()
+    var able1 = mutableListOf<String>()
+    var able2 = mutableListOf<String>()
 
     fun printMap(route: List<String>, bridge: List<String>) {
 
         if (route[route.lastIndex] == "U") {
+            println(route[route.lastIndex])
             able1.add(oxJudge(route[route.lastIndex], bridge[route.lastIndex], "U"))
             able2.add("  ")
         } else if (route[route.lastIndex] == "D") {
+            println(route[route.lastIndex])
             able1.add("  ")
             able2.add(oxJudge(route[route.lastIndex], bridge[route.lastIndex], "D"))
         }
@@ -44,6 +46,17 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {
+    fun printResult(route: List<String>, bridge: List<String>, tryNum: Int) {
+
+        println("최종 게임 결과")
+        printMap(route, bridge)
+
+        if(route[route.lastIndex] == bridge[bridge.lastIndex]){
+            println("게임 성공 여부: 성공")
+        } else {
+            println("게임 성공 여부: 실패")
+        }
+
+        println("총 시도한 횟수: $tryNum")
     }
 }
