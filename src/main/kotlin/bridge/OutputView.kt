1e18bbbc36d32ff1e4c39974edd7b94b0820261b
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-11-22 11:18:20 +0900

    feat: 성공하지 못하고 게임을 종료했을 때 출력값

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import output.Output
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -19,5 +21,10 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+    fun printResult(markUp: MutableList<String>, markDown: MutableList<String>, numberOfTry: Int) {
+        println(Output.RESULT_GAME.output)
+        OutputView().printMap(markUp, markDown)
+        println(Output.SUCCESS_OR_NOT.output.plus(Output.FAIL.output))
+        println(Output.TRY_ATTEMPT.output.plus(numberOfTry))
+    }
 }
