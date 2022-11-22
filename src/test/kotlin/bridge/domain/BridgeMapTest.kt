cafc505249c3fec032b6a2ce89f5508f16864118
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-22 23:16:28 +0900

    test: Add BridgeMap Test

---
+++ src/test/kotlin/bridge/domain/BridgeMapTest.kt
@@ -0,0 +1,17 @@
+package bridge.domain
+
+import org.junit.jupiter.api.Test
+
+class BridgeMapTest {
+    @Test
+    fun `경로 이동 테스트 1, U 입력시 이동 경로 반영`() {
+    }
+
+    @Test
+    fun `경로 이동 테스트 2, D 입력시 이동 경로 반영`() {
+    }
+
+    @Test
+    fun `게임 시도 횟수 테스트`() {
+    }
+}
