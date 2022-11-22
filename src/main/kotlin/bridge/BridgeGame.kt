690368abdb089e1de6303f821787bf354c431b30
lee989898 <wnsrud1352@gmail.com>
2022-11-22 15:36:00 +0900

    [refactor](BridgeGame) 변수명 가독성 좋게 변경

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -2,88 +2,88 @@ package bridge
 
 class BridgeGame(private val bridgeLength: Int) {
 
-    private lateinit var bridge: List<List<String>>
-    private lateinit var bridgeLine: List<String>
-    private lateinit var oxBridge: List<MutableList<String>>
+    private lateinit var upDownBridge: List<List<String>>
+    private lateinit var upDownBridgeOneLine: List<String>
+    private lateinit var oXBridge: List<MutableList<String>>
     private var totalAttempts = 0
-    private var correctDirection = 0
+    private var correctNum = 0
     private var restartGame = true
-    private var showResult = false
+    private var showBridgeResult = false
 
     init {
-        createBridgeLine()
-        createBridge()
+        createUpDownBridgeOneLine()
+        createUpDownBridge()
     }
 
-    private fun createBridge() {
-        bridge = listOf(
-            bridgeLine.map { it.replace("D", " ") },
-            bridgeLine.map { it.replace("U", " ") }
+    private fun createUpDownBridge() {
+        upDownBridge = listOf(
+            upDownBridgeOneLine.map { it.replace("D", " ") },
+            upDownBridgeOneLine.map { it.replace("U", " ") }
         )
     }
 
-    private fun createBridgeLine() {
-        bridgeLine = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeLength)
+    private fun createUpDownBridgeOneLine() {
+        upDownBridgeOneLine = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeLength)
     }
 
-    fun retry(restartCommand: String): Boolean {
-        showResult = false
-        if (restartCommand == "Q") {
-            showResult = true
+    fun retry(restartOrQuitCommand: String): Boolean {
+        showBridgeResult = false
+        if (restartOrQuitCommand == "Q") {
+            showBridgeResult = true
             restartGame = false
-        } else if (restartCommand == "R") {
+        } else if (restartOrQuitCommand == "R") {
             restartGame = true
         }
         return true
     }
 
-    fun getRestart() = restartGame
+    fun getRestartGame() = restartGame
 
-    fun setRestart(restart: Boolean) {
+    fun setRestartGame(restart: Boolean) {
         restartGame = restart
     }
 
-    fun getShowResult() = showResult
+    fun getShowBridgeResult() = showBridgeResult
 
-    fun getOXBridge() = oxBridge
+    fun getOXBridge() = oXBridge
 
     fun getTotalAttempts() = totalAttempts
 
-    fun getCorrectDirection() = correctDirection
+    fun getCorrectNum() = correctNum
 
     fun initStartGame() {
-        oxBridge = mutableListOf(
+        oXBridge = mutableListOf(
             mutableListOf(),
             mutableListOf()
         )
-        correctDirection = 0
+        correctNum = 0
         totalAttempts++
     }
 
-    private fun checkD(move: String, index: Int) {
-        if (move == bridge[1][index]) {
-            oxBridge[0].add(" ")
-            oxBridge[1].add("O")
-            correctDirection++
-        } else if (bridge[0][index] == " ") {
-            oxBridge[0].add("X")
-            oxBridge[1].add(" ")
+    private fun checkUpDownBridgeContainD(move: String, index: Int) {
+        if (move == upDownBridge[1][index]) {
+            oXBridge[0].add(" ")
+            oXBridge[1].add("O")
+            correctNum++
+        } else if (upDownBridge[0][index] == " ") {
+            oXBridge[0].add("X")
+            oXBridge[1].add(" ")
         }
     }
 
-    private fun checkU(move: String, index: Int) {
-        if (move == bridge[0][index]) {
-            oxBridge[0].add("O")
-            oxBridge[1].add(" ")
-            correctDirection++
-        } else if (bridge[1][index] == " ") {
-            oxBridge[0].add(" ")
-            oxBridge[1].add("X")
+    private fun checkUpDownBridgeContainU(move: String, index: Int) {
+        if (move == upDownBridge[0][index]) {
+            oXBridge[0].add("O")
+            oXBridge[1].add(" ")
+            correctNum++
+        } else if (upDownBridge[1][index] == " ") {
+            oXBridge[0].add(" ")
+            oXBridge[1].add("X")
         }
     }
 
     fun move(move: String, index: Int) {
-        checkU(move, index)
-        checkD(move, index)
+        checkUpDownBridgeContainU(move, index)
+        checkUpDownBridgeContainD(move, index)
     }
 }
