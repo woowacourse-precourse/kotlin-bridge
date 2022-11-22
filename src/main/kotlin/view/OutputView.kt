097a6376f2efd4cc2e70c4e4e1fff23128bdb6e9
lee989898 <wnsrud1352@gmail.com>
2022-11-22 14:17:33 +0900

    [feat](OutputView) printMap에서 결과표 출력 기능 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -14,7 +14,9 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(ox: List<MutableList<String>>) {
+        printGameResult(ox)
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
@@ -33,3 +35,6 @@ class OutputView {
         println()
     }
 }
+
+// OutputView의 메서드의 이름은 변경할 수 없고, 인자와 반환 타입은 필요에 따라 추가하거나 변경할 수 있다.
+// 값 출력을 위해 필요한 메서드를 추가할 수 있다.
