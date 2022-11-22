49cae8421715014ce2eb9847c1f0fe16871845e6
no1msh <no1msh1217@gmail.com>
2022-11-22 17:05:34 +0900

    feat: 유저가 잘못된 값을 선택했을 때 임시적으로 보여줄 다리를 반환하는 메소드 구현

--- src/main/kotlin/bridge/domain/Bridge.kt
+++ src/main/kotlin/bridge/domain/Bridge.kt
@@ -4,4 +4,13 @@ class Bridge(private val bridge: MutableList<String>) {
 
     fun get(index: Int): String = bridge[index]
 
+    fun setWrongAnswer(position: Int, value: String): Bridge {
+        val tempBridge = mutableListOf<String>()
+        tempBridge.addAll(bridge)
+        tempBridge[position] = value
+        return Bridge(tempBridge).subBridge(position)
+    }
+
+    fun subBridge(lastIndex: Int): Bridge = Bridge(bridge.subList(0, lastIndex + 1))
+
 }
