12687d31289b4718bae5169c56262b0f68fc2e03
Hyuna Kim <akxmcse@gmail.com>
2022-11-22 23:01:51 +0900

    feat(OutputView): Down 결과를 출력하는 함수 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -17,7 +17,7 @@ class OutputView {
      */
     fun printMap(resultBridge: MutableList<Pair<String, String>>) {
         printUpLine(resultBridge)
-
+        printDownLine(resultBridge)
     }
 
     /**
@@ -28,7 +28,7 @@ class OutputView {
     fun printResult(resultBridge: MutableList<Pair<String, String>>) {
         println(Game.RESULT.message)
         printUpLine(resultBridge)
-
+        printDownLine(resultBridge)
     }
 
     private fun printUpLine(resultBridge: MutableList<Pair<String, String>>){
@@ -48,4 +48,20 @@ class OutputView {
         println(" ]")
     }
 
+    private fun printDownLine(resultBridge: MutableList<Pair<String, String>>) {
+        var i = 0
+        print("[")
+        resultBridge.forEach { (first, second) ->
+            if (first == "D") {
+                print(" $second")
+            } else {
+                print("  ")
+            }
+            i++
+            if(i != resultBridge.size){
+                print(" |")
+            }
+        }
+        println(" ]")
+    }
 }
