18f90fe350322d0dfd5fa3423b7bb3e73acb5c63
lee989898 <wnsrud1352@gmail.com>
2022-11-22 16:02:10 +0900

    [refactor](BridgeMessage) OutputView 매직 리터럴 요소 추가

--- src/main/kotlin/util/BridgeMessage.kt
+++ src/main/kotlin/util/BridgeMessage.kt
@@ -8,4 +8,15 @@ object BridgeMessage {
     const val READ_BRIDGE_SIZE_ERROR_MESSAGE = "[ERROR] 3이상 20이하 숫자를 입력해주세요."
     const val READ_MOVE_ERROR_MESSAGE = "[ERROR] \"U\" 나 \"D\"를 입력해주세요."
     const val READ_COMMAND_ERROR_MESSAGE = "[ERROR] \"R\" 이나 \"Q\"를 입력해주세요."
+
+    // OutputView
+    const val GAME_START_NOTIFICATION = "다리 건너기 게임을 시작합니다.\n"
+    const val SUCCESS = "성공"
+    const val FAIL = "실패"
+    const val FAIL_ENGLISH = "FAIL"
+    const val FINAL_GAME_RESULT = "최종 게임 결과"
+    const val IS_SUCCESS = "게임 성공 여부:"
+    const val TOTAL_ATTEMPT = "총 시도한 횟수:"
+    const val LEFT_SQUARE_BRACKETS = "["
+    const val RIGHT_SQUARE_BRACKETS = "]"
 }
