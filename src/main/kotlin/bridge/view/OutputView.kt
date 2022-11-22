93efba562d04e10b0bae0fe934f380f1ca1cdb37
no1msh <no1msh1217@gmail.com>
2022-11-22 19:18:25 +0900

    refactor: 하드코딩된 값 상수화

--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -12,8 +12,8 @@ class OutputView {
     fun init(answerBridge: List<String>, bridgeMaker: BridgeMaker) {
         this.answerBridge = answerBridge
         this.bridgeMaker = bridgeMaker
-        this.upBridge = Bridge(bridgeMaker.makeCompletedBridge(answerBridge, isUpBridge = true))
-        this.downBridge = Bridge(bridgeMaker.makeCompletedBridge(answerBridge, isUpBridge = false))
+        this.upBridge = Bridge(bridgeMaker.makeCompletedBridge(answerBridge, UP_BRIDGE))
+        this.downBridge = Bridge(bridgeMaker.makeCompletedBridge(answerBridge, DOWN_BRIDGE))
     }
 
     fun printGameStart() {
@@ -43,13 +43,13 @@ class OutputView {
     }
 
     private fun printMapUnCorrectDirection(position: Int, upBridge: Bridge, downBridge: Bridge) {
-        if (answerBridge[position] == "U") {
-            println(upBridge.getWrongAnswerBridge(position, " ").toString())
-            println(downBridge.getWrongAnswerBridge(position, "X").toString())
+        if (answerBridge[position] == ANSWER_UP) {
+            println(upBridge.getWrongAnswerBridge(position, SPACE).toString())
+            println(downBridge.getWrongAnswerBridge(position, WRONG_ANSWER).toString())
         }
-        if (answerBridge[position] == "D") {
-            println(upBridge.getWrongAnswerBridge(position, "X").toString())
-            println(downBridge.getWrongAnswerBridge(position, " ").toString())
+        if (answerBridge[position] == ANSWER_DOWN) {
+            println(upBridge.getWrongAnswerBridge(position, WRONG_ANSWER).toString())
+            println(downBridge.getWrongAnswerBridge(position, SPACE).toString())
         }
     }
 
@@ -59,22 +59,30 @@ class OutputView {
 
     fun printResult(position: Int, isCorrectDirection: Boolean) {
         println(GAME_RESULT)
-        printMap(position - 1, isCorrectDirection)
-
+        printMap(position - OVER_POSITION, isCorrectDirection)
     }
 
     fun printResultDetail(isWin: Boolean, tryCount: Int) {
-        println(IS_GAME_SUCCESS.format(if (isWin) "성공" else "실패"))
+        println(IS_GAME_SUCCESS.format(if (isWin) SUCCESS else FAILURE))
         println(TOTAL_TRY_COUNT.format(tryCount))
     }
 
     companion object {
-        const val GAME_START = "다리 건너기 게임을 시작합니다."
-        const val REQUEST_INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요."
-        const val REQUEST_INPUT_DIRECTION_TO_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
-        const val REQUEST_RESTART = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
-        const val GAME_RESULT = "최종 게임 결과"
-        const val IS_GAME_SUCCESS = "게임 성공 여부: %s"
-        const val TOTAL_TRY_COUNT = "총 시도한 횟수: %d"
+        private const val GAME_START = "다리 건너기 게임을 시작합니다."
+        private const val REQUEST_INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요."
+        private const val REQUEST_INPUT_DIRECTION_TO_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
+        private const val REQUEST_RESTART = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
+        private const val GAME_RESULT = "최종 게임 결과"
+        private const val IS_GAME_SUCCESS = "게임 성공 여부: %s"
+        private const val TOTAL_TRY_COUNT = "총 시도한 횟수: %d"
+        private const val SUCCESS = "성공"
+        private const val FAILURE = "실패"
+        private const val ANSWER_DOWN = "D"
+        private const val ANSWER_UP = "U"
+        private const val SPACE = " "
+        private const val WRONG_ANSWER = "X"
+        private const val OVER_POSITION = 1
+        private const val UP_BRIDGE = true
+        private const val DOWN_BRIDGE = false
     }
 }
