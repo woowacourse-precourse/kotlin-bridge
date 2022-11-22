6d37a2ba500261e4057b029e449bcceef3021eb6
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-22 16:06:51 +0900

    test(ControllerTest): 게임 성공 여부 테스트 코드 추가

--- src/test/kotlin/bridge/controller/ControllerTest.kt
+++ src/test/kotlin/bridge/controller/ControllerTest.kt
@@ -6,15 +6,21 @@ import org.junit.jupiter.api.Test
 class ControllerTest {
     companion object {
         val bridge = listOf<String>("U", "D", "U")
-        val BRIDGE_TOP = mutableListOf<String>("O", "X")
-        val BRIDGE_DOWN = mutableListOf<String>(" ", " ")
     }
 
     @Test
     fun `게임 실패 여부 테스트 1`() {
         val controller = BridgeGameController()
-        controller.upLine = BRIDGE_TOP
-        controller.downLine = BRIDGE_DOWN
+        controller.upLine = mutableListOf<String>("O", "X")
+        controller.downLine = mutableListOf<String>(" ", " ")
         assertThat(controller.judgeGameFailed(1)).isTrue
+    }
+
+    @Test
+    fun `게임 성공 여부 테스트 1`() {
+        val controller = BridgeGameController()
+        controller.upLine = mutableListOf<String>("O", " ", "O")
+        controller.downLine = mutableListOf<String>(" ", "O", " ")
+        assertThat(controller.judgeGameClear(2, bridge)).isTrue
     }
 }
