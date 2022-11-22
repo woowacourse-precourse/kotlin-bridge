93907260b0f4b8ad73639971ec652d75ddc579a8
inseonyun <yuninseon@naver.com>
2022-11-22 22:13:40 +0900

    refactor: ERROR 문구 상수로 관리

--- src/main/kotlin/bridge/GameCommandException.kt
+++ src/main/kotlin/bridge/GameCommandException.kt
@@ -1,12 +1,16 @@
 package bridge
 
 class GameCommandException(inputData: String) {
+    companion object {
+        const val GAME_COMMAND_IS_NOT_R_OR_Q = "[ERROR] 입력한 값은 'R' 또는 'Q'가 아닙니다."
+    }
+
     init {
         isNotRetryOrExitCharacter(inputData)
     }
 
     private fun isNotRetryOrExitCharacter(inputData: String) {
         if(!(inputData == "R" || inputData == "Q"))
-            throw IllegalArgumentException("[ERROR] 입력한 값은 'R' 또는 'Q'가 아닙니다.")
+            throw IllegalArgumentException(GAME_COMMAND_IS_NOT_R_OR_Q)
     }
 }
