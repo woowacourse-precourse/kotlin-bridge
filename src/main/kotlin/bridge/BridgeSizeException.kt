0b34ec7e1d7911c2b58483f078df153448c7d4e8
inseonyun <yuninseon@naver.com>
2022-11-22 22:11:09 +0900

    refactor: ERROR 문구 상수로 관리

--- src/main/kotlin/bridge/BridgeSizeException.kt
+++ src/main/kotlin/bridge/BridgeSizeException.kt
@@ -1,6 +1,11 @@
 package bridge
 
 class BridgeSizeException(inputData: String) {
+    companion object {
+        const val BRIDGE_SIZE_IS_NOT_INTEGER = "[ERROR] 입력한 다리 길이는 정수가 아닙니다."
+        const val BRIDGE_SIZE_IS_NOT_AVAILABLE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."
+    }
+
     init{
         isNotInteger(inputData)
         isNotBridgeNumber(inputData)
@@ -8,11 +13,11 @@ class BridgeSizeException(inputData: String) {
 
     private fun isNotInteger(inputData: String) {
         if(!inputData.chars().allMatch { Character.isDigit(it) })
-            throw IllegalArgumentException("[ERROR] 입력한 다리 길이는 정수가 아닙니다.")
+            throw IllegalArgumentException(BRIDGE_SIZE_IS_NOT_INTEGER)
     }
 
     private fun isNotBridgeNumber(inputData: String) {
         if(inputData.toInt() < 3 || inputData.toInt() > 20)
-            throw IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.")
+            throw IllegalArgumentException(BRIDGE_SIZE_IS_NOT_AVAILABLE)
     }
 }
