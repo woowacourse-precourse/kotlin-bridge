aa3bd67d058e03b713fb420806a9c02fa6e4ab3e
s9hn <tnghqn@naver.com>
2022-11-22 20:56:23 +0900

    [FEAT(Message)] 필요 문자열 값 추가

--- src/main/kotlin/bridge/data/Message.kt
+++ src/main/kotlin/bridge/data/Message.kt
@@ -12,13 +12,13 @@ const val ENTER_DIRECTION = "이동할 칸을 선택해주세요. (위: U, 아�
 const val ENTER_GAME_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
 
 const val GAME_RESULT = "최종 게임 결과"
-const val GAME_BOOLEAN = "게임 성공 여부: "
+const val GAME_BOOLEAN = "\n게임 성공 여부: "
 const val COUNT_RETRY = "총 시도한 횟수: "
 
 const val SUCCESS = "성공"
 const val FAIL = "실패"
 
-
 const val ERROR_RANGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."
 const val ERROR_DIGIT = "[ERROR] 숫자만 입력 가능합니다"
-const val ERROR_NULL ="[ERROR] 값이 존재하지 않습니다"
+const val ERROR_NULL = "[ERROR] 값이 존재하지 않습니다"
+const val ERROR_DIRECTION = "[ERROR] 'U' 또는 'D' 만 입력 가능합니다"
