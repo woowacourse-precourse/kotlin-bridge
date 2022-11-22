8a916e90f35d5502f1045943e7483a95f92f11e4
Hyuna Kim <akxmcse@gmail.com>
2022-11-22 23:23:56 +0900

    feat(Application): 프로그램을 실행하는 함수 구현

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,23 @@
 package bridge
 
+import util.Game
+import view.InputView
+import view.OutputView
+
 fun main() {
-    // TODO: 프로그램 구현
+    var isDone = true
+    var attempts = 1
+    while(isDone){
+        isDone = false
+        val bridgeSize = InputView().readBridgeSize()
+        val bridgeLetter = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)
+        if(BridgeGame().startGame(bridgeSize, bridgeLetter)){
+            attempts++
+            isDone = true
+        }
+    }
+    print("${Game.TOTAL_ATTEMPTS.message}$attempts")
 }
+
+
+
