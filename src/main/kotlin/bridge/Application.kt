acaf78fe65aec1807272a7d89bc08c1ceb2e8b7c
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-11-22 16:06:53 +0900

    refactor(Application): 게임 시작을 위한 기능을 이동
    Application -> BridgeGameController로 코드 이동

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,31 +1,10 @@
 package bridge
 
-import bridge.view.InputView
-import bridge.view.OutputView
 import bridge.domain.BridgeGameController
 
-private val inputView = InputView()
-private val outputView = OutputView()
 private val bridgeGameController = BridgeGameController()
 
 fun main() {
-    outputView.printStartMent()
-    val bridge = generateBridge(readBridgeSize())
-    bridgeGameController.play(bridge)
-}
-
-private fun readBridgeSize(): Int {
-    outputView.printReadBridgeSizeMent()
-    do {
-        try {
-            return inputView.readBridgeSize()
-        } catch (e: IllegalArgumentException) {
-            outputView.printErrorMessage(e.message.toString())
-        }
-    } while (true)
-}
-
-private fun generateBridge(size: Int): List<String> {
-    val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
-    return bridgeMaker.makeBridge(size)
+    bridgeGameController.start()
+    bridgeGameController.play()
 }
