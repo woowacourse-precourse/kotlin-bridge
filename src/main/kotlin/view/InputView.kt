7885d3798d16f2e1f6d57944ce9d8e638c3c882f
lee989898 <wnsrud1352@gmail.com>
2022-11-22 16:44:05 +0900

    [reactor](InputView) 매직리터럴 companion object로 변경

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,16 +1,6 @@
 package view
 
 import camp.nextstep.edu.missionutils.Console
-import util.BridgeMessage.DOWN
-import util.BridgeMessage.QUIT
-import util.BridgeMessage.READ_BRIDGE_SIZE
-import util.BridgeMessage.READ_BRIDGE_SIZE_ERROR_MESSAGE
-import util.BridgeMessage.READ_COMMAND
-import util.BridgeMessage.READ_COMMAND_ERROR_MESSAGE
-import util.BridgeMessage.READ_MOVE
-import util.BridgeMessage.READ_MOVE_ERROR_MESSAGE
-import util.BridgeMessage.RESTART
-import util.BridgeMessage.UP
 import java.lang.IllegalArgumentException
 
 class InputView {
@@ -53,5 +43,18 @@ class InputView {
         val command = readGameCommand()
         require(command == RESTART || command == QUIT) { READ_COMMAND_ERROR_MESSAGE }
         return command
+    }
+
+    companion object {
+        const val READ_BRIDGE_SIZE = "다리의 길이를 입력해주세요."
+        const val READ_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
+        const val READ_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
+        const val READ_BRIDGE_SIZE_ERROR_MESSAGE = "[ERROR] 3이상 20이하 숫자를 입력해주세요."
+        const val READ_MOVE_ERROR_MESSAGE = "[ERROR] \"U\" 나 \"D\"를 입력해주세요."
+        const val READ_COMMAND_ERROR_MESSAGE = "[ERROR] \"R\" 이나 \"Q\"를 입력해주세요."
+        const val UP = "U"
+        const val DOWN = "D"
+        const val RESTART = "R"
+        const val QUIT = "Q"
     }
 }
