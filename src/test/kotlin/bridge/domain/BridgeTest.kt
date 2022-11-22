28f1fcc4964a3bd375297e1d3fc82ea5bb2ea0e7
vrexpert <vrexpert2302@gmail.com>
2022-11-22 17:34:08 +0900

    test(BridgeTest): BridgeTest crossable 함수 테스트
    다리를 건널 수 없는 경우와 있는 경우 테스트 추가

--- src/test/kotlin/bridge/domain/BridgeTest.kt
+++ src/test/kotlin/bridge/domain/BridgeTest.kt
@@ -4,14 +4,33 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class BridgeTest {
+    private val bridge = Bridge(blocks)
+
     @Test
     fun `다리 개수 생성 테스트`() {
-        val bridge = Bridge(blocks)
         val result = bridge.getSize()
         assertThat(result).isEqualTo(blocks.size)
     }
 
+    @Test
+    fun `다리를 건널 수 없음 테스트`() {
+        val index = 0
+        val playerMove = DOWN
+        val result = bridge.crossable(index, playerMove)
+        assertThat(result).isEqualTo(false)
+    }
+
+    @Test
+    fun `다리를 건널 수 있음 테스트`() {
+        val index = 3
+        val playerMove = UP
+        val result = bridge.crossable(index, playerMove)
+        assertThat(result).isEqualTo(true)
+    }
+
     companion object {
-        val blocks = listOf("U","D","D","U")
+        val blocks = listOf("U", "D", "D", "U")
+        private const val UP = "U"
+        private const val DOWN = "D"
     }
 }
