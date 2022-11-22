ffde65274d99c5e43be7ae8af157e3779981fe2a
krrong <khk9664@naver.com>
2022-11-22 20:03:35 +0900

    feat(Controller): BridgeGame 클래스가 다리 상태를 갖도록 변경

--- src/main/kotlin/service/Controller.kt
+++ src/main/kotlin/service/Controller.kt
@@ -18,32 +18,28 @@ class Controller {
         endGame()
     }
 
-    private fun setUpClass() {
+    private fun setUpGame() {
         inputView = InputView()
         outputView = OutputView()
-        bridgeGame = BridgeGame()
-    }
-
-    private fun setUpGame(){
-        setUpClass()
         inputView.startGame()
         val bridgeSize = inputView.readBridgeSize()
         bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeSize)
+        bridgeGame = BridgeGame(bridge)
     }
 
     private fun endGame() {
-        outputView.printResult(bridgeGame.getResult(bridge))
+        outputView.printResult(bridgeGame.getResult())
         outputView.printResult(bridgeGame.getAnalysis(isPlaying))
     }
 
     private fun mainLogic() {
         do {
             bridgeGame.move(inputView.readMoving())                         // U/D 를 입력받아 moves에 저장
-            outputView.printMap(bridgeGame.getState(bridge))                // 결과 출력
-            if (bridgeGame.isFail(bridge)) {                                // 건널 수 없는 경우
+            outputView.printMap(bridgeGame.getState())                // 결과 출력
+            if (bridgeGame.isFail()) {                                // 건널 수 없는 경우
                 isPlaying = bridgeGame.retry(inputView.readGameCommand())   // 재시작 여부 확인
             }
-            if (bridgeGame.isSuccess(bridge)) break                          // 정상 종료
+            if (bridgeGame.isSuccess()) break                          // 정상 종료
         } while (isPlaying)
     }
 
