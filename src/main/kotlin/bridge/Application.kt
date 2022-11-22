0d626c83b82a6b364c248851ffae4826a3e43751
LHS0731 <gustjq2536@gmail.com>
2022-11-22 19:29:15 +0900

    refator: 메인함수 기능 RunGame클래스로 모듈화

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -4,41 +4,6 @@ package bridge
 fun main() {
     // TODO: 프로그램 구현
     val bridgeMade = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(InputView().readBridgeSize())
-    gameMaker(bridgeMade)
+    RunGame().gameMaker(bridgeMade)
 }
 
-fun gameMaker(bridge: List<String>){
-    var regame = ""
-    var tryCount = 1
-    while (true) {
-        val caseList = mutableListOf<Int>()
-        val answer = caseMaker(bridge, caseList)
-        if (!answer)
-            regame = InputView().readGameCommand()
-        if (!answer && BridgeGame().retry(regame)) {
-            OutputView().printResult(false, caseList, tryCount)
-            break
-        }
-        if (!answer && !BridgeGame().retry(regame)) {
-            regame = ""
-            tryCount += 1
-        }
-        if (answer) {
-            OutputView().printResult(true, caseList, tryCount)
-            break
-        }
-    }
-}
-
-fun caseMaker(bridge: List<String>, caseList: MutableList<Int>): Boolean {
-    for (i in bridge.indices) {
-        val case = BridgeGame().move(bridge[i], InputView().readMoving())
-        caseList.add(case)
-        OutputView().printMap(caseList)
-        if (case == 3 || case == 4)
-            break
-        if (i == bridge.size - 1)
-            return true
-    }
-    return false
-}
