c2f05ee29d9e8a94e00d59e2476579aff2d6aaee
s9hn <tnghqn@naver.com>
2022-11-22 17:33:08 +0900

    [FEAT(Message)] ERROR문구 추가

--- src/main/kotlin/bridge/data/Message.kt
+++ src/main/kotlin/bridge/data/Message.kt
@@ -2,9 +2,9 @@ package bridge.data
 
 const val UP = "U"
 const val DOWN = "D"
-const val CORRECT = " O "
-const val INCORRECT = " X "
-const val BLANK = "   "
+const val CORRECT = "O"
+const val INCORRECT = "X"
+const val BLANK = " "
 
 const val GAME_START = "다리 건너기 게임을 시작합니다.\n"
 const val ENTER_BRIDGE_SIZE = "다리의 길이를 입력해주세요."
@@ -15,3 +15,8 @@ const val COUNT_RETRY = "총 시도한 횟수"
 
 const val SUCCESS = "성공"
 const val FAIL = "실패"
+
+
+const val ERROR_RANGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."
+const val ERROR_DIGIT = "[ERROR] 숫자만 입력 가능합니다"
+const val ERROR_NULL ="[ERROR] 값이 존재하지 않습니다"
