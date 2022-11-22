0eca0bdc043a7adb930ca126c70e3f4b1c2d4b71
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-22 13:39:41 +0900

    [Feat] (BridgeGame.kt) 종료, 재시작, 이동 기능 추가
    moveUnable: 틀렸을 때
    move: 맞았을 때
    finish: 모든 라운드 성공

--- src/main/kotlin/bridge/controller/BridgeGame.kt
+++ src/main/kotlin/bridge/controller/BridgeGame.kt
@@ -1,8 +1,13 @@
 package bridge.controller
 
+import bridge.controller.processor.BridgeGameProcessor
 import bridge.model.BridgeMaker
+import bridge.util.Moving
+import bridge.util.QUIT_COMMAND
+import bridge.util.RESTART_COMMAND
 import bridge.view.InputView
 import bridge.view.OutputView
+import com.sun.tools.javac.jvm.ByteCodes.ret
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -31,6 +36,8 @@ class BridgeGame(
     override fun process() {
         do {
             move()
+            moveUnable()
+            finish()
         } while (round < bridge.size)
     }
 
@@ -41,15 +48,40 @@ class BridgeGame(
 
     // 게임 다시 시작
     override fun retry() {
+        BridgeGameProcessor.deleteBridgePassInfo()
         trialCount++
         round = 0
     }
 
-
+    // 라운드 이동 ( 커멘드가 U일 때 조건)
     private fun move() {
         val moving = inputView.readMoving()
 
+        val inputMove = if (moving == "U") Moving.UP else Moving.DOWN
+        val hasPassed = (moving == bridge[round])
+        BridgeGameProcessor.updateBridgePassInfo(inputMove, hasPassed)
+
+        outputView.printMap(BridgeGameProcessor.updateMapList())
         round++
+    }
+
+    // 틀렸을 때, 게임 커맨드 입력
+    private fun moveUnable() {
+        if (BridgeGameProcessor.updateFail()) {
+            when (inputView.readGameCommand()) {
+                RESTART_COMMAND -> retry()
+                QUIT_COMMAND -> quit()
+            }
+        }
+    }
+
+    // 모든 라운드를 성공했을 때 ( 라운드와 사이즈가 같고, 틀린게 없을 때 )
+    private fun finish() {
+        if (round == bridge.size) {
+            if (!BridgeGameProcessor.updateFail()) {
+                quit()
+            }
+        }
     }
 
 }
