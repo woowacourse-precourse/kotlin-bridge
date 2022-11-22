89c3fe6a61aec529a5fcd3de107cc861d4044f3d
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-22 21:21:13 +0900

    feat: add printReadMovingMessage function Add function that print require input message in InputView.readMoving

--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -38,6 +38,10 @@ class OutputView {
         println("다리의 길이를 입력해주세요.")
     }
 
+    fun printReadMovingMessage(){
+        println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
+    }
+
     fun printErr(errMessage: String){
         println(errMessage)
     }
