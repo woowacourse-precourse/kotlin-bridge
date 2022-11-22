6416f7065489aeb967b21cc3eac2e1caf63a13da
dev-worthwhile <dev.lboram@gmail.com>
2022-11-22 18:13:27 +0900

    feat<Application>: main bridge game

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,7 +1,28 @@
 package bridge
 
 fun main() {
-    // TODO: 프로그램 구현
+    val inputView = InputView()
+    val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
+
+    println("다리 건너기 게임을 시작합니다.")
+    val size = inputView.readBridgeSize()
+    val answerBridge = bridgeMaker.makeBridge(size)
+    var attempt = 0
+    var gamePlay = true
+    while (gamePlay){
+        attempt++
+        val attemptOver = crossBridge(answerBridge, size)
+        if(attemptOver.isInterrupted()){ //실패해서 중간에 중단된 경우
+            val gameCommand = inputView.readGameCommand()
+            gamePlay = BridgeGame().retry(gameCommand) //재시도하는 경우 -> 처음부터 시작
+        }
+        if(!attemptOver.isInterrupted()){ //성공으로 인한 게임 종료
+            gamePlay = false
+        }
+        if(!gamePlay){ //최종 결과 출력
+            OutputView().printResult(attemptOver.getUserMoveHistory(), answerBridge, attempt)
+        }
+    }
 }
 fun showResult(moves: List<String>, answers: List<String>, attempt:Int) : Boolean{
     OutputView().printMap(moves,answers)
