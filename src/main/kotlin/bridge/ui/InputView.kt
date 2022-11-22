fb73f3fd98ceba77fda32eefa9458ca16e0dc494
윤동현 <donghyunyoon81@gmail.com>
2022-11-22 17:19:53 +0900

    feat<InputView> : 게임을 다시 시도할지 여부를 입력 받는 readGameCommand
    어떻게 : require에 사용자가 R 또는 Q 를 입력시 true를 반환하도록 매개변수를 작성합니다
            false일시에 IllegalArgumentException을 발생시키고 println으로 에러 텍스트를 출력합니다.
            readGameCommand()을 반환하여 오류 발생후에 다시 입력을  시작 할 수 있도록 구현합니다.
    왜 :     사용자에게 잘못된 값을 입력 받았을때 오류 발생과 안내를 하기 위해서

--- src/main/kotlin/bridge/ui/InputView.kt
+++ src/main/kotlin/bridge/ui/InputView.kt
@@ -38,6 +38,12 @@ class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+        val userInputText = Console.readLine()
+        require(userInputText == "R" || userInputText == "Q") {
+            println("[ERROR] R 또는 Q중 하나의 문자만 입력해주세요")
+            return readGameCommand()
+        }
+        return userInputText
     }
 }
