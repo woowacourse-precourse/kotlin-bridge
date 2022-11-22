cff188bb6fd7a2031950eeb29ce4d616978c59be
nayoung511 <nayoungbae511@gmail.com>
2022-11-22 19:28:54 +0900

    feat (BridgeGamePlay.kt): 게임 실행 함수 추가
    fun prepare: 다리 생성
    fun playFirst: 게임 첫 실행
    fun playContinue: X를 마주치거나 끝에 도달할 때까지 게임 실행 함수
    fun playAgain: 재시도 / 종료 게임 실행 여부 결정
    fun display: 각 칸마다 이동 결과 보여줌
    fun showFinalResult: 최종 결과 이동

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -2,7 +2,7 @@ package bridge
 
 fun main() {
     // TODO: 프로그램 구현
-
-    BridgeGamePlay().play()
+    println("다리 건너기 게임을 시작합니다.\n")
+    BridgeGamePlay().prepare()
 
 }
--- src/main/kotlin/bridge/BridgeGamePlay.kt
+++ src/main/kotlin/bridge/BridgeGamePlay.kt
@@ -1,20 +1,82 @@
 package bridge
 
 class BridgeGamePlay: BridgeNumberGenerator {
+    val bridgeRandomNumberGenerator = BridgeRandomNumberGenerator()
+    val inputView = InputView()
+    var finalAttempt = 1
+
     override fun generate(): Int {
-        TODO("Not yet implemented")
+        val bridgeNumber = bridgeRandomNumberGenerator.generate()
+        return bridgeNumber
     }
 
-
-    fun play(){
-        val inputView = InputView()
-
+    fun prepare(){
+        println("다리의 길이를 입력해주세요.")
         val bridgeSize = inputView.readBridgeSize()
 
-        println(bridgeSize)
+        // 다리 만들기
+        val bridgeMaker = BridgeMaker(bridgeRandomNumberGenerator)
+        val bridge = bridgeMaker.makeBridge(bridgeSize)
+        println(bridge)
 
+        playFirst(bridge, bridgeSize, 1)
     }
 
+    fun playFirst(bridge: List<String>, bridgeSize: Int, attempt: Int){
+        var step = 0
+        var continuePlay = true
+        // 끝까지 도달했거나, 또는 X를 만났을 때
+        while (continuePlay){
+            if (finalAttempt == attempt) {
+                continuePlay = playContinue(bridge, bridgeSize, step, attempt)
+                step++
+            } else break
+        }
+    }
 
+    fun playContinue(bridge: List<String>, bridgeSize: Int, step: Int, attempt: Int): Boolean {
+        println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)")
+        val direction = inputView.readMoving()
+        display(bridge, direction, step)
+        if ( bridge[step] != direction )
+            if (!playAgain(bridge, step, attempt, bridgeSize)) return false
+        if ( step == bridge.size - 1 && finalAttempt == attempt){
+            showFinalResult(bridge, bridgeSize, "W", step, attempt)
+            return false
+        }
+        return true
+    }
+
+    fun playAgain(bridge: List<String>, step: Int, attempt: Int, bridgeSize: Int): Boolean {
+        println("\n\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+        val gameCommand = inputView.readGameCommand()
+        if ( gameCommand == "R" ) { // 재시도
+            finalAttempt = attempt+1
+            playFirst(bridge, bridgeSize, attempt+1)
+
+        } else if (gameCommand == "Q"){ // 게임 종료
+            showFinalResult(bridge, step, gameCommand, step, attempt)
+            return false
+        }
+        return true
+    }
+
+    fun display(bridge: List<String>, direction: String, step: Int){
+        val outputView = OutputView()
+        if ( step == 0 ) outputView.printMapFirst(bridge, direction, step)
+        else outputView.printMap(bridge, direction, step)
+    }
+
+    fun showFinalResult(bridge: List<String>, bridgeSize: Int, gameCommand: String, step: Int, attempt:Int){
+        // Q - 재시도, W - 승리
+        println("\n최종 게임 결과")
+        val outputView = OutputView()
+        if ( gameCommand == "W" ) outputView.printResult(bridge, bridgeSize)
+        else if (gameCommand == "Q" ) outputView.printResultQuit(bridge, step)
+
+        print("\n\n게임 성공 여부: ")
+        if ( gameCommand == "W" ) print("성공\n") else print("실패\n")
+        println("총 시도한 횟수: $attempt")
+    }
 
 }
