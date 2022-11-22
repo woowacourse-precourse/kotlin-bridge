582b7e7ab518c761dd8b62aeeca6fdbfb4b814e0
krrong <khk9664@naver.com>
2022-11-22 13:23:26 +0900

    chore(BridgeGame): 함수 위치 변경

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -3,29 +3,16 @@ package bridge
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame() {
+class BridgeGame {
     private var tryCount: Int = 1   // 총 시도 횟수
     private var moves: MutableList<String> = mutableListOf()    // 이동 방법
 
     /**
      * 사용자가 칸을 이동할 때 사용
-     * BridgeGame이 관리하는 moves에 추가
+     * BridgeGame의 멤버변수 moves에 추가
      */
     fun move(location: String) {
         moves.add(location)
-    }
-
-    /**
-     * 다리 상태와 입력의 비교 값을 리스트로 반환
-     * 같으면 1 다르면 0
-     */
-    fun compareBridgeWithMoves(bridge: List<String>): List<Int> {
-        val match = mutableListOf<Int>()
-        for (i in moves.indices) {
-            // 1이면 같고, 0이면 다른 값
-            match.add(if (bridge[i] == moves[i]) 1 else 0)
-        }
-        return match
     }
 
     /**
@@ -35,6 +22,47 @@ class BridgeGame() {
         val upperState = makeUpperState(bridge)
         val lowerState = makeLowerState(bridge)
         return Pair(modifySuffix(upperState), modifySuffix(lowerState))
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     */
+    fun retry(restartCommand: String): Boolean {
+        if (restartCommand == "R") {
+            tryCount += 1
+            moves = mutableListOf()
+            return true
+        }
+        return false
+    }
+
+    fun isFail(bridge: List<String>): Boolean = (compareBridgeWithMoves(bridge)).contains(0)
+
+    fun isSuccess(bridge: List<String>): Boolean =
+            !(compareBridgeWithMoves(bridge)).contains(0)
+                    && bridge.size == moves.size
+
+    fun getResult(bridge: List<String>): String {
+        val state = getState(bridge)
+        return "최종 게임 결과\n${state.first}\n${state.second}"
+    }
+
+    fun getAnalysis(isSuccess: Boolean): String {
+        return if (isSuccess) Message.BridgeGameEnum.SUCCESS.toString().format(tryCount)
+        else Message.BridgeGameEnum.FAIL.toString().format(tryCount)
+    }
+
+    /**
+     * 다리 상태와 입력의 비교 값을 리스트로 반환
+     * 같으면 1 다르면 0
+     */
+    private fun compareBridgeWithMoves(bridge: List<String>): List<Int> {
+        val match = mutableListOf<Int>()
+        for (i in moves.indices) {
+            // 1이면 같고, 0이면 다른 값
+            match.add(if (bridge[i] == moves[i]) 1 else 0)
+        }
+        return match
     }
 
     /**
@@ -70,37 +98,9 @@ class BridgeGame() {
     /**
      * 마지막 값을 ']'로 변환
      */
-    fun modifySuffix(state: String): String {
+    private fun modifySuffix(state: String): String {
         val result = state.toCharArray()
         result[state.length - 1] = ']'
         return String(result)
-    }
-
-    fun isFail(bridge: List<String>): Boolean = (compareBridgeWithMoves(bridge)).contains(0)
-
-    fun isSuccess(bridge: List<String>): Boolean =
-            !(compareBridgeWithMoves(bridge)).contains(0)
-                    && bridge.size == moves.size
-
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     */
-    fun retry(restartCommand: String): Boolean {
-        if (restartCommand == "R") {
-            tryCount += 1
-            moves = mutableListOf()
-            return true
-        }
-        return false
-    }
-
-    fun getResult(bridge: List<String>): String {
-        val state = getState(bridge)
-        return "최종 게임 결과\n${state.first}\n${state.second}"
-    }
-
-    fun getAnalysis(isSuccess: Boolean): String {
-        return if (isSuccess) Message.BridgeGameEnum.SUCCESS.toString().format(tryCount)
-        else Message.BridgeGameEnum.FAIL.toString().format(tryCount)
     }
 }
