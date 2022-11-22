13191641de668b1360da80569ed16383768c84dd
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-11-22 11:15:28 +0900

    feat: 게임 횟수 세는 함수
    한번은 무조건 시작하므로 첫 시작을 1로 두고 재시작할 때마다 +1씩 해준다.

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -12,6 +12,9 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
+
+    private var tryGame = 1
+
     fun move(mark: MutableMap<String, String>, markUp: MutableList<String>, markDown: MutableList<String>) {
         mark.forEach { (key, value) ->
             markUp.add(key)
@@ -50,4 +53,9 @@ class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry() {}
+
+    fun countingGame(): Int {
+        tryGame++
+        return tryGame
+    }
 }
