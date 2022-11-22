838de1754d357c45203f66378dcc3c31aa3f9bce
Lim-Joosung <kinglim1224@naver.com>
2022-11-22 21:47:04 +0900

    fix: Application.kt 수정

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -4,7 +4,6 @@ fun main() {
     val bridgeLength = BridgeException().readBridgeSizeError()
     val bridgeUpdown = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeLength)
     val bridge = mutableListOf<String>()
-    println(bridgeUpdown)
     var playcount = 1
     while (BridgePlay().playBridge(bridgeLength,bridgeUpdown,bridge))
         playcount = BridgePlay().playCount(playcount,bridgeLength,bridge)
