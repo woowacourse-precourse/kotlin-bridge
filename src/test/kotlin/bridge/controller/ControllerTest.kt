ebc83cf7a83e98fe0d43a9af5b85642cf0307c54
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-22 16:14:02 +0900

    test(ControllerTest): 메소드 인자 변경으로 인한 테스트 코드 수정

--- src/test/kotlin/bridge/controller/ControllerTest.kt
+++ src/test/kotlin/bridge/controller/ControllerTest.kt
@@ -11,16 +11,18 @@ class ControllerTest {
     @Test
     fun `게임 실패 여부 테스트 1`() {
         val controller = BridgeGameController()
+        controller.index = 1
         controller.upLine = mutableListOf<String>("O", "X")
         controller.downLine = mutableListOf<String>(" ", " ")
-        assertThat(controller.judgeGameFailed(1)).isTrue
+        assertThat(controller.judgeGameFailed()).isTrue
     }
 
     @Test
     fun `게임 성공 여부 테스트 1`() {
         val controller = BridgeGameController()
+        controller.index = 2
         controller.upLine = mutableListOf<String>("O", " ", "O")
         controller.downLine = mutableListOf<String>(" ", "O", " ")
-        assertThat(controller.judgeGameClear(2, bridge)).isTrue
+        assertThat(controller.judgeGameClear(bridge)).isTrue
     }
 }
