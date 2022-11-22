dfcdc515a0678038f018f5d66de6a658fe7acb3e
kimsonac <sonac.kim@gmail.com>
2022-11-22 19:22:23 +0900

    ** 테스트 코드 작성법 학습

--- src/test/kotlin/bridge/UnitTest.kt
+++ src/test/kotlin/bridge/UnitTest.kt
@@ -1,16 +1,18 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
-class UnitTest {
+class UnitTest : NsTest() {
     private val inputView = InputView()
     private val bridgeNumberGenerator = BridgeRandomNumberGenerator()
     private val bridgeMaker = BridgeMaker(bridgeNumberGenerator)
 
     @Test
     fun `다리 길이 입력값 범위 초과`() {
-        assertThrows<IllegalArgumentException> {
+        {
             inputView.readBridgeSize()
         }
     }
@@ -28,10 +30,18 @@ class UnitTest {
     @Test
     fun `이동 입력값 예외`() {
         inputView.readMoving()
+        assertSimpleTest {
+            run { "21" }
+            Assertions.assertThat(output()).contains("[ERROR]")
+        }
     }
 
     @Test
     fun `다리 생성 테스트`(){
         println(bridgeMaker.makeBridge(5))
+    }
+
+    override fun runMain() {
+        TODO("Not yet implemented")
     }
 }
