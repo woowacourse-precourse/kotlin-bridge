4ba194601a20f29782c0d78a2826bd9bcebf3c5b
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-22 23:07:24 +0900

    style(view): GAME_SUCCESS_FIELD -> GAME_RESULT_FIELD 변경

--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -11,7 +11,7 @@ class OutputView : MovingEventListener, QuitEventListener {
     private fun printResult(map: GameMap, result: GameResult) {
         println(FINAL_GAME_RESULT_SUBJECT)
         printMap(map)
-        println(GAME_SUCCESS_FIELD + if (result.success) SUCCESS else FAILED)
+        println(GAME_RESULT_FIELD + if (result.success) SUCCESS else FAILED)
         println(ATTEMPTS_FIELD + result.attempts)
     }
 
@@ -22,7 +22,7 @@ class OutputView : MovingEventListener, QuitEventListener {
 
     companion object {
         private const val FINAL_GAME_RESULT_SUBJECT = "최종 게임 결과"
-        private const val GAME_SUCCESS_FIELD = "게임 성공 여부: "
+        private const val GAME_RESULT_FIELD = "게임 성공 여부: "
         private const val ATTEMPTS_FIELD = "총 시도한 횟수: "
         private const val SUCCESS = "성공"
         private const val FAILED = "실패"
