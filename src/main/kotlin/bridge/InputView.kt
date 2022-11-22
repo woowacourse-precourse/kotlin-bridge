d869deeabefce37fa95e8b36738fb3fcc26e1f48
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-11-22 10:41:50 +0900

    feat: 다리 길이에 대한 예외처리

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,10 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+import output.ErrorMessage
+import output.ExceptionHandler
+import output.Output
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,7 +13,18 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        println(Output.INPUT_LENGTH.output)
+        val bridgeSize = try {
+            Console.readLine()!!.toInt()
+        } catch (e: NumberFormatException) {
+            println(ErrorMessage.ONLY_NUMBER.errorMessage)
+            readBridgeSize()
+        }
+        if (!ExceptionHandler.rangeException(bridgeSize)) {
+            println(ErrorMessage.RANGE.errorMessage)
+            readBridgeSize()
+        }
+        return bridgeSize
     }
 
     /**
