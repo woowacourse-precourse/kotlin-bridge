3d685c949332710a92c65cb602da63e2861cd821
hyungmin <ramkid91@naver.com>
2022-11-22 19:16:03 +0900

    feat:  결과 Map 출력, 최종 결과 출력 로직 구현
    - Map은 위쪽 다리 결과와 아래쪽 다리 결과 출력을 메서드로 분리
    - 최종결과 출력은 Map 결과 출력 호출 + parameter로 받은 값은 게임 성공 실패 여부 판별 + 시도회수 출력 순으로 구현

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -9,12 +9,85 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(userInput:List<String>, bridge: List<String>) {
+        print("[")
+        printUpMap(userInput,bridge)
+        println("]")
+        print("[")
+        printDownMap(userInput,bridge)
+        println("]")
 
+    }
+
+    private fun printUpMap(userInput: List<String>, bridge: List<String>){
+        var crossCount = userInput.size
+        for(i in userInput.indices){
+            crossCount--
+            checkSameWithBridgeUpCase(userInput[i], bridge[i])
+            if(crossCount!=0){
+                print("|")
+            }
+        }
+    }
+
+    private fun checkSameWithBridgeUpCase(userInput:String, bridge: String){
+        if(userInput == "U" && bridge=="U"){
+            print(" O ")
+        }
+        else if(userInput=="U" && bridge =="D"){
+            print(" X ")
+        }
+        else {
+            print("   ")
+        }
+    }
+
+    private fun checkSameWithBridgeDownCase(userInput:String, bridge: String){
+        if(userInput == "D" && bridge=="D"){
+            print(" O ")
+        }
+        else if(userInput=="D" && bridge =="U"){
+            print(" X ")
+        }
+        else {
+            print("   ")
+        }
+    }
+    private fun printDownMap(userInput: List<String>, bridge: List<String>){
+        var crossCount = userInput.size
+        for(i in userInput.indices){
+            crossCount--
+            checkSameWithBridgeDownCase(userInput[i], bridge[i])
+            if(crossCount!=0){
+                print("|")
+            }
+        }
+    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+    fun printResult(userInput: List<String>, bridge: List<String>, tryCount:Int) {
+        println("최종 게임 결과")
+        printMap(userInput, bridge)
+        print("게임 성공 여부: ")
+        val res = checkResult(userInput,bridge)
+        print(if(res) "성공" else "실패")
+        println("총 시도한 횟수: $tryCount")
+    }
+
+    private fun checkResult(userInput: List<String>, bridge: List<String>):Boolean{
+        if(userInput.size == bridge.size){
+            return checkAllElementSame(userInput,bridge)
+        }
+        return false
+    }
+
+    private fun checkAllElementSame(userInput: List<String>, bridge: List<String>):Boolean{
+        for(i in userInput.indices){
+            if(userInput[i]!= bridge[i]) return false
+        }
+        return true
+    }
 }
