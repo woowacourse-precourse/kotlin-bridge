16e6c842d8f3827246220cc41622feaa4ddf37c4
inseonyun <yuninseon@naver.com>
2022-11-22 22:12:31 +0900

    refactor: ERROR 문구 상수로 관리

--- src/main/kotlin/bridge/BridgeMoveException.kt
+++ src/main/kotlin/bridge/BridgeMoveException.kt
@@ -1,12 +1,16 @@
 package bridge
 
 class BridgeMoveException(inputData: String) {
+    companion object {
+        const val BRIDGE_MOVE_IS_NOT_U_OR_D = "[ERROR] 입력한 값은 'U' 또는 'D'가 아닙니다."
+    }
+
     init {
         isNotMoveCharacter(inputData)
     }
 
     private fun isNotMoveCharacter(inputData: String) {
         if(!(inputData == "U" || inputData == "D"))
-            throw IllegalArgumentException("[ERROR] 입력한 값은 'U' 또는 'D'가 아닙니다.")
+            throw IllegalArgumentException(BRIDGE_MOVE_IS_NOT_U_OR_D)
     }
 }
