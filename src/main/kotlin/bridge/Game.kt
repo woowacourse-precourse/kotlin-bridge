acfea2aa0af46bde6ff75224ee5a40f3ac09984e
Jaeheon Jeong <drdd1120@naver.com>
2022-11-22 15:47:17 +0900

    <refac> Game의 gameStart 코드 라인 줄이기

--- src/main/kotlin/bridge/Game.kt
+++ src/main/kotlin/bridge/Game.kt
@@ -2,23 +2,30 @@ package bridge
 
 class Game {
     fun gameStart() {
-        val (inputView, size, bridge) = makeBridge()
+        val (inputView, size, bridgeGame) = makeBridge()
 
-        val bridgeGame = BridgeGame(bridge as MutableList<String>)
+        val triple = mainGame(size, inputView, bridgeGame)
+
+        val outputView = triple.first
+        var lastBridge = triple.second
+        var flag = triple.third
+        outputView.printResult(lastBridge, flag, bridgeGame.getCount())
+    }
+
+    private fun mainGame(size: Int, inputView: InputView, bridgeGame: BridgeGame): Triple<OutputView, MutableList<String>, Boolean> {
         val outputView = OutputView()
-
         var lastBridge = mutableListOf<String>()
         var idx = 0
         var flag = true
         while (idx < size) {
             val upOrDown = inputView.readMoving()
-            if(bridgeGame.move(upOrDown)){
+            if (bridgeGame.move(upOrDown)) {
                 lastBridge = outputView.printMap(bridgeGame.userBridge, true)
                 idx++
                 continue
             }
             lastBridge = outputView.printMap(bridgeGame.userBridge, false)
-            if(inputView.readGameCommand() == "R"){
+            if (inputView.readGameCommand() == "R") {
                 bridgeGame.retry()
                 idx = 0
                 continue
@@ -26,17 +33,19 @@ class Game {
             flag = false
             break
         }
-        outputView.printResult(lastBridge, flag, bridgeGame.getCount())
+        return Triple(outputView, lastBridge, flag)
     }
 
-    private fun makeBridge(): Triple<InputView, Int, List<String>> {
+    private fun makeBridge(): Triple<InputView, Int, BridgeGame> {
         val inputView = InputView()
         val size = inputView.readBridgeSize()
 
         val bridgeRandomNumberGenerator = BridgeRandomNumberGenerator()
         val bridgeMaker = BridgeMaker(bridgeRandomNumberGenerator)
         val bridge = bridgeMaker.makeBridge(size)
-        println(bridge.toString())
-        return Triple(inputView, size, bridge)
+        //        println(bridge.toString())
+
+        val bridgeGame = BridgeGame(bridge as MutableList<String>)
+        return Triple(inputView, size, bridgeGame)
     }
 }
