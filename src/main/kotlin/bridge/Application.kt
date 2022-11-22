b4c864f81e3fef0b1b217bf696a0d285f7aa1e2f
hyunji1203 <hj27720@gmail.com>
2022-11-22 15:29:06 +0900

    Style: 코드 줄 바꿈

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -2,9 +2,7 @@ package bridge
 
 fun main() {
     println("다리 건너기 게임을 시작합니다.")
-
     var bridgeLength = InputView().readBridgeSize()
-
     var bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
     var bridge = bridgeMaker.makeBridge(bridgeLength)
 
