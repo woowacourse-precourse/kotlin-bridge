99b08608a6d1e13c386db9897bd117c5e64777ad
dlgocks1 <cksgodl1@naver.com>
2022-11-22 14:12:55 +0900

    refactor(Bridge.kt) : BridgeStatus 업데이트로 구조 변경

--- src/main/kotlin/bridge/model/Bridge.kt
+++ src/main/kotlin/bridge/model/Bridge.kt
@@ -8,9 +8,10 @@ class Bridge(private val bridge: List<String>) {
         })
     }
 
-    fun move(idx: Int, direction: String): BridgeResult {
-        if (bridge[idx] == direction && idx + 1 == bridge.size) return BridgeResult.FINISH
-        if (bridge[idx] == direction) return BridgeResult.SUCCESS
-        return BridgeResult.FAIL
+    fun move(idx: Int, direction: String): BridgeStatus {
+        require(direction == "U" || direction == "D")
+        if (bridge[idx] == direction && idx + 1 == bridge.size) return BridgeStatus.FINISH(direction)
+        if (bridge[idx] == direction) return BridgeStatus.SUCCESS(direction)
+        return BridgeStatus.FAIL(direction)
     }
 }
