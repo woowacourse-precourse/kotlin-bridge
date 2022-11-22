7523d57bb11f1f0051e2dfac6ddc497e528820b5
no1msh <no1msh1217@gmail.com>
2022-11-22 17:37:33 +0900

    feat: 다리건너기 게임을 시작하는 메소드 구현

--- src/main/kotlin/bridge/BridgeGameController.kt
+++ src/main/kotlin/bridge/BridgeGameController.kt
@@ -12,4 +12,15 @@ class BridgeGameController(
 ) {
     private lateinit var bridgeGame: BridgeGame
 
+    fun startGame() {
+        outputView.printGameStart()
+        outputView.printRequestInputBridgeSize()
+        val size = inputView.readBridgeSize()
+        bridgeGame = BridgeGame(bridgeMaker.makeBridge(size))
+        outputView.init(bridgeGame.getAnswerBridge(), bridgeMaker)
+        val isWin = gameProgress(size)
+        outputView.printResult(bridgeGame.getPosition(), isWin)
+        outputView.printResultDetail(isWin, bridgeGame.getTryCount())
+    }
+
 }
