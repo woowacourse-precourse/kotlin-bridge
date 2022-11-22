bf0aafecc7fe1a2c1f943ef0574deaea47b5fe81
s9hn <tnghqn@naver.com>
2022-11-22 15:57:53 +0900

    [FEAT(Message)] 게임 내에서 사용 할 스트링 문자열 추가

---
+++ src/main/kotlin/bridge/data/Message.kt
@@ -0,0 +1,17 @@
+package bridge.data
+
+const val UP = "U"
+const val DOWN = "D"
+const val CORRECT = " O "
+const val INCORRECT = " X "
+const val BLANK = "   "
+
+const val GAME_START = "다리 건너기 게임을 시작합니다.\n"
+const val ENTER_BRIDGE_SIZE = "다리의 길이를 입력해주세요."
+
+const val GAME_RESULT = "최종 게임 결과"
+const val GAME_BOOLEAN = "게임 성공 여부"
+const val COUNT_RETRY = "총 시도한 횟수"
+
+const val SUCCESS = "성공"
+const val FAIL = "실패"
