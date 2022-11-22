cc929cf0f5bba59a3d5450be23b2f053337fc709
vrexpert <vrexpert2302@gmail.com>
2022-11-22 17:33:06 +0900

    test(InputViewTest): 소수 입력 테스트 추가
    기존 문자나 공백 입력 예외 테스트에 소수도 테스트도 포함
    소수는 문자가 들어가기 때문에 같이 예외처리

--- src/test/kotlin/bridge/ui/InputViewTest.kt
+++ src/test/kotlin/bridge/ui/InputViewTest.kt
@@ -9,18 +9,18 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class InputViewTest : NsTest() {
-    @ValueSource(strings = ["-1","0","2","21","200"])
+    @ValueSource(strings = ["-1", "0", "2", "21", "200"])
     @ParameterizedTest
-    fun `숫자 범위 입력 예외 테스트`(input:String) {
+    fun `숫자 범위 입력 예외 테스트`(input: String) {
         camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest {
             runException(input)
             assertThat(output()).contains(ERROR_NUM_NOT_IN_RANGE)
         }
     }
 
-    @ValueSource(strings = [" ","a","b1","$"])
+    @ValueSource(strings = [" ", "a", "b1", "$", "UU", "D U", "10.5"])
     @ParameterizedTest
-    fun `문자나 공백 입력 예외 테스트`(input:String) {
+    fun `문자나 공백 소수 입력 예외 테스트`(input: String) {
         camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest {
             runException(input)
             assertThat(output()).contains(ERROR_NOT_NUMBERS)
