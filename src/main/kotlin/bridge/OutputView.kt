b73b74f53aa458be4d41aab1bf62d31e8e637e7b
jjw0071 <jjw0071@naver.com>
2022-11-22 17:51:45 +0900

    feat : 이동할 때마다 형식에 맞게 다리 출력하는 함수

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -9,7 +9,60 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(bridge : List<String>, location : Int, moveCommand : String) {
+        println(printUp(bridge,location,moveCommand))
+        println(printDown(bridge,location,moveCommand))
+    }
+
+    private fun printDown(bridge: List<String>, location: Int, moveCommand: String): String {
+        var print = "[ "
+        for(i: Int in 0..location-1){
+            print += checkDown(bridge,i)
+            print +=" | "
+        }
+        if(moveCommand == "D") print += checkCanGoDown(bridge, location, moveCommand)
+        else print += "  ]"
+        return print
+    }
+
+    private fun checkCanGoDown(bridge: List<String>, location: Int, moveCommand: String): String {
+        return if(bridge[location] == moveCommand)
+            "O ]"
+        else
+            "X ]"
+    }
+
+    private fun checkDown(bridge: List<String>, i: Int): String {
+        return if(bridge[i] == "D")
+            "O"
+        else
+            " "
+    }
+
+
+    private fun printUp(bridge: List<String>, location: Int, moveCommand : String) : String {
+        var print = "[ "
+        for(i: Int in 0..location-1){
+           print += checkUp(bridge,i)
+           print +=" | "
+        }
+        if(moveCommand == "U") print += checkCanGoUp(bridge, location, moveCommand)
+        else print += "  ]"
+        return print
+    }
+    private fun checkCanGoUp(bridge: List<String>, location : Int, moveCommand : String): String {
+        return if(bridge[location] == moveCommand)
+            "O ]"
+        else
+            "X ]"
+    }
+
+    private fun checkUp(bridge: List<String>, i: Int) : String {
+        return if(bridge[i] == "U")
+            "O"
+        else
+            " "
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
