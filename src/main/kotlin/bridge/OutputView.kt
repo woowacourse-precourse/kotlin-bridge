fb7982d81c303183e7665e30d63952ebcad9ebda
Hyeongseon Jeon <hsun5163@gmail.com>
2022-11-22 18:59:32 +0900

    feat : printResult 함수 구현
    parameter 형태에 따른 오버로딩 처리

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -52,7 +52,18 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+    fun printResult(bool: Boolean, count: Int) {
+        println()
+        when(bool){
+            true -> println("게임 성공 여부: 성공")
+            false -> println("게임 성공 여부: 실패")
+        }
+        println("총 시도한 횟수: $count")
+    }
+    fun printResult(){
+        println("\n최종 게임 결과")
+        printMap()
+    }
 
     /** <출력> 다리 건너기 게임을 시작합니다. */
     fun printGameStart() {
