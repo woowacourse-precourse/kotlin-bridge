e41cf79e1f7d7033af1279652a799351c633f36c
Sujin Kim <sujinkim919@gmail.com>
2022-11-22 16:22:31 +0900

    refactor(BridgeGame): bridge 변수 전역 변수로 수정
    - 거의 모든 함수에서 사용되고 있기 때문에 private 지역 변수로 꺼냄

--- src/main/kotlin/bridge/domain/BridgeGame.kt
+++ src/main/kotlin/bridge/domain/BridgeGame.kt
@@ -7,15 +7,17 @@ class BridgeGame(
     private val gameManager: BridgeGameManager,
     private val bridgeMaker: BridgeMaker
 ) {
+    private lateinit var bridge: List<String>
+
     fun runGame() {
         gameManager.startGame()
         val bridgeSize = gameManager.getBridgeSize()
-        val bridge = bridgeMaker.makeBridge(bridgeSize)
+        bridge = bridgeMaker.makeBridge(bridgeSize)
 
-        move(bridge)
+        move()
     }
 
-    fun move(bridge: List<String>) {
+    fun move() {
         val path = mutableListOf<String>()
 
         for (i in bridge.indices) {
@@ -25,7 +27,7 @@ class BridgeGame(
             gameManager.printMap(path, bridge)
 
             if(movement != bridge[i]) {
-                retry(bridge)
+                retry()
                 break
             }
         }
@@ -33,12 +35,12 @@ class BridgeGame(
         // TODO: 다리 건너기 성공
     }
 
-    fun retry(bridge: List<String>) {
+    fun retry() {
         val command = gameManager.getRestartCommand()
 
         when (command) {
-            GAME_RESTART -> move(bridge)
-        //    GAME_END ->
+            GAME_RESTART -> move()
+        //    GAME_END -> // TODO: 다리 건너기 실패
         }
     }
 }
