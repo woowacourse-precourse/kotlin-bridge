94d8e02b0e8ae9661b1fc75c7f603cb1c7eaad31
윤동현 <donghyunyoon81@gmail.com>
2022-11-22 19:55:48 +0900

    feat<BridgeResource> : print 출력 내용을 저장하는 BridgeOutputText enum class 구현
    어떻게 : 각 상수에 게임의 진행과 안내를 도와줄 출력 내용을 저장합니다.
    왜 :     enum class로 상수 선언하면 코드 가독성 이점과 타입 안전성 보장을 위해서

--- src/main/kotlin/bridge/domain/data/BridgeResource.kt
+++ src/main/kotlin/bridge/domain/data/BridgeResource.kt
@@ -16,3 +16,13 @@ enum class BridgeNumber(val number: Int) {
     MAX_BRIDGE_SIZE(20),
     MIN_BRIDGE_SIZE(3)
 }
+
+enum class BridgeOutputText(val text: String) {
+    SUCCESS_TEXT("게임 성공 여부: 성공"),
+    FAIL_TEXT("게임 성공 여부: 실패"),
+    TOTAL_CHALLENGE("총 시도한 횟수: "),
+    FINAL_RESULT("최종 게임 결과"),
+    RETRY_TEXT("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    CHOOSE_ROWS("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    BRIDGE_SIZE("다리의 길이를 입력해주세요.")
+}
