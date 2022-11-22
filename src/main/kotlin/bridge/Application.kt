0a4f5334e7de4597ffe2b72b2929ba7ba0eeab77
dev-worthwhile <dev.lboram@gmail.com>
2022-11-22 17:18:54 +0900

    feat<Application>: print bridge status each time user moves

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -3,3 +3,10 @@ package bridge
 fun main() {
     // TODO: 프로그램 구현
 }
+fun showResult(moves: List<String>, answers: List<String>, attempt:Int) : Boolean{
+    OutputView().printMap(moves,answers)
+    if(moves[attempt] != answers[attempt]){ //정답이 아닌 경우
+        return true
+    }
+    return false
+}
