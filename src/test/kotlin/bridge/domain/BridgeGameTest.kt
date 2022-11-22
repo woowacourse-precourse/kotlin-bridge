2816873dbfc9e93e1421030902445a14287f4bc0
jiingom <vvankatam0@gmail.com>
2022-11-22 17:04:14 +0900

    test: 칸을 이동할 때 건널 수 있는지 여부를 반환해주는 테스트 추가

--- src/test/kotlin/bridge/domain/BridgeGameTest.kt
+++ src/test/kotlin/bridge/domain/BridgeGameTest.kt
@@ -1,5 +1,18 @@
 package bridge.domain
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
 
-internal class BridgeGameTest
+internal class BridgeGameTest {
+    private val bridgeGame = BridgeGame(Player(Bridge(listOf<String>("U", "D", "U"))))
+
+    @Test
+    fun `이동하는 칸이 건널 수 있는 칸이면 true를 반환한다`() {
+        assertThat(bridgeGame.move("U")).isTrue
+    }
+
+    @Test
+    fun `이동하는 칸이 건널 수 없는 칸이면 false를 반환한다`() {
+        assertThat(bridgeGame.move("D")).isFalse
+    }
+}
