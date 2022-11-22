4ac1c4645f3d033a9053ff5639f55bbb4906f4a7
dlwgnud <wngud980411@naver.com>
2022-11-22 15:09:47 +0900

    feat(retry): 재시작을 할 때 사용하는 함수

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -5,9 +5,12 @@ package bridge
  */
 // InputView, OutputView 를 사용하지 않는다.
 
-class BridgeGame(bridgeLength: Int) {
-    private val upBridge = MutableList<String>(bridgeLength){" "}
-    private val downBridge = MutableList<String>(bridgeLength){" "}
+class BridgeGame(private val bridgeLength: Int) {
+    private val upBridge = MutableList<String>(bridgeLength) { " " }
+    private val downBridge = MutableList<String>(bridgeLength) { " " }
+    private var countRetry = 1
+    private var progressStatus = true
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      *
@@ -41,5 +44,12 @@ class BridgeGame(bridgeLength: Int) {
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry() {}
+    fun retry() {
+        countRetry++
+        for (i in 0 until bridgeLength) {
+            upBridge[i] = " "
+            downBridge[i] = " "
+        }
+        progressStatus = true
+    }
 }
