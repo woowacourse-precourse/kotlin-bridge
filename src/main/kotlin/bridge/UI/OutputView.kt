78b5c114614eec665eca26ace66082602de4c607
Yuhee Kim <dbgml4509@naver.com>
2022-11-22 19:25:55 +0900

    feat : 최종 결과 출력하는 함수 생성

--- src/main/kotlin/bridge/UI/OutputView.kt
+++ src/main/kotlin/bridge/UI/OutputView.kt
@@ -1,5 +1,8 @@
 package bridge.UI
 
+import bridge.Constants.Companion.DOWN
+import bridge.Constants.Companion.UP
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -20,12 +23,12 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-//    fun printResult(upSelectedBridge : List<String>, downSelectedBridge : List<String>) {
-//        println("최종 게임 결과")
-//        printMap(upSelectedBridge, downSelectedBridge)
-//        print("게임 성공 여부: ")
-//        print("총 시도한 횟수: ")
-//    }
+    fun printResult(selectedBridge : Map<String, List<String>>, resultOfGame : String, attemptedNumber : Int) {
+        println("최종 게임 결과")
+        selectedBridge[UP]?.let { selectedBridge[DOWN]?.let { it1 -> printMap(it, it1) } }
+        println("게임 성공 여부: $resultOfGame")
+        println("총 시도한 횟수: $attemptedNumber")
+    }
 
     fun printStartLog(){
         println(START_LOG)
