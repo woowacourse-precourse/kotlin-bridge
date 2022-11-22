1be6d204c7002818eb27c8d7b1ba2c6b4fe50f70
dlwgnud <wngud980411@naver.com>
2022-11-22 15:13:29 +0900

    feat: 게임 재시작/종료 여부를 입력 받을 떄 유효성 검사

--- src/main/kotlin/bridge/InputCheck.kt
+++ src/main/kotlin/bridge/InputCheck.kt
@@ -4,6 +4,7 @@ enum class Error(val message: String) {
     NOT_NUMBER("[ERROR] 숫자를 입력해야 합니다."),
     NOT_RANGE("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
     NOT_U_OR_D("[ERROR] U(위 칸)와 D(아래 칸) 중 하나의 문자만 입력할 수 있습니다."),
+    NOT_R_OR_Q("[ERROR] R(재시작)와 Q(종료) 중 하나의 문자만 입력할 수 있습니다."),
 }
 
 class InputCheck {
@@ -16,8 +17,17 @@ class InputCheck {
         require(isUOrD(input)) { Error.NOT_U_OR_D.message }
     }
 
+    fun checkReGameCommand(input: String) {
+        require(isROrQ(input)) { Error.NOT_R_OR_Q.message }
+    }
+
+    private fun isROrQ(input: String): Boolean {
+        if (input != "R" && input != "Q") return false
+        return true
+    }
+
     private fun isUOrD(input: String): Boolean {
-        if(input != "U" && input != "D") return false
+        if (input != "U" && input != "D") return false
         return true
     }
 
