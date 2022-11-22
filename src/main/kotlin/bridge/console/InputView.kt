f27373e4c70932017c9f5bc3d7e0aae70bbc4cb2
joh9911 <joh9911@naver.com>
2022-11-22 15:23:02 +0900

    feat(InputView.kt): 잘못된 값 입력 시 다시 입력받는 기능 구현

--- src/main/kotlin/bridge/console/InputView.kt
+++ src/main/kotlin/bridge/console/InputView.kt
@@ -1,8 +1,10 @@
 package bridge.console
+import bridge.utils.Constants
 import bridge.utils.ExceptionHandler.validateInRange
 import bridge.utils.ExceptionHandler.validateIntType
 import bridge.utils.ExceptionHandler.validateMoveLaneSymbol
 import bridge.utils.ExceptionHandler.validateRetrialSymbol
+import camp.nextstep.edu.missionutils.Console
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -12,18 +14,34 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        val inputBridgeSize = readLine()
-        validateIntType(inputBridgeSize!!)
+        return try{
+            inputBridgeSize()
+        }catch (e: IllegalArgumentException){
+            readBridgeSize()
+        }
+    }
+
+    private fun inputBridgeSize(): Int{
+        val inputBridgeSize = Console.readLine()
+        validateIntType(inputBridgeSize)
         validateInRange(inputBridgeSize.toInt())
-        return inputBridgeSize!!.toInt()
+        return inputBridgeSize.toInt()
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        val inputMoveLaneSymbol = readLine()
-        validateMoveLaneSymbol(inputMoveLaneSymbol!!)
+        return try{
+            inputMoveLaneSymbol()
+        }catch (e: IllegalArgumentException){
+            readMoving()
+        }
+    }
+
+    private fun inputMoveLaneSymbol(): String{
+        val inputMoveLaneSymbol = Console.readLine()
+        validateMoveLaneSymbol(inputMoveLaneSymbol)
         return inputMoveLaneSymbol
     }
 
@@ -31,8 +49,16 @@ class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        val inputRetrialSymbol = readLine()
-        validateRetrialSymbol(inputRetrialSymbol!!)
+        return try{
+            inputGameCommandSymbol()
+        }catch (e: IllegalArgumentException){
+            readGameCommand()
+        }
+    }
+
+    private fun inputGameCommandSymbol(): String{
+        val inputRetrialSymbol = Console.readLine()
+        validateRetrialSymbol(inputRetrialSymbol)
         return inputRetrialSymbol
     }
 }
