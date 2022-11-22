58aef929cf4be004f022d7c9b344d0fe322dcc23
dohun_ <silver2679@gmail.com>
2022-11-22 16:52:40 +0900

    Test(BridgeMakerTest): BridgeMaker에 대한 테스트 코드 추가

--- src/test/kotlin/bridge/BridgeMakerTest.kt
+++ src/test/kotlin/bridge/BridgeMakerTest.kt
@@ -1,4 +1,23 @@
 package bridge
 
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
 class BridgeMakerTest {
+
+    @Test
+    fun `다리를 생성한다`() {
+        val numberGenerator: BridgeNumberGenerator = TestNumberGenerator(listOf(1, 0, 0))
+        val bridgeMaker = BridgeMaker(numberGenerator)
+        val bridge: List<String> = bridgeMaker.makeBridge(3)
+        Assertions.assertThat(bridge.toString()).isEqualTo("[U, D, D]")
+    }
+
+    class TestNumberGenerator(numbers: List<Int>) : BridgeNumberGenerator {
+        private val numbers: MutableList<Int> = numbers.toMutableList()
+
+        override fun generate(): Int {
+            return numbers.removeAt(0)
+        }
+    }
 }
