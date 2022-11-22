508b05166a30a151691aee47c3655ca9684760b3
KimDongHyun0907 <ehdgus8397@naver.com>
2022-11-22 23:40:13 +0900

    [make] BridgeGame fun retry

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -11,7 +11,19 @@ class BridgeGame(bridgeMaker: BridgeMaker, size:Int) {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     val moveBlock = mutableListOf<String>()
+    var trycnt=1
     fun move(selectBlock: String) {
         moveBlock.add(selectBlock)
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     *
+     *
+     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    fun retry() {
+        moveBlock.clear()
+        trycnt++
     }
 }
