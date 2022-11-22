c9b5691029da6dba27ae75f85c047c7246a79251
윤동현 <donghyunyoon81@gmail.com>
2022-11-22 21:07:01 +0900

    refactor<BridgeActivity> : bridgeGame에 게임시작 println 수정
    어떻게 : BridgeOutputText enum class에 다리 건너기 게임 시작 print 텍스트 추가
    	 bridgeGame println 내용에 추가한 텍스트 대입

--- src/main/kotlin/bridge/BridgeActivity.kt
+++ src/main/kotlin/bridge/BridgeActivity.kt
@@ -1,12 +1,13 @@
 package bridge
 
+import bridge.domain.data.BridgeOutputText
 import bridge.ui.InputView
 import bridge.ui.OutputView
 
 class BridgeActivity {
 
     fun bridgeGame() {
-        println("다리 건너기 게임을 시작합니다.")
+        println(BridgeOutputText.BRIDGE_GAME_START.text)
         val bridge = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(InputView().readBridgeSize())
         val bridgeGame = OutputView().printMap(bridge)
         OutputView().printResult(bridgeGame.first.upRowsResult,bridgeGame.first.downRowsResult,bridgeGame.second)
--- src/main/kotlin/bridge/domain/data/BridgeResource.kt
+++ src/main/kotlin/bridge/domain/data/BridgeResource.kt
@@ -24,7 +24,8 @@ enum class BridgeOutputText(val text: String) {
     FINAL_RESULT("최종 게임 결과"),
     RETRY_TEXT("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     CHOOSE_ROWS("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-    BRIDGE_SIZE("다리의 길이를 입력해주세요.")
+    BRIDGE_SIZE("다리의 길이를 입력해주세요."),
+    BRIDGE_GAME_START("다리 건너기 게임을 시작합니다.")
 }
 
 enum class BridgeErrorText(val text: String) {
