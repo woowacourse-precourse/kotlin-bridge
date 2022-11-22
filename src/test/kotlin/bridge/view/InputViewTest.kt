d4245838388537806779303b7c72b4cc0d80e7db
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-22 15:23:27 +0900

    test(view): InputViewTest 예외 테스트 검증 로직 수정

--- src/test/kotlin/bridge/view/InputViewTest.kt
+++ src/test/kotlin/bridge/view/InputViewTest.kt
@@ -1,9 +1,7 @@
 package bridge.view
 
-import org.junit.jupiter.api.BeforeEach
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -23,11 +21,10 @@ internal class InputViewTest : NsTest() {
     @ValueSource(strings = ["3a", ".5", "1,3", "1.2"])
     fun `다리길이입력_문자포함_에러`(input: String) {
         assertSimpleTest {
-            val thrown = assertThrows<IllegalArgumentException> {
+            assertThrows<IllegalArgumentException> {
                 runException(input)
                 InputView.readBridgeSize()
             }
-            assertThat(thrown.message).contains(ERROR_MESSAGE)
         }
     }
 
@@ -35,11 +32,10 @@ internal class InputViewTest : NsTest() {
     @ValueSource(strings = ["-1", "0", "2", "21", "100"])
     fun `다리길이입력_벗어난범위_에러`(input: String) {
         assertSimpleTest {
-            val thrown = assertThrows<IllegalArgumentException> {
+            assertThrows<IllegalArgumentException> {
                 runException(input)
                 InputView.readBridgeSize()
             }
-            assertThat(thrown.message).contains(ERROR_MESSAGE)
         }
     }
 
@@ -56,11 +52,10 @@ internal class InputViewTest : NsTest() {
     @ValueSource(strings = ["u", "d", "R", "Q", "1", "UD"])
     fun `이동할칸입력_notUorD_에러`(input: String) {
         assertSimpleTest {
-            val thrown = assertThrows<IllegalArgumentException> {
+            assertThrows<IllegalArgumentException> {
                 runException(input)
                 InputView.readMoving()
             }
-            assertThat(thrown.message).contains(ERROR_MESSAGE)
         }
     }
 
@@ -77,17 +72,12 @@ internal class InputViewTest : NsTest() {
     @ValueSource(strings = ["U", "D", "r", "q", "RQ"])
     fun `게임재시작종료입력_notRorQ_에러`(input: String) {
         assertSimpleTest {
-            val thrown = assertThrows<IllegalArgumentException> {
+            assertThrows<IllegalArgumentException> {
                 runException(input)
                 InputView.askRetryGame()
             }
-            assertThat(thrown.message).contains(ERROR_MESSAGE)
         }
     }
 
     override fun runMain() {}
-
-    companion object {
-        private const val ERROR_MESSAGE = "[ERROR]"
-    }
 }
