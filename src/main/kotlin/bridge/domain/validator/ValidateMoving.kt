c49c8205a0be1b5fbb75611a5160bd2f7656015b
wonseok <dnjstjr245@gmail.com>
2022-11-22 19:22:35 +0900

    [feat] 플레이어가 이동할 칸 입력에 대한 예외처리를 하는 ValidateMoving 클래스 구현

---
+++ src/main/kotlin/bridge/domain/validator/ValidateMoving.kt
@@ -0,0 +1,34 @@
+package bridge.domain.validator
+
+import bridge.data.InputError
+import bridge.data.InputDataSource.movingCommands
+import bridge.ui.Views
+import java.lang.IllegalArgumentException
+
+class ValidateMoving(
+    private val views: Views = Views()
+) {
+    operator fun invoke() {
+        do {
+            try {
+                views.inputView.readMoving().apply {
+                    require(isUpOrDown())
+                    movingCommands.add(this)
+                    views.outputView.printMap()
+                }
+            } catch (exception: IllegalArgumentException) {
+                views.outputView.printError(InputError.MovingInputError(UP_OR_DOWN_EXCEPTION))
+            }
+        } while (movingCommands.size == 0)
+    }
+
+    private fun String.isUpOrDown(): Boolean {
+        return (this == UP_CHARACTER) || (this == DOWN_CHARACTER)
+    }
+
+    companion object {
+        const val UP_CHARACTER = "U"
+        const val DOWN_CHARACTER = "D"
+        const val UP_OR_DOWN_EXCEPTION = "이동할 칸은 U(위 칸)와 D(아래 칸) 중 하나의 문자만 입력해야 합니다."
+    }
+}
