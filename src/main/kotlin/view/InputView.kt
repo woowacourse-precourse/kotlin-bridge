19cd99571fbdaf19e1ef08003752d08fce3ca8f0
username <ieejo716@naver.com>
2022-11-22 19:15:08 +0900

    [REFACTOR] : 다리 길이 입력 예외 발생시 계속 입력 가능

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -2,6 +2,7 @@ package view
 
 import camp.nextstep.edu.missionutils.Console
 import util.InputException
+import util.InputException.Companion.RANGE_ERROR
 
 
 /**
@@ -14,15 +15,42 @@ class InputView {
     private val inputException = InputException()
 
     fun readBridgeSize(): Int {
-        val size = Console.readLine()
-        inputException.checkBridgeLength(size)
-        return size.toInt()
+        while (true) {
+            val size = Console.readLine()
+            try {
+                inputException.checkRangeException(size)
+                return size.toInt()
+            } catch (e: IllegalArgumentException) {
+                inputException.printException(RANGE_ERROR)
+            }
+        }
     }
+
+    private fun getMoveCommand() {
+        val command = Console.readLine()
+
+    }
+
+//    private fun getBridgeSize(): String {
+//        val size = Console.readLine()
+//        val inputCheck = inputException.checkRangeException(size)
+//        if (!inputCheck)
+//            return " "
+//        return size
+//    }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
+//        while (true) {
+//            val command =
+//            try {
+//                require(inputException.checkMoveCommand(i))
+//            } catch (e: IllegalArgumentException) {
+//
+//            }
+//        }
         val command = Console.readLine()
         inputException.checkMoveCommand(command)
         return command
