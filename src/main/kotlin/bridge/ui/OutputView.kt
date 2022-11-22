9c9d6db63b86f8e3fd235e5565f065af16131257
윤동현 <donghyunyoon81@gmail.com>
2022-11-22 15:04:11 +0900

    feat<OutputView> : 이동 결과를 출력하고 MovingResultData를 반환하는 getMovingResult 기능 구현
    어떻게 : 지나갈 수 있는 다리 목록인 bridge를 매개변수로 받습니다.
    	upRowsResult와 downRowsResult를 공백으로 선언하고 mapIndexed를 사용해 bridge의 index와bridgeColumn을 받습니다.
    	userInputRow에 사용자의 입력값을 받는 InputView().readMoving을 대입합니다.
    	upRowsResult와 downRowsResult에 사용자 텍스트를 받아 bridge의 행의 값과 비교하는 getUpRowsResult,getDownRowdResult를
    	복합 대입 연산자 +=를 사용하여 사용자가 텍스트를 입력 할 때마다 추가로 결과를 받습니다.
    	printRowsResult 기능으로 행들의 결과를 출력합니다.
    	isNotPassableInRows로 행들의 결과 중 X가 생길 시에 재시도하거나 게임을 종류 할 수 있는 bridgeGameRetry를 반환합니다.
    왜 :	 사용자가 다리를 지날 때마다 결과를 출력하고 실패 시 재시도 할 수 있는 기능을 만들기 위해서

--- src/main/kotlin/bridge/ui/OutputView.kt
+++ src/main/kotlin/bridge/ui/OutputView.kt
@@ -14,7 +14,18 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printMap() {}
-
+    private fun getMovingResult(bridge: List<String>): MovingResultData {
+        var upRowsResult = ""
+        var downRowsResult = ""
+        bridge.mapIndexed { index, bridgeColumn ->
+            val userInputRow = InputView().readMoving()
+            upRowsResult += getUpRowsResult(index, bridgeColumn, userInputRow)
+            downRowsResult += getDownRowsResult(index, bridgeColumn, userInputRow)
+            printRowsResult(upRowsResult, downRowsResult)
+            if (isNotPassableInRows(upRowsResult, downRowsResult)) return bridgeGameRetry(upRowsResult, downRowsResult)
+        }
+        return MovingResultData(upRowsResult, downRowsResult, false)
+    }
 
     private fun getUpRowsResult(bridgeIndex: Int, bridgeRows: String, userInputRow: String) =
         "${getLineOrBlank(bridgeIndex)}${BridgeGame().move(bridgeRows, userInputRow).first}"
