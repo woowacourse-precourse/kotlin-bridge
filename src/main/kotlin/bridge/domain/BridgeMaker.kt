3b18085a25d0a15565b818b4a5e9ed212e585f71
no1msh <no1msh1217@gmail.com>
2022-11-22 18:59:40 +0900

    refactor: BridgeMaker 클래스 하드코딩된 인자들 상수화

--- src/main/kotlin/bridge/domain/BridgeMaker.kt
+++ src/main/kotlin/bridge/domain/BridgeMaker.kt
@@ -4,23 +4,33 @@ import bridge.BridgeNumberGenerator
 
 class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
     fun makeBridge(size: Int): List<String> {
-        val bridge = mutableListOf<String>()
-        for (index in 1..size) {
+        val answerBridge = mutableListOf<String>()
+        for (index in BRIDGE_START_INDEX until size) {
             val bridgeNumber = bridgeNumberGenerator.generate()
-            if (bridgeNumber == 0) bridge.add("D")
-            if (bridgeNumber == 1) bridge.add("U")
+            if (bridgeNumber == DOWN_BRIDGE_NUMBER) answerBridge.add(ANSWER_DOWN)
+            if (bridgeNumber == UP_BRIDGE_NUMBER) answerBridge.add(ANSWER_UP)
         }
-        return bridge.toList()
+        return answerBridge.toList()
     }
 
     fun makeCompletedBridge(answerBridge: List<String>, isUpBridge: Boolean): MutableList<String> {
         val bridge = mutableListOf<String>()
         for (answer in answerBridge) {
             when (answer) {
-                "U" -> if (isUpBridge) bridge.add("O") else bridge.add(" ")
-                "D" -> if (isUpBridge) bridge.add(" ") else bridge.add("O")
+                ANSWER_UP -> if (isUpBridge) bridge.add(CORRECT_ANSWER) else bridge.add(SPACE)
+                ANSWER_DOWN -> if (isUpBridge) bridge.add(SPACE) else bridge.add(CORRECT_ANSWER)
             }
         }
         return bridge
+    }
+
+    companion object{
+        private const val BRIDGE_START_INDEX = 0
+        private const val DOWN_BRIDGE_NUMBER = 0
+        private const val UP_BRIDGE_NUMBER = 1
+        private const val ANSWER_DOWN = "D"
+        private const val ANSWER_UP = "U"
+        private const val CORRECT_ANSWER = "O"
+        private const val SPACE = " "
     }
 }
