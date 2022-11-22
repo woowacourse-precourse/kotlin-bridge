d2699eaee1f022926283cf5a77e2a55d5ec58035
2taezeat <xorudfl772@naver.com>
2022-11-22 10:36:06 +0900

    test(BridgeMakerTest): TestNumberGenerator 중복 코드 제거

--- src/test/kotlin/bridge/BridgeMakerTest.kt
+++ src/test/kotlin/bridge/BridgeMakerTest.kt
@@ -7,20 +7,9 @@ class BridgeMakerTest {
 
     @Test
     fun `유효한 다리 생성 테스트`() {
-        val bridgeMaker = BridgeMaker(TestNumberGenerator(listOf(1,1,1,1,0)))
+        val bridgeMaker = BridgeMaker(ApplicationTest.TestNumberGenerator(listOf(1, 1, 1, 1, 0)))
         val bridge: List<String> = bridgeMaker.makeBridge(5)
         Assertions.assertThat(bridge).containsExactly("U", "U", "U", "U", "D")
-    }
-
-
-
-
-    class TestNumberGenerator(numbers: List<Int>) : BridgeNumberGenerator {
-        private val numbers: MutableList<Int> = numbers.toMutableList()
-
-        override fun generate(): Int {
-            return numbers.removeAt(0)
-        }
     }
 
 }
