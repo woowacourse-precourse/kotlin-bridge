d8fd28edf26a5ca11c447c3ef1f88c776f93954b
bmlsj <gaga2353@gmail.com>
2022-11-22 22:10:30 +0900

    feat(Application) : 게임 전체 로직 구현

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,51 @@
 package bridge
 
 fun main() {
-    // TODO: 프로그램 구현
+
+    val inputView = InputView()
+    val outputView = OutputView()
+    val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
+    val game: BridgeGame = gameStart(inputView, bridgeMaker)
+
+    while (!game.quit()) {
+        val moveCheck = selectDirection(inputView, outputView, game)
+
+        if (!moveCheck && !retry(inputView, game)) {
+            break
+        }
+    }
+
+    outputView.printResult(game.route, game.bridge, game.tryNum)
+}
+
+fun gameStart(inputView: InputView, bridgeMaker: BridgeMaker): BridgeGame {
+    println("다리 건너기 게임을 시작합니다.")
+    println("다리의 길이를 입력해주세요.")
+
+    val bridgeSize = inputView.readBridgeSize()
+    println()
+    return BridgeGame(bridgeMaker.makeBridge(bridgeSize))
+}
+
+
+fun selectDirection(inputView: InputView, outputView: OutputView, game: BridgeGame): Boolean {
+    println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
+
+    val direction = inputView.readMoving()
+    val moveCheck = game.move(direction)
+
+    outputView.printMap(game.route, game.bridge)
+    return moveCheck
+}
+
+fun retry(inputView: InputView, game: BridgeGame): Boolean {
+    println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+    val retry = inputView.readGameCommand()
+    println(retry)
+
+    if (retry == "R") {
+        game.retry()
+        return true
+    }
+    return false
 }
