77ef10d73ca70f5ac53d0c8ebef2d4047bfbb8dd
Lim-Joosung <kinglim1224@naver.com>
2022-11-22 15:25:06 +0900

    feat: OutputView.kt printResult() 구현

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -53,5 +53,12 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+    fun printResult(case: MutableList<String>,success:String,count:Int) {
+        println()
+        println("최종 게임 결과")
+        printMap(case)
+        println()
+        println("게임 성공 여부: $success")
+        print("총 시도한 횟수: $count")
+    }
 }
