f83e0278ae12675e2a943029dd37974b6ef065ec
Yuhee Kim <dbgml4509@naver.com>
2022-11-22 16:20:16 +0900

    feat : 다리 건너기 시작 문구 출력하는 함수 생성

--- src/main/kotlin/bridge/UI/OutputView.kt
+++ src/main/kotlin/bridge/UI/OutputView.kt
@@ -20,8 +20,15 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {
+//    fun printResult(upSelectedBridge : List<String>, downSelectedBridge : List<String>) {
+//        println("최종 게임 결과")
+//        printMap(upSelectedBridge, downSelectedBridge)
+//        print("게임 성공 여부: ")
+//        print("총 시도한 횟수: ")
+//    }
 
+    fun printStartLog(){
+        println("다리 건너기 게임을 시작합니다.\n")
     }
 
     companion object{
