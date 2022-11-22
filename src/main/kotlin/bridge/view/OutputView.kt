ff1af8f543f82a210a2c47dbb9331a8190a2304a
brian <briandr@naver.com>
2022-11-22 21:57:55 +0900

    feat(OutputView): 주석 제거

--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -7,9 +7,6 @@ private const val BRIDGE_START = "[ "
 private const val BRIDGE_END = " ]"
 private const val DIVISION_LINE = " | "
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 private const val GAME_START = "다리 건너기 게임을 시작합니다."
 private const val ENTER_BRIDGE_SIZE = "다리의 길이를 입력해주세요."
 private const val SELECT_UP_DOWN = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
@@ -39,11 +36,6 @@ class OutputView {
         println(SELECT_UP_DOWN)
     }
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     fun printMap(upperBlocks: List<String>, downerBlocks: List<String>) {
         printBlocks(upperBlocks)
         printBlocks(downerBlocks)
@@ -59,11 +51,6 @@ class OutputView {
         println(ENTER_RETRY_END)
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     fun printResult(gameResult: GameResult) {
         printFinalResult()
         printMap(gameResult.upperBlocks, gameResult.downerBlocks)
