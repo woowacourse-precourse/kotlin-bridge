572e8927c3fa25289e44914633eeeaf2aa177201
Yuhee Kim <dbgml4509@naver.com>
2022-11-22 15:32:27 +0900

    fix : 출력 결과 수정

--- src/main/kotlin/bridge/UI/OutputView.kt
+++ src/main/kotlin/bridge/UI/OutputView.kt
@@ -10,8 +10,8 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printMap(upSelectedBridge : List<String>, downSelectedBridge : List<String>) {
-        println(upSelectedBridge.joinToString(prefix = "[", separator = "|", postfix = "]"))
-        println(downSelectedBridge.joinToString(prefix = "[", separator = "|", postfix = "]"))
+        println(upSelectedBridge.joinToString(prefix = "[ ", separator = " | ", postfix = " ]"))
+        println(downSelectedBridge.joinToString(prefix = "[ ", separator = " | ", postfix = " ]"))
         println()
     }
 
