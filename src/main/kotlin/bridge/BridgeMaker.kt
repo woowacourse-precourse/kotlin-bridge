804015ae44f271ed9d1281e21d6094e01727da18
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-22 22:55:33 +0900

    style(controller): BridgeMaker의 상수명 변경
    RANDOM_LOWER_INCLUSIVE -> LOWER_INCLUSIVE
    RANDOM_UPPER_INCLUSIVE -> UPPER_INCLUSIVE

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -8,8 +8,8 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
 
     private fun createBridgeElement(bridgeNumber: Int): String {
         return when (bridgeNumber) {
-            RANDOM_LOWER_INCLUSIVE -> "D"
-            RANDOM_UPPER_INCLUSIVE -> "U"
+            LOWER_INCLUSIVE -> "D"
+            UPPER_INCLUSIVE -> "U"
             else -> {
                 throw IllegalArgumentException("다리의 칸을 생성하려면 0또는 1만 가능합니다.")
             }
@@ -17,8 +17,8 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
     }
 
     companion object {
-        private const val RANDOM_LOWER_INCLUSIVE = 0
-        private const val RANDOM_UPPER_INCLUSIVE = 1
+        private const val LOWER_INCLUSIVE = 0
+        private const val UPPER_INCLUSIVE = 1
         private const val UP = "U"
         private const val DOWN = "D"
     }
