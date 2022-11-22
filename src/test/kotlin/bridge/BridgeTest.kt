bb001e1c23443e306b6c8780f42dfdf033884c2d
dlgocks1 <cksgodl1@naver.com>
2022-11-22 14:13:25 +0900

    test(BridgeTest.kt) : move() 메서드에 대한 단위 테스트 추가

--- src/test/kotlin/bridge/BridgeTest.kt
+++ src/test/kotlin/bridge/BridgeTest.kt
@@ -1,7 +1,11 @@
 package bridge
 
 import bridge.model.Bridge
+import bridge.model.BridgeStatus
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -15,6 +19,20 @@ class BridgeTest {
     fun constructorTest(bridge: String) {
         assertThrows<IllegalArgumentException> {
             Bridge(bridge.split(',').toList())
+        }
+    }
+
+    @Test
+    @DisplayName("다리를 이동할 때 올바른 올바른 결과 값을 반환하는지 테스트한다.")
+    fun moveTest() {
+        assertSimpleTest {
+            val bridge = Bridge(listOf("U", "D", "U"))
+            assertThat(bridge.move(0, "U"))
+                .isInstanceOf(BridgeStatus.SUCCESS::class.java)
+            assertThat(bridge.move(0, "D"))
+                .isInstanceOf(BridgeStatus.FAIL::class.java)
+            assertThat(bridge.move(1, "D"))
+                .isInstanceOf(BridgeStatus.SUCCESS::class.java)
         }
     }
 }
