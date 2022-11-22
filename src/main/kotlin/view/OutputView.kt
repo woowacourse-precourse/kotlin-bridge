1d02e3e3bb38745baa4ee40b1e7b85ad6fc5752a
lee989898 <wnsrud1352@gmail.com>
2022-11-22 14:18:20 +0900

    [feat](OutputView) 최종게임 결과 성공인지 실패인지 총시도한 횟수 출력 기능 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -23,7 +23,16 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+    fun printResult(ox: List<MutableList<String>>, count: Int, isSuccesss: String) {
+        var isSuccess = "성공"
+        if (isSuccesss == "FAIL") {
+            isSuccess = "실패"
+        }
+        println("최종 게임 결과")
+        printGameResult(ox)
+        println("게임 성공 여부: $isSuccess")
+        println("총 시도한 횟수: $count")
+    }
 
     private fun printGameResult(ox: List<MutableList<String>>) {
         for (i in 0..1) {
