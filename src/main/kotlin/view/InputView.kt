8e95891270ddb5c2a762db69c15237fc9ae656f3
lee989898 <wnsrud1352@gmail.com>
2022-11-22 15:44:54 +0900

    [refactor](InputView) 불필요한 주석 제거

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -3,13 +3,8 @@ package view
 import camp.nextstep.edu.missionutils.Console
 import java.lang.IllegalArgumentException
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 class InputView {
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+
     fun readBridgeSize(): String {
         println("다리의 길이를 입력해주세요.")
         val bridgeSize = Console.readLine()
@@ -17,17 +12,11 @@ class InputView {
         return bridgeSize
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     fun readMoving(): String {
         println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
         return Console.readLine()
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     fun readGameCommand(): String {
         println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
         return Console.readLine()
