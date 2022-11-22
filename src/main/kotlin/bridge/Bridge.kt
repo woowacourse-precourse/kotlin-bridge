530068760c7f0f8e4a18ebc98af82979be14d44c
Yuhee Kim <dbgml4509@naver.com>
2022-11-22 17:49:32 +0900

    feat : 다리 길이 입력 받는 출력 문구, 입력 기능 함수 생성

--- src/main/kotlin/bridge/Bridge.kt
+++ src/main/kotlin/bridge/Bridge.kt
@@ -1,7 +1,22 @@
 package bridge
 
+import bridge.UI.InputView
+import bridge.UI.OutputView
+
 class Bridge {
 
+    private val numberGenerator = BridgeRandomNumberGenerator()
+    private val bridgeMaker = BridgeMaker(numberGenerator)
 
+    fun makeBridge(){
+
+    }
+
+    fun getBridgeLength() : Int{
+        OutputView().printGetBridgeLengthLog()
+        val inputSize = InputView().readBridgeSize()
+        InputException().checkBridgeLenInputIsNumeric(inputSize)
+        return inputSize.toInt()
+    }
 
 }
