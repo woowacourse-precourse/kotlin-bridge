c0a17233f7ee00242266a6b6117096d2b05f6f82
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-22 14:02:15 +0900

    feat(Application): main 함수 작성

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,11 @@
 package bridge
 
+import bridge.controller.BridgeGameController
+
 fun main() {
-    // TODO: 프로그램 구현
+    val bridgeGameController = BridgeGameController()
+    try {
+        bridgeGameController.play()
+    } catch (e: IllegalArgumentException) {
+    }
 }
