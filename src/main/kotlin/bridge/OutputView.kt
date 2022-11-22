5ffeaad11879ca1f5b07ca2acc398340f05437d3
[alsswl] <[alswl4223@naver.com]>
2022-11-22 15:14:45 +0900

    refector(OutputView) : 함수 줄 줄이기

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -9,43 +9,37 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap(correct : Boolean, bridge : List<String>,number : Int) {
-        var bridgePrintU = mutableListOf<String>()
-        var bridgePrintD = mutableListOf<String>()
-        bridgePrintU.add("[")
-        bridgePrintD.add("[")
-        if(correct){
-            for(i in 0 .. number){
-                if(bridge[i] == "U"){
-                    bridgePrintU.add(" O ")
-                    bridgePrintD.add("   ")
-                } else{
-                    bridgePrintU.add("   ")
-                    bridgePrintD.add(" O ")
-                }
-                if(i != number) {
-                    bridgePrintD.add("|")
-                    bridgePrintU.add("|")
-                }
-            }
-            bridgePrintU.add("]")
-            bridgePrintD.add("]")
-            var bridgeUS = bridgePrintU.joinToString("")
-            var bridgeDS = bridgePrintD.joinToString("")
 
-            println(bridgeUS)
-            println(bridgeDS)
-        }else{
+
+    fun printMap(bridge : List<String>,number : Int) {
+        var bridgePrintU = mutableListOf<String>("[")
+        var bridgePrintD = mutableListOf<String>("[")
+        for(i in 0 .. number){
+            if(bridge[i] == "U"){
+                bridgePrintU.add(" O ")
+                bridgePrintD.add("   ")
+            } else{
+                bridgePrintU.add("   ")
+                bridgePrintD.add(" O ")
+            }
+            if(i == number) break
+            bridgePrintD.add("|")
+            bridgePrintU.add("|")
+        }
+        stringPrint(bridgePrintU,bridgePrintD)
+    }
+
+    fun printMapFalse(bridge : List<String>,number : Int) {
+        var bridgePrintU = mutableListOf<String>("[")
+        var bridgePrintD = mutableListOf<String>("[")
             for(i in 0 .. (number-1)){
                 if(bridge[i] == "U"){
-                    bridgePrintU.add(" O ")
-                    bridgePrintD.add("   ")
+                    bridgePrintU.add(" O |")
+                    bridgePrintD.add("   |")
                 } else{
-                    bridgePrintU.add("   ")
-                    bridgePrintD.add(" O ")
+                    bridgePrintU.add("   |")
+                    bridgePrintD.add(" O |")
                 }
-                bridgePrintD.add("|")
-                bridgePrintU.add("|")
             }
             if(bridge[number-1] == "U"){
                 bridgePrintU.add("   ")
@@ -54,32 +48,39 @@ class OutputView {
                 bridgePrintU.add(" X ")
                 bridgePrintD.add("   ")
             }
-            bridgePrintU.add("]")
-            bridgePrintD.add("]")
 
-            var bridgeUS = bridgePrintU.joinToString("")
-            var bridgeDS = bridgePrintD.joinToString("")
-
-            println(bridgeUS)
-            println(bridgeDS)
+        stringPrint(bridgePrintU,bridgePrintD)
         }
+
+    fun stringPrint(bridgePrintU: MutableList<String>, bridgePrintD: MutableList<String>){
+        bridgePrintU.add("]")
+        bridgePrintD.add("]")
+        var bridgeUS = bridgePrintU.joinToString("")
+        var bridgeDS = bridgePrintD.joinToString("")
+        println(bridgeUS)
+        println(bridgeDS)
     }
+
+
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult(correct: Boolean, bridge: List<String>, size:Int,count : Int) {
+    fun printResult(bridge: List<String>, size:Int,count : Int) {
         var outputView = OutputView()
         println("최종 게임 결과")
-        outputView.printMap(correct,bridge,size)
-        if(correct){
-            println("게임 성공 여부: 성공")
-            println("총 시도한 횟수: ${count}")
-        }else{
-            println("게임 성공 여부: 실패")
-            println("총 시도한 횟수: ${count}")
-        }
+        outputView.printMap(bridge,size)
+        println("게임 성공 여부: 성공")
+        println("총 시도한 횟수: ${count}")
+    }
+
+    fun printResultFail(bridge: List<String>, size:Int,count : Int){
+        var outputView = OutputView()
+        println("최종 게임 결과")
+        outputView.printMapFalse(bridge,size)
+        println("게임 성공 여부: 실패")
+        println("총 시도한 횟수: ${count}")
     }
 }
