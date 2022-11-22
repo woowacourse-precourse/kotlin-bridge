5ad0b397a3a317ae441b6d04983e20b138a5ad99
junhyungss <daba44@naver.com>
2022-11-22 18:35:31 +0900

    test(UnitTest): 다리 생성 테스트 추가

--- src/test/kotlin/bridge/UnitTest.kt
+++ src/test/kotlin/bridge/UnitTest.kt
@@ -1,4 +1,30 @@
 package bridge
 
-class UnitTest {
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import domain.BridgeMaker
+import domain.BridgeNumberGenerator
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class UnitTest: NsTest() {
+
+    private val numberGenerator: BridgeNumberGenerator = ApplicationTest.TestNumberGenerator(listOf(1, 0, 0))
+    private val bridgeMaker = BridgeMaker(numberGenerator)
+    private val bridge: List<String> = bridgeMaker.makeBridge(3)
+
+    @Test
+    fun `다리 생성 테스트`() {
+        assertThat(bridge).containsExactly("U", "D", "D")
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE = "[ERROR]"
+    }
+
+    override fun runMain() {
+        main()
+    }
+
 }
