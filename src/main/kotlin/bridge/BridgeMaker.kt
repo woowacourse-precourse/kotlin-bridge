abd03dbaee2bb6398b9ec0e0081c194a8f76a968
lee989898 <wnsrud1352@gmail.com>
2022-11-22 16:31:49 +0900

    [refactor](BridgeMaker) 불필요한 공백 제거

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -4,10 +4,8 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
 
     fun makeUpDownBridge(size: Int): List<String> {
         val upDownBridge = mutableListOf<String>()
-
         repeat(size) {
             val bridgeNumber = bridgeNumberGenerator.generate()
-
             if (bridgeNumber == 0) {
                 upDownBridge.add("D")
             } else if (bridgeNumber == 1) {
