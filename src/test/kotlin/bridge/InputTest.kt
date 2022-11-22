34dbb67d4234fe3b2b7a4d45094a9fb30b720488
Yuhee Kim <dbgml4509@naver.com>
2022-11-22 18:33:36 +0900

    refactor : InputViewTest 클래스명 InputTest로 변경

--- src/test/kotlin/bridge/InputTest.kt
+++ src/test/kotlin/bridge/InputTest.kt
@@ -1,16 +1,21 @@
 package bridge
 
-import org.assertj.core.api.Assertions
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-class InputViewTest {
+class InputTest {
 
     @Test
     fun `입력 값에 아무 것도 없을 경우 테스트`() {
         assertThrows<IllegalArgumentException> {
             InputException().checkInputIsNull("")
+        }
+    }
+
+    @Test
+    fun `다리 길이 입력 값이 숫자가 아닐 경우 테스트`() {
+        assertThrows<IllegalArgumentException> {
+            InputException().checkBridgeLenInputIsNumeric("aa")
         }
     }
 
