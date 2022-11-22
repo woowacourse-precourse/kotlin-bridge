b1329534e1a7b503542f319b76fa32014265a7b3
dlwgnud <wngud980411@naver.com>
2022-11-22 15:14:04 +0900

    feat(readGameCommand): 게임 재시작/종료 여부를 입력 받는 함수

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -29,6 +29,8 @@ class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        val input = Console.readLine()
+        inputCheck.checkReGameCommand(input)
+        return input
     }
 }
