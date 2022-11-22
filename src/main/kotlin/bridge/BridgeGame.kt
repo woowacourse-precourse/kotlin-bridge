eeb89cd8fdc11edbfb36d9f51bc18fa24586785a
lee989898 <wnsrud1352@gmail.com>
2022-11-22 15:28:11 +0900

    [refactor](BridgeGame) 불필요한 주석 제거

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -1,8 +1,5 @@
 package bridge
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 class BridgeGame(private val bridgeLength: Int) {
 
     private lateinit var bridge: List<List<String>>
@@ -29,12 +26,6 @@ class BridgeGame(private val bridgeLength: Int) {
         bridgeLine = BridgeMaker(BridgeRandomNumberGenerator()).makeBridge(bridgeLength)
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     *
-     *
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     fun retry(restartCommand: String): Boolean {
         showResult = false
         if (restartCommand == "Q") {
@@ -91,12 +82,6 @@ class BridgeGame(private val bridgeLength: Int) {
         }
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     *
-     *
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     fun move(move: String, index: Int) {
         checkU(move, index)
         checkD(move, index)
