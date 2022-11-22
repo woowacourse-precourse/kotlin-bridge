96c7169fe57e5bcf79bc557f214bb5e4e0901f74
jjw0071 <jjw0071@naver.com>
2022-11-22 14:37:33 +0900

    feat : 다리 길이 입력함수 구현

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,5 +1,7 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,7 +10,7 @@ class InputView {
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        return toBridgeSize()
     }
 
     /**
@@ -23,5 +25,30 @@ class InputView {
      */
     fun readGameCommand(): String {
         return ""
+    }
+    /**
+     * 사용자가 입력한 값을 숫자로 변환한다.
+     */
+    private fun toInt() : Int {
+        while(true){
+            try{
+                return Console.readLine().toInt()
+            }catch (e : NumberFormatException ){
+                println("[ERROR] 3이상 20이하의 숫자를 입력해 주세요.")
+            }
+        }
+    }
+
+    /**
+     * 사용자가 입력한 값을 다리길이에 부합하는지 확인한다.
+     */
+
+    private fun toBridgeSize() : Int{
+        var bridgeSize = toInt()
+        while(bridgeSize<3 || bridgeSize>20){
+            println("3 이상 20이하의 숫자를 입력해 주세요.")
+            bridgeSize = toInt()
+        }
+        return bridgeSize
     }
 }
