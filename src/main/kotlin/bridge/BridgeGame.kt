650d5711dd65f7864d61a6be46b2198b1012605b
zqwz <zqwz@naver.com>
2022-12-02 02:58:31 +0900

    refactor(BridgeGame): 이름 변경

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -16,15 +16,15 @@ class BridgeGame(private val bridges: List<String>) {
     fun move(direction: String) {
         when (direction) {
             bridges[bridgePosition] -> {
-                checkSide(direction, Result.SUCCESS.emoji)
+                checkMoveDirection(direction, Result.SUCCESS.emoji)
                 bridgePosition++
             }
 
-            else -> checkSide(direction, Result.FAILURE.emoji)
+            else -> checkMoveDirection(direction, Result.FAILURE.emoji)
         }
     }
 
-    private fun checkSide(side: String, result: String) {
+    private fun checkMoveDirection(side: String, result: String) {
         when (side) {
             Side.DOWN.command -> addDownSide(result)
 
@@ -43,23 +43,21 @@ class BridgeGame(private val bridges: List<String>) {
     }
 
     fun retry() {
-        increaseTryCount()
+        retryCount++
         bridgePosition = Constant.INIT_BRIDGE_POSITION
         upSide.clear()
         downSide.clear()
     }
 
-    private fun increaseTryCount() = retryCount++
+    private fun changeUpSideFormat() = upSide.joinToString(" | ", "[ ", " ]")
+
+    private fun changeDownSideFormat() = downSide.joinToString(" | ", "[ ", " ]\n")
 
     fun isFail() = downSide.contains(Result.FAILURE.emoji) || upSide.contains(Result.FAILURE.emoji)
 
     fun isEnd() = bridgePosition == bridges.size
 
     fun getMap() = listOf(changeUpSideFormat(), changeDownSideFormat())
-
-    private fun changeUpSideFormat() = upSide.joinToString(" | ", "[ ", " ]")
-
-    private fun changeDownSideFormat() = downSide.joinToString(" | ", "[ ", " ]\n")
 
     fun getRetryCount() = retryCount
 }
